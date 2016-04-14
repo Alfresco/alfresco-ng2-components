9275c31b26a1fb901fe705b86ff343f7d2b5f648
Denys Vuika <denis.vuyka@gmail.com>
2016-04-14 12:16:15 +0100

    Fixed component binding

--- demo-shell-ng2/app/components/page2.view.ts
+++ demo-shell-ng2/app/components/page2.view.ts
@@ -7,7 +7,7 @@ import {Component} from "angular2/core";
                 <h1>Page 2</h1>
                 <input [(ngModel)]="username">
                 <span>Username: {{username}}</span>
-                <hello-world [who]="username"></hello-world>
+                <hello-world [attr.who]="username"></hello-world>
             </div>
         </div>
     `
