2aaf4a0a771036f2d4932c1fe5877d090e3d98cd
Denys Vuika <denis.vuyka@gmail.com>
2016-04-14 12:04:25 +0100

    Simple hello world web component

--- demo-shell-ng2/app/components/page2.view.ts
+++ demo-shell-ng2/app/components/page2.view.ts
@@ -5,10 +5,13 @@ import {Component} from "angular2/core";
         <div class="container">
             <div class="row">
                 <h1>Page 2</h1>
+                <input [(ngModel)]="username">
+                <span>Username: {{username}}</span>
+                <hello-world [who]="username"></hello-world>
             </div>
         </div>
     `
 })
 export class Page2View {
-
+    username: string = 'Unicorn';
 }
