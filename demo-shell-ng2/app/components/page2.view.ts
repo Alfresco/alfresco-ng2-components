6cb98e516f02075f50f7dc7597e97361df41d0c7
Denys Vuika <denis.vuyka@gmail.com>
2016-04-15 11:30:10 +0100

    Simple list component with array watching

--- demo-shell-ng2/app/components/page2.view.ts
+++ demo-shell-ng2/app/components/page2.view.ts
@@ -12,6 +12,10 @@ import {Component} from "angular2/core";
                 <hello-world text="test user"></hello-world>
             </div>
             <div class="row">
+                <p>
+                    List below is a web component bound to array of items coming from angular.<br>
+                    Add/remove buttons control angular array and web component reacts on changes.
+                </p>
                 <simple-list [items]="items"></simple-list>
                 <button (click)="addItem()">add item</button>
                 <button (click)="removeItem()">remove item</button>
