f931372d618b920a5d08785592ec1be14f60b35f
Denys Vuika <denis.vuyka@gmail.com>
2016-04-15 11:13:10 +0100

    Stub for list web component experiments

--- demo-shell-ng2/app/components/page2.view.ts
+++ demo-shell-ng2/app/components/page2.view.ts
@@ -4,15 +4,30 @@ import {Component} from "angular2/core";
     template: `
         <div class="container">
             <div class="row">
-                <h1>Page 2</h1>
                 <input [(ngModel)]="username">
                 <span>Username: {{username}}</span>
-                <hello-world [text]="username"></hello-world>
+                <hello-world [text]="username">
+                    <button>{{'interpolation: ' + username}}</button>
+                </hello-world>
                 <hello-world text="test user"></hello-world>
+            </div>
+            <div class="row">
+                <simple-list [items]="items"></simple-list>
+                <button (click)="addItem()">add item</button>
+                <button (click)="removeItem()">remove item</button>
             </div>
         </div>
     `
 })
 export class Page2View {
     username: string = 'Unicorn';
+    items = ['one', 'two', 'three'];
+
+    addItem() {
+        this.items.push('test');
+    }
+
+    removeItem() {
+        this.items.pop();
+    }
 }
