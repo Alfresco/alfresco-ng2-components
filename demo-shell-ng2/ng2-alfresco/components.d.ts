4735bee08d2658d91121bd1bb60276772b858f05
Denys Vuika <denis.vuyka@gmail.com>
2016-04-18 20:24:39 +0100

    Additional packaging settings

--- demo-shell-ng2/ng2-alfresco/components.d.ts
+++ demo-shell-ng2/ng2-alfresco/components.d.ts
@@ -1,7 +1,6 @@
 import { HelloWorld } from './src/HelloWorld';
 export * from './src/HelloWorld';
-
 declare var _default: {
-    directives: (typeof HelloWorld)[];
+    directives: typeof HelloWorld[];
 };
 export default _default;
