125a73d3e31c55120f51ed38311211e40f1301e3
Mario Romano <dnamario@hotmail.com>
2016-04-21 12:49:37 +0100

    #33 remove node_modules

--- ng2-components/ng2-alfresco-documentslist/ng2-alfresco-documentslist.d.ts
+++ ng2-components/ng2-alfresco-documentslist/ng2-alfresco-documentslist.d.ts
@@ -1,9 +1,9 @@
 import { HelloWorld } from './HelloWorld';
 import { DocumentList } from "./document-list.component.ts";
 import { AlfrescoService } from "./alfresco.service.ts";
-export * from './HelloWorld';
-export * from './document-list.component.ts';
-export * from './alfresco.service.ts';
+export * from 'src/HelloWorld';
+export * from 'src/document-list.component.ts';
+export * from 'src/alfresco.service.ts';
 declare var _default: {
     directives: (typeof HelloWorld | typeof DocumentList)[];
     providers: typeof AlfrescoService[];
