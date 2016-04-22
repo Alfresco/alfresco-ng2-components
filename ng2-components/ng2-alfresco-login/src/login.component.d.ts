408fa8ee325764513939aff48fa92c2d976791d6
Denys Vuika <denis.vuyka@gmail.com>
2016-04-22 19:46:53 +0100

    Added missing definition files

--- ng2-components/ng2-alfresco-login/src/login.component.d.ts
+++ ng2-components/ng2-alfresco-login/src/login.component.d.ts
@@ -4,9 +4,26 @@ import { Authentication } from './authentication.service';
 export declare class Login {
     auth: Authentication;
     router: Router;
+    method: string;
     form: ControlGroup;
     error: boolean;
-    isErrorStyle(field: ControlGroup): boolean;
+    /**
+     * Constructor
+     * @param fb
+     * @param auth
+     * @param router
+     */
     constructor(fb: FormBuilder, auth: Authentication, router: Router);
+    /**
+     * Method called on submit form
+     * @param value
+     * @param event
+     */
     onSubmit(value: any, event: any): void;
+    /**
+     * The method return if a field is valid or not
+     * @param field
+     * @returns {boolean}
+     */
+    isErrorStyle(field: ControlGroup): boolean;
 }
