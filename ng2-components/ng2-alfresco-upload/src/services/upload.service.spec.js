a92f706c7d06d3279378a46a68d800d4275dd7f2
mauriziovitale84 <maurizio.vitale@alfresco.com>
2016-05-05 21:41:56 +0100

    #46 upload component Unit test

---
+++ ng2-components/ng2-alfresco-upload/src/services/upload.service.spec.js
@@ -0,0 +1,72 @@
+/**
+ * @license
+ * Copyright 2016 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+System.register(['angular2/testing', 'angular2/core', 'angular2/http', 'angular2/http/testing', './upload.service'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var testing_1, core_1, http_1, testing_2, upload_service_1;
+    return {
+        setters:[
+            function (testing_1_1) {
+                testing_1 = testing_1_1;
+            },
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
+            function (http_1_1) {
+                http_1 = http_1_1;
+            },
+            function (testing_2_1) {
+                testing_2 = testing_2_1;
+            },
+            function (upload_service_1_1) {
+                upload_service_1 = upload_service_1_1;
+            }],
+        execute: function() {
+            testing_1.describe('AlfrescoUploadService', function () {
+                var injector, backend, mockBackend, httpService, service, options;
+                beforeEach(function () {
+                    injector = core_1.Injector.resolveAndCreate([
+                        http_1.HTTP_PROVIDERS,
+                        testing_2.MockBackend,
+                        core_1.provide(http_1.XHRBackend, { useClass: testing_2.MockBackend })
+                    ]);
+                    mockBackend = injector.get(testing_2.MockBackend);
+                    backend = injector.get(http_1.XHRBackend);
+                    httpService = injector.get(http_1.Http);
+                    options = {
+                        url: 'http://mockUpload',
+                        withCredentials: true,
+                        authToken: btoa('fakeadmin:fakeadmin'),
+                        authTokenPrefix: 'Basic',
+                        fieldName: 'fakeFileData',
+                        formFields: {
+                            siteid: 'fakeSite',
+                            containerid: 'fakeFolder'
+                        }
+                    };
+                    service = new upload_service_1.UploadService(options);
+                });
+                testing_1.it('should return true if is a file', function () {
+                    var filesFake = [{ name: 'fake-name', size: 0 }];
+                    service.addToQueue(filesFake);
+                    expect(true).toEqual(true);
+                });
+            });
+        }
+    }
+});
+//# sourceMappingURL=upload.service.spec.js.map
---
+++ ng2-components/ng2-alfresco-upload/src/services/upload.service.spec.ts
@@ -0,0 +1,65 @@
+/**
+ * @license
+ * Copyright 2016 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {it, describe} from 'angular2/testing';
+import {provide, Injector} from 'angular2/core';
+import {Http, HTTP_PROVIDERS, XHRBackend, Response, ResponseOptions} from 'angular2/http';
+import {MockBackend} from 'angular2/http/testing';
+import {UploadService} from './upload.service';
+
+describe('AlfrescoUploadService', () => {
+    let injector,
+        backend,
+        mockBackend,
+        httpService,
+        service,
+        options;
+
+    beforeEach(() => {
+        injector = Injector.resolveAndCreate([
+            HTTP_PROVIDERS,
+            MockBackend,
+            provide(XHRBackend, {useClass: MockBackend})
+        ]);
+
+        mockBackend = injector.get(MockBackend);
+        backend = injector.get(XHRBackend);
+        httpService = injector.get(Http);
+
+        options = {
+            url: 'http://mockUpload',
+            withCredentials: true,
+            authToken: btoa('fakeadmin:fakeadmin'),
+            authTokenPrefix: 'Basic',
+            fieldName: 'fakeFileData',
+            formFields: {
+                siteid: 'fakeSite',
+                containerid: 'fakeFolder'
+            }
+        }
+        service = new UploadService(options);
+    });
+
+    it('should return true if is a file', () => {
+        let filesFake = [{name: 'fake-name', size: 0}];
+        service.addToQueue(filesFake);
+        expect(true).toEqual(true);
+    });
+
+
+
+});
