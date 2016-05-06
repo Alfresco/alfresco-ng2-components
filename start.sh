efe608b5c1a16c774cd31d781a61e8aacea1b096
Mario Romano <dnamario@hotmail.com>
2016-05-06 01:50:31 +0100

    #46 add upload to start.sh

--- start.sh
+++ start.sh
@@ -5,5 +5,6 @@ npm install
 npm install ../ng2-components/ng2-alfresco-core
 npm install ../ng2-components/ng2-alfresco-documentlist
 npm install ../ng2-components/ng2-alfresco-login
+npm install ../ng2-components/ng2-alfresco-upload
 
 npm run build.dev
