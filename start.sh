21592e90bfe25a0d4262e3d00d738448cded106d
Will Abson <will.abson@gmail.com>
2016-05-06 13:53:18 +0100

    Add info on dev-platform-js-api dependency to README and start.sh
    Refs #7

--- start.sh
+++ start.sh
@@ -6,5 +6,6 @@ npm install ../ng2-components/ng2-alfresco-core
 npm install ../ng2-components/ng2-alfresco-documentlist
 npm install ../ng2-components/ng2-alfresco-login
 npm install ../ng2-components/ng2-alfresco-upload
+npm install ../../dev-platform-js-api
 
 npm run build.dev
