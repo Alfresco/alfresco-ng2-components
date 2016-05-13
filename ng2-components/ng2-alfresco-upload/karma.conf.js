bbcd83b377becb7ba96d2195d7775587cb6d6f41
mauriziovitale84 <maurizio.vitale@alfresco.com>
2016-05-13 17:28:04 +0100

    #46 Added karma-jasmine-ajax to fix test problems

--- ng2-components/ng2-alfresco-upload/karma.conf.js
+++ ng2-components/ng2-alfresco-upload/karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function(config) {
 
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['jasmine'],
+        frameworks: ['jasmine-ajax', 'jasmine'],
 
 
         // list of files / patterns to load in the browser
