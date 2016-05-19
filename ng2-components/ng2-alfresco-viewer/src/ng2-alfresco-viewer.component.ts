127eb300662075b322d89b59db16a168ef83ff65
Eugenio Romano <eugenio.romano@alfresco.com>
2016-05-19 15:04:20 +0100

    #68 load first internal pdf

--- ng2-components/ng2-alfresco-viewer/src/ng2-alfresco-viewer.component.ts
+++ ng2-components/ng2-alfresco-viewer/src/ng2-alfresco-viewer.component.ts
@@ -31,6 +31,32 @@ import {Component} from 'angular2/core';
 export class Ng2AlfrescoViewerComponent {
 
     constructor() {
+        PDFJS.getDocument('../localTestFile.pdf').then(function getPdfHelloWorld(pdf) {
+            //
+            // Fetch the first page
+            //
+            pdf.getPage(1).then(function getPageHelloWorld(page) {
+                let scale = 1.5;
+                let viewport = page.getViewport(scale);
+
+                //
+                // Prepare canvas using PDF page dimensions
+                //
+                let  canvas:any = document.getElementById('the-canvas');
+                let context = canvas.getContext('2d');
+                canvas.height = viewport.height;
+                canvas.width = viewport.width;
+
+                //
+                // Render PDF page into canvas context
+                //
+                let renderContext = {
+                    canvasContext: context,
+                    viewport: viewport
+                };
+                page.render(renderContext);
+            });
+        });
         console.log('../contructor');
     }
 
