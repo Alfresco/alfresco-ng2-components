339b84f042e74d16c40da24226c6b6c6c40da7db
mauriziovitale84 <maurizio.vitale@alfresco.com>
2016-05-23 17:32:01 +0100

    Removed ng-translate code from the core Added ng-translate dependency

--- ng2-components/ng2-alfresco-login/karma.conf.js
+++ ng2-components/ng2-alfresco-login/karma.conf.js
@@ -18,6 +18,7 @@ module.exports = function (config) {
             {pattern: 'node_modules/angular2/bundles/router.dev.js', included: true, watched: false},
             {pattern: 'node_modules/alfresco-core-rest-api/bundle.js', included: true, watched: false},
             {pattern: 'node_modules/ng2-alfresco-core/dist/**/*.js', included: false, served: true, watched: false},
+            {pattern: 'node_modules/ng2-translate/**/*.js', included: false, served: true, watched: false},
 
             {pattern: 'karma-test-shim.js', included: true, watched: true},
 
