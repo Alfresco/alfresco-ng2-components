8bdbccb138a40d1250522a73b14582757aa6044f
Will Abson <will.abson@gmail.com>
2016-06-07 13:34:06 +0100

    Display message when errors occur whilst searching
    Refs #144

--- ng2-components/ng2-alfresco-search/src/components/alfresco-search.component.ts
+++ ng2-components/ng2-alfresco-search/src/components/alfresco-search.component.ts
@@ -50,7 +50,7 @@ export class AlfrescoSearchComponent implements OnChanges, OnInit {
     {
         translate.addComponent('node_modules/ng2-alfresco-search');
 
-        this.results = [];
+        this.results = null;
         if (params) {
             this.searchTerm = params.get('q');
         }
@@ -97,8 +97,14 @@ export class AlfrescoSearchComponent implements OnChanges, OnInit {
             this._alfrescoService
                 .getLiveSearchResults(searchTerm)
                 .subscribe(
-                    results => this.results = results.list.entries,
-                    error => this.errorMessage = <any>error
+                    results => {
+                        this.results = results.list.entries
+                        this.errorMessage = null;
+                    },
+                    error => {
+                        this.results = null;
+                        this.errorMessage = <any>error;
+                    }
                 );
         }
     }
