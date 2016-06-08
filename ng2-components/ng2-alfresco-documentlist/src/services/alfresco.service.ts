586d33b288c281f842ffab9c94e4ebe9dc60553e
Denys Vuika <denis.vuyka@gmail.com>
2016-06-08 11:18:07 +0100

    #9 more mime-type icons supported

--- ng2-components/ng2-alfresco-documentlist/src/services/alfresco.service.ts
+++ ng2-components/ng2-alfresco-documentlist/src/services/alfresco.service.ts
@@ -38,11 +38,27 @@ export class AlfrescoService {
         'image/gif': 'ft_ic_raster_image.svg',
         'application/pdf': 'ft_ic_pdf.svg',
         'application/vnd.ms-excel': 'ft_ic_ms_excel.svg',
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': 'ft_ic_ms_excel.svg',
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.template': 'ft_ic_ms_excel.svg',
         'application/msword': 'ft_ic_ms_word.svg',
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document': 'ft_ic_ms_word.svg',
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.template': 'ft_ic_ms_word.svg',
         'application/vnd.ms-powerpoint': 'ft_ic_ms_powerpoint.svg',
+        'application/vnd.openxmlformats-officedocument.presentationml.presentation': 'ft_ic_ms_powerpoint.svg',
+        'application/vnd.openxmlformats-officedocument.presentationml.template': 'ft_ic_ms_powerpoint.svg',
+        'application/vnd.openxmlformats-officedocument.presentationml.slideshow': 'ft_ic_ms_powerpoint.svg',
         'video/mp4': 'ft_ic_video.svg',
         'text/plain': 'ft_ic_document.svg',
-        'application/x-javascript': 'ft_ic_document.svg'
+        'application/x-javascript': 'ft_ic_document.svg',
+        'application/json': 'ft_ic_document.svg',
+        'image/svg+xml': 'ft_ic_vector_image.svg',
+        'text/html': 'ft_ic_website.svg',
+        'application/x-compressed': 'ft_ic_archive.svg',
+        'application/x-zip-compressed': 'ft_ic_archive.svg',
+        'application/zip': 'ft_ic_archive.svg',
+        'application/vnd.apple.keynote': 'ft_ic_presentation.svg',
+        'application/vnd.apple.pages': 'ft_ic_document.svg',
+        'application/vnd.apple.numbers': 'ft_ic_spreadsheet.svg'
     };
 
     constructor(
