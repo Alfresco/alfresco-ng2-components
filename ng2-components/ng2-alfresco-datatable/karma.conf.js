114c5bd479510b12d6e80783c46473b609fcc9dc
Denys Vuika <denis.vuyka@gmail.com>
2016-06-15 09:33:47 +0100

    Fix datatable unit tests

--- ng2-components/ng2-alfresco-datatable/karma.conf.js
+++ ng2-components/ng2-alfresco-datatable/karma.conf.js
@@ -4,7 +4,7 @@ module.exports = function (config) {
   var configuration = {
     basePath: '.',
 
-    frameworks: ['jasmine-ajax', 'jasmine'],
+    frameworks: [/*'jasmine-ajax',*/ 'jasmine'],
 
     files: [
       // paths loaded by Karma
@@ -58,7 +58,7 @@ module.exports = function (config) {
     plugins: [
       'karma-jasmine',
       'karma-coverage',
-      'karma-jasmine-ajax',
+      //'karma-jasmine-ajax',
       'karma-chrome-launcher',
       'karma-mocha-reporter',
       'karma-jasmine-html-reporter'
@@ -82,7 +82,7 @@ module.exports = function (config) {
         {type: 'html'}
       ]
     }
-  }
+  };
 
   if (process.env.TRAVIS) {
     configuration.browsers = ['Chrome_travis_ci'];
