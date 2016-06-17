f4bc782363c1a3570bab7305c23e0179ab16f5dc
Denys Vuika <denis.vuyka@gmail.com>
2016-06-17 12:03:33 +0100

    Script to update all libs and demo projects

---
+++ ng2-components/update.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+(cd ng2-alfresco-core; npm update;)
+
+(cd ng2-alfresco-datatable; npm update;)
+(cd ng2-alfresco-datatable/demo; npm update;)
+
+(cd ng2-alfresco-documentlist; npm update;)
+(cd ng2-alfresco-documentlist/demo; npm update;)
+
+(cd ng2-alfresco-login; npm update;)
+(cd ng2-alfresco-login/demo; npm update;)
+
+(cd ng2-alfresco-search; npm update;)
+(cd ng2-alfresco-search/demo; npm update;)
+
+(cd ng2-alfresco-upload; npm update;)
+(cd ng2-alfresco-upload/demo; npm update;)
+
+(cd ng2-alfresco-viewer; npm update;)
+(cd ng2-alfresco-viewer/demo; npm update;)
