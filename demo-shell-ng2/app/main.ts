8f51415733614509a07fbcf47b4171a55facbc68
Denys Vuika <denis.vuyka@gmail.com>
2016-06-20 16:54:11 +0100

    Fix demo shell runtime issue with Search

--- demo-shell-ng2/app/main.ts
+++ demo-shell-ng2/app/main.ts
@@ -18,6 +18,7 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { ROUTER_PROVIDERS } from '@angular/router-deprecated';
 import { HTTP_PROVIDERS } from '@angular/http';
+import { ALFRESCO_SEARCH_PROVIDERS } from 'ng2-alfresco-search';
 import { ALFRESCO_CORE_PROVIDERS } from 'ng2-alfresco-core';
 import { UploadService } from 'ng2-alfresco-upload';
 import { AppComponent } from './app.component';
@@ -26,5 +27,6 @@ bootstrap(AppComponent, [
     ROUTER_PROVIDERS,
     HTTP_PROVIDERS,
     ALFRESCO_CORE_PROVIDERS,
+    ALFRESCO_SEARCH_PROVIDERS,
     UploadService
 ]);
