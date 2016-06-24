6575a6143db9d59d9c74720584691dd69f3541d0
Denys Vuika <denis.vuyka@gmail.com>
2016-06-24 11:50:32 +0100

    Revert "Fix authentication mock"
    This reverts commit 3d748b61b56fb7656288fa10b2a06a019c9324a8.

--- ng2-components/ng2-alfresco-login/src/assets/authentication.service.mock.ts
+++ ng2-components/ng2-alfresco-login/src/assets/authentication.service.mock.ts
@@ -16,17 +16,14 @@
  */
 
 import { Observable } from 'rxjs/Rx';
-import { AlfrescoSettingsService, AlfrescoAuthenticationService } from 'ng2-alfresco-core';
 
-export class AuthenticationMock extends AlfrescoAuthenticationService  {
+// TODO: should be extending AlfrescoAuthenticationService
+export class AuthenticationMock /*extends AlfrescoAuthenticationService*/ {
 
-    constructor(settings: AlfrescoSettingsService = null) {
-        super(settings);
-    }
-
-    login(username: string, password: string): Observable<string> {
+    // TODO: real auth service returns Observable<string>
+    login(username: string, password: string): Observable<boolean> {
         if (username === 'fake-username' && password === 'fake-password') {
-            return Observable.of('fake-token');
+            return Observable.of(true);
         } else {
             return Observable.throw('Fake server error');
         }
