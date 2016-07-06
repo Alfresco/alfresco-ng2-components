4ded58aecc5bd282f593db0eb42129cdc67b7ffc
Mario Romano <dnamario@hotmail.com>
2016-07-06 11:32:03 +0100

    change update version script

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 set -ex
 
 cd `dirname $0`
@@ -12,5 +14,34 @@ then
   exit 1
 fi
 
-echo "====== RENAMING 0.0.0-PLACEHOLDER to N.N.N version ======"
-find ././../ -type f -name package.json -print0 | xargs -0 sed -i '' "s/0\\.0\\.0-PLACEHOLDER/${VERSION}/g"
+for PACKAGE in \
+  ng2-alfresco-core \
+  ng2-alfresco-datatable \
+  ng2-alfresco-documentlist \
+  ng2-alfresco-login \
+  ng2-alfresco-search \
+  ng2-alfresco-upload \
+  ng2-alfresco-viewer
+do
+  DESTDIR="$DIR/../ng2-components/${PACKAGE}"
+  echo "====== UPDATE VERSION of ${PACKAGE} to ${VERSION} version in all the package.json ======"
+  find ././../ -type f -name package.json -print0 | xargs -0 sed -i '' "s/\"${PACKAGE}\": \"0\\.2\\.0\"/\"${PACKAGE}\": \"${VERSION}\"/g"
+done
+
+for PACKAGE in \
+  ng2-alfresco-core \
+  ng2-alfresco-datatable \
+  ng2-alfresco-documentlist \
+  ng2-alfresco-login \
+  ng2-alfresco-search \
+  ng2-alfresco-upload \
+  ng2-alfresco-viewer
+do
+  DESTDIR="$DIR/../ng2-components/${PACKAGE}"
+  echo "====== UPDATE VERSION OF ${PACKAGE} to ${VERSION} version ======"
+  sed -i '' "s/\"version\": \"0\\.2\\.0\"/\"version\": \"${VERSION}\"/g"  ${DESTDIR}/package.json
+done
+
+echo "====== UPDATE VERSION OF DEMO-SHELL to ${VERSION} version ======"
+
+sed -i '' "s/\"version\": \"0\\.2\\.0\"/\"version\": \"${VERSION}\"/g"  ${DIR}/../demo-shell-ng2/package.json
