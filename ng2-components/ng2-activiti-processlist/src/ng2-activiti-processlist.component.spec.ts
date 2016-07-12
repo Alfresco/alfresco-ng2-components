3272bf2fadc22a98521f7f152ecd5022de313158
Will Abson <will.abson@alfresco.com>
2016-07-12 17:47:56 +0100

    Initial process list component
    Refs #344

---
+++ ng2-components/ng2-activiti-processlist/src/ng2-activiti-processlist.component.spec.ts
@@ -0,0 +1,38 @@
+/*!
+ * @license
+ * Copyright 2016 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {describe, expect, it, inject, setBaseTestProviders} from '@angular/core/testing';
+import { TestComponentBuilder } from '@angular/compiler/testing';
+import {
+  TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS,
+  TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS
+} from '@angular/platform-browser-dynamic/testing';
+import {Ng2ActivitiProcesslistComponent} from '../src/ng2-activiti-processlist.component';
+
+describe('Basic Example test ng2-activiti-processlist', () => {
+  setBaseTestProviders(TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS, TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS);
+
+  it('Test hello world', inject([TestComponentBuilder], (tcb: TestComponentBuilder) => {
+    return tcb
+      .createAsync(Ng2ActivitiProcesslistComponent)
+      .then((fixture) => {
+        let element = fixture.nativeElement;
+        expect(element.querySelector('h1')).toBeDefined();
+        expect(element.getElementsByTagName('h1')[0].innerHTML).toEqual('Hello World Angular 2 ng2-activiti-processlist');
+      });
+  }));
+});
