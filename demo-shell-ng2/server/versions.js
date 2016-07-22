4f86f04a290c1bf556de2a7f862d1b473fff2d11
Denys Vuika <denis.vuyka@gmail.com>
2016-07-22 11:14:36 +0100

    #455 migrate to wsrv, versions server extension

---
+++ demo-shell-ng2/server/versions.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// wsrv extension that provides dynamic '/versions' route
+
+exports.register = function (server, options, next) {
+
+    var packages = [
+        'ng2-activiti-form',
+        'ng2-alfresco-core',
+        'ng2-alfresco-datatable',
+        'ng2-alfresco-documentlist',
+        'ng2-alfresco-login',
+        'ng2-alfresco-search',
+        'ng2-alfresco-upload',
+        'ng2-alfresco-viewer',
+        'ng2-alfresco-webscript'
+    ];
+
+    server.route({
+        method: 'GET',
+        path: '/versions',
+        handler: function (request, reply) {
+
+            var result = {};
+            packages.map(function (packageName) {
+                result[packageName] = require('./../node_modules/' + packageName + '/package.json').version
+            });
+
+            reply(result);
+        }
+    });
+
+    next();
+};
+
+exports.register.attributes = {
+    name: 'ng2-module-versions',
+    version: '1.0.0'
+};
