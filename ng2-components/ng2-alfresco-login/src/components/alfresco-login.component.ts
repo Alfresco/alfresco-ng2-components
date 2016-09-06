d2fa3fdb4395851f30c23afdd8c5d4b358fc1bdb
Mario Romano <dnamario@hotmail.com>
2016-09-06 19:19:52 +0100

    #687 fix rebase

--- ng2-components/ng2-alfresco-login/src/components/alfresco-login.component.ts
+++ ng2-components/ng2-alfresco-login/src/components/alfresco-login.component.ts
@@ -150,7 +150,7 @@ export class AlfrescoLoginComponent implements OnInit {
             .subscribe(
             (token: any) => {
                 this.success = true;
-                this.onSuccess.emit({token: token, username: value.username, password: value.password});
+                this.onSuccess.emit({token: token, username: values.username, password: values.password});
             },
             (err: any) => {
                 this.enableError();
