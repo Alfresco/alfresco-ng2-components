c7fb84b7f942ae0aacffcb4fe0aa0f47af5a3ea5
Eugenio Romano <eugenio.romano@alfresco.com>
2016-10-04 11:17:08 +0100

    fix coverage

--- ng2-components/ng2-alfresco-webscript/karma.conf.js
+++ ng2-components/ng2-alfresco-webscript/karma.conf.js
@@ -46,7 +46,6 @@ module.exports = function (config) {
       // ng2-components
       { pattern: 'node_modules/ng2-alfresco-core/dist/**/*.js', included: false, served: true, watched: false },
       { pattern: 'node_modules/ng2-alfresco-datatable/dist/**/*.*', included: false, served: true, watched: false },
-      { pattern: 'node_modules/ng2-alfresco-webscript/dist/**/*.js', included: false, served: true, watched: false },
 
       // paths to support debugging with source maps in dev tools
       {pattern: 'src/**/*.ts', included: false, watched: false},
