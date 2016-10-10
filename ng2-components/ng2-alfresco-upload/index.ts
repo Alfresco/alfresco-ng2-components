c1a78e8c5f5f2fe77c4de4ff549514024529412e
Denys Vuika <denis.vuyka@gmail.com>
2016-10-10 11:18:53 +0100

    Fix demo shell and upload component

--- ng2-components/ng2-alfresco-upload/index.ts
+++ ng2-components/ng2-alfresco-upload/index.ts
@@ -24,7 +24,6 @@ import { UploadButtonComponent } from './src/components/upload-button.component'
 import { FileUploadingDialogComponent } from './src/components/file-uploading-dialog.component';
 import { FileUploadingListComponent } from './src/components/file-uploading-list.component';
 import { UploadService } from './src/services/upload.service';
-import { TranslateModule } from 'ng2-translate';
 
 /**
  * ng2-alfresco-upload, provide components to upload files to alfresco repository.
@@ -63,8 +62,7 @@ export const UPLOAD_PROVIDERS: any[] = [
 
 @NgModule({
     imports: [
-        CoreModule,
-        TranslateModule.forRoot()
+        CoreModule
     ],
     declarations: [
         ...UPLOAD_DIRECTIVES
