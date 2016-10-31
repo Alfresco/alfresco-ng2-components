f0207fe7183cc56bb805003c4a958d3c441d0bd0
mauriziovitale84 <maurizio.vitale@alfresco.com>
2016-10-31 14:03:53 +0000

    Add diagrams Event unit test

---
+++ ng2-components/ng2-activiti-diagrams/src/assets/diagramEvents.mock.ts
@@ -0,0 +1,91 @@
+/*!
+ * @license
+ * Copyright 2016 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export let startEvent = {
+    id: 'startEvent1',
+    type: 'StartEvent',
+    width: 30,
+    height: 30,
+    x: 15,
+    y: 48.5,
+    properties: [{}]
+};
+
+export let startTimeEvent = {
+    id: 'startEvent1',
+    type: 'StartEvent',
+    width: 30,
+    height: 30,
+    x: 15,
+    y: 48.5,
+    eventDefinition: {type: 'timer'},
+    properties: [{}]
+};
+
+export let startSignalEvent = {
+    id: 'startEvent1',
+    type: 'StartEvent',
+    width: 30,
+    height: 30,
+    x: 15,
+    y: 48.5,
+    eventDefinition: {type: 'signal'},
+    properties: [{}]
+};
+
+export let startMessageEvent = {
+    id: 'startEvent1',
+    type: 'StartEvent',
+    width: 30,
+    height: 30,
+    x: 15,
+    y: 48.5,
+    eventDefinition: {type: 'message'},
+    properties: [{}]
+};
+
+export let startErrorEvent = {
+    id: 'startEvent1',
+    type: 'StartEvent',
+    width: 30,
+    height: 30,
+    x: 15,
+    y: 48.5,
+    eventDefinition: {type: 'error'},
+    properties: [{}]
+};
+
+export let endEvent = {
+    id: 'sid-CED2A8DB-47E2-4057-A7B8-3ABBE5CE795E',
+    type: 'EndEvent',
+    width: 28,
+    height: 28,
+    x: 15,
+    y: 48.5,
+    properties: [{}]
+};
+
+export let endErrorEvent = {
+    id: 'sid-CED2A8DB-47E2-4057-A7B8-3ABBE5CE795E',
+    type: 'EndEvent',
+    width: 28,
+    height: 28,
+    x: 15,
+    y: 48.5,
+    eventDefinition: {type: 'error'},
+    properties: [{}]
+};
--- ng2-components/ng2-activiti-diagrams/src/components/diagram.component.spec.ts
+++ ng2-components/ng2-activiti-diagrams/src/components/diagram.component.spec.ts
@@ -24,6 +24,7 @@ import { DIAGRAM_DIRECTIVES, DIAGRAM_PROVIDERS } from './index';
 import { RAPHAEL_DIRECTIVES, RAPHAEL_PROVIDERS } from './raphael/index';
 import { DiagramComponent } from './index';
 import { DebugElement } from '@angular/core';
+import * as diagramsEventsMock from '../assets/diagramEvents.mock';
 
 declare let jasmine: any;
 
@@ -64,41 +65,157 @@ describe('Test ng2-activiti-diagrams ', () => {
         window['componentHandler'] = componentHandler;
     });
 
-    describe('Diagrams component ', () => {
+    describe('Diagrams component Events: ', () => {
         beforeEach(() => {
             jasmine.Ajax.install();
+            component.processDefinitionId = 'fakeprocess:24:38399';
+            component.metricPercentages = {startEvent: 0};
         });
 
         afterEach(() => {
             jasmine.Ajax.uninstall();
         });
 
-        it('Should render the StartEvent', async(() => {
-            component.processDefinitionId = 'fakeprocess:24:38399';
-            component.metricPercentages = {startEvent: 0};
+        it('Should render the Start Event', async(() => {
+            component.onSuccess.subscribe((res) => {
+                fixture.detectChanges();
+                fixture.whenStable().then(() => {
+                    expect(res).not.toBeNull();
+                    let event: any = element.querySelector('diagram-start-event > diagram-event > raphael-circle');
+                    expect(event).not.toBeNull();
+                });
+            });
+            component.ngOnChanges();
+            let resp = {elements: [diagramsEventsMock.startEvent]};
+            jasmine.Ajax.requests.mostRecent().respondWith({
+                status: 200,
+                contentType: 'json',
+                responseText: resp
+            });
+        }));
 
+        it('Should render the Start Timer Event', async(() => {
             component.onSuccess.subscribe((res) => {
                 fixture.detectChanges();
                 fixture.whenStable().then(() => {
                     expect(res).toBeDefined();
-                    let startEvent: any = element.querySelector('diagram-start-event');
-                    expect(startEvent).toBeDefined();
-                    let raphaelCircle: any = element.querySelector('raphael-circle');
-                    expect(raphaelCircle).toBeDefined();
+                    let event: any = element.querySelector('diagram-start-event > diagram-event > raphael-circle');
+                    expect(event).not.toBeNull();
+
+                    let iconEvent: any = element.querySelector('diagram-start-event > diagram-event >' +
+                        ' diagram-container-icon-event > div > div > diagram-icon-timer > raphael-icon-timer');
+                    expect(iconEvent).not.toBeNull();
                 });
             });
             component.ngOnChanges();
-            let startEvent = {
-                id: 'startEvent1',
-                type: 'StartEvent',
-                width: 30,
-                height: 30,
-                x: 15,
-                y: 48.5,
-                properties: [{}]
-            };
-            let resp = {elements : [startEvent] };
+            let resp = {elements: [diagramsEventsMock.startTimeEvent]};
 
+            jasmine.Ajax.requests.mostRecent().respondWith({
+                status: 200,
+                contentType: 'json',
+                responseText: resp
+            });
+        }));
+
+        it('Should render the Start Signal Event', async(() => {
+            component.onSuccess.subscribe((res) => {
+                fixture.detectChanges();
+                fixture.whenStable().then(() => {
+                    expect(res).toBeDefined();
+                    let event: any = element.querySelector('diagram-start-event > diagram-event > raphael-circle');
+                    expect(event).not.toBeNull();
+
+                    let iconEvent: any = element.querySelector('diagram-start-event > diagram-event >' +
+                        ' diagram-container-icon-event > div > div > diagram-icon-signal > raphael-icon-signal');
+                    expect(iconEvent).not.toBeNull();
+                });
+            });
+            component.ngOnChanges();
+            let resp = {elements: [diagramsEventsMock.startSignalEvent]};
+            jasmine.Ajax.requests.mostRecent().respondWith({
+                status: 200,
+                contentType: 'json',
+                responseText: resp
+            });
+        }));
+
+        it('Should render the Start Message Event', async(() => {
+            component.onSuccess.subscribe((res) => {
+                fixture.detectChanges();
+                fixture.whenStable().then(() => {
+                    expect(res).toBeDefined();
+                    let event: any = element.querySelector('diagram-start-event > diagram-event > raphael-circle');
+                    expect(event).not.toBeNull();
+
+                    let iconEvent: any = element.querySelector('diagram-start-event > diagram-event >' +
+                        ' diagram-container-icon-event > div > div > diagram-icon-message > raphael-icon-message');
+                    expect(iconEvent).not.toBeNull();
+                });
+            });
+            component.ngOnChanges();
+            let resp = {elements: [diagramsEventsMock.startMessageEvent]};
+            jasmine.Ajax.requests.mostRecent().respondWith({
+                status: 200,
+                contentType: 'json',
+                responseText: resp
+            });
+        }));
+
+        it('Should render the Start Error Event', async(() => {
+            component.onSuccess.subscribe((res) => {
+                fixture.detectChanges();
+                fixture.whenStable().then(() => {
+                    expect(res).toBeDefined();
+                    let event: any = element.querySelector('diagram-start-event > diagram-event > raphael-circle');
+                    expect(event).not.toBeNull();
+
+                    let iconEvent: any = element.querySelector('diagram-start-event > diagram-event >' +
+                        ' diagram-container-icon-event > div > div > diagram-icon-error > raphael-icon-error');
+                    expect(iconEvent).not.toBeNull();
+                });
+            });
+            component.ngOnChanges();
+            let resp = {elements: [diagramsEventsMock.startErrorEvent]};
+            jasmine.Ajax.requests.mostRecent().respondWith({
+                status: 200,
+                contentType: 'json',
+                responseText: resp
+            });
+        }));
+
+        it('Should render the End Event', async(() => {
+            component.onSuccess.subscribe((res) => {
+                fixture.detectChanges();
+                fixture.whenStable().then(() => {
+                    expect(res).toBeDefined();
+                    let event: any = element.querySelector('diagram-end-event > diagram-event > raphael-circle');
+                    expect(event).not.toBeNull();
+                });
+            });
+            component.ngOnChanges();
+            let resp = {elements: [diagramsEventsMock.endEvent]};
+            jasmine.Ajax.requests.mostRecent().respondWith({
+                status: 200,
+                contentType: 'json',
+                responseText: resp
+            });
+        }));
+
+        it('Should render the End Error Event', async(() => {
+            component.onSuccess.subscribe((res) => {
+                fixture.detectChanges();
+                fixture.whenStable().then(() => {
+                    expect(res).toBeDefined();
+                    let event: any = element.querySelector('diagram-end-event > diagram-event > raphael-circle');
+                    expect(event).not.toBeNull();
+
+                    let iconEvent: any = element.querySelector('diagram-end-event > diagram-event >' +
+                        ' diagram-container-icon-event > div > div > diagram-icon-error > raphael-icon-error');
+                    expect(iconEvent).not.toBeNull();
+                });
+            });
+            component.ngOnChanges();
+            let resp = {elements: [diagramsEventsMock.endErrorEvent]};
             jasmine.Ajax.requests.mostRecent().respondWith({
                 status: 200,
                 contentType: 'json',
