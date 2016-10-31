7b0e8e10167b58fe250113a6e693eb36e7e58a88
Vito Albano <valbano@MacPro.local>
2016-10-31 17:11:09 +0000

    Added component search for users

---
+++ ng2-components/ng2-activiti-tasklist/src/components/activiti-people-search.component.ts
@@ -0,0 +1,81 @@
+/*!
+ * @license
+ * Copyright 2016 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component, Input, Output, EventEmitter, OnInit, AfterViewInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { User } from '../models/user.model';
+import { Observable } from 'rxjs/Observable';
+
+declare let componentHandler: any;
+
+@Component({
+    selector: 'activiti-people-search',
+    moduleId: module.id,
+    templateUrl: './activiti-people-search.component.html',
+    styleUrls: ['./activiti-people-search.component.css']
+})
+
+export class ActivitiPeopleSearch implements OnInit, AfterViewInit {
+
+    @Input()
+    results: Observable<User[]>;
+
+    @Output()
+    onSearch: EventEmitter<any> = new EventEmitter();
+
+    @Output()
+    onModalRowClicked: EventEmitter<any> = new EventEmitter();
+
+    private searchUser: FormControl = new FormControl();
+
+    userList: User[] = [];
+
+    constructor() {
+        this.searchUser
+            .valueChanges
+            .debounceTime(200)
+            .subscribe((event) => {
+                this.onSearch.emit(event);
+            });
+    }
+
+    ngOnInit() {
+        this.results.subscribe((list) => {
+            this.userList = list;
+        });
+    }
+
+    ngAfterViewInit() {
+        this.setupMaterialComponents(componentHandler);
+    }
+
+    setupMaterialComponents(handler?: any): boolean {
+        // workaround for MDL issues with dynamic components
+        if (handler) {
+            handler.upgradeAllRegistered();
+            return true;
+        }
+        return false;
+    }
+
+    onRowClick(userClicked: User) {
+        this.onModalRowClicked.emit(userClicked);
+        this.userList = this.userList.filter((user) => {
+            return user.id !== userClicked.id;
+        });
+    }
+}
