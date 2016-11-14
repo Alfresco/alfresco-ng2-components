3bace01d1c1a1add2cdcdf9b0bb6fac710def7c3
Vito Albano <valbano@MacPro.local>
2016-11-14 17:28:43 +0000

    Added changes to select the default value

--- ng2-components/ng2-activiti-form/src/components/widgets/dropdown/dropdown.widget.ts
+++ ng2-components/ng2-activiti-form/src/components/widgets/dropdown/dropdown.widget.ts
@@ -19,6 +19,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormService } from '../../../services/form.service';
 import { WidgetComponent } from './../widget.component';
 import { FormFieldOption } from './../core/form-field-option';
+import { WidgetVisibilityService } from '../../../services/widget-visibility.service';
 
 @Component({
     moduleId: module.id,
@@ -80,7 +81,7 @@ export class DropdownWidget extends WidgetComponent implements OnInit {
                 this.handleError
             );
     }
-    
+
     getOptionValue(option: FormFieldOption): string {
         let optionValue: string = '';
         if (option.id === 'empty') {
@@ -90,7 +91,11 @@ export class DropdownWidget extends WidgetComponent implements OnInit {
         }
         return optionValue;
     }
-    
+
+    checkVisibility() {
+        this.visibilityService.refreshVisibility(this.field.form);
+    }
+
     handleError(error: any) {
         console.error(error);
     }
