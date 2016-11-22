1931993c3097022c8e44f2f89696ba8f4ea23ca7
Will Abson <will.abson@alfresco.com>
2016-11-22 13:38:31 +0000

    New tests for process list
    Refs #1048

--- ng2-components/ng2-activiti-processlist/src/components/activiti-processlist.component.ts
+++ ng2-components/ng2-activiti-processlist/src/components/activiti-processlist.component.ts
@@ -19,6 +19,8 @@ import { Component, Input, Output, EventEmitter, OnInit, OnChanges, SimpleChange
 import { AlfrescoTranslationService } from 'ng2-alfresco-core';
 import { ObjectDataTableAdapter, DataTableAdapter, DataRowEvent, ObjectDataRow } from 'ng2-alfresco-datatable';
 import { TaskQueryRequestRepresentationModel } from 'ng2-activiti-tasklist';
+
+import { ProcessInstance } from '../models/process-instance.model';
 import { ActivitiProcessService } from '../services/activiti-process.service';
 
 @Component({
@@ -53,7 +55,7 @@ export class ActivitiProcessInstanceListComponent implements OnInit, OnChanges {
     rowClick: EventEmitter<string> = new EventEmitter<string>();
 
     @Output()
-    onSuccess: EventEmitter<any> = new EventEmitter<any>();
+    onSuccess: EventEmitter<ProcessInstance[]> = new EventEmitter<ProcessInstance[]>();
 
     @Output()
     onError: EventEmitter<any> = new EventEmitter<any>();
