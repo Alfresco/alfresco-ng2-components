62d7abcb72c7fc5dc3271cf10e1a1d9811786f22
Denys Vuika <denis.vuyka@gmail.com>
2016-12-08 15:51:46 +0000

    extend system loader with moduleName support

--- demo-shell-ng2/config/loaders/system.js
+++ demo-shell-ng2/config/loaders/system.js
@@ -1,15 +1,22 @@
 const moduleIdRegex = /moduleId: module.id,/g;
+const moduleNameRegex = /moduleId: __moduleName,/g;
 
 module.exports = function(source) {
     this.cacheable();
 
     let result = source;
-    let modified = false;
+
     if (moduleIdRegex.test(source)) {
         result = source.replace(moduleIdRegex, (match) => {
             return `// ${match}`;
         });
-        // console.log(result);
     }
+
+    if (moduleNameRegex.test(source)) {
+        result = source.replace(moduleNameRegex, (match) => {
+            return `// ${match}`;
+        });
+    }
+
     return result;
 }
