ca77608fa3d6a485cf146de1f733539c445142b5
Denys Vuika <denis.vuyka@gmail.com>
2016-12-12 18:36:57 +0000

    fix unit testing configuration (#1227)

--- demo-shell-ng2/config/karma.conf.js
+++ demo-shell-ng2/config/karma.conf.js
@@ -24,7 +24,7 @@ module.exports = function (config) {
       noInfo: true
     },
 
-    reporters: ['progress'],
+    reporters: ['mocha'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
--- demo-shell-ng2/config/webpack.test.js
+++ demo-shell-ng2/config/webpack.test.js
@@ -1,17 +1,64 @@
 var helpers = require('./helpers');
+var fs = require('fs');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var alfrescoLibs = [
+    helpers.root('node_modules', 'alfresco-js-api'),
+    helpers.root('node_modules', 'ng2-activiti-analytics'),
+    helpers.root('node_modules', 'ng2-activiti-diagrams'),
+    helpers.root('node_modules', 'ng2-activiti-form'),
+    helpers.root('node_modules', 'ng2-activiti-processlist'),
+    helpers.root('node_modules', 'ng2-activiti-tasklist'),
+    helpers.root('node_modules', 'ng2-alfresco-core'),
+    helpers.root('node_modules', 'ng2-alfresco-datatable'),
+    helpers.root('node_modules', 'ng2-alfresco-documentlist'),
+    helpers.root('node_modules', 'ng2-alfresco-login'),
+    helpers.root('node_modules', 'ng2-alfresco-search'),
+    helpers.root('node_modules', 'ng2-alfresco-tag'),
+    helpers.root('node_modules', 'ng2-alfresco-upload'),
+    helpers.root('node_modules', 'ng2-alfresco-userinfo'),
+    helpers.root('node_modules', 'ng2-alfresco-viewer'),
+    helpers.root('node_modules', 'ng2-alfresco-webscript')
+].map((entry) => fs.realpathSync(entry));
+
+const rootPath = helpers.root('node_modules');
 
 module.exports = {
   devtool: 'inline-source-map',
 
   resolve: {
-    extensions: ['', '.ts', '.js']
+    extensions: ['', '.ts', '.js'],
+    modules: [
+      helpers.root('app'),
+      helpers.root('node_modules')
+    ],
+    alias: {
+        'alfresco-js-api': helpers.root('node_modules', 'alfresco-js-api', 'dist', 'alfresco-js-api.js')
+    },
+    root: rootPath,
+    fallback: rootPath
+  },
+
+  resolveLoader: {
+      alias: {
+          'systemjs-loader': helpers.root('config', 'loaders', 'system.js')
+      },
+      fallback: rootPath
   },
 
   module: {
     loaders: [
       {
         test: /\.ts$/,
+        exclude: /node_modules/,
         loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+      },
+      {
+        test: /\.js$/,
+        include: [
+          ...alfrescoLibs
+        ],
+        loaders: ['angular2-template-loader', 'source-map-loader', 'systemjs-loader']
       },
       {
         test: /\.html$/,
@@ -23,15 +70,14 @@ module.exports = {
         loader: 'null'
       },
       {
-        test: /\.css$/,
-        exclude: helpers.root('src', 'app'),
-        loader: 'null'
-      },
-      {
-        test: /\.css$/,
-        include: helpers.root('src', 'app'),
-        loader: 'raw'
+          test: /\.css$/,
+          loader: 'raw'
       }
     ]
+  },
+
+  node: {
+      fs: 'empty',
+      module: false
   }
 }
