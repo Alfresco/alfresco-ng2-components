8b0621f5e114a01b1b4b69a9bc3ee4bdd206df8a
Mario Romano <dnamario@hotmail.com>
2016-12-12 22:02:00 +0000

    improve mobile login,home and settings styles

--- demo-shell-ng2/app/components/home/home.component.ts
+++ demo-shell-ng2/app/components/home/home.component.ts
@@ -19,6 +19,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'home-view',
-    templateUrl: './home.component.html'
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent {}
