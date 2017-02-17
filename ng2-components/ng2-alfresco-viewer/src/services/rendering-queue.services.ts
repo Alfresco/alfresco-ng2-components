6b8ed3bd1bd998fc6bc295ec517124e66e07be1f
Eugenio Romano <eugenio.romano@alfresco.com>
2017-02-17 17:26:09 +0000

    rendering service header

--- ng2-components/ng2-alfresco-viewer/src/services/rendering-queue.services.ts
+++ ng2-components/ng2-alfresco-viewer/src/services/rendering-queue.services.ts
@@ -1,6 +1,4 @@
-/*!
- * @license
- * Copyright 2016 Alfresco Software, Ltd.
+/* Copyright 2012 Mozilla Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
