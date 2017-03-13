64565c44f411ff3fc123a2e487949b396a9f4c63
Denys Vuika <denis.vuyka@gmail.com>
2017-03-13 11:25:47 +0000

    migrate from PhantomJS to Chrome (#1716)

--- demo-shell-ng2/config/karma.conf.js
+++ demo-shell-ng2/config/karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome'],
     singleRun: true
   };
 
