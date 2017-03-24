aa87a374dd375befd3836d09bfcc2cacf47b4abe
Mario Romano <dnamario@hotmail.com>
2017-03-24 10:32:09 +0000

    #1214 use angular 2 mdl input field (#1762)

--- demo-shell-ng2/app/vendor.ts
+++ demo-shell-ng2/app/vendor.ts
@@ -37,7 +37,7 @@ import '../public/css/app.css';
 import '../public/css/muli-font.css';
 
 // Load the Angular Material 2 stylesheet
-import '@angular/material/core/theming/prebuilt/deeppurple-amber.css'
+import '../public/css/angular-material.css'
 
 import 'ng2-activiti-form/stencils/runtime.ng1';
 import 'ng2-activiti-form/stencils/runtime.adf';
