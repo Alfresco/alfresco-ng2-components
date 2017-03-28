32603f52e84a13a0b29070c27596d13f6724a3ba
Denys Vuika <denis.vuyka@gmail.com>
2017-03-28 09:48:23 +0100

    bundle improvements (#1774)
    - add missing alfresco-js-api to the vendors
    - add missing 3D viewer related libs to the vendors (fixes issue with
    app bundle having duplicates)

--- demo-shell-ng2/app/vendor.ts
+++ demo-shell-ng2/app/vendor.ts
@@ -10,6 +10,7 @@ import '@angular/router';
 import 'rxjs';
 
 // Alfresco
+import 'alfresco-js-api';
 import 'ng2-alfresco-core';
 import 'ng2-alfresco-datatable';
 import 'ng2-activiti-diagrams';
@@ -37,7 +38,7 @@ import '../public/css/app.css';
 import '../public/css/muli-font.css';
 
 // Load the Angular Material 2 stylesheet
-import '../public/css/angular-material.css'
+import '../public/css/angular-material.css';
 
 import 'ng2-activiti-form/stencils/runtime.ng1';
 import 'ng2-activiti-form/stencils/runtime.adf';
@@ -64,3 +65,7 @@ if (process.env.ENV === 'production') {
 
 require('pdfjs-dist/web/pdf_viewer.js');
 require('three/build/three.min.js');
+
+// 3D viewer
+import 'three';
+import 'ng2-3d-editor';
