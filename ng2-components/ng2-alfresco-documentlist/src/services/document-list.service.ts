c27aa5b997c2a60be277d30ae212a5dc2dd42c1c
VitoAlbano <vito.albano.123@gmail.com>
2017-04-19 15:16:23 +0100

    #ADF-166 - removed wrong pushed change

--- ng2-components/ng2-alfresco-documentlist/src/services/document-list.service.ts
+++ ng2-components/ng2-alfresco-documentlist/src/services/document-list.service.ts
@@ -115,7 +115,6 @@ export class DocumentListService {
     getFolder(folder: string, opts?: any) {
         return Observable.fromPromise(this.getNodesPromise(folder, opts))
             .map(res => <NodePaging> res)
-            .do(res => console.log(res))
             .catch(err => this.handleError(err));
     }
 
