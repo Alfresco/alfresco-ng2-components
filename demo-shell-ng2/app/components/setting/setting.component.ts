93f12b9ef8915c4cd7d2e1b2f3cbe4fe66a0805d
VitoAlbano <vito.albano.123@gmail.com>
2017-04-20 17:19:03 +0100

    #ADF-498 added check for valid settings addresses

--- demo-shell-ng2/app/components/setting/setting.component.ts
+++ demo-shell-ng2/app/components/setting/setting.component.ts
@@ -15,15 +15,17 @@
  * limitations under the License.
  */
 
-import { Component } from '@angular/core';
+import { Component, AfterViewChecked } from '@angular/core';
 import { AlfrescoSettingsService, StorageService, LogService } from 'ng2-alfresco-core';
+
+declare var componentHandler: any;
 
 @Component({
     selector: 'alfresco-setting-demo',
     templateUrl: './setting.component.html',
     styleUrls: ['./setting.component.css']
 })
-export class SettingComponent {
+export class SettingComponent implements AfterViewChecked {
 
     ecmHost: string;
     bpmHost: string;
@@ -35,24 +37,39 @@ export class SettingComponent {
         this.bpmHost = this.settingsService.bpmHost;
     }
 
+    ngAfterViewChecked() {
+        // workaround for MDL issues with dynamic components
+        if (componentHandler) {
+            componentHandler.upgradeAllRegistered();
+        }
+    }
+
     public onChangeECMHost(event: KeyboardEvent): void {
         let value = (<HTMLInputElement>event.target).value.trim();
-        if (value) {
+        if (value && this.isValidUrl(value)) {
             this.logService.info(`ECM host: ${value}`);
             this.ecmHost = value;
             this.settingsService.ecmHost = value;
             this.storage.setItem(`ecmHost`, value);
+        } else {
+            console.error('Ecm address does not match the pattern');
         }
     }
 
     public onChangeBPMHost(event: KeyboardEvent): void {
         let value = (<HTMLInputElement>event.target).value.trim();
-        if (value) {
+        if (value && this.isValidUrl(value)) {
             this.logService.info(`BPM host: ${value}`);
             this.bpmHost = value;
             this.settingsService.bpmHost = value;
             this.storage.setItem(`bpmHost`, value);
+        } else {
+            console.error('Bpm address does not match the pattern');
         }
+    }
+
+    isValidUrl(url: string) {
+        return /^(http|https):\/\/.*/.test(url);
     }
 
 }
