dd7b8c4fca585ce51e85ae5cd02d223758b1ec25
Eugenio Romano <eugenio.romano@alfresco.com>
2017-05-23 15:55:39 +0100

    update version script refactoring

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -3,7 +3,23 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 eval JS_API=true
 
-set -ex
+eval projects=( "ng2-alfresco-core"
+    "ng2-alfresco-datatable"
+    "ng2-activiti-diagrams"
+    "ng2-activiti-analytics"
+    "ng2-activiti-form"
+    "ng2-activiti-tasklist"
+    "ng2-activiti-processlist"
+    "ng2-alfresco-documentlist"
+    "ng2-alfresco-login"
+    "ng2-alfresco-search"
+    "ng2-alfresco-social"
+    "ng2-alfresco-tag"
+    "ng2-alfresco-social"
+    "ng2-alfresco-upload"
+    "ng2-alfresco-viewer"
+    "ng2-alfresco-webscript"
+    "ng2-alfresco-userinfo" )
 
 cd `dirname $0`
 
@@ -24,6 +40,74 @@ version_change() {
     VERSION=$1
 }
 
+
+update_component_version() {
+   echo "====== UPDATE PACKAGE VERSION of ${PACKAGE} to ${VERSION} version in all the package.json ======"
+   DESTDIR="$DIR/../ng2-components/${1}"
+   sed -i '' "s/\"version\": \"[0-9]\\.[0-9]\\.[0-9]\"/\"version\": \"${VERSION}\"/g"  ${DESTDIR}/package.json
+}
+
+update_component_dependency_version(){
+   echo "====== UPDATE DEPENDENCY VERSION of ${1} to ${VERSION} in ${1}======"
+   DESTDIR="$DIR/../ng2-components/${1}"
+
+   for PACKAGETOCHANGE in ${projects[@]}
+   do
+       sed -i '' "s/\"${PACKAGETOCHANGE}\": \"[0-9]\\.[0-9]\\.[0-9]\"/\"${PACKAGETOCHANGE}\": \"${VERSION}\"/g"  ${DESTDIR}/package.json
+
+       echo "====== UPDATE DEPENDENCY VERSION of ${1} to ~${VERSION} in ${1}======"
+       sed -i '' "s/\"${PACKAGETOCHANGE}\": \"~[0-9]\\.[0-9]\\.[0-9]\"/\"${PACKAGETOCHANGE}\": \"~${VERSION}\"/g"  ${DESTDIR}/package.json
+
+       echo "====== UPDATE DEPENDENCY VERSION of ${1} to ${VERSION} in ${1} DEMO ======"
+       sed -i '' "s/\"${PACKAGETOCHANGE}\": \"[0-9]\\.[0-9]\\.[0-9]\"/\"${PACKAGETOCHANGE}\": \"${VERSION}\"/g"  ${DESTDIR}/demo/package.json
+
+       echo "====== UPDATE DEPENDENCY VERSION of ${1} to ~${VERSION} in ${1} DEMO ======"
+       sed -i '' "s/\"${PACKAGETOCHANGE}\": \"~[0-9]\\.[0-9]\\.[0-9]\"/\"${PACKAGETOCHANGE}\": \"~${VERSION}\"/g"  ${DESTDIR}/demo/package.json
+   done
+}
+
+update_component_js_version(){
+   echo "====== UPDATE DEPENDENCY VERSION of alfresco-js-api in ${1} to ${VERSION} in ${1}======"
+   DESTDIR="$DIR/../ng2-components/${1}"
+
+   PACKAGETOCHANGE="alfresco-js-api"
+   sed -i '' "s/\"${PACKAGETOCHANGE}\": \"[0-9]\\.[0-9]\\.[0-9]\"/\"${PACKAGETOCHANGE}\": \"${VERSION}\"/g"  ${DESTDIR}/package.json
+
+   echo "====== UPDATE DEPENDENCY VERSION of ${1} to ~${VERSION} in ${1}======"
+   sed -i '' "s/\"${PACKAGETOCHANGE}\": \"~[0-9]\\.[0-9]\\.[0-9]\"/\"${PACKAGETOCHANGE}\": \"~${VERSION}\"/g"  ${DESTDIR}/package.json
+
+   echo "====== UPDATE DEPENDENCY VERSION of ${1} to ${VERSION} in ${1} DEMO ======"
+   sed -i '' "s/\"${PACKAGETOCHANGE}\": \"[0-9]\\.[0-9]\\.[0-9]\"/\"${PACKAGETOCHANGE}\": \"${VERSION}\"/g"  ${DESTDIR}/demo/package.json
+
+   echo "====== UPDATE DEPENDENCY VERSION of ${1} to ~${VERSION} in ${1} DEMO ======"
+   sed -i '' "s/\"${PACKAGETOCHANGE}\": \"~[0-9]\\.[0-9]\\.[0-9]\"/\"${PACKAGETOCHANGE}\": \"~${VERSION}\"/g"  ${DESTDIR}/demo/package.json
+}
+
+update_demo_shell_dependency_version(){
+
+   for PACKAGETOCHANGE in ${projects[@]}
+   do
+    echo "====== UPDATE VERSION OF DEMO-SHELL to ${PACKAGETOCHANGE} version ${VERSION} ======"
+    DESTDIR="$DIR/../demo-shell-ng2/"
+    sed -i '' "s/\"${PACKAGETOCHANGE}\": \"[0-9]\\.[0-9]\\.[0-9]\"/\"${PACKAGETOCHANGE}\": \"${VERSION}\"/g"  ${DESTDIR}/package.json
+
+    echo "====== UPDATE DEPENDENCY VERSION of ${1} to ~${VERSION} in ${DESTDIR}======"
+    sed -i '' "s/\"${PACKAGETOCHANGE}\": \"~[0-9]\\.[0-9]\\.[0-9]\"/\"${PACKAGETOCHANGE}\": \"~${VERSION}\"/g"  ${DESTDIR}/package.json
+   done
+}
+
+update_demo_shell_js_version(){
+    echo "====== UPDATE VERSION OF DEMO-SHELL to  alfresco-js-api version ${VERSION} ======"
+    DESTDIR="$DIR/../demo-shell-ng2/"
+    PACKAGETOCHANGE="alfresco-js-api"
+
+    sed -i '' "s/\"${PACKAGETOCHANGE}\": \"[0-9]\\.[0-9]\\.[0-9]\"/\"${PACKAGETOCHANGE}\": \"${VERSION}\"/g"  ${DESTDIR}/package.json
+
+    echo "====== UPDATE DEPENDENCY VERSION of ${1} to ~${VERSION} in ${DESTDIR}======"
+    sed -i '' "s/\"${PACKAGETOCHANGE}\": \"~[0-9]\\.[0-9]\\.[0-9]\"/\"${PACKAGETOCHANGE}\": \"~${VERSION}\"/g"  ${DESTDIR}/package.json
+}
+
+
 while [[ $1  == -* ]]; do
     case "$1" in
       -h|--help|-\?) show_help; exit 0;;
@@ -40,122 +124,27 @@ then
   exit 1
 fi
 
-for PACKAGE in \
-  ng2-activiti-diagrams \
-  ng2-activiti-analytics \
-  ng2-activiti-form \
-  ng2-activiti-processlist \
-  ng2-activiti-tasklist \
-  ng2-alfresco-core \
-  ng2-alfresco-datatable \
-  ng2-alfresco-documentlist \
-  ng2-alfresco-login \
-  ng2-alfresco-search \
-  ng2-alfresco-tag \
-  ng2-alfresco-upload \
-  ng2-alfresco-viewer \
-  ng2-alfresco-webscript \
-  ng2-alfresco-userinfo \
-  ng2-alfresco-social
+cd "$DIR/../"
+
+echo "====== UPDATE COMPONENTS ======"
+
+for PACKAGE in ${projects[@]}
 do
-  echo "====== UPDATE PACKAGE VERSION of ${PACKAGE} to ${VERSION} version in all the package.json ======"
-  find ././../ -type f -maxdepth 4 -name package.json -print0 | xargs -0 sed -i '' "s/\"${PACKAGE}\": \"[0-9]\\.[0-9]\\.[0-9]\"/\"${PACKAGE}\": \"${VERSION}\"/g"
+   update_component_version $PACKAGE
+   update_component_dependency_version $PACKAGE
+
+   if $JS_API == true; then
+    update_component_js_version $PACKAGE
+   fi
 done
 
+echo "====== UPDATE DEMO SHELL ======"
+
+update_demo_shell_dependency_version
 
 if $JS_API == true; then
-    for PACKAGE in \
-      alfresco-js-api
-    do
-      echo "====== UPDATE PACKAGE VERSION of ${PACKAGE} to ${VERSION} version in all the package.json ======"
-      find ././../ -type f -maxdepth 4 -name package.json -print0 | xargs -0 sed -i '' "s/\"${PACKAGE}\": \"[0-9]\\.[0-9]\\.[0-9]\"/\"${PACKAGE}\": \"${VERSION}\"/g"
-    done
+    update_demo_shell_js_version
 fi
 
-for PACKAGE in \
-  ng2-activiti-diagrams \
-  ng2-activiti-analytics \
-  ng2-activiti-form \
-  ng2-activiti-processlist \
-  ng2-activiti-tasklist \
-  ng2-alfresco-core \
-  ng2-alfresco-datatable \
-  ng2-alfresco-documentlist \
-  ng2-alfresco-login \
-  ng2-alfresco-search \
-  ng2-alfresco-tag \
-  ng2-alfresco-upload \
-  ng2-alfresco-viewer \
-  ng2-alfresco-webscript \
-  ng2-alfresco-social \
-  ng2-alfresco-userinfo
-do
-  DESTDIR="$DIR/../ng2-components/${PACKAGE}"
-  echo "====== UPDATE VERSION OF ${PACKAGE} to ${VERSION} version ======"
-  sed -i '' "s/\"version\": \"[0-9]\\.[0-9]\\.[0-9]\"/\"version\": \"${VERSION}\"/g"  ${DESTDIR}/package.json
-done
-
-echo "====== UPDATE VERSION OF DEMO-SHELL to ${VERSION} version ======"
-
+DESTDIR="$DIR/../demo-shell-ng2/"
 sed -i '' "s/\"version\": \"[0-9]\\.[0-9]\\.[0-9]\"/\"version\": \"${VERSION}\"/g"  ${DIR}/../demo-shell-ng2/package.json
-
-for PACKAGE in \
-  ng2-activiti-diagrams \
-  ng2-activiti-analytics \
-  ng2-activiti-form \
-  ng2-activiti-processlist \
-  ng2-activiti-tasklist \
-  ng2-alfresco-core \
-  ng2-alfresco-datatable \
-  ng2-alfresco-documentlist \
-  ng2-alfresco-login \
-  ng2-alfresco-search \
-  ng2-alfresco-tag \
-  ng2-alfresco-upload \
-  ng2-alfresco-viewer \
-  ng2-alfresco-webscript \
-  ng2-alfresco-userinfo \
-  ng2-alfresco-social
-do
-  DESTDIR="$DIR/../ng2-components/${PACKAGE}"
-  echo "====== UPDATE PACKAGE VERSION of ${PACKAGE} to ~${VERSION} version in all the package.json ======"
-  find ././../ -type f -maxdepth 4 -name package.json -print0 | xargs -0 sed -i '' "s/\"${PACKAGE}\": \"~[0-9]\\.[0-9]\\.[0-9]\"/\"${PACKAGE}\": \"~${VERSION}\"/g"
-done
-
-
-if $JS_API == true; then
-    for PACKAGE in \
-      alfresco-js-api
-    do
-        DESTDIR="$DIR/../ng2-components/${PACKAGE}"
-        echo "====== UPDATE PACKAGE VERSION of ${PACKAGE} to ~${VERSION} version in all the package.json ======"
-        find ././../ -type f -maxdepth 4 -name package.json -print0 | xargs -0 sed -i '' "s/\"${PACKAGE}\": \"~[0-9]\\.[0-9]\\.[0-9]\"/\"${PACKAGE}\": \"~${VERSION}\"/g"
-    done
-fi
-
-for PACKAGE in \
-  ng2-activiti-diagrams \
-  ng2-activiti-analytics \
-  ng2-activiti-form \
-  ng2-activiti-processlist \
-  ng2-activiti-tasklist \
-  ng2-alfresco-core \
-  ng2-alfresco-datatable \
-  ng2-alfresco-documentlist \
-  ng2-alfresco-login \
-  ng2-alfresco-search \
-  ng2-alfresco-tag \
-  ng2-alfresco-upload \
-  ng2-alfresco-viewer \
-  ng2-alfresco-webscript \
-  ng2-alfresco-social \
-  ng2-alfresco-userinfo
-do
-  DESTDIR="$DIR/../ng2-components/${PACKAGE}"
-  echo "====== UPDATE VERSION OF ${PACKAGE} to ~${VERSION} version ======"
-  sed -i '' "s/\"version\": \"~[0-9]\\.[0-9]\\.[0-9]\"/\"version\": \"~${VERSION}\"/g"  ${DESTDIR}/package.json
-done
-
-echo "====== UPDATE VERSION OF DEMO-SHELL to ${VERSION} version ======"
-
-sed -i '' "s/\"version\": \"~[0-9]\\.[0-9]\\.[0-9]\"/\"version\": \"~${VERSION}\"/g"  ${DIR}/../demo-shell-ng2/package.json
