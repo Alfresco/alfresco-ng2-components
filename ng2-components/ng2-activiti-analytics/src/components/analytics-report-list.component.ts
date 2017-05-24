418364c04ef89bad17bb62f7b5ec4ae9bf2ad04c
VitoAlbano <vito.albano.123@gmail.com>
2017-05-24 12:35:41 +0100

    [ADF-242] - fix for deleting - saving a report

--- ng2-components/ng2-activiti-analytics/src/components/analytics-report-list.component.ts
+++ ng2-components/ng2-activiti-analytics/src/components/analytics-report-list.component.ts
@@ -94,7 +94,6 @@ export class AnalyticsReportListComponent implements OnInit {
                         this.reportObserver.next(report);
                     });
                     if (reportId) {
-                        console.log('SELEZIONO IL REPORT!');
                         this.selectReportByReportId(reportId);
                     }
                     if (this.selectFirst) {
