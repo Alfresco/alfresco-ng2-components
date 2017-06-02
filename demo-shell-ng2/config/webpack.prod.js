889e23dd4a1af816adfd0dcdf65308afca2e319c
Eugenio Romano <eromano@users.noreply.github.com>
2017-06-02 09:17:08 +0100

    [ADF-721] Fix translation reference for dev task (#1923)
    * move translation files in the bundles folder
    
    * fix after review
    ripristinate tslint and remove override tsconfig

--- demo-shell-ng2/config/webpack.prod.js
+++ demo-shell-ng2/config/webpack.prod.js
@@ -74,6 +74,14 @@ module.exports = webpackMerge(commonConfig, {
         }),
 
         new CopyWebpackPlugin([
+            //Deprecation old files translation strategy from 1.6.0 translation files are in the bundles folder
+            ... alfrescoLibs.map(lib => {
+                return {
+                    context: 'node_modules',
+                    from: `${lib}/src/i18n/*`,
+                    to: `assets/${lib}/i18n/*`
+                }
+            }),
             ... alfrescoLibs.map(lib => {
                 return {
                     context: `node_modules/${lib}/bundles/assets/`,
