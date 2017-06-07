95fdebb0240841d8a221126d04aaf5214aac7264
Eugenio Romano <eromano@users.noreply.github.com>
2017-06-07 07:32:56 +0100

    add info and link on current last git commit (#1940)

--- demo-shell-ng2/app/components/about/about.component.ts
+++ demo-shell-ng2/app/components/about/about.component.ts
@@ -17,8 +17,8 @@
 
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
-import { AlfrescoAuthenticationService, AppConfigService, BpmProductVersionModel, DiscoveryApiService, EcmProductVersionModel, LogService  } from 'ng2-alfresco-core';
 import { ObjectDataTableAdapter } from 'ng2-alfresco-datatable';
+import { LogService } from 'ng2-alfresco-core';
 
 @Component({
     selector: 'about-page',
@@ -28,36 +28,14 @@ import { ObjectDataTableAdapter } from 'ng2-alfresco-datatable';
 export class AboutComponent implements OnInit {
 
     data: ObjectDataTableAdapter;
+
     githubUrlCommitAlpha: string = 'https://github.com/Alfresco/alfresco-ng2-components/commits/';
 
-    configFile: string = '';
-    ecmHost: string = '';
-    bpmHost: string = '';
-
-    ecmVersion: EcmProductVersionModel = null;
-    bpmVersion: BpmProductVersionModel = null;
-
     constructor(private http: Http,
-                private appConfig: AppConfigService,
-                private authService: AlfrescoAuthenticationService,
-                private logService: LogService,
-                private discovery: DiscoveryApiService) {
+                private logService: LogService) {
     }
 
     ngOnInit() {
-
-        if (this.authService.isEcmLoggedIn()) {
-            this.discovery.getEcmProductInfo().subscribe((ecmVers) => {
-                this.ecmVersion = ecmVers;
-            });
-        }
-
-        if (this.authService.isBpmLoggedIn()) {
-            this.discovery.getBpmProductInfo().subscribe((bpmVers) => {
-                this.bpmVersion = bpmVers;
-            });
-        }
-
         this.http.get('/versions.json').subscribe(response => {
             let regexp = new RegExp('^(ng2-activiti|ng2-alfresco|alfresco-)');
 
@@ -84,10 +62,6 @@ export class AboutComponent implements OnInit {
                 {type: 'text', key: 'version', title: 'Version', sortable: true}
             ]);
         });
-
-        this.configFile = this.appConfig.configFile;
-        this.ecmHost = this.appConfig.get<string>('ecmHost');
-        this.bpmHost = this.appConfig.get<string>('bpmHost');
     }
 
     private gitHubLinkCreation(alfrescoPackagesTableRepresentation): void {
@@ -98,7 +72,7 @@ export class AboutComponent implements OnInit {
         if (corePackage) {
             let commitIsh = corePackage.version.split('-');
             if (commitIsh.length > 1) {
-                this.githubUrlCommitAlpha = this.githubUrlCommitAlpha + commitIsh[1];
+                this.githubUrlCommitAlpha = this.githubUrlCommitAlpha + commitIsh;
             } else {
                 this.githubUrlCommitAlpha = this.githubUrlCommitAlpha + corePackage.version;
             }
