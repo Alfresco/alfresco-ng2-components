6faf2b2ec11dd688794f43f3320f8885a0726ca2
Eugenio Romano <eromano@users.noreply.github.com>
2017-06-09 18:17:25 +0100

    fix thumbnail task process list (#1951)

--- ng2-components/ng2-activiti-tasklist/src/components/adf-task-attachment-list.component.spec.ts
+++ ng2-components/ng2-activiti-tasklist/src/components/adf-task-attachment-list.component.spec.ts
@@ -22,6 +22,7 @@ import { DataTableModule } from 'ng2-alfresco-datatable';
 import { ActivitiContentService } from 'ng2-activiti-form';
 import { TaskAttachmentListComponent } from './adf-task-attachment-list.component';
 import { Observable } from 'rxjs/Rx';
+import { By } from '@angular/platform-browser';
 
 declare let jasmine: any;
 
@@ -31,14 +32,10 @@ describe('TaskAttachmentList', () => {
     let component: TaskAttachmentListComponent;
     let fixture: ComponentFixture<TaskAttachmentListComponent>;
     let service: ActivitiContentService;
-
-    beforeEach(() => {
-        jasmine.Ajax.install();
-    });
-
-    afterEach(() => {
-        jasmine.Ajax.uninstall();
-    });
+    let getTaskRelatedContentSpy: jasmine.Spy;
+    let deleteContentSpy: jasmine.Spy;
+    let getFileRawContentSpy: jasmine.Spy;
+    let mockAttachment: any;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -56,7 +53,9 @@ describe('TaskAttachmentList', () => {
 
         let translateService = TestBed.get(AlfrescoTranslationService);
         spyOn(translateService, 'addTranslationFolder').and.stub();
-        spyOn(translateService, 'get').and.callFake((key) => { return Observable.of(key); });
+        spyOn(translateService, 'get').and.callFake((key) => {
+            return Observable.of(key);
+        });
     }));
 
     beforeEach(() => {
@@ -66,6 +65,51 @@ describe('TaskAttachmentList', () => {
 
         service = TestBed.get(ActivitiContentService);
 
+        mockAttachment = {
+            size: 2,
+            total: 2,
+            start: 0,
+            data: [
+                {
+                    id: 8,
+                    name: 'fake.zip',
+                    created: 1494595697381,
+                    createdBy: {id: 2, firstName: 'user', lastName: 'user', email: 'user@user.com'},
+                    relatedContent: true,
+                    contentAvailable: true,
+                    link: false,
+                    mimeType: 'application/zip',
+                    simpleType: 'content',
+                    previewStatus: 'unsupported',
+                    thumbnailStatus: 'unsupported'
+                },
+                {
+                    id: 9,
+                    name: 'fake.jpg',
+                    created: 1494595655381,
+                    createdBy: {id: 2, firstName: 'user', lastName: 'user', email: 'user@user.com'},
+                    relatedContent: true,
+                    contentAvailable: true,
+                    link: false,
+                    mimeType: 'image/jpeg',
+                    simpleType: 'image',
+                    previewStatus: 'unsupported',
+                    thumbnailStatus: 'unsupported'
+                }
+            ]
+        };
+
+        getTaskRelatedContentSpy = spyOn(service, 'getTaskRelatedContent').and.returnValue(Observable.of(
+            mockAttachment
+        ));
+
+        deleteContentSpy = spyOn(service, 'deleteRelatedContent').and.returnValue(Observable.of({successCode: true}));
+
+        let blobObj = new Blob();
+        getFileRawContentSpy = spyOn(service, 'getFileRawContent').and.returnValue(Observable.of(
+            blobObj
+        ));
+
         componentHandler = jasmine.createSpyObj('componentHandler', [
             'upgradeAllRegistered',
             'upgradeElement'
@@ -73,58 +117,100 @@ describe('TaskAttachmentList', () => {
         window['componentHandler'] = componentHandler;
     });
 
-    it('should fetch all the attachments of a taskId', (done) => {
+    it('should load attachments when taskId specified', () => {
+        let change = new SimpleChange(null, '123', true);
+        component.ngOnChanges({'taskId': change});
+        expect(getTaskRelatedContentSpy).toHaveBeenCalled();
+    });
 
-        component.success.subscribe((res) => {
-            expect(res).toBeDefined();
-            expect(res.length).toBe(2);
-            expect(res[0].name).toBe('fake.zip');
-            expect(res[0].icon).toBe('ft_ic_archive.svg');
-            expect(res[1].name).toBe('fake.jpg');
-            expect(res[1].icon).toBe('ft_ic_raster_image.svg');
-            done();
+    it('should emit an error when an error occurs loading attachments', () => {
+        let emitSpy = spyOn(component.error, 'emit');
+        getTaskRelatedContentSpy.and.returnValue(Observable.throw({}));
+        let change = new SimpleChange(null, '123', true);
+        component.ngOnChanges({'taskId': change});
+        expect(emitSpy).toHaveBeenCalled();
+    });
+
+    it('should emit a success event when the attachments are loaded', () => {
+        let change = new SimpleChange(null, '123', true);
+        component.success.subscribe((attachments) => {
+            expect(attachments[0].name).toEqual(mockAttachment.data[0].name);
+            expect(attachments[0].id).toEqual(mockAttachment.data[0].id);
         });
 
-        let taskId = '1';
-        let change = new SimpleChange(null, taskId, true);
-        component.ngOnChanges({ 'taskId': change });
+        component.ngOnChanges({'taskId': change});
+    });
 
-        jasmine.Ajax.requests.mostRecent().respondWith({
-            'status': 200,
-            contentType: 'application/json',
-            responseText: JSON.stringify({
-                size: 2,
-                total: 2,
-                start: 0,
-                data: [
-                    {
-                        id: 8,
-                        name: 'fake.zip',
-                        created: 1494595697381,
-                        createdBy: {id: 2, firstName: 'user', lastName: 'user', email: 'user@user.com'},
-                        relatedContent: true,
-                        contentAvailable: true,
-                        link: false,
-                        mimeType: 'application/zip',
-                        simpleType: 'content',
-                        previewStatus: 'unsupported',
-                        thumbnailStatus: 'unsupported'
-                    },
-                    {
-                        id: 9,
-                        name: 'fake.jpg',
-                        created: 1494595655381,
-                        createdBy: {id: 2, firstName: 'user', lastName: 'user', email: 'user@user.com'},
-                        relatedContent: true,
-                        contentAvailable: true,
-                        link: false,
-                        mimeType: 'image/jpeg',
-                        simpleType: 'image',
-                        previewStatus: 'unsupported',
-                        thumbnailStatus: 'unsupported'
-                    }
-                ]
-            })
+    it('should not attach when no taskId is specified', () => {
+        fixture.detectChanges();
+        expect(getTaskRelatedContentSpy).not.toHaveBeenCalled();
+    });
+
+    it('should display attachments when the task has attachments', async(() => {
+        let change = new SimpleChange(null, '123', true);
+        component.ngOnChanges({'taskId': change});
+
+        fixture.whenStable().then(() => {
+            fixture.detectChanges();
+            expect(fixture.debugElement.queryAll(By.css('alfresco-datatable tbody tr')).length).toBe(2);
         });
+    }));
+
+    it('should not display attachments when the task has no attachments', async(() => {
+        component.taskId = '123';
+        getTaskRelatedContentSpy.and.returnValue(Observable.of({
+            'size': 0,
+            'total': 0,
+            'start': 0,
+            'data': []
+        }));
+        fixture.detectChanges();
+        fixture.whenStable().then(() => {
+            fixture.detectChanges();
+            expect(fixture.debugElement.queryAll(By.css('alfresco-datatable tbody tr')).length).toBe(0);
+        });
+    }));
+
+    describe('change detection', () => {
+
+        let change = new SimpleChange('123', '456', true);
+        let nullChange = new SimpleChange('123', null, true);
+
+        beforeEach(async(() => {
+            component.taskId = '123';
+            fixture.detectChanges();
+            fixture.whenStable().then(() => {
+                getTaskRelatedContentSpy.calls.reset();
+            });
+        }));
+
+        it('should fetch new attachments when taskId changed', () => {
+            component.ngOnChanges({'taskId': change});
+            expect(getTaskRelatedContentSpy).toHaveBeenCalledWith('456');
+        });
+
+        it('should NOT fetch new attachments when empty changeset made', () => {
+            component.ngOnChanges({});
+            expect(getTaskRelatedContentSpy).not.toHaveBeenCalled();
+        });
+
+        it('should NOT fetch new attachments when taskId changed to null', () => {
+            component.ngOnChanges({'taskId': nullChange});
+            expect(getTaskRelatedContentSpy).not.toHaveBeenCalled();
+        });
+    });
+
+    describe('Delete attachments', () => {
+
+        beforeEach(async(() => {
+            component.taskId = '123';
+            fixture.detectChanges();
+            fixture.whenStable();
+        }));
+
+        it('should display a dialog to the user when the Add button clicked', () => {
+            expect(true).toBe(true);
+        });
+
     });
 });
