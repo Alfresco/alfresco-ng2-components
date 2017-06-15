f83e6428f9611874f71593719cba5fdac7dbf2a5
Vito <vito.albano@alfresco.com>
2017-06-15 05:04:36 -0700

    [ADF-494] fixed readonly rendering for forms (#1972)
    * [ADF-494] improved disabling for form
    
    * [ADF-494] fixed readonly rendering for forms

--- ng2-components/ng2-activiti-form/src/components/widgets/upload/upload.widget.spec.ts
+++ ng2-components/ng2-activiti-form/src/components/widgets/upload/upload.widget.spec.ts
@@ -19,9 +19,14 @@ import { UploadWidget } from './upload.widget';
 import { FormFieldModel } from './../core/form-field.model';
 import { FormFieldTypes } from '../core/form-field-types';
 import { FormService } from '../../../services/form.service';
+import { EcmModelService } from '../../../services/ecm-model.service';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { CoreModule } from 'ng2-alfresco-core';
+import { FormModel } from '../core/form.model';
 
 describe('UploadWidget', () => {
 
+    let componentHandler;
     let widget: UploadWidget;
     let formService: FormService;
 
@@ -37,7 +42,7 @@ describe('UploadWidget', () => {
         widget.field = new FormFieldModel(null, {
             type: FormFieldTypes.UPLOAD,
             value: [
-                {name: encodedFileName}
+                { name: encodedFileName }
             ]
         });
 
@@ -71,12 +76,66 @@ describe('UploadWidget', () => {
         widget.field = new FormFieldModel(null, {
             type: FormFieldTypes.UPLOAD,
             value: [
-                {name: 'filename'}
+                { name: 'filename' }
             ]
         });
         widget.reset();
         expect(widget.field.value).toBeNull();
         expect(widget.field.json.value).toBeNull();
+    });
+
+    describe('when template is ready', () => {
+        let uploadWidget: UploadWidget;
+        let fixture: ComponentFixture<UploadWidget>;
+        let element: HTMLInputElement;
+        let inputElement: HTMLInputElement;
+
+        beforeEach(async(() => {
+            componentHandler = jasmine.createSpyObj('componentHandler', ['upgradeAllRegistered', 'upgradeElement']);
+            window['componentHandler'] = componentHandler;
+        }));
+
+        beforeEach(async(() => {
+            TestBed.configureTestingModule({
+                imports: [CoreModule],
+                declarations: [UploadWidget],
+                providers: [FormService, EcmModelService]
+            }).compileComponents().then(() => {
+                fixture = TestBed.createComponent(UploadWidget);
+                uploadWidget = fixture.componentInstance;
+                element = fixture.nativeElement;
+            });
+        }));
+
+        afterEach(() => {
+            fixture.destroy();
+            TestBed.resetTestingModule();
+        });
+
+        beforeEach(() => {
+            uploadWidget.field = new FormFieldModel(new FormModel({ taskId: 'fake-upload-id' }), {
+                id: 'upload-id',
+                name: 'upload-name',
+                value: '',
+                type: FormFieldTypes.UPLOAD,
+                readOnly: false
+            });
+
+            fixture.detectChanges();
+            inputElement = <HTMLInputElement>element.querySelector('#upload-id');
+        });
+
+        it('should be disabled on readonly forms', async(() => {
+            uploadWidget.field.form.readOnly = true;
+
+            fixture.whenStable().then(() => {
+                fixture.detectChanges();
+                expect(inputElement).toBeDefined();
+                expect(inputElement).not.toBeNull();
+                expect(inputElement.disabled).toBeTruthy();
+            });
+        }));
+
     });
 
 });
