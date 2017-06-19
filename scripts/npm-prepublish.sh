71d2b4f49a2bfdb0712c5ca3933cb584226ecd28
Daniel Monteiro <daniel.monteiro@alfresco.com>
2017-06-19 11:41:49 +0100

    removed useless code to the script

--- scripts/npm-prepublish.sh
+++ scripts/npm-prepublish.sh
@@ -2,51 +2,16 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-eval FORCE_PUBLISH=false
-eval EXEC_CHANGE_REGISTRY=false
-eval NPM_REGISTRY=false
-eval TOKEN_REGISTRY=""
 eval OPTIONS=""
 eval EXEC_GIT_NPM_INSTALL_JSAPI=false
 eval GIT_ISH=""
-eval EXEC_SLEEP=false
-eval SLEEP_TIME="0"
 
 cd "$DIR/../demo-shell-ng2"
 
 show_help() {
     echo "Usage: npm-prepublish.sh"
     echo ""
-    echo "-f or -force publish the package with force"
-    echo "-r or -registry to publish in an alternative npm registry -registry 'http://npm.local.me:8080/' "
-    echo "-token auth token for publish in the npm registry"
-    echo "-t or -tag to add a tag when publish a package"
-    echo "--sleep add a sleep before any publish"
     echo "-gitjsapi to build all the components against a commit-ish version of the JS-API"
-}
-
-enable_force(){
-    OPTIONS="$OPTIONS -force"
-}
-
-enable_change_registry(){
-    NPM_REGISTRY=$1
-    EXEC_CHANGE_REGISTRY=true
-}
-
-set_sleep(){
-    SLEEP_TIME=$1
-    EXEC_SLEEP=true
-}
-
-get_token_registry(){
-    TOKEN_REGISTRY=$1
-
-    if [[ "${TOKEN_REGISTRY}" == "" ]]
-    then
-      echo "token missing -token"
-      exit 0
-    fi
 }
 
 enable_js_api_git_link() {
@@ -54,41 +19,9 @@ enable_js_api_git_link() {
     EXEC_GIT_NPM_INSTALL_JSAPI=true
 }
 
-add_tag(){
-    eval TAG=$1
-
-    if [[ "${TAG}" == "" ]]
-    then
-      echo "tag missing -t | -tag"
-      exit 0
-    fi
-
-    echo "====== TAG WILL BE ADDED DURING THE PUBLISH: ${TAG} ====="
-    OPTIONS="$OPTIONS --tag $1"
-}
-
-change_registry(){
-    if [[ "${NPM_REGISTRY}" == "" ]]
-    then
-      echo "NPM registry required WITH OPTION -r | -registry"
-      exit 0
-    fi
-
-    echo "====== CHANGE REGISTRY: ${NPM_REGISTRY} ====="
-    touch .npmrc
-    echo 'strict-ssl=false' >> .npmrc
-    echo 'registry=http://'${NPM_REGISTRY} >> .npmrc
-    echo '//'${NPM_REGISTRY}'/:_authToken="'${TOKEN_REGISTRY}'"' >> .npmrc
-}
-
 while [[ $1 == -* ]]; do
     case "$1" in
       -h|--help|-\?) show_help; exit 0;;
-      -t|--tag)  add_tag $2; shift 2;;
-      -f|--force)  enable_force; shift;;
-      -token) get_token_registry $2; shift 2;;
-      --sleep) set_sleep $2; shift 2;;
-      -r|--registry) enable_change_registry $2; shift 2;;
       -gitjsapi)  enable_js_api_git_link $2; shift 2;;
       -*) echo "invalid option: $1" 1>&2; show_help; exit 0;;
     esac
@@ -120,10 +53,6 @@ do
   npm install rimraf
   npm run clean
 
-  if $EXEC_CHANGE_REGISTRY == true; then
-    change_registry
-  fi
-
   if $EXEC_GIT_NPM_INSTALL_JSAPI == true; then
     echo "====== Use the alfresco JS-API  '$GIT_ISH'====="
     npm install $GIT_ISH
@@ -136,15 +65,6 @@ do
 
   echo "====== PREPUBLISHING: ${DESTDIR} ===== npm prepublish ${OPTIONS}"
   npm run prepublish || exit 1
-
-  if $EXEC_CHANGE_REGISTRY == true; then
-      npm run rimraf .npmrc
-  fi
-
-  if $EXEC_SLEEP == true; then
-      echo "====== SLEEP ${SLEEP_TIME}"
-      sleep ${SLEEP_TIME}
-  fi
 
   cd ${DIR}
 done
