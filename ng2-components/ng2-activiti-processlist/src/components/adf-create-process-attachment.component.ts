bffaf2c407b3f26a63ffa41e328f6b5be60589ca
Maurizio Vitale <maurizio.vitale@alfresco.com>
2017-06-22 11:36:11 +0100

    Create task/process attachment Compilation error (#2004)

--- ng2-components/ng2-activiti-processlist/src/components/adf-create-process-attachment.component.ts
+++ ng2-components/ng2-activiti-processlist/src/components/adf-create-process-attachment.component.ts
@@ -50,10 +50,10 @@ export class ActivitiCreateProcessAttachmentComponent implements OnChanges {
     }
 
     onFileUpload(event: any) {
-        let filesList: File[] = event.detail.files;
+        let filesList: File[] = event.detail.files.map(obj => obj.file);
 
         for (let fileInfoObj of filesList) {
-            let file: File = fileInfoObj.file;
+            let file: File = fileInfoObj;
             this.activitiContentService.createProcessRelatedContent(this.processInstanceId, file).subscribe(
                 (res) => {
                     this.contentCreated.emit(res);
