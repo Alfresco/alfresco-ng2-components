8b655e5c19b7f1087904d00414f5e88dd0831d91
Eugenio Romano <eugenio.romano@alfresco.com>
2017-06-30 01:21:09 +0100

    regenerate lock

--- demo-shell-ng2/config/webpack.common.js
+++ demo-shell-ng2/config/webpack.common.js
@@ -4,7 +4,6 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const helpers = require('./helpers');
 const path = require('path');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
     entry: {
