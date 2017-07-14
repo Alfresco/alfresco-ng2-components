0d402c72cfcb57ba14e76ff8958309de36329702
Eugenio Romano <eugenio.romano@alfresco.com>
2017-07-14 16:49:52 +0100

    add sass configuration in all the component common webpack configuration

--- ng2-components/ng2-alfresco-viewer/config/webpack.common.js
+++ ng2-components/ng2-alfresco-viewer/config/webpack.common.js
@@ -61,9 +61,18 @@ module.exports = {
                 exclude: [/node_modules/, /bundles/, /dist/, /demo/, /assets/]
             },
             {
-                test: /\.component.scss$/,
-                use: ['to-string-loader', 'raw-loader', 'sass-loader'],
-                exclude: [/node_modules/, /bundles/, /dist/, /demo/, /assets/]
+                test: /\.scss$/,
+                use: [{
+                    loader: "to-string-loader"
+                }, {
+                    loader: "raw-loader"
+                }, {
+                    loader: "sass-loader",
+                    options: {
+                        includePaths: [ path.resolve(__dirname, '../../ng2-alfresco-core/styles')]
+                    }
+                }],
+                exclude: [/node_modules/, /bundles/, /dist/, /demo/]
             },
             {
                 enforce: 'pre',
