00fb8d1fad83e87ebb29ab7a83ad9aa95d37278d
Denys Vuika <denis.vuyka@gmail.com>
2017-07-14 16:04:52 +0100

    fix datatable sass config (webpack)

--- ng2-components/ng2-alfresco-datatable/config/webpack.common.js
+++ ng2-components/ng2-alfresco-datatable/config/webpack.common.js
@@ -62,9 +62,18 @@ module.exports = {
                 loader: ['to-string-loader', 'css-loader'],
                 exclude: [/node_modules/, /bundles/, /dist/, /demo/]
             },
-            {
-                test: /\.component.scss$/,
-                use: ['to-string-loader', 'raw-loader', 'sass-loader'],
+             {
+                test: /\.scss$/,
+                use: [
+                    { loader: "to-string-loader" },
+                    { loader: "raw-loader" },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            includePaths: [ path.resolve(__dirname, '../../ng2-alfresco-core/styles')]
+                        }
+                    }
+                ],
                 exclude: [/node_modules/, /bundles/, /dist/, /demo/]
             },
             {
