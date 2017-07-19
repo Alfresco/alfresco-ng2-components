df7af049b8e4148c8e1fee78f87887b50181baea
Eugenio Romano <eromano@users.noreply.github.com>
2017-07-19 20:59:04 +0100

    create external package on npm for adf-tslint-rules
    create external package on npm for adf-tslint-rules

---
+++ adf-rules/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var adfClassNameRule_1 = require("./adfClassNameRule");
+exports.ADFClassNameRule = adfClassNameRule_1.Rule;
+var adfFileNameRule_1 = require("./adfFileNameRule");
+exports.ComponentSelectorRule = adfFileNameRule_1.Rule;
