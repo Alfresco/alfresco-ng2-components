f7272bf467399d04b3d9516211e98578a4627d6b
Eugenio Romano <eugenio.romano@alfresco.com>
2017-07-20 13:23:23 +0100

    remove md-date-time-picker dependency in ng2-alfresco-from

--- ng2-components/ng2-activiti-form/index.ts
+++ ng2-components/ng2-activiti-form/index.ts
@@ -17,7 +17,9 @@
 
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { MdDatepickerModule, MdSelectModule, MdRadioModule, MdAutocompleteModule, MdButtonModule, MdCardModule, MdCheckboxModule, MdIconModule, MdInputModule, MdSlideToggleModule, MdTabsModule } from '@angular/material';
+import { MdAutocompleteModule, MdButtonModule, MdCardModule, MdCheckboxModule,
+    MdDatepickerModule, MdIconModule, MdInputModule, MdRadioModule, MdSelectModule,
+    MdSlideToggleModule, MdTabsModule } from '@angular/material';
 import { CoreModule } from 'ng2-alfresco-core';
 import { DataTableModule } from 'ng2-alfresco-datatable';
 import { ActivitiContentComponent } from './src/components/activiti-content.component';
