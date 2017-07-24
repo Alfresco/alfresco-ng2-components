1ed56460cc1a6be31ca43995eea532aece940de7
Cilibiu Bogdan <pionnegru@users.noreply.github.com>
2017-07-24 18:50:20 +0300

    [ADF-924] Upload Component enhancements (#2115)
    * changed logic/design
    
    * restored dialog component spec
    
    * revert changes
    
    * update upload dialog documentation
    
    * public over private
    
    * component close method

--- ng2-components/ng2-alfresco-upload/index.ts
+++ ng2-components/ng2-alfresco-upload/index.ts
@@ -20,28 +20,33 @@ import { MdButtonModule, MdIconModule, MdProgressSpinnerModule } from '@angular/
 import { CoreModule } from 'ng2-alfresco-core';
 
 import { FileUploadingDialogComponent } from './src/components/file-uploading-dialog.component';
+import { FileUploadingListRowComponent } from './src/components/file-uploading-list-row.component';
 import { FileUploadingListComponent } from './src/components/file-uploading-list.component';
 import { UploadButtonComponent } from './src/components/upload-button.component';
 import { UploadDragAreaComponent } from './src/components/upload-drag-area.component';
 import { FileDraggableDirective } from './src/directives/file-draggable.directive';
+import { FileUploadService } from './src/services/file-uploading.service';
 
 export * from './src/components/upload-button.component';
 export * from './src/components/file-uploading-dialog.component';
 export * from './src/components/upload-drag-area.component';
 export * from './src/directives/file-draggable.directive';
 export * from './src/components/file-uploading-list.component';
+export * from './src/components/file-uploading-list-row.component';
 export * from './src/models/permissions.model';
+export * from './src/services/file-uploading.service';
 
 export const UPLOAD_DIRECTIVES: any[] = [
     FileDraggableDirective,
     UploadDragAreaComponent,
     UploadButtonComponent,
     FileUploadingDialogComponent,
-    FileUploadingListComponent
+    FileUploadingListComponent,
+    FileUploadingListRowComponent
 ];
 
 export const UPLOAD_PROVIDERS: any[] = [
-
+    FileUploadService
 ];
 
 @NgModule({
