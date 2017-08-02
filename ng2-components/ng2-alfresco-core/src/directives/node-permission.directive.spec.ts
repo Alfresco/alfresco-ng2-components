4f44fa85edbba9edcfd7a40dfb48626279a5b00d
Denys Vuika <denis.vuyka@gmail.com>
2017-08-02 17:34:48 +0100

    [ASF-1227] new adf-node-permission directive (#2154)
    * directive implementation
    
    * update docs
    
    * unit tests and api docs
    
    * code improvements
    
    * simplify code
    
    * update type def and docs

--- ng2-components/ng2-alfresco-core/src/directives/node-permission.directive.spec.ts
+++ ng2-components/ng2-alfresco-core/src/directives/node-permission.directive.spec.ts
@@ -15,138 +15,98 @@
  * limitations under the License.
  */
 
-import { Component, ElementRef, SimpleChange } from '@angular/core';
+import { ElementRef, SimpleChange } from '@angular/core';
 import { AlfrescoContentService } from './../services/alfresco-content.service';
-import { NodePermissionDirective, NodePermissionSubject } from './node-permission.directive';
-
-@Component({
-    selector: 'adf-text-subject'
-})
-class TestComponent implements NodePermissionSubject {
-    disabled: boolean = false;
-}
+import { NodePermissionDirective } from './node-permission.directive';
 
 describe('NodePermissionDirective', () => {
 
-    describe('HTML nativeElement as subject', () => {
+    it('updates element once it is loaded', () => {
+        const directive = new NodePermissionDirective(null, null, null);
+        spyOn(directive, 'updateElement').and.stub();
 
-        it('updates element once it is loaded', () => {
-            const directive = new NodePermissionDirective(null, null, null);
-            spyOn(directive, 'updateElement').and.stub();
+        directive.ngAfterViewInit();
 
-            directive.ngAfterViewInit();
-
-            expect(directive.updateElement).toHaveBeenCalled();
-        });
-
-        it('updates element on nodes change', () => {
-            const directive = new NodePermissionDirective(null, null, null);
-            spyOn(directive, 'updateElement').and.stub();
-
-            const nodes = [{}, {}];
-            const change = new SimpleChange([], nodes, false);
-            directive.ngOnChanges({ nodes: change });
-
-            expect(directive.updateElement).toHaveBeenCalled();
-        });
-
-        it('updates element only on subsequent change', () => {
-            const directive = new NodePermissionDirective(null, null, null);
-            spyOn(directive, 'updateElement').and.stub();
-
-            const nodes = [{}, {}];
-            const change = new SimpleChange([], nodes, true);
-            directive.ngOnChanges({ nodes: change });
-
-            expect(directive.updateElement).not.toHaveBeenCalled();
-        });
-
-        it('enables decorated element', () => {
-            const renderer = jasmine.createSpyObj('renderer', ['removeAttribute']);
-            const elementRef = new ElementRef({});
-            const directive = new NodePermissionDirective(elementRef, renderer, null);
-
-            directive.enableElement();
-
-            expect(renderer.removeAttribute).toHaveBeenCalledWith(elementRef.nativeElement, 'disabled');
-        });
-
-        it('disables decorated element', () => {
-            const renderer = jasmine.createSpyObj('renderer', ['setAttribute']);
-            const elementRef = new ElementRef({});
-            const directive = new NodePermissionDirective(elementRef, renderer, null);
-
-            directive.disableElement();
-
-            expect(renderer.setAttribute).toHaveBeenCalledWith(elementRef.nativeElement, 'disabled', 'true');
-        });
-
-        it('disables element when nodes not available', () => {
-            const directive = new NodePermissionDirective(null, null, null);
-            spyOn(directive, 'disableElement').and.stub();
-
-            directive.nodes = null;
-            expect(directive.updateElement()).toBeFalsy();
-
-            directive.nodes = [];
-            expect(directive.updateElement()).toBeFalsy();
-        });
-
-        it('enables element when all nodes have expected permission', () => {
-            const contentService = new AlfrescoContentService(null, null, null);
-            spyOn(contentService, 'hasPermission').and.returnValue(true);
-
-            const directive = new NodePermissionDirective(null, null, contentService);
-            spyOn(directive, 'enableElement').and.stub();
-
-            directive.nodes = <any> [{}, {}];
-
-            expect(directive.updateElement()).toBeTruthy();
-            expect(directive.enableElement).toHaveBeenCalled();
-        });
-
-        it('disables element when one of the nodes have no permission', () => {
-            const contentService = new AlfrescoContentService(null, null, null);
-            spyOn(contentService, 'hasPermission').and.returnValue(false);
-
-            const directive = new NodePermissionDirective(null, null, contentService);
-            spyOn(directive, 'disableElement').and.stub();
-
-            directive.nodes = <any> [{}, {}];
-
-            expect(directive.updateElement()).toBeFalsy();
-            expect(directive.disableElement).toHaveBeenCalled();
-        });
+        expect(directive.updateElement).toHaveBeenCalled();
     });
 
-    describe('Angular component as subject', () => {
+    it('updates element on nodes change', () => {
+        const directive = new NodePermissionDirective(null, null, null);
+        spyOn(directive, 'updateElement').and.stub();
 
-        it('disables decorated component', () => {
-            const contentService = new AlfrescoContentService(null, null, null);
-            spyOn(contentService, 'hasPermission').and.returnValue(false);
+        const nodes = [{}, {}];
+        const change = new SimpleChange([], nodes, false);
+        directive.ngOnChanges({ nodes: change });
 
-            let testComponent = new TestComponent();
-            testComponent.disabled = false;
-            const directive = new NodePermissionDirective(null, null, contentService, testComponent);
-            directive.nodes = <any> [{}, {}];
-
-            directive.updateElement();
-
-            expect(testComponent.disabled).toBeTruthy();
-        });
-
-        it('enables decorated component', () => {
-            const contentService = new AlfrescoContentService(null, null, null);
-            spyOn(contentService, 'hasPermission').and.returnValue(true);
-
-            let testComponent = new TestComponent();
-            testComponent.disabled = true;
-            const directive = new NodePermissionDirective(null, null, contentService, testComponent);
-            directive.nodes = <any> [{}, {}];
-
-            directive.updateElement();
-
-            expect(testComponent.disabled).toBeFalsy();
-        });
+        expect(directive.updateElement).toHaveBeenCalled();
     });
+
+    it('updates element only on subsequent change', () => {
+        const directive = new NodePermissionDirective(null, null, null);
+        spyOn(directive, 'updateElement').and.stub();
+
+        const nodes = [{}, {}];
+        const change = new SimpleChange([], nodes, true);
+        directive.ngOnChanges({ nodes: change });
+
+        expect(directive.updateElement).not.toHaveBeenCalled();
+    });
+
+    it('enables decorated element', () => {
+        const renderer = jasmine.createSpyObj('renderer', ['removeAttribute']);
+        const elementRef = new ElementRef({});
+        const directive = new NodePermissionDirective(elementRef, renderer, null);
+
+        directive.enableElement();
+
+        expect(renderer.removeAttribute).toHaveBeenCalledWith(elementRef.nativeElement, 'disabled');
+    });
+
+    it('disables decorated element', () => {
+        const renderer = jasmine.createSpyObj('renderer', ['setAttribute']);
+        const elementRef = new ElementRef({});
+        const directive = new NodePermissionDirective(elementRef, renderer, null);
+
+        directive.disableElement();
+
+        expect(renderer.setAttribute).toHaveBeenCalledWith(elementRef.nativeElement, 'disabled', 'true');
+    });
+
+    it('disables element when nodes not available', () => {
+        const directive = new NodePermissionDirective(null, null, null);
+        spyOn(directive, 'disableElement').and.stub();
+
+        directive.nodes = null;
+        expect(directive.updateElement()).toBeFalsy();
+
+        directive.nodes = [];
+        expect(directive.updateElement()).toBeFalsy();
+    });
+
+    it('enables element when all nodes have expected permission', () => {
+        const contentService = new AlfrescoContentService(null, null, null);
+        spyOn(contentService, 'hasPermission').and.returnValue(true);
+
+        const directive = new NodePermissionDirective(null, null, contentService);
+        spyOn(directive, 'enableElement').and.stub();
+
+        directive.nodes = <any> [{}, {}];
+
+        expect(directive.updateElement()).toBeTruthy();
+        expect(directive.enableElement).toHaveBeenCalled();
+    });
+
+    it('disables element when one of the nodes have no permission', () => {
+        const contentService = new AlfrescoContentService(null, null, null);
+        spyOn(contentService, 'hasPermission').and.returnValue(false);
+
+        const directive = new NodePermissionDirective(null, null, contentService);
+        spyOn(directive, 'disableElement').and.stub();
+
+        directive.nodes = <any> [{}, {}];
+
+        expect(directive.updateElement()).toBeFalsy();
+        expect(directive.disableElement).toHaveBeenCalled();
+    });
+
 });
