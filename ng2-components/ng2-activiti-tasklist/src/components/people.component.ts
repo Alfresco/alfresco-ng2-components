fb0b034a6c035fc19d9270b5979c313377ea50f1
VitoAlbano <vito.albano.123@gmail.com>
2017-08-04 10:14:29 +0100

    [ADF-1083] start added image for user search

--- ng2-components/ng2-activiti-tasklist/src/components/people.component.ts
+++ ng2-components/ng2-activiti-tasklist/src/components/people.component.ts
@@ -91,10 +91,16 @@ export class PeopleComponent implements AfterViewInit {
     }
 
     searchUser(searchedWord: string) {
+        let userList = [];
         this.peopleService.getWorkflowUsers(this.taskId, searchedWord)
-            .subscribe((users) => {
-                this.peopleSearchObserver.next(users);
-            },         error => this.logService.error('Could not load users'));
+            .subscribe(
+            (user) => {
+                userList.push(user);
+            },
+            (error) => this.logService.error('Could not load users'),
+            () => {
+                this.peopleSearchObserver.next(userList);
+            });
     }
 
     involveUser(user: User) {
@@ -146,5 +152,4 @@ export class PeopleComponent implements AfterViewInit {
     onCloseSearch() {
         this.showAssignment = false;
     }
-
 }
--- ng2-components/ng2-activiti-tasklist/src/services/people.service.ts
+++ ng2-components/ng2-activiti-tasklist/src/services/people.service.ts
@@ -17,22 +17,34 @@
 
 import { Injectable } from '@angular/core';
 import { Response } from '@angular/http';
-import { AlfrescoApiService, LogService } from 'ng2-alfresco-core';
+import { AlfrescoApiService, LogService, ContentService } from 'ng2-alfresco-core';
 import { Observable } from 'rxjs/Rx';
 import { User } from '../models/user.model';
 
 @Injectable()
 export class PeopleService {
-
+o
     constructor(private alfrescoJsApi: AlfrescoApiService,
-                private logService: LogService) {
+                private logService: LogService,
+                private contentService: ContentService) {
     }
 
     getWorkflowUsers(taskId?: string, searchWord?: string): Observable<User[]> {
         let option = {excludeTaskId: taskId, filter: searchWord};
         return Observable.fromPromise(this.getWorkflowUserApi(option))
-            .map((response: any) => <User[]> response.data || [])
-            .catch(err => this.handleError(err));
+            .flatMap((response: any) =>  <User[]> response.data )
+            .map( (user: User) => {
+                this.getUserImage(user).subscribe((img)=> {
+                    user.userImage = this.contentService.createTrustedUrl(img);
+                });
+                return user;
+            })
+            .catch(err => this.handleErrorImproved(err));
+    }
+
+    getUserImage(user: User): Observable<Blob> {
+        return Observable.fromPromise(this.getUserProfileImageApi(user.id+''))
+            .catch(err => this.handleErrorImproved(err));
     }
 
     involveUserWithTask(taskId: string, idToInvolve: string): Observable<User[]> {
@@ -57,6 +69,15 @@ export class PeopleService {
 
     private removeInvolvedUserFromTaskApi(taskId: string, node: any) {
         return this.alfrescoJsApi.getInstance().activiti.taskActionsApi.removeInvolvedUser(taskId, node);
+    }
+
+    private getUserProfileImageApi(userId: string) {
+        return this.alfrescoJsApi.getInstance().activiti.userApi.getProfilePicture(userId);
+    }
+
+    private handleErrorImproved(error: Response) {
+        this.logService.log(error);
+        return error.status === 404? null : Observable.throw(error || 'Server error');
     }
 
     /**
