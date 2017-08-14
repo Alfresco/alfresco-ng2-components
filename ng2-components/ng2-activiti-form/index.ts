567a5b7d84a3b4db07cd7aca971997c439fea48a
Eugenio Romano <eugenio.romano@alfresco.com>
2017-08-14 14:48:25 +0100

    fix placeholder overlapping with label in form

--- ng2-components/ng2-activiti-form/index.ts
+++ ng2-components/ng2-activiti-form/index.ts
@@ -17,9 +17,7 @@
 
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { MdAutocompleteModule, MdButtonModule, MdCardModule, MdCheckboxModule,
-    MdDatepickerModule, MdGridListModule, MdIconModule, MdInputModule, MdListModule,
-    MdOptionModule, MdRadioModule, MdSelectModule, MdSlideToggleModule, MdTableModule, MdTabsModule } from '@angular/material';
+import { MaterialModule } from './src/components/material.module';
 import { CoreModule, TRANSLATION_PROVIDER } from 'ng2-alfresco-core';
 import { DataTableModule } from 'ng2-alfresco-datatable';
 import { FormFieldComponent } from './src/components/form-field/form-field.component';
@@ -82,18 +80,12 @@ export const ACTIVITI_FORM_PROVIDERS: any[] = [
     FormRenderingService
 ];
 
-export const MATERIAL_MODULE: any[] = [
-    MdAutocompleteModule, MdButtonModule, MdCardModule, MdCheckboxModule,
-    MdDatepickerModule, MdGridListModule, MdIconModule, MdInputModule, MdRadioModule,
-    MdSelectModule, MdSlideToggleModule, MdTableModule, MdTabsModule, MdOptionModule, MdListModule
-];
-
 @NgModule({
     imports: [
         CoreModule,
         DataTableModule,
         HttpModule,
-        ...MATERIAL_MODULE
+        MaterialModule
     ],
     declarations: [
         ...ACTIVITI_FORM_DIRECTIVES,
@@ -117,7 +109,7 @@ export const MATERIAL_MODULE: any[] = [
     exports: [
         ...ACTIVITI_FORM_DIRECTIVES,
         ...DEPRECATED_FORM_DIRECTIVES,
-        ...MATERIAL_MODULE
+        MaterialModule
     ]
 })
 export class ActivitiFormModule {
