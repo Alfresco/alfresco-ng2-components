fdf3b9a12facfa851e7f462a19b1ca291cf9a669
Andras Popovics <popovics@ndras.hu>
2017-08-16 18:46:03 +0200

    Usage of FormFieldTypes.isReadOnlyType

--- ng2-components/ng2-activiti-form/src/components/widgets/core/form-field.model.ts
+++ ng2-components/ng2-activiti-form/src/components/widgets/core/form-field.model.ts
@@ -166,7 +166,7 @@ export class FormFieldModel extends FormWidgetModel {
                 this.placeholder = json.placeholder;
             }
 
-            if (json.type === 'readonly') {
+            if (FormFieldTypes.isReadOnlyType(json.type)) {
                 if (json.params && json.params.field && json.params.field.responseVariable) {
                     this.value = this.getVariablesValue(json.params.field.name, form);
                 }
