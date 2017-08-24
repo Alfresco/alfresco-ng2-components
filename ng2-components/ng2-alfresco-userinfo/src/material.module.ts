98cb6de5955cb26bec2af09b618e8294272e7c1b
Vito <vito.albano@alfresco.com>
2017-08-24 02:53:59 -0700

    [ADF-1237] removed material design lite from userinfo component (#2238)
    * [ADF-1083] removing material design lite from userinfo component
    
    * [ADF-1237] - styled userinfo component]
    
    * [ADF-1237] removed material design lite from userinfo component
    
    * [ADF-1237] updated readme with the two new options

---
+++ ng2-components/ng2-alfresco-userinfo/src/material.module.ts
@@ -0,0 +1,39 @@
+/*!
+ * @license
+ * Copyright 2016 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { NgModule } from '@angular/core';
+import {
+    MdButtonModule,
+    MdCardModule,
+    MdMenuModule,
+    MdTabsModule
+} from '@angular/material';
+
+export function modules() {
+    return [
+        MdButtonModule,
+        MdMenuModule,
+        MdTabsModule,
+        MdCardModule
+    ];
+}
+
+@NgModule({
+    imports: modules(),
+    exports: modules()
+})
+export class MaterialModule {}
