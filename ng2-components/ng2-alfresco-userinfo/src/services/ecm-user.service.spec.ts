55126c09ae5833b95ecca6c2f79eb5684f8b4e2d
Vito <vito.albano@alfresco.com>
2017-08-24 02:53:59 -0700

    [ADF-1237] removed material design lite from userinfo component (#2238)
    * [ADF-1083] removing material design lite from userinfo component
    
    * [ADF-1237] - styled userinfo component]
    
    * [ADF-1237] removed material design lite from userinfo component
    
    * [ADF-1237] updated readme with the two new options

--- ng2-components/ng2-alfresco-userinfo/src/services/ecm-user.service.spec.ts
+++ ng2-components/ng2-alfresco-userinfo/src/services/ecm-user.service.spec.ts
@@ -16,7 +16,7 @@
  */
 
 import { async, TestBed } from '@angular/core/testing';
-import { AlfrescoAuthenticationService, AlfrescoContentService, CoreModule} from 'ng2-alfresco-core';
+import { AlfrescoAuthenticationService, AlfrescoContentService, CoreModule } from 'ng2-alfresco-core';
 import { fakeEcmUser } from '../assets/fake-ecm-user.service.mock';
 import { EcmUserService } from '../services/ecm-user.service';
 
