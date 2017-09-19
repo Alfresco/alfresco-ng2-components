7a31a16178c9333b5014e3019012b5487a946db1
Maurizio Vitale <maurizio.vitale@alfresco.com>
2017-09-19 13:36:05 +0100

    Disable the attachment component when the task/process is completed (#2351)

--- demo-shell-ng2/app/components/activiti/activiti-task-attachments.component.ts
+++ demo-shell-ng2/app/components/activiti/activiti-task-attachments.component.ts
@@ -15,8 +15,9 @@
  * limitations under the License.
  */
 
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, ViewChild } from '@angular/core';
 import { ProcessUploadService, TaskAttachmentListComponent } from 'ng2-activiti-tasklist';
+import { ActivitiTaskListService } from 'ng2-activiti-tasklist';
 import { UploadService } from 'ng2-alfresco-core';
 
 @Component({
@@ -28,7 +29,7 @@ import { UploadService } from 'ng2-alfresco-core';
     ]
 })
 
-export class ActivitiTaskAttachmentsComponent implements OnInit {
+export class ActivitiTaskAttachmentsComponent implements OnInit, OnChanges {
 
     @Input()
     taskId: string;
@@ -40,12 +41,24 @@ export class ActivitiTaskAttachmentsComponent implements OnInit {
     content: Blob;
     contentName: string;
 
-    constructor(private uploadService: UploadService) {
+    taskDetails: any;
+
+    constructor(private uploadService: UploadService,
+                private activitiTaskList: ActivitiTaskListService) {
 
     }
 
     ngOnInit() {
         this.uploadService.fileUploadComplete.subscribe(value => this.onFileUploadComplete(value.data));
+    }
+
+    ngOnChanges() {
+        if (this.taskId) {
+            this.activitiTaskList.getTaskDetails(this.taskId).map((res) => res).subscribe(
+                (res: any) => {
+                    this.taskDetails = res;
+                });
+        }
     }
 
     onFileUploadComplete(content: any) {
@@ -58,4 +71,7 @@ export class ActivitiTaskAttachmentsComponent implements OnInit {
         this.contentName = content.name;
     }
 
+    isCompletedTask(): boolean {
+        return this.taskDetails && this.taskDetails.endDate !== undefined && this.taskDetails.endDate !== null;
+    }
 }
