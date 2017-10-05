00311e9c3e420964d3e1fb04b4e30734ca27e4eb
Denys Vuika <denis.vuyka@gmail.com>
2017-10-05 15:50:44 +0100

    unit tests

--- ng2-components/ng2-alfresco-core/src/utils/object-utils.spec.ts
+++ ng2-components/ng2-alfresco-core/src/utils/object-utils.spec.ts
@@ -51,4 +51,83 @@ describe('ObjectUtils', () => {
         expect(ObjectUtils.getValue(null, 'id')).toBeUndefined();
     });
 
+    describe('merge', () => {
+        it('should merge top level properties', () => {
+            const obj1 = {
+                prop1: 'value1'
+            };
+
+            const obj2 = {
+                prop2: 'value2'
+            };
+
+            const result = ObjectUtils.merge(obj1, obj2);
+            expect(result).toEqual({
+                prop1: 'value1',
+                prop2: 'value2'
+            });
+        });
+
+        it('should merge object properties', () => {
+            const obj1 = {
+                child: {
+                    prop1: 1
+                },
+                prop: 1
+            };
+            const obj2 = {
+                child: {
+                    prop1: 2,
+                    prop2: 3
+                }
+            };
+
+            const result = ObjectUtils.merge(obj1, obj2);
+            expect(result).toEqual({
+                child: {
+                    prop1: 2,
+                    prop2: 3
+                },
+                prop: 1
+            });
+        });
+
+        it('should merge arrays', () => {
+            const obj1 = {
+                arr: [1, 2, 3]
+            };
+            const obj2 = {
+                arr: [4, 5, 6]
+            };
+
+            const result = ObjectUtils.merge(obj1, obj2);
+            expect(result).toEqual({
+                arr: [1, 2, 3, 4, 5, 6]
+            });
+        });
+
+        it('shoud overwrite only single property in the object', () => {
+            const obj1 = {
+                child: {
+                    prop1: 1,
+                    prop2: 2,
+                    prop3: 3
+                }
+            };
+            const obj2 = {
+                child: {
+                    prop3: 0
+                }
+            };
+
+            const result = ObjectUtils.merge(obj1, obj2);
+            expect(result).toEqual({
+                child: {
+                    prop1: 1,
+                    prop2: 2,
+                    prop3: 0
+                }
+            });
+        });
+    });
 });
