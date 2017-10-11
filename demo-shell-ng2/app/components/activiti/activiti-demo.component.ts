45db903d49eacc0443b182dd1115ebd815f96d80
Vito <vito.albano@alfresco.com>
2017-10-11 10:57:09 +0100

    [ADF-1663] refreshing process list when a task is completed (#2452)
    * [ADF-1663] refreshing process list when a task is completed
    
    * [ADF-1663] added check for process list

--- demo-shell-ng2/app/components/activiti/activiti-demo.component.ts
+++ demo-shell-ng2/app/components/activiti/activiti-demo.component.ts
@@ -366,7 +366,12 @@ export class ActivitiDemoComponent implements AfterViewInit, OnDestroy, OnInit {
             this.taskPagination.skipCount -= maxItems;
         }
         this.taskPage = this.currentPage(this.taskPagination.skipCount, maxItems);
-        this.taskList.reload();
+        if (this.taskList) {
+            this.taskList.reload();
+        }
+        if (this.processList) {
+            this.processList.reload();
+        }
     }
 
     onFormContentClick(content: any): void {
