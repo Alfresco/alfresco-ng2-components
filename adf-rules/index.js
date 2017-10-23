157a75642a46a4e3ee0f674ff08d68fe38aa8236
Eugenio Romano <eugenio.romano@alfresco.com>
2017-10-23 14:11:08 +0100

    move adf-tslin-rules in a separate repo https://github.com/Alfresco/adf-tslint-rules

--- adf-rules/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var adfClassNameRule_1 = require("./adfClassNameRule");
-exports.ADFClassNameRule = adfClassNameRule_1.Rule;
-var adfFileNameRule_1 = require("./adfFileNameRule");
-exports.ComponentSelectorRule = adfFileNameRule_1.Rule;
