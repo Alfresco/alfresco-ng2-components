e7e2007c934903fb2af6ee9ca426688b3eebab5c
Denys Vuika <denis.vuyka@gmail.com>
2017-10-26 14:18:00 +0100

    extend proxy config with native auth dialog fix (#2553)

--- demo-shell-ng2/proxy.conf.js
+++ demo-shell-ng2/proxy.conf.js
@@ -1,11 +1,18 @@
-{
+module.exports = {
   "/alfresco": {
     "target": "http://localhost:8080",
     "secure": false,
     "pathRewrite": {
       "^/alfresco/alfresco": ""
     },
-    "changeOrigin": true
+    "changeOrigin": true,
+    // workaround for REPO-2260
+    onProxyRes: function (proxyRes, req, res) {
+      const header = proxyRes.headers['www-authenticate'];
+      if (header && header.startsWith('Basic')) {
+          proxyRes.headers['www-authenticate'] = 'x' + header;
+      }
+    }
   },
   "/activiti-app": {
     "target": "http://localhost:9999",
@@ -15,4 +22,4 @@
     },
     "changeOrigin": true
   }
-}
+};
