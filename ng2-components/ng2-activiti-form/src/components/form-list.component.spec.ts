4b1cf6bb5d5fd5c01f2b8a3e3fb018620a111d1f
Eugenio Romano <eugenio.romano@alfresco.com>
2017-10-31 10:55:47 +0000

    fix missing datatable changes

--- ng2-components/ng2-activiti-form/src/components/form-list.component.spec.ts
+++ ng2-components/ng2-activiti-form/src/components/form-list.component.spec.ts
@@ -74,7 +74,7 @@ describe('TaskAttachmentList', () => {
         fixture.whenStable()
             .then(() => {
                 fixture.detectChanges();
-                expect(fixture.debugElement.queryAll(By.css('alfresco-datatable tbody tr')).length).toBe(2);
+                expect(fixture.debugElement.queryAll(By.css('adf-datatable tbody tr')).length).toBe(2);
             });
     }));
 
