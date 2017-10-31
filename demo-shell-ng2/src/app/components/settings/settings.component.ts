e16039264c73444d0c137d3e684ea8f23b9d5738
Eugenio Romano <eromano@users.noreply.github.com>
2017-10-31 17:16:14 +0000

    setting component (#2571)

--- demo-shell-ng2/src/app/components/settings/settings.component.ts
+++ demo-shell-ng2/src/app/components/settings/settings.component.ts
@@ -15,69 +15,19 @@
  * limitations under the License.
  */
 
-import { Component, ViewEncapsulation } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
-import { AlfrescoSettingsService, LogService, StorageService } from 'ng2-alfresco-core';
+import { Component } from '@angular/core';
+import { LogService } from 'ng2-alfresco-core';
 
 @Component({
     selector: 'app-settings',
-    templateUrl: 'settings.component.html',
-    host: {
-        'class': 'adf-app-settings'
-    },
-    styleUrls: ['settings.component.scss'],
-    encapsulation: ViewEncapsulation.None
+    templateUrl: 'settings.component.html'
 })
 export class SettingsComponent {
 
-    HOST_REGEX: string = '^(http|https):\/\/.*[^/]$';
+  constructor(public logService: LogService){
+  }
 
-    ecmHost: string;
-    ecmHostTmp: string;
-    bpmHost: string;
-    bpmHostTmp: string;
-    urlFormControlEcm = new FormControl('', [Validators.required, Validators.pattern(this.HOST_REGEX)]);
-    urlFormControlBpm = new FormControl('', [Validators.required, Validators.pattern(this.HOST_REGEX)]);
-
-    constructor(private settingsService: AlfrescoSettingsService,
-                private storage: StorageService,
-                private logService: LogService) {
-        this.ecmHostTmp = this.ecmHost = storage.getItem('ecmHost') || this.settingsService.ecmHost;
-        this.bpmHostTmp = this.bpmHost = storage.getItem('bpmHost') || this.settingsService.bpmHost;
-    }
-
-    public onChangeECMHost(event: KeyboardEvent): void {
-        let value = (<HTMLInputElement> event.target).value.trim();
-        if (value && this.isValidUrl(value)) {
-            this.logService.info(`ECM host: ${value}`);
-            this.ecmHostTmp = value;
-        } else {
-            console.error('Ecm address does not match the pattern');
-        }
-    }
-
-    public onChangeBPMHost(event: KeyboardEvent): void {
-        let value = (<HTMLInputElement> event.target).value.trim();
-        if (value && this.isValidUrl(value)) {
-            this.logService.info(`BPM host: ${value}`);
-            this.bpmHostTmp = value;
-        } else {
-            console.error('Bpm address does not match the pattern');
-        }
-    }
-
-    public save(event: KeyboardEvent): void {
-        if (this.bpmHost !== this.bpmHostTmp) {
-            this.storage.setItem(`bpmHost`, this.bpmHostTmp);
-        }
-        if (this.ecmHost !== this.ecmHostTmp) {
-            this.storage.setItem(`ecmHost`, this.ecmHostTmp);
-        }
-        window.location.href = '/';
-    }
-
-    isValidUrl(url: string) {
-        return /^(http|https):\/\/.*/.test(url);
-    }
-
+  onError(error: string){
+    this.logService.log(error)
+  }
 }
