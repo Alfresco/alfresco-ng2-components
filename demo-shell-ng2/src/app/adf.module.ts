737f8cc767a7bb3f693865146d9ed8acb77171af
Eugenio Romano <eugenio.romano@alfresco.com>
2017-11-08 19:03:06 +0000

    content service tag social and webscript demoshell

--- demo-shell-ng2/src/app/adf.module.ts
+++ demo-shell-ng2/src/app/adf.module.ts
@@ -9,14 +9,13 @@ import { LoginModule } from 'ng2-alfresco-login';
 import { UploadModule } from 'ng2-alfresco-upload';
 import { SearchModule } from 'ng2-alfresco-search';
 import { UserInfoModule } from 'ng2-alfresco-userinfo';
-import { SocialModule } from 'ng2-alfresco-social';
-import { TagModule } from 'ng2-alfresco-tag';
-import { WebScriptModule } from 'ng2-alfresco-webscript';
 import { ActivitiFormModule } from 'ng2-activiti-form';
 import { ActivitiTaskListModule } from 'ng2-activiti-tasklist';
 import { ActivitiProcessListModule } from 'ng2-activiti-processlist';
 import { DiagramsModule } from 'ng2-activiti-diagrams';
 import { AnalyticsModule } from 'ng2-activiti-analytics';
+
+import { SocialModule, WebScriptModule, TagModule } from '@adf/content-service';
 
 export function modules() {
   return [
