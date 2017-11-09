b53a5374ae12ee3b8aef9f0fc9769534ecc73de0
Eugenio Romano <eugenio.romano@alfresco.com>
2017-11-09 20:20:06 +0000

    move diagrams and analytics

---
+++ ng2-components/process-services/diagram/assets/diagramEvents.mock.ts
@@ -0,0 +1,269 @@
+/*!
+ * @license
+ * Copyright 2016 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export let startEvent = {
+    id: 'startEvent1',
+    type: 'StartEvent',
+    width: 30,
+    height: 30,
+    x: 15,
+    y: 48.5,
+    properties: [{}]
+};
+
+export let startEventActive = {
+    completed: false,
+    current: true,
+    id: 'startEvent1',
+    type: 'StartEvent',
+    width: 30,
+    height: 30,
+    x: 15,
+    y: 48.5,
+    properties: [{}]
+};
+
+export let startEventCompleted = {
+    completed: true,
+    current: false,
+    id: 'startEvent1',
+    type: 'StartEvent',
+    width: 30,
+    height: 30,
+    x: 15,
+    y: 48.5,
+    properties: [{}]
+};
+
+export let startTimeEvent = {
+    id: 'startEvent1',
+    type: 'StartEvent',
+    width: 30,
+    height: 30,
+    x: 15,
+    y: 48.5,
+    eventDefinition: {type: 'timer'},
+    properties: [{}]
+};
+
+export let startTimeEventActive = {
+    completed: false,
+    current: true,
+    id: 'startEvent1',
+    type: 'StartEvent',
+    width: 30,
+    height: 30,
+    x: 15,
+    y: 48.5,
+    eventDefinition: {type: 'timer'},
+    properties: [{}]
+};
+
+export let startTimeEventCompleted = {
+    completed: true,
+    current: false,
+    id: 'startEvent1',
+    type: 'StartEvent',
+    width: 30,
+    height: 30,
+    x: 15,
+    y: 48.5,
+    eventDefinition: {type: 'timer'},
+    properties: [{}]
+};
+
+export let startSignalEvent = {
+    id: 'startEvent1',
+    type: 'StartEvent',
+    width: 30,
+    height: 30,
+    x: 15,
+    y: 48.5,
+    eventDefinition: {type: 'signal'},
+    properties: [{}]
+};
+
+export let startSignalEventActive = {
+    completed: false,
+    current: true,
+    id: 'startEvent1',
+    type: 'StartEvent',
+    width: 30,
+    height: 30,
+    x: 15,
+    y: 48.5,
+    eventDefinition: {type: 'signal'},
+    properties: [{}]
+};
+
+export let startSignalEventCompleted = {
+    completed: true,
+    current: false,
+    id: 'startEvent1',
+    type: 'StartEvent',
+    width: 30,
+    height: 30,
+    x: 15,
+    y: 48.5,
+    eventDefinition: {type: 'signal'},
+    properties: [{}]
+};
+
+export let startMessageEvent = {
+    id: 'startEvent1',
+    type: 'StartEvent',
+    width: 30,
+    height: 30,
+    x: 15,
+    y: 48.5,
+    eventDefinition: {type: 'message'},
+    properties: [{}]
+};
+
+export let startMessageEventActive = {
+    completed: false,
+    current: true,
+    id: 'startEvent1',
+    type: 'StartEvent',
+    width: 30,
+    height: 30,
+    x: 15,
+    y: 48.5,
+    eventDefinition: {type: 'message'},
+    properties: [{}]
+};
+
+export let startMessageEventCompleted = {
+    completed: true,
+    current: false,
+    id: 'startEvent1',
+    type: 'StartEvent',
+    width: 30,
+    height: 30,
+    x: 15,
+    y: 48.5,
+    eventDefinition: {type: 'message'},
+    properties: [{}]
+};
+
+export let startErrorEvent = {
+    id: 'startEvent1',
+    type: 'StartEvent',
+    width: 30,
+    height: 30,
+    x: 15,
+    y: 48.5,
+    eventDefinition: {type: 'error'},
+    properties: [{}]
+};
+
+export let startErrorEventActive = {
+    completed: false,
+    current: true,
+    id: 'startEvent1',
+    type: 'StartEvent',
+    width: 30,
+    height: 30,
+    x: 15,
+    y: 48.5,
+    eventDefinition: {type: 'error'},
+    properties: [{}]
+};
+
+export let startErrorEventCompleted = {
+    completed: true,
+    current: false,
+    id: 'startEvent1',
+    type: 'StartEvent',
+    width: 30,
+    height: 30,
+    x: 15,
+    y: 48.5,
+    eventDefinition: {type: 'error'},
+    properties: [{}]
+};
+
+export let endEvent = {
+    id: 'sid-CED2A8DB-47E2-4057-A7B8-3ABBE5CE795E',
+    type: 'EndEvent',
+    width: 28,
+    height: 28,
+    x: 15,
+    y: 48.5,
+    properties: [{}]
+};
+
+export let endEventActive = {
+    completed: false,
+    current: true,
+    id: 'sid-CED2A8DB-47E2-4057-A7B8-3ABBE5CE795E',
+    type: 'EndEvent',
+    width: 28,
+    height: 28,
+    x: 15,
+    y: 48.5,
+    properties: [{}]
+};
+
+export let endEventCompleted = {
+    completed: true,
+    current: false,
+    id: 'sid-CED2A8DB-47E2-4057-A7B8-3ABBE5CE795E',
+    type: 'EndEvent',
+    width: 28,
+    height: 28,
+    x: 15,
+    y: 48.5,
+    properties: [{}]
+};
+
+export let endErrorEvent = {
+    id: 'sid-CED2A8DB-47E2-4057-A7B8-3ABBE5CE795E',
+    type: 'EndEvent',
+    width: 28,
+    height: 28,
+    x: 15,
+    y: 48.5,
+    eventDefinition: {type: 'error'},
+    properties: [{}]
+};
+
+export let endErrorEventActive = {
+    completed: false,
+    current: true,
+    id: 'sid-CED2A8DB-47E2-4057-A7B8-3ABBE5CE795E',
+    type: 'EndEvent',
+    width: 28,
+    height: 28,
+    x: 15,
+    y: 48.5,
+    eventDefinition: {type: 'error'},
+    properties: [{}]
+};
+
+export let endErrorEventCompleted = {
+    completed: true,
+    current: false,
+    id: 'sid-CED2A8DB-47E2-4057-A7B8-3ABBE5CE795E',
+    type: 'EndEvent',
+    width: 28,
+    height: 28,
+    x: 15,
+    y: 48.5,
+    eventDefinition: {type: 'error'},
+    properties: [{}]
+};
---
+++ ng2-components/process-services/diagram/assets/diagramSwimlanes.mock.ts
@@ -0,0 +1,44 @@
+/*!
+ * @license
+ * Copyright 2016 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export let pool = {
+    id: 'sid-0FF10DA3-E2BD-4E6A-9013-6D66FC8A4716',
+    name: 'Activiti',
+    width: 600,
+    height: 500,
+    x: 60,
+    y: 45,
+    properties: [{}]
+};
+
+export let poolLanes = {
+    id: 'sid-0FF10DA3-E2BD-4E6A-9013-6D66FC8A4716',
+    name: 'Activiti',
+    width: 600,
+    height: 500,
+    x: 60,
+    y: 45,
+    lanes: [{
+        id: 'sid-332204AB-D0F8-44CD-87B3-BF9DF59FF8AB',
+        name: 'Beckend',
+        width: 570,
+        height: 250,
+        x: 90,
+        y: 45
+    }],
+    properties: [{}]
+};
