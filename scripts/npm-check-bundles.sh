d4472b04cbaff36c5eda85e659d7157e8df682e2
Eugenio Romano <eugenio.romano@alfresco.com>
2017-11-13 11:18:41 +0000

    add adf- prefix to pacakges

--- scripts/npm-check-bundles.sh
+++ scripts/npm-check-bundles.sh
@@ -4,9 +4,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 eval VERSION=""
 
-eval projects=( "@alfresco/core"
-    "@alfresco/content-services"
-    "@alfresco/process-services" )
+eval projects=( "@alfresco/adf-core"
+    "@alfresco/adf-content-services"
+    "@alfresco/adf-process-services" )
 
 show_help() {
     echo "Usage: npm-check-bundles.sh"
