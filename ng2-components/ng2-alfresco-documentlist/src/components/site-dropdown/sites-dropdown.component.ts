ba6a2a8a78276d31cb2d55b78c92ad86dc3bc6d5
suzana_dirla <suzana@dirla.ro>
2017-11-14 12:15:39 +0200

    [ADF-1599] fix Dropdown	Placeholder text from 'Site List' to 'Select Location'

--- ng2-components/ng2-alfresco-documentlist/src/components/site-dropdown/sites-dropdown.component.ts
+++ ng2-components/ng2-alfresco-documentlist/src/components/site-dropdown/sites-dropdown.component.ts
@@ -28,6 +28,9 @@ export class DropdownSitesComponent implements OnInit {
     @Input()
     hideMyFiles: boolean = false;
 
+    @Input()
+    placeholder: string = 'DROPDOWN.PLACEHOLDER_LABEL';
+
     @Output()
     change: EventEmitter<SiteModel> = new EventEmitter();
 
