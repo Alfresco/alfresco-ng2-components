88b788fc48f961993c3275f3c605c1ffb5428ae6
Eugenio Romano <eromano@users.noreply.github.com>
2017-11-23 14:06:56 +0000

    datatable demo mobile improvement (#2711)

--- demo-shell/src/app/components/datatable/datatable.component.ts
+++ demo-shell/src/app/components/datatable/datatable.component.ts
@@ -83,10 +83,10 @@ export class DataTableComponent {
             ],
             [
                 { type: 'image', key: 'icon', title: '', srTitle: 'Thumbnail' },
-                { type: 'text', key: 'id', title: 'Id', sortable: true },
+                { type: 'text', key: 'id', title: 'Id', sortable: true , cssClass: 'desktop-only'},
                 { type: 'text', key: 'createdOn', title: 'Created On', sortable: true },
                 { type: 'text', key: 'name', title: 'Name', cssClass: 'full-width name-column', sortable: true },
-                { type: 'text', key: 'createdBy.name', title: 'Created By', sortable: true }
+                { type: 'text', key: 'createdBy.name', title: 'Created By', sortable: true, cssClass: 'desktop-only'}
             ]
         );
 
