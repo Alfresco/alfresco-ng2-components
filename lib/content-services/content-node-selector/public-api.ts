f9e1a344609fc65933f41d113b51a492087d3f17
Denys Vuika <denis.vuyka@gmail.com>
2017-11-23 12:34:58 +0000

    add missing export (#2706)

--- lib/content-services/content-node-selector/public-api.ts
+++ lib/content-services/content-node-selector/public-api.ts
@@ -15,5 +15,6 @@
  * limitations under the License.
  */
 
+export * from './content-node-selector.component-data.interface';
 export * from './content-node-selector.component';
 export * from './content-node-selector.service';
