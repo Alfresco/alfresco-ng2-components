b178d8856a390b47eed983980cb721ebf82fc86a
Eugenio Romano <eugenio.romano@alfresco.com>
2017-12-06 19:51:48 +0000

    remove >>> deprec selector exclude last test

--- lib/process-services/task-list/services/task-filter.service.spec.ts
+++ lib/process-services/task-list/services/task-filter.service.spec.ts
@@ -32,20 +32,14 @@ describe('Activiti Tas filter Service', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-
             providers: [
                 TaskFilterService
             ]
-        }).compileComponents();
+        }).compileComponents().then(() => {
+            service = TestBed.get(TaskFilterService);
+            jasmine.Ajax.install();
+        });
     }));
-
-    beforeEach(() => {
-        service = TestBed.get(TaskFilterService);
-    });
-
-    beforeEach(() => {
-        jasmine.Ajax.install();
-    });
 
     afterEach(() => {
         jasmine.Ajax.uninstall();
@@ -54,15 +48,13 @@ describe('Activiti Tas filter Service', () => {
     describe('Content tests', () => {
 
         it('should return the task list filters', (done) => {
-            service.getTaskListFilters().subscribe(
-                (res) => {
-                    expect(res).toBeDefined();
-                    expect(res.length).toEqual(2);
-                    expect(res[0].name).toEqual('FakeInvolvedTasks');
-                    expect(res[1].name).toEqual('FakeMyTasks');
-                    done();
-                }
-            );
+            service.getTaskListFilters().subscribe((res) => {
+                expect(res).toBeDefined();
+                expect(res.length).toEqual(2);
+                expect(res[0].name).toEqual('FakeInvolvedTasks');
+                expect(res[1].name).toEqual('FakeMyTasks');
+                done();
+            });
 
             jasmine.Ajax.requests.mostRecent().respondWith({
                 'status': 200,
@@ -72,17 +64,15 @@ describe('Activiti Tas filter Service', () => {
         });
 
         it('should return the task filter by id', (done) => {
-            service.getTaskFilterById(2).subscribe(
-                (taskFilter: FilterRepresentationModel) => {
-                    expect(taskFilter).toBeDefined();
-                    expect(taskFilter.id).toEqual(2);
-                    expect(taskFilter.name).toEqual('FakeMyTasks');
-                    expect(taskFilter.filter.sort).toEqual('created-desc');
-                    expect(taskFilter.filter.state).toEqual('open');
-                    expect(taskFilter.filter.assignment).toEqual('fake-assignee');
-                    done();
-                }
-            );
+            service.getTaskFilterById(2).subscribe((taskFilter: FilterRepresentationModel) => {
+                expect(taskFilter).toBeDefined();
+                expect(taskFilter.id).toEqual(2);
+                expect(taskFilter.name).toEqual('FakeMyTasks');
+                expect(taskFilter.filter.sort).toEqual('created-desc');
+                expect(taskFilter.filter.state).toEqual('open');
+                expect(taskFilter.filter.assignment).toEqual('fake-assignee');
+                done();
+            });
 
             jasmine.Ajax.requests.mostRecent().respondWith({
                 'status': 200,
@@ -92,8 +82,7 @@ describe('Activiti Tas filter Service', () => {
         });
 
         it('should return the task filter by name', (done) => {
-            service.getTaskFilterByName('FakeMyTasks').subscribe(
-                (res: FilterRepresentationModel) => {
+            service.getTaskFilterByName('FakeMyTasks').subscribe((res: FilterRepresentationModel) => {
                     expect(res).toBeDefined();
                     expect(res.id).toEqual(2);
                     expect(res.name).toEqual('FakeMyTasks');
@@ -111,28 +100,24 @@ describe('Activiti Tas filter Service', () => {
             });
         });
 
-        it('should call the api withthe appId', (done) => {
+        it('should call the api with the appId', (done) => {
             spyOn(service, 'callApiTaskFilters').and.returnValue((fakeAppPromise));
 
             let appId = 1;
-            service.getTaskListFilters(appId).subscribe(
-                (res) => {
-                    expect(service.callApiTaskFilters).toHaveBeenCalledWith(appId);
-                    done();
-                }
-            );
+            service.getTaskListFilters(appId).subscribe((res) => {
+                expect(service.callApiTaskFilters).toHaveBeenCalledWith(appId);
+                done();
+            });
         });
 
         it('should return the app filter by id', (done) => {
             let appId = 1;
-            service.getTaskListFilters(appId).subscribe(
-                (res) => {
-                    expect(res).toBeDefined();
-                    expect(res.length).toEqual(1);
-                    expect(res[0].name).toEqual('FakeInvolvedTasks');
-                    done();
-                }
-            );
+            service.getTaskListFilters(appId).subscribe((res) => {
+                expect(res).toBeDefined();
+                expect(res.length).toEqual(1);
+                expect(res[0].name).toEqual('FakeInvolvedTasks');
+                done();
+            });
 
             jasmine.Ajax.requests.mostRecent().respondWith({
                 'status': 200,
@@ -142,17 +127,15 @@ describe('Activiti Tas filter Service', () => {
         });
 
         it('should return the default filters', (done) => {
-            service.createDefaultFilters(1234).subscribe(
-                (res: FilterRepresentationModel []) => {
-                    expect(res).toBeDefined();
-                    expect(res.length).toEqual(4);
-                    expect(res[0].name).toEqual('Involved Tasks');
-                    expect(res[1].name).toEqual('My Tasks');
-                    expect(res[2].name).toEqual('Queued Tasks');
-                    expect(res[3].name).toEqual('Completed Tasks');
-                    done();
-                }
-            );
+            service.createDefaultFilters(1234).subscribe((res: FilterRepresentationModel []) => {
+                expect(res).toBeDefined();
+                expect(res.length).toEqual(4);
+                expect(res[0].name).toEqual('Involved Tasks');
+                expect(res[1].name).toEqual('My Tasks');
+                expect(res[2].name).toEqual('Queued Tasks');
+                expect(res[3].name).toEqual('Completed Tasks');
+                done();
+            });
 
             jasmine.Ajax.requests.at(0).respondWith({
                 'status': 200,
@@ -187,21 +170,19 @@ describe('Activiti Tas filter Service', () => {
             });
         });
 
-        it('should add a filter ', (done) => {
+        it('should add a filter', (done) => {
             let filterFake = new FilterRepresentationModel({
                 name: 'FakeNameFilter',
                 assignment: 'fake-assignement'
             });
 
-            service.addFilter(filterFake).subscribe(
-                (res: FilterRepresentationModel) => {
-                    expect(res).toBeDefined();
-                    expect(res.id).not.toEqual(null);
-                    expect(res.name).toEqual('FakeNameFilter');
-                    expect(res.filter.assignment).toEqual('fake-assignement');
-                    done();
-                }
-            );
+            service.addFilter(filterFake).subscribe((res: FilterRepresentationModel) => {
+                expect(res).toBeDefined();
+                expect(res.id).not.toEqual(null);
+                expect(res.name).toEqual('FakeNameFilter');
+                expect(res.filter.assignment).toEqual('fake-assignement');
+                done();
+            });
 
             jasmine.Ajax.requests.mostRecent().respondWith({
                 'status': 200,
