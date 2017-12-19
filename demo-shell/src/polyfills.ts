d7473ae78de2ca4a996268c5c34198b3abe5e35b
Eugenio Romano <eugenio.romano@alfresco.com>
2017-12-19 13:03:47 +0000

    fix different material depencncies

--- demo-shell/src/polyfills.ts
+++ demo-shell/src/polyfills.ts
@@ -34,11 +34,10 @@ import 'core-js/es6/map';
 import 'core-js/es6/set';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
- import 'classlist.js';  // Run `npm install --save classlist.js`.
+import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /** IE10 and IE11 requires the following to support `@angular/animation`. */
- import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
+import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 /** Evergreen browsers require these. **/
 import 'core-js/es6/reflect';
@@ -47,17 +46,10 @@ import 'core-js/es6/array';
 import 'core-js/es7/array';
 
 
-/** ALL Firefox browsers require the following to support `@angular/animation`. **/
-import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
-
-
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
-
-
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
