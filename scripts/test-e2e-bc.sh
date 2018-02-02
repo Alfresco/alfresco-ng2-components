5a5a58ed08a16b39e5eccac2818ed419c695e72f
Eugenio Romano <eugenio.romano@alfresco.com>
2018-02-02 09:57:49 +0000

    improve script

--- scripts/test-e2e-bc.sh
+++ scripts/test-e2e-bc.sh
@@ -4,13 +4,20 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 rm -rf "$DIR/../lib/node_modules/@angular"
 cd "$DIR/../integration/base_ver_2_app"
 
+ADF_VERSION=$(npm view @alfresco/adf-core version)
+ANGULAR_VERSION="5.1.1"
+MATERIAL_VERSION="5.0.1"
+
+npm install
 
 #Use last js-api
 npm install --save alfresco-js-api@alpha
 
 #New documented dependency
-npm install --save-exact --save @alfresco/adf-content-services@2.1.0 @alfresco/adf-core@2.1.0  @alfresco/adf-insights@2.0.1 @alfresco/adf-process-services@2.1.0
-npm install --save-exact --save-dev @angular-devkit/core@0.0.28 @angular/compiler-cli@5.1.1 typescript@2.6.2
+npm install --save-exact --save @alfresco/adf-content-services@${ADF_VERSION} @alfresco/adf-core@${ADF_VERSION}  @alfresco/adf-insights@${ADF_VERSION} @alfresco/adf-process-services@${ADF_VERSION}
+npm install --save-exact --save-dev @angular-devkit/core@0.0.28 @angular/compiler-cli@${ANGULAR_VERSION} typescript@2.6.2
 npm install --save  @mat-datetimepicker/core @mat-datetimepicker/moment
-npm install --save-exact --save  @angular/animations@5.1.1 @angular/cdk@5.0.1 @angular/common@5.1.1  @angular/compiler@5.1.1 @angular/core@5.1.1  @angular/platform-browser@5.1.1 @angular/router@5.1.1 @angular/flex-layout@2.0.0-beta.12  @angular/forms@5.1.1 @angular/forms@5.1.1 @angular/http@5.1.1 @angular/material@5.0.1 @angular/platform-browser-dynamic@5.1.1
-npm run e2e
+npm install --save-exact --save  @angular/animations@${ANGULAR_VERSION} @angular/common@${ANGULAR_VERSION}  @angular/compiler@${ANGULAR_VERSION} @angular/core@${ANGULAR_VERSION}  @angular/platform-browser@${ANGULAR_VERSION} @angular/router@${ANGULAR_VERSION} @angular/flex-layout@2.0.0-beta.12  @angular/forms@${ANGULAR_VERSION} @angular/forms@${ANGULAR_VERSION} @angular/http@${ANGULAR_VERSION}  @angular/platform-browser-dynamic@${ANGULAR_VERSION}
+npm install --save-exact --save @angular/cdk@${MATERIAL_VERSION} @angular/material@${MATERIAL_VERSION}
+
+pm run e2e
