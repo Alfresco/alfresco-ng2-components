bee2f9ead369c2aac9be3356f003fab262c586cc
Bogdan Cilibiu <Bogdan.Cilibiu@ness.com>
2018-02-16 13:09:21 +0200

    update TestBed

--- lib/core/viewer/components/pdfViewer.component.spec.ts
+++ lib/core/viewer/components/pdfViewer.component.spec.ts
@@ -26,6 +26,8 @@ import { ToolbarModule } from '../../toolbar/toolbar.module';
 import { EventMock } from '../../mock/event.mock';
 import { RenderingQueueServices } from '../services/rendering-queue.services';
 import { PdfViewerComponent } from './pdfViewer.component';
+import { PdfThumbListComponent } from './pdfViewer-thumbnails.component';
+import { PdfThumbComponent } from './pdfViewer-thumb.component';
 
 declare var require: any;
 
@@ -42,7 +44,9 @@ describe('Test PdfViewer component', () => {
                 MaterialModule
             ],
             declarations: [
-                PdfViewerComponent
+                PdfViewerComponent,
+                PdfThumbListComponent,
+                PdfThumbComponent
             ],
             providers: [
                 SettingsService,
