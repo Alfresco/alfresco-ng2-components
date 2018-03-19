8da98afacaea70ee06edab1d9974d6c47dd12c1d
Eugenio Romano <eugenio.romano@alfresco.com>
2018-03-19 22:38:57 +0000

    test distribution

--- scripts/simulate-publish.sh
+++ scripts/simulate-publish.sh
@@ -4,12 +4,20 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 npm set registry https://registry.npmjs.org/
 
+echo "====== Install verdaccio ===== "
+
 npm install -g verdaccio
 npm install -g concurrently
 npm install -g verdaccio-auth-memory
 
+echo "====== Update version ===== "
+
 $DIR/update-version.sh -nextalpha
+
+echo "====== Change registry ===== "
 
 npm set registry http://localhost:4873/
 
-concurrently "verdaccio --listen 4873 --config $DIR/../assets/config-verdaccio.yaml" "$DIR/npm-publish.sh"
+echo "====== Run verdaccio ===== "
+
+concurrently "verdaccio --listen 4873 --config $DIR/config/config-verdaccio.yaml" "$DIR/npm-publish.sh"
