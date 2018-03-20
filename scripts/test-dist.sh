26c8f954756ca4a6c20ec3093aa40282bc1de5f2
Eugenio Romano <eromano@users.noreply.github.com>
2018-03-20 11:53:11 +0000

    [ADF-2514][ADF-2517][ADF-2143] Fix build isssues (#3097)
    * fix remove angular folder problem
    
    * fix npm import cli
    
    * fix search module
    
    * fix comment
    
    * fix tsconfig output
    
    * es 20016 tsconfig
    
    * travis add demo shell dist
    
    * exclude datapicker process services
    
    * demo shell script fix
    
    * bundle check new build
    
    * add simulate publish
    
    * invert publish
    
    * test distribution
    
    * improve sh help
    
    * uncomment script
    
    * finally got the guilty piece of code
    
    * run e2e

---
+++ scripts/test-dist.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "====== Install demo-shell ===== "
+
+cd $DIR/../demo-shell
+npm install
+
+echo "====== Install lib ===== "
+
+cd $DIR/../lib
+npm install
+
+echo "====== Install JS-API alpha ===== "
+
+npm install alfresco-js-api@alpha
+
+echo "====== Build ADF ===== "
+
+npm run new-build
+
+echo "====== COPY new build in demo shell node_modules ===== "
+
+rm -rf ../demo-shell/node_modules/@alfresco
+
+mkdir -p $DIR/../demo-shell/node_modules/@alfresco/adf-core
+mkdir -p $DIR/../demo-shell/node_modules/@alfresco/adf-content-services
+mkdir -p $DIR/../demo-shell/node_modules/@alfresco/adf-process-services
+mkdir -p $DIR/../demo-shell/node_modules/@alfresco/adf-insights
+
+cp -R $DIR/../lib/dist/core/* $DIR/../demo-shell/node_modules/@alfresco/adf-core
+cp -R $DIR/../lib/dist/content-services/* $DIR/../demo-shell/node_modules/@alfresco/adf-content-services
+cp -R $DIR/../lib/dist/process-services/* $DIR/../demo-shell/node_modules/@alfresco/adf-process-services
+cp -R $DIR/../lib/dist/insights/* $DIR/../demo-shell/node_modules/@alfresco/adf-insights
+
+echo "====== Build dist demo shell ===== "
+
+cd $DIR/../demo-shell
+npm run build:dist
+
+echo "====== e2e test ===== "
+
+npm run e2e
