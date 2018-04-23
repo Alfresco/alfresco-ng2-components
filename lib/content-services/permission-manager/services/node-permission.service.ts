6bcb6503bc3c3282b570fb3d55fa98420faad083
VitoAlbano <vito.albano.123@gmail.com>
2018-04-23 17:11:01 +0100

    [ADF-2556] integrated service for add and remove permission from node

--- lib/content-services/permission-manager/services/node-permission.service.ts
+++ lib/content-services/permission-manager/services/node-permission.service.ts
@@ -18,10 +18,8 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { AlfrescoApiService, SearchService, NodesApiService } from '@alfresco/adf-core';
-import { QueryBody, MinimalNodeEntryEntity, MinimalNodeEntity, PathElement, GroupMemberEntry, GroupsPaging, GroupMemberPaging, PermissionElement } from 'alfresco-js-api';
+import { QueryBody, MinimalNodeEntryEntity, PathElement, GroupMemberEntry, GroupsPaging, GroupMemberPaging, PermissionElement } from 'alfresco-js-api';
 import 'rxjs/add/operator/switchMap';
-import { of } from 'rxjs/observable/of';
-import { switchMap } from 'rxjs/operators';
 
 @Injectable()
 export class NodePermissionService {
@@ -31,11 +29,6 @@ export class NodePermissionService {
                 private nodeService: NodesApiService) {
     }
 
-    /**
-     * Gets a list of roles for the current node.
-     * @param node The target node
-     * @returns Array of strings representing the roles
-     */
     getNodeRoles(node: MinimalNodeEntryEntity): Observable<string[]> {
         const retrieveSiteQueryBody: QueryBody = this.buildRetrieveSiteQueryBody(node.path.elements);
         return this.searchApiService.searchByQueryBody(retrieveSiteQueryBody)
@@ -49,14 +42,7 @@ export class NodePermissionService {
             });
     }
 
-    /**
-     * Updates the permission for a node.
-     * @param node Target node
-     * @param updatedPermissionRole Permission role to update or add
-     * @returns Node with updated permission
-     */
-    updatePermissionRole(node: MinimalNodeEntryEntity, updatedPermissionRole: PermissionElement): Observable<MinimalNodeEntryEntity> {
-
+    updatePermissionRoles(node: MinimalNodeEntryEntity, updatedPermissionRole: PermissionElement): Observable<MinimalNodeEntryEntity> {
         let permissionBody = { permissions: { locallySet: []} };
         const index = node.permissions.locallySet.map((permission) => permission.authorityId).indexOf(updatedPermissionRole.authorityId);
         permissionBody.permissions.locallySet = permissionBody.permissions.locallySet.concat(node.permissions.locallySet);
@@ -68,38 +54,13 @@ export class NodePermissionService {
         return this.nodeService.updateNode(node.id, permissionBody);
     }
 
-    updateNodePermissions(nodeId: string, permissionList: MinimalNodeEntity[]): Observable<MinimalNodeEntryEntity> {
-       return this.nodeService.getNode(nodeId).pipe(
-           switchMap(node => {
-                return this.getNodeRoles(node).pipe(
-                    switchMap((nodeRoles) => of({node, nodeRoles}) )
-                );
-            }),
-            switchMap(({node, nodeRoles}) => this.updateLocallySetPermissions(node, permissionList, nodeRoles))
-        );
-    }
-
-    updateLocallySetPermissions(node: MinimalNodeEntryEntity, nodes: MinimalNodeEntity[], nodeRole: string[]): Observable<MinimalNodeEntryEntity> {
+    updateLocallySetPermissions(node: MinimalNodeEntryEntity, permissionList: any[]): Observable<MinimalNodeEntryEntity> {
         let permissionBody = { permissions: { locallySet: []} };
-        const permissionList = this.transformNodeToPermissionElement(nodes, nodeRole[0]);
         permissionBody.permissions.locallySet = node.permissions.locallySet ? node.permissions.locallySet.concat(permissionList) : permissionList;
         return this.nodeService.updateNode(node.id, permissionBody);
     }
 
-    private transformNodeToPermissionElement(nodes: MinimalNodeEntity[], nodeRole: any): PermissionElement[] {
-        return nodes.map((node) => {
-            let newPermissionElement: PermissionElement = <PermissionElement> {
-                'authorityId': node.entry.properties['cm:authorityName'] ?
-                    node.entry.properties['cm:authorityName'] :
-                    node.entry.properties['cm:userName'],
-                'name': nodeRole,
-                'accessStatus': 'ALLOWED'
-            };
-            return newPermissionElement;
-        });
-    }
-
-    removePermission(node: MinimalNodeEntryEntity, permissionToRemove: PermissionElement): Observable<MinimalNodeEntryEntity> {
+    removePermission(node: MinimalNodeEntryEntity, permissionToRemove: PermissionElement): Observable<MinimalNodeEntryEntity>{
         let permissionBody = { permissions: { locallySet: [] } };
         const index = node.permissions.locallySet.map((permission) => permission.authorityId).indexOf(permissionToRemove.authorityId);
         if (index !== -1) {
@@ -121,12 +82,6 @@ export class NodePermissionService {
             });
     }
 
-    /**
-     * Gets all members related to a group name.
-     * @param groupName Name of group to look for members
-     * @param opts Extra options supported by JSAPI
-     * @returns List of members
-     */
     getGroupMemeberByGroupName(groupName: string, opts?: any): Observable<GroupMemberPaging> {
         return Observable.fromPromise(this.apiService.groupsApi.getGroupMembers(groupName, opts));
     }
