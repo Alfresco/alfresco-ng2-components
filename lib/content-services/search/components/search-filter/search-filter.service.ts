186551757a19867e50bff38500f621cee6c26a26
Denys Vuika <denys.vuika@gmail.com>
2018-04-24 14:28:24 +0100

    [ADF-2786] AOT support for search filter widgets (#3224)
    * AOT support for search filter widgets
    
    * json schema for search settings

---
+++ lib/content-services/search/components/search-filter/search-filter.service.ts
@@ -0,0 +1,37 @@
+/*!
+ * @license
+ * Copyright 2016 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Injectable, Type } from '@angular/core';
+import { SearchTextComponent } from '../search-text/search-text.component';
+import { SearchRadioComponent } from '../search-radio/search-radio.component';
+import { SearchFieldsComponent } from '../search-fields/search-fields.component';
+import { SearchScopeLocationsComponent } from '../search-scope-locations/search-scope-locations.component';
+
+@Injectable()
+export class SearchFilterService {
+
+    /**
+     * Contains string-to-type mappings for registered widgets.
+     */
+    widgets: { [id: string]: Type<{}> } = {
+        'text': SearchTextComponent,
+        'radio': SearchRadioComponent,
+        'fields': SearchFieldsComponent,
+        'scope-locations': SearchScopeLocationsComponent
+    };
+
+}
