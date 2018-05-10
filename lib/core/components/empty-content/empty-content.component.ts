b0111b72d6e44542b1a7684c4dc4fb4bb48c3180
Denys Vuika <denis.vuyka@gmail.com>
2018-05-10 07:46:18 +0100

    empty content component

---
+++ lib/core/components/empty-content/empty-content.component.ts
@@ -0,0 +1,39 @@
+/*!
+ * @license
+ * Copyright 2016 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component, ChangeDetectionStrategy, ViewEncapsulation, Input } from '@angular/core';
+
+@Component({
+    selector: 'adf-empty-content',
+    templateUrl: './empty-content.component.html',
+    styleUrls: ['./empty-content.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    encapsulation: ViewEncapsulation.None,
+    host: { class: 'app-empty-content' }
+})
+export class EmptyContentComponent {
+
+    @Input()
+    icon = 'cake';
+
+    @Input()
+    title = '';
+
+    @Input()
+    subtitle = '';
+
+}
