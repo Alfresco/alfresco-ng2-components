4ced4164bf470795bb369fe5728ae1ba82304d36
Eugenio Romano <eromano@users.noreply.github.com>
2018-05-14 16:05:27 +0100

    pipeline-update (#3309)
    * deploy PR script

--- scripts/test-dist.sh
+++ scripts/test-dist.sh
@@ -2,6 +2,26 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+eval NAME=
+
+show_help() {
+    echo "Usage: test-dist.sh"
+    echo ""
+    echo "-n or --name pr name"
+}
+
+name(){
+    NAME="/$1"
+}
+
+while [[ $1 == -* ]]; do
+    case "$1" in
+      -h|--help|-\?) show_help; exit 0;;
+      -n|--name)  name $2; shift 2;;
+      -*) echo "invalid option: $1" 1>&2; show_help; exit 0;;
+    esac
+done
+
 echo "====== Install lib ===== "
 
 cd $DIR/../
@@ -13,7 +33,7 @@ npm install alfresco-js-api@alpha
 
 echo "====== Build ADF ===== "
 
-npm run build-lib
+npm run build-lib || exit 1
 
 echo "====== COPY new build in node_modules ===== "
 
@@ -31,8 +51,9 @@ cp -R $DIR/../lib/dist/insights/* $DIR/../node_modules/@alfresco/adf-insights
 
 echo "====== Build dist demo shell ===== "
 
-npm run build:dist
+npm run server-versions
+ng build --prod --app dist --base-href=$NAME/ -op demo-shell/dist/$NAME || exit 1
 
 echo "====== e2e test ===== "
 
-npm run e2e
+npm run e2e || exit 1
