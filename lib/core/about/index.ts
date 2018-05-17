e8ac2181d850f9667e147546fb8727abb47d959b
Eugenio Romano <eromano@users.noreply.github.com>
2018-05-17 09:58:22 +0100

    about component (#3337)

--- lib/core/about/index.ts
+++ lib/core/about/index.ts
@@ -15,12 +15,4 @@
  * limitations under the License.
  */
 
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-about-page',
-    templateUrl: './about.component.html'
-})
-export class AboutComponent  {
-
-}
+export * from './public-api';
