2aac8036a5943de090e6707e826defebe4791b0f
sivakumar414ram <siva.kumar@muraai.com>
2018-06-13 20:46:53 +0530

    [ADF-3196] [Task list / Process list] - The 'Task details' are not displayed for the selected Task

--- lib/core/datatable/components/datatable/datatable.component.ts
+++ lib/core/datatable/components/datatable/datatable.component.ts
@@ -216,10 +216,6 @@ export class DataTableComponent implements AfterContentInit, OnChanges, DoCheck,
             this.resetSelection();
             this.emitRowSelectionEvent('row-unselect', null);
         }
-
-        if (this.isPropertyChanged(changes['sorting'])) {
-            this.setTableSorting(changes['sorting'].currentValue);
-        }
     }
 
     ngDoCheck() {
