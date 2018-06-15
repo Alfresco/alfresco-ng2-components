ab7982c4d67715ebf5ac936d1fa589afc0fd2cff
VitoAlbano <vito.albano.123@gmail.com>
2018-06-15 09:35:52 +0100

    [ADF-2769] start adding more examples to node selector page

--- demo-shell/src/app/components/content-node-selector/content-node-selector.component.ts
+++ demo-shell/src/app/components/content-node-selector/content-node-selector.component.ts
@@ -15,12 +15,59 @@
  * limitations under the License.
  */
 
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { SitePaging, SiteEntry, MinimalNodeEntryEntity } from 'alfresco-js-api';
+import { ShareDataRow } from '@alfresco/adf-content-services';
 
 @Component({
-    templateUrl: './content-node-selector.component.html'
+    templateUrl: './content-node-selector.component.html',
+    styleUrls: [`./content-node-selector.component.scss`],
+    encapsulation: ViewEncapsulation.None
 })
 export class ContentNodeSelectorComponent {
 
     dropdownHideMyFiles = false;
+    showFiles = false;
+    showFolders = false;
+
+    customSideGuid = '';
+    customSideTitle = '';
+
+    defaultSites: SiteEntry[] = [
+        { entry: { title: 'MINE', guid: '-my-' } },
+        { entry: { title: 'ROOTY', guid: '-root-' } }];
+
+    customSites: SitePaging = {
+        list: {
+            entries: [
+                { entry: { title: 'MINE', guid: '-my-' } },
+                { entry: { title: 'ROOTY', guid: '-root-' } }],
+            pagination: {}
+        }
+    };
+
+    onClickAddSite() {
+        const newSiteEntry: SiteEntry = { entry: { title: this.customSideTitle, guid: this.customSideGuid } };
+        this.customSites.list.entries.push(newSiteEntry);
+        this.customSideGuid = '';
+        this.customSideTitle = '';
+    }
+
+    onClickResetSite() {
+        this.customSites.list.entries = this.defaultSites;
+        this.customSideGuid = '';
+        this.customSideTitle = '';
+    }
+
+    rowFilteringExample(row: ShareDataRow) {
+        let showNode = true;
+        const node: MinimalNodeEntryEntity = row.node.entry;
+        if (this.showFiles) {
+            showNode = node.isFile;
+        }
+        if (this.showFolders) {
+            showNode = node.isFolder;
+        }
+        return showNode;
+    }
 }
