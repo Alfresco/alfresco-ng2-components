27158b5bdd55fe2a30ece79590b2c51c328f6e65
VitoAlbano <vito.albano.123@gmail.com>
2018-06-20 14:28:18 +0100

    [ADF-3242] added try catch for circular dependency objects

--- demo-shell/src/app/components/log/log.component.ts
+++ demo-shell/src/app/components/log/log.component.ts
@@ -32,8 +32,13 @@ export class LogComponent {
     constructor(public logService: LogService) {
 
         logService.onMessage.subscribe((message) => {
-
-            this.logs.push({ type: message.type, text: JSON.stringify(message.text) });
+            let contentMessage = '';
+            try {
+                contentMessage = JSON.stringify(message.text);
+            } catch (error) {
+                contentMessage = error.message;
+            }
+            this.logs.push({ type: message.type, text: contentMessage});
             this.logsData = new ObjectDataTableAdapter(this.logs, [
                 { type: 'text', key: 'type', title: 'Log level', sortable: true },
                 { type: 'text', key: 'text', title: 'Message', sortable: false }
