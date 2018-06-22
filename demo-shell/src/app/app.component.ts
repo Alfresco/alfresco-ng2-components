ba4f5ad11923622afc13a30e86c3a3d20ac6d725
Eugenio Romano <eugenio.romano@alfresco.com>
2018-06-22 09:56:52 +0100

    remove 404 check in demo shell

--- demo-shell/src/app/app.component.ts
+++ demo-shell/src/app/app.component.ts
@@ -38,10 +38,7 @@ export class AppComponent implements OnInit {
         this.pageTitleService.setTitle('title');
 
         this.alfrescoApiService.getInstance().on('error', (error) => {
-
-            if (error.status === 404) {
-                this.router.navigate(['/error', error.status]);
-            } else if (error.status === 401) {
+            if (error.status === 401) {
                 if (!this.authenticationService.isLoggedIn()) {
                     this.router.navigate(['/login']);
                 }
