17b73eb7734c69ea4b4a2af6f35aeeda24499b28
Eugenio Romano <eugenio.romano@alfresco.com>
2018-06-27 19:55:00 +0100

    firs test js-api integrated

--- protractor.conf.js
+++ protractor.conf.js
@@ -5,25 +5,12 @@ const path = require('path');
 const {SpecReporter} = require('jasmine-spec-reporter');
 const jasmineReporters = require('jasmine-reporters');
 
-console.log(0);
-
 const projectRoot = path.resolve(__dirname);
 
 const width = 1366;
 const height = 768;
 
 var HOST = process.env.URL_HOST_ADF;
-var BROWSER_RUN = process.env.BROWSER_RUN;
-
-var args_options = [];
-
-if (BROWSER_RUN === 'true') {
-    args_options = ['--incognito'];
-} else {
-    args_options = ['--incognito', '--headless'];
-}
-
-var downloadFolder = path.join(__dirname,'e2e/downloads');
 
 exports.config = {
     allScriptsTimeout: 60000,
@@ -39,10 +26,10 @@ exports.config = {
                 'credentials_enable_service': false,
                 'download': {
                     'prompt_for_download': false,
-                    'default_directory': downloadFolder
+                    'default_directory': './e2e/downloads/',
                 }
             },
-            args: args_options
+            args: ['--incognito', '--headless']
         }
     },
 
