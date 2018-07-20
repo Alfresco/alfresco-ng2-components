e8d2e216868c6e214d2d8a0a2e3c5068e2a3631f
bbcodrin <bobucodrin@gmail.com>
2018-07-20 18:54:20 +0300

    [ADF-3308] Process list filter by fields (#3588)
    * process list component created
    
    * process list renamed
    
    * routing fix

--- demo-shell/src/app/components/app-layout/app-layout.component.ts
+++ demo-shell/src/app/components/app-layout/app-layout.component.ts
@@ -40,6 +40,7 @@ export class AppLayoutComponent implements OnInit {
         { href: '/header-data', icon: 'edit', title: 'APP_LAYOUT.HEADER_DATA'},
         { href: '/node-selector', icon: 'attachment', title: 'APP_LAYOUT.NODE-SELECTOR' },
         { href: '/task-list', icon: 'assignment', title: 'APP_LAYOUT.TASK_LIST' },
+        { href: '/process-list', icon: 'assignment', title: 'APP_LAYOUT.PROCESS_LIST' },
         { href: '/activiti', icon: 'device_hub', title: 'APP_LAYOUT.PROCESS_SERVICES' },
         { href: '/login', icon: 'vpn_key', title: 'APP_LAYOUT.LOGIN' },
         { href: '/trashcan', icon: 'delete', title: 'APP_LAYOUT.TRASHCAN' },
