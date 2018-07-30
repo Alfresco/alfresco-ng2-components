689c7711df4e25297eec7d111d418b53a91aaecb
jdosti <jenny.dosti@alfresco.com>
2018-07-30 17:39:23 +0100

    [ADF-3399] Create automated tests to cover Header component (#3633)

---
+++ e2e/pages/adf/core/headerPage.ts
@@ -0,0 +1,73 @@
+/*!
+ * @license
+ * Copyright 2016 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import Util = require('../../../util/util');
+import TestConfig = require('../../../test.config');
+
+export class HeaderPage {
+
+    checkBox = element(by.cssContainingText('.mat-checkbox-label', 'Show menu button'));
+    headerColor = element(by.css('option[value="primary"]'));
+    titleInput = element(by.css('input[name="title"]'));
+    iconInput = element(by.css('input[placeholder="URL path"]'));
+
+    checkShowMenuCheckBoxIsDisplayed() {
+        return Util.waitUntilElementIsVisible(this.checkBox);
+    }
+
+    checkChooseHeaderColourIsDisplayed() {
+        return Util.waitUntilElementIsVisible(this.headerColor);
+    }
+
+    checkChangeTitleIsDisplayed() {
+        return Util.waitUntilElementIsVisible(this.titleInput);
+    }
+
+    checkChangeUrlPathIsDisplayed() {
+        return Util.waitUntilElementIsVisible(this.iconInput);
+    }
+
+    clickShowMenuButton() {
+        let checkBox = element.all(by.css('mat-checkbox'));
+        Util.waitUntilElementIsVisible(checkBox);
+        return checkBox.get(0).click();
+    }
+
+    changeHeaderColor(color) {
+        let headerColor = element(by.css('option[value="'+color+'"]'));
+        return headerColor.click();
+    }
+
+    checkAppTitle(name) {
+        let title = element(by.cssContainingText('.adf-app-title', name));
+        return Util.waitUntilElementIsVisible(title);
+    }
+
+    addTitle(title) {
+        Util.waitUntilElementIsVisible(this.titleInput);
+        return this.titleInput.click().sendKeys(title).sendKeys(protractor.Key.ENTER);
+    }
+
+    checkIconIsDisplayed(url) {
+        let icon = element(by.css('img[src="'+ url +'"]'));
+        Util.waitUntilElementIsVisible(icon);
+    }
+
+    addIcon(url) {
+        Util.waitUntilElementIsVisible(this.iconInput);
+        return this.iconInput.click().sendKeys(url).sendKeys(protractor.Key.ENTER);
+    }
+}
