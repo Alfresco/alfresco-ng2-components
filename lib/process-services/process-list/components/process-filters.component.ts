e6b1f4c55974933a32a82eb2d35ae6d8f9b315b5
Codrin Bobu <codrin.bobu@ness.com>
2018-08-02 10:08:35 +0300

    task filter icons fixed

--- lib/process-services/process-list/components/process-filters.component.ts
+++ lib/process-services/process-list/components/process-filters.component.ts
@@ -58,7 +58,7 @@ export class ProcessFiltersComponent implements OnInit, OnChanges {
 
     /** Toggle to show or hide the filter's icon. */
     @Input()
-    showIcon: boolean = true;
+    hasIcon: boolean;
 
     private filterObserver: Observer<ProcessInstanceFilterRepresentation>;
     filter$: Observable<ProcessInstanceFilterRepresentation>;
