9c6a732b75339c9c56d3f13994ec1ed166014316
David Cano <david.cano.nieto@gmail.com>
2018-08-07 11:01:00 +0100

    [ADF-3282] Unit tests fixed

--- lib/process-services/task-list/components/start-task.component.ts
+++ lib/process-services/task-list/components/start-task.component.ts
@@ -15,11 +15,7 @@
  * limitations under the License.
  */
 
-<<<<<<< HEAD
 import { LogService, UserPreferencesService, UserProcessModel, TranslationService, FormModel, FormFieldModel } from '@alfresco/adf-core';
-=======
-import { LogService, UserPreferencesService, UserProcessModel, TranslationService, FormFieldModel, FormModel } from '@alfresco/adf-core';
->>>>>>> [ADF-3282] Conflicts solved
 import { Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation } from '@angular/core';
 import { DateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
 import { MOMENT_DATE_FORMATS, MomentDateAdapter } from '@alfresco/adf-core';
