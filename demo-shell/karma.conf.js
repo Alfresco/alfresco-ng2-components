83bf4198527615c92281197f8e383d205fb2b6d7
Eugenio Romano <eugenio.romano@alfresco.com>
2018-08-07 23:58:44 +0100

    demo shell headless chrome

--- demo-shell/karma.conf.js
+++ demo-shell/karma.conf.js
@@ -10,7 +10,8 @@ module.exports = function (config) {
             require('karma-chrome-launcher'),
             require('karma-jasmine-html-reporter'),
             require('karma-coverage-istanbul-reporter'),
-            require('@angular-devkit/build-angular/plugins/karma')
+            require('@angular-devkit/build-angular/plugins/karma'),
+            require('karma-mocha-reporter')
         ],
         client: {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -25,14 +26,25 @@ module.exports = function (config) {
             fixWebpackSourcePaths: true
         },
 
+        customLaunchers: {
+            ChromeHeadless: {
+                base: 'Chrome',
+                flags: [
+                    '--no-sandbox',
+                    '--headless',
+                    '--disable-gpu',
+                    '--remote-debugging-port=9222'
+                ]
+            }
+        },
+
         captureTimeout: 180000,
         browserDisconnectTimeout: 180000,
         browserDisconnectTolerance: 3,
         browserNoActivityTimeout: 300000,
 
-        reporters: config.angularCli && config.angularCli.codeCoverage
-            ? ['progress', 'coverage-istanbul']
-            : ['progress', 'kjhtml'],
+        reporters: ['mocha', 'kjhtml'],
+
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
