9f2ed2d9e867f3a5b5a26c1d51101793f719f30e
Eugenio Romano <eugenio.romano@alfresco.com>
2018-09-13 14:58:36 +0100

    emit Forms error

--- demo-shell/src/app/components/form/form.component.ts
+++ demo-shell/src/app/components/form/form.component.ts
@@ -15,27 +15,13 @@
  * limitations under the License.
  */
 
-/*!
-* @license
-* Copyright 2016 Alfresco Software, Ltd.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
-import { Component, Inject, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormModel, FormService, FormOutcomeEvent } from '@alfresco/adf-core';
+import { Component, Inject, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+    FormModel, FormService, FormOutcomeEvent, ValidateFormEvent, LogService
+} from '@alfresco/adf-core';
 import { InMemoryFormService } from '../../services/in-memory-form.service';
 import { DemoForm } from './demo-form';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-form',
@@ -46,19 +32,31 @@ import { DemoForm } from './demo-form';
     ],
     encapsulation: ViewEncapsulation.None
 })
-export class FormComponent implements OnInit {
+export class FormComponent implements OnInit, OnDestroy {
 
     form: FormModel;
+    private subscriptions: Subscription[] = [];
 
-    constructor(@Inject(FormService) private formService: InMemoryFormService) {
-        formService.executeOutcome.subscribe((formOutcomeEvent: FormOutcomeEvent) => {
-            formOutcomeEvent.preventDefault();
-        });
+    constructor(@Inject(FormService) private formService: InMemoryFormService, private logService: LogService) {
+
+        this.subscriptions.push(
+            formService.executeOutcome.subscribe((formOutcomeEvent: FormOutcomeEvent) => {
+                formOutcomeEvent.preventDefault();
+            }),
+            formService.validateForm.subscribe((validateFormEvent: ValidateFormEvent) => {
+                this.logService.log('Error form:' + validateFormEvent.errorsField);
+            })
+        );
     }
 
     ngOnInit() {
         const formDefinitionJSON: any = DemoForm.getDefinition();
         this.form = this.formService.parseForm(formDefinitionJSON);
+    }
+
+    ngOnDestroy() {
+        this.subscriptions.forEach(subscription => subscription.unsubscribe());
+        this.subscriptions = [];
     }
 
 }
