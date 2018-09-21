20fd4f138c6eda3071f8a84eb74a3968bb4e6aca
davidcanonieto <david.cano.nieto@gmail.com>
2018-09-21 13:50:26 +0100

    [ADF-3555] [ADF-3382] Start Process component refactored

--- demo-shell/src/app/components/process-service/process-service.component.ts
+++ demo-shell/src/app/components/process-service/process-service.component.ts
@@ -134,6 +134,7 @@ export class ProcessServiceComponent implements AfterViewInit, OnDestroy, OnInit
 
     defaultProcessDefinitionName: string;
     defaultProcessName: string;
+    defaultProcessSelected: boolean;
 
     activeTab: number = this.tabs.tasks; // tasks|processes|reports
 
@@ -172,6 +173,7 @@ export class ProcessServiceComponent implements AfterViewInit, OnDestroy, OnInit
 
         this.defaultProcessName = this.appConfig.get<string>('adf-start-process.name');
         this.defaultProcessDefinitionName = this.appConfig.get<string>('adf-start-process.processDefinitionName');
+        this.defaultProcessSelected = this.appConfig.get<boolean>('adf-start-process.defaultProcessSelected');
 
         // Uncomment this line to replace all 'text' field editors with custom component
         // formRenderingService.setComponentTypeResolver('text', () => CustomEditorComponent, true);
