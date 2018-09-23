c9f8bc892e421b549f3c1ea028617c094089fa51
Eugenio Romano <eugenio.romano@alfresco.com>
2018-09-23 22:22:08 +0100

    stringify error messge

--- scripts/upload-build-in-cs.js
+++ scripts/upload-build-in-cs.js
@@ -60,7 +60,7 @@ async function main() {
                 'overwrite': true
             });
         } catch (error) {
-            console.log(`Folder Builds/${program.folder} creation error ${error}` );
+            console.log(`Folder Builds/${program.folder} creation error ${JSON.stringify(error)}` );
 
             folder = await alfrescoJsApi.nodes.getNode('-my-', {
                 'relativePath': `Builds/${program.folder}`,
