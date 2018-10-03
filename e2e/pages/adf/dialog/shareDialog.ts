27d4a8109bfb2727eee493a1036b19c9ccb7a218
Bogdan Cilibiu <cilibiu.bogdan@yahoo.com>
2018-10-03 13:38:07 +0300

    methods and locators

--- e2e/pages/adf/dialog/shareDialog.ts
+++ e2e/pages/adf/dialog/shareDialog.ts
@@ -15,15 +15,29 @@
  * limitations under the License.
  */
 
-import { element, by } from 'protractor';
+import { element, by, browser } from 'protractor';
 import Util = require('../../../util/util');
 
 export class ShareDialog {
-
+    shareDialog = element(by.css('adf-share-dialog'));
     dialogTitle = element(by.css('[data-automation-id="adf-share-dialog-title"]'));
-    shareToggle = element(by.id('adf-share-toggle'));
-    shareLink = element(by.css('[data-automation-id="adf-share-link"'));
-    closeButton = element(by.css('button[class="mat-button mat-primary"]'));
+    shareToggle = element(by.css('[data-automation-id="adf-share-toggle"] label'));
+    shareToggleChecked = element(by.css('mat-dialog-container mat-slide-toggle.mat-checked'));
+    shareToggleUnchecked = element(by.css('mat-dialog-container mat-slide-toggle:not(.mat-checked)'));
+    shareToggleDisabled = element(by.css('mat-dialog-container mat-slide-toggle.mat-disabled'));
+    shareLink = element(by.css('[data-automation-id="adf-share-link"]'));
+    closeButton = element(by.css('button[data-automation-id="adf-share-dialog-close"]'));
+    snackBar = element(by.css('simple-snack-bar'));
+    copySharedLinkButton = element(by.css('.input-action'));
+    timeDatePickerButton = element(by.css('mat-datetimepicker-toggle button'));
+    monthPicker = element(by.css('mat-datetimepicker-month-view'));
+    clockPicker = element(by.css('mat-datetimepicker-clock'));
+    hoursPicker = element(by.css('.mat-datetimepicker-clock-hours'));
+    minutePicker = element(by.css('.mat-datetimepicker-clock-minutes'));
+    expirationDateInput = element(by.css('input[formcontrolname="time"]'));
+    confirmationDialog = element(by.css('adf-confirm-dialog'));
+    confirmationCancelButton = element(by.id('adf-confirm-cancel'));
+    confirmationRemoveButton = element(by.id('adf-confirm-accept'));
 
     checkDialogIsDisplayed() {
         return Util.waitUntilElementIsVisible(this.dialogTitle);
@@ -34,6 +48,16 @@ export class ShareDialog {
         Util.waitUntilElementIsClickable(this.shareToggle);
         return this.shareToggle.click();
 
+    }
+
+    clickConfirmationDialogCancelButton() {
+        Util.waitUntilElementIsVisible(this.confirmationCancelButton);
+        this.confirmationCancelButton.click();
+    }
+
+    clickConfirmationDialogRemoveButton() {
+        Util.waitUntilElementIsVisible(this.confirmationRemoveButton);
+        this.confirmationRemoveButton.click();
     }
 
     checkShareLinkIsDisplayed() {
@@ -48,5 +72,78 @@ export class ShareDialog {
     clickCloseButton() {
         Util.waitUntilElementIsVisible(this.closeButton);
         return this.closeButton.click();
+    }
+
+    clickShareLinkButton() {
+        Util.waitUntilElementIsVisible(this.copySharedLinkButton);
+        return this.copySharedLinkButton.click();
+    }
+
+    shareToggleButtonIsChecked() {
+        Util.waitUntilElementIsPresent(this.shareToggleChecked);
+    }
+
+    shareToggleButtonIsDisabled() {
+        Util.waitUntilElementIsPresent(this.shareToggleDisabled);
+    }
+
+    shareToggleButtonIsUnchecked() {
+        Util.waitUntilElementIsVisible(this.shareToggleUnchecked);
+    }
+
+    checkNotificationWithMessage(message) {
+        Util.waitUntilElementIsVisible(
+            element(by.cssContainingText('simple-snack-bar', message))
+        );
+    }
+
+    waitForNotificationToClose() {
+        Util.waitUntilElementIsStale(element(by.css('simple-snack-bar')));
+    }
+
+    dialogIsClosed() {
+        Util.waitUntilElementIsStale(this.shareDialog);
+    }
+
+    clickDateTimePickerButton() {
+        Util.waitUntilElementIsVisible(this.timeDatePickerButton);
+        this.timeDatePickerButton.click();
+    }
+
+    setDefaultMonth() {
+        Util.waitUntilElementIsVisible(this.monthPicker);
+        this.monthPicker.element(by.css('.mat-datetimepicker-calendar-body-today')).click();
+    }
+
+    setDefaultHour() {
+        const selector = '.mat-datetimepicker-clock-cell:not(.mat-datetimepicker-clock-cell-disabled)';
+        Util.waitUntilElementIsVisible(this.clockPicker);
+        this.hoursPicker.all(by.css(selector)).first().click();
+    }
+
+    setDefaultMinutes() {
+        const selector = '.mat-datetimepicker-clock-cell:not(.mat-datetimepicker-clock-cell-disabled)';
+        Util.waitUntilElementIsVisible(this.minutePicker);
+        this.minutePicker.all(by.css(selector)).first().click();
+    }
+
+    dateTimePickerDialogIsClosed() {
+        Util.waitUntilElementIsStale(element(by.css('mat-datetimepicker-content')));
+    }
+
+    getExpirationDate() {
+        return this.expirationDateInput.getAttribute('value');
+    }
+
+    expirationDateInputHasValue(value) {
+        Util.waitUntilElementHasValue(this.expirationDateInput, value);
+    }
+
+    confirmationDialogIsDisplayed() {
+        return Util.waitUntilElementIsVisible(this.confirmationDialog);
+    }
+
+    confirmationDialogIsNotDisplayed() {
+        return Util.waitUntilElementIsNotVisible(this.confirmationDialog);
     }
 }
