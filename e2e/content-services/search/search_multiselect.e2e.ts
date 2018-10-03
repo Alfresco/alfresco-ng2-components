c9aae2bb53c0b62e828798f3b2ee92258f004fcc
Eugenio Romano <eugenio.romano@alfresco.com>
2018-10-03 22:26:12 +0100

    increase timeout waiting search

--- e2e/content-services/search/search_multiselect.e2e.ts
+++ e2e/content-services/search/search_multiselect.e2e.ts
@@ -80,7 +80,7 @@ describe('Search Component - Multi-Select Facet', () => {
 
             txtFileSite = await uploadActions.uploadFile(this.alfrescoJsApi, txtFileInfo.location, txtFileInfo.name, site.entry.guid);
 
-            await browser.driver.sleep(15000);
+            await browser.driver.sleep(20000);
 
             loginPage.loginToContentServicesUsingUserModel(acsUser);
 
@@ -162,7 +162,7 @@ describe('Search Component - Multi-Select Facet', () => {
 
             jpgFile = await uploadActions.uploadFile(this.alfrescoJsApi, jpgFileInfo.location, jpgFileInfo.name, site.entry.guid);
 
-            await browser.driver.sleep(15000);
+            await browser.driver.sleep(20000);
 
             loginPage.loginToContentServicesUsingUserModel(userUploadingImg);
 
@@ -226,7 +226,7 @@ describe('Search Component - Multi-Select Facet', () => {
             });
 
             txtFile = await uploadActions.uploadFile(this.alfrescoJsApi, txtFileInfo.location, txtFileInfo.name, '-my-');
-            await browser.driver.sleep(15000);
+            await browser.driver.sleep(20000);
 
             loginPage.loginToContentServicesUsingUserModel(acsUser);
 
