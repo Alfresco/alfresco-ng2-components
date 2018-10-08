d637109053fb799ce768795d5f81ca7b28900f54
Marizio Vitale <maurizio.vitale@alfresco.com>
2018-10-08 13:26:40 +0100

    add basic structure

--- lib/process-services-cloud/src/lib/process-services-cloud.module.ts
+++ lib/process-services-cloud/src/lib/process-services-cloud.module.ts
@@ -1,22 +1,9 @@
 import { NgModule } from '@angular/core';
-import { TRANSLATION_PROVIDER } from '@alfresco/adf-core';
-import { AppListCloudModule } from './app-list-cloud/app-list-cloud.module';
 
 @NgModule({
-    imports: [
-        AppListCloudModule
-    ],
-    providers: [
-        {
-            provide: TRANSLATION_PROVIDER,
-            multi: true,
-            useValue: {
-                name: 'adf-process-services-cloud',
-                source: 'assets/adf-process-services-cloud'
-            }
-        }
-    ],
-    declarations: [],
-    exports: [AppListCloudModule]
+  imports: [
+  ],
+  declarations: [],
+  exports: []
 })
 export class ProcessServicesCloudModule { }
