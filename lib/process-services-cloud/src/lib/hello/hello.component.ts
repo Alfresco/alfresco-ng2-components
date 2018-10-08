075bc43c58967a842d78fe2c315d244c36378801
Marizio Vitale <maurizio.vitale@alfresco.com>
2018-10-08 16:50:34 +0100

    Add a cloud component as example

---
+++ lib/process-services-cloud/src/lib/hello/hello.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'adf-cloud-hello',
+  templateUrl: './hello.component.html',
+  styleUrls: ['./hello.component.css']
+})
+export class HelloComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
