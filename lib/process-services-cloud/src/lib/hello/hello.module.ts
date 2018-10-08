527ae7f9d846c81b8eea1db6b2b61e276196a96e
Marizio Vitale <maurizio.vitale@alfresco.com>
2018-10-08 16:50:34 +0100

    Add a cloud component as example

--- lib/process-services-cloud/src/lib/hello/hello.module.ts
+++ lib/process-services-cloud/src/lib/hello/hello.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HelloComponent } from './hello.component';
-import { TranslateModule } from '@ngx-translate/core';
+
 @NgModule({
   imports: [
-    CommonModule,
-    TranslateModule
+    CommonModule
   ],
   declarations: [HelloComponent],
   exports: [HelloComponent]
