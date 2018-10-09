8fefbc7acc9c76bd158e9a9b2c5103e674243c39
Marizio Vitale <maurizio.vitale@alfresco.com>
2018-10-09 14:53:06 +0100

    add the app-list-cloud-component add the app-details-cloud-component

---
+++ lib/process-services-cloud/src/lib/app-list-cloud/app-list-cloud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'adf-cloud-app-list-cloud',
+  templateUrl: './app-list-cloud.component.html',
+  styleUrls: ['./app-list-cloud.component.css']
+})
+export class AppListCloudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
