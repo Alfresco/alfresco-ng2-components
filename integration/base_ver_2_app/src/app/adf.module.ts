c7a6b8085e8e5fdebb3af4a42a844344918c7f41
Marizio Vitale <maurizio.vitale@alfresco.com>
2018-10-09 08:45:13 +0100

    remove useless codes

--- integration/base_ver_2_app/src/app/adf.module.ts
+++ integration/base_ver_2_app/src/app/adf.module.ts
@@ -3,15 +3,13 @@ import { NgModule } from '@angular/core';
 // ADF modules
 import { ContentModule } from '@alfresco/adf-content-services';
 import { ProcessModule } from '@alfresco/adf-process-services';
-import { ProcessCloudModule } from '@alfresco/adf-process-services-cloud';
 import { CoreModule } from '@alfresco/adf-core';
 
 export function modules() {
   return [
       CoreModule,
       ContentModule,
-      ProcessModule,
-      ProcessCloudModule
+      ProcessModule
   ];
 }
 
