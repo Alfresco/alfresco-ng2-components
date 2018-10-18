1ab76c9d0af745c19d91580fb9604ac4ddcb35b8
VitoAlbano <vito.albano.123@gmail.com>
2018-10-19 00:55:11 +0100

    [ADF-3538] forcing update of app-list file

--- lib/process-services-cloud/src/lib/app-list-cloud/components/app-list-cloud.component.ts
+++ lib/process-services-cloud/src/lib/app-list-cloud/components/app-list-cloud.component.ts
@@ -26,6 +26,7 @@
      templateUrl: './app-list-cloud.component.html',
      styleUrls: ['./app-list-cloud.component.scss']
  })
+
  export class AppListCloudComponent implements OnInit, AfterContentInit {
 
      public static LAYOUT_LIST: string = 'LIST';
