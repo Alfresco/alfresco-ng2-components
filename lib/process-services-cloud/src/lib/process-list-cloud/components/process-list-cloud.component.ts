61925f95e5127ed4a7a07b432def688b6b1bf629
VitoAlbano <vito.albano.123@gmail.com>
2018-10-26 16:42:20 +0100

    [ADF-3539] missed import

--- lib/process-services-cloud/src/lib/process-list-cloud/components/process-list-cloud.component.ts
+++ lib/process-services-cloud/src/lib/process-list-cloud/components/process-list-cloud.component.ts
@@ -8,6 +8,7 @@ import { BehaviorSubject } from 'rxjs';
 import { processCloudPresetsDefaultModel } from '../models/process-cloud-preset.model';
 import { ProcessQueryCloudRequestModel } from '../models/process-cloud-query-request.model';
 import { ProcessListCloudSortingModel } from '../models/process-list-sorting.model';
+import { MinimalNodeEntity } from 'alfresco-js-api';
 @Component({
     selector: 'adf-cloud-process-list',
     templateUrl: './process-list-cloud.component.html',
