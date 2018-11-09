c26b7300db0f6ea6868e91764f415b68be44b5fe
Denys Vuika <denys.vuika@gmail.com>
2018-11-09 11:34:45 +0000

    upgrade Angular CLI

--- demo-shell/src/polyfills.ts
+++ demo-shell/src/polyfills.ts
@@ -55,7 +55,7 @@ import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /** Evergreen browsers require these. */
 import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
+
 import 'core-js/es6/array';
 import 'core-js/es7/array';
 
