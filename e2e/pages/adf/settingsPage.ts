aaf709498a40b34ffa57b7405e6dc0f50d9268e9
Roxana Gherghelas <Roxana.Gherghelas@ness.com>
2018-11-12 13:10:56 +0200

    remove comment

--- e2e/pages/adf/settingsPage.ts
+++ e2e/pages/adf/settingsPage.ts
@@ -110,7 +110,6 @@ export class SettingsPage {
         this.goToSettingsPage();
         this.setProvider(this.bpm.option, this.bpm.text);
         Util.waitUntilElementIsVisible(this.bpmText);
-        // expect(this.ecmText.isPresent()).toBe(false);
         Util.waitUntilElementIsNotOnPage(this.ecmText);
         await this.clickSsoRadioButton();
         await this.setProcessServicesURL(processServiceURL);
