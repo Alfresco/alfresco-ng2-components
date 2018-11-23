6598f2f60c01eeef8c9be1b511a6d172c6f396ff
Eugenio Romano <eugenio.romano@alfresco.com>
2018-11-23 09:49:00 +0000

    tslint fix

--- lib/process-services-cloud/src/lib/process-list-cloud/components/process-list-cloud.component.spec.ts
+++ lib/process-services-cloud/src/lib/process-list-cloud/components/process-list-cloud.component.spec.ts
@@ -174,7 +174,7 @@ describe('ProcessListCloudComponent', () => {
             }
         });
         let rowEvent = new DataRowEvent(row, null);
-        component.rowClick.subscribe( (taskId) => {
+        component.rowClick.subscribe((taskId) => {
             expect(taskId).toEqual('999');
             expect(component.getCurrentId()).toEqual('999');
             done();
