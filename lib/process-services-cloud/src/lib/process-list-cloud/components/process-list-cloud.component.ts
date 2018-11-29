08c704c6dd0ebf096909cb23321eb75e48dd4bab
Eugenio Romano <eugenio.romano@alfresco.com>
2018-11-29 14:55:17 +0000

    fix lint extension and process cloud

--- lib/process-services-cloud/src/lib/process-list-cloud/components/process-list-cloud.component.ts
+++ lib/process-services-cloud/src/lib/process-list-cloud/components/process-list-cloud.component.ts
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import { Component, ViewEncapsulation, OnChanges, AfterContentInit, ContentChild, Output, EventEmitter, SimpleChanges, SimpleChange, Input } from '@angular/core';
 import { DataTableSchema, PaginatedComponent,
          EmptyCustomContentDirective, AppConfigService,
