e75eec24b729bd4a8a4078364ab31c910a4f9c39
Deepak Paul <deepak.paul@muraai.com>
2018-11-29 20:52:28 +0530

    [ADF-3775] Created breadcrumb component for layout demo

---
+++ demo-shell/src/app/components/app-layout/cloud/cloud-breadcrumb-component.ts
@@ -0,0 +1,42 @@
+/*!
+ * @license
+ * Copyright 2016 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-cloud-breadcrumbs',
+  templateUrl: './cloud-breadcrumb-component.html',
+  styleUrls: ['cloud-breadcrumb-component.scss']
+})
+export class CloudBreadcrumbsComponent implements OnInit {
+  applicationName: string;
+  filterName: string;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.parent.params.subscribe(params => {
+      this.applicationName = params.applicationName;
+    });
+    this.route.queryParams.subscribe(params => {
+      if (params.filterName) {
+        this.filterName = params.filterName;
+      }
+    });
+  }
+}
