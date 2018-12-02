efcea7486d7f0d8e59e0eb96f9b1ba45db82beea
Eugenio Romano <eugenio.romano@alfresco.com>
2018-12-02 16:47:48 +0000

    disable browser execute

--- e2e/content-services/search/search_component.e2e.ts
+++ e2e/content-services/search/search_component.e2e.ts
@@ -112,6 +112,7 @@ describe('Search component - Search Bar', () => {
 
     beforeEach(() => {
         contentServicesPage.goToDocumentList();
+        browser.refresh();
     });
 
     it('[C272798] Search bar should be visible', () => {
