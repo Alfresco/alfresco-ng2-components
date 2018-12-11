78a82057dceddc6006e59a80238d48f9a7fdbd2d
Eugenio Romano <eugenio.romano@alfresco.com>
2018-12-11 13:13:45 +0000

    create subfolder

--- scripts/download-build-in-cs.js
+++ scripts/download-build-in-cs.js
@@ -1,16 +1,20 @@
 var program = require('commander');
 var fs = require('fs');
 var path = require('path');
+var ncp = require('ncp').ncp;
+var rimraf = require('rimraf');
+
+ncp.limit = 16;
 
 replaceHrefInIndex = (href) => {
-    fs.readFile(`demo-shell/${href}/index.html`, 'utf8', function (err, data) {
+    fs.readFile(`demo-shell/dist/${href}/index.html`, 'utf8', function (err, data) {
         if (err) {
             return console.log(err);
         }
 
         let result = data.replace(`base href="/"`, `base href=\"/${href}/\"`);
 
-        fs.writeFile(`demo-shell/${href}/index.html`, result, 'utf8', function (err) {
+        fs.writeFile(`demo-shell/dist/${href}/index.html`, result, 'utf8', function (err) {
             if (err) return console.log(err);
         });
     });
@@ -25,15 +29,35 @@ async function main() {
         .parse(process.argv);
 
 
-    let output = path.resolve(__dirname, '../demo-shell/', program.baseHref);
-    console.log('output ' + output);
+    let outputTemp = path.resolve(__dirname, '../demo-shell/', program.baseHref);
+    console.log('output ' + outputTemp);
 
-    fs.rename(path.resolve(__dirname, '../demo-shell/dist'),output,  (err)=> {
+    let distFolder = path.resolve(__dirname, '../demo-shell/dist');
+
+    fs.rename(distFolder, outputTemp, (err) => {
         if (err) throw err;
         console.log('renamed complete');
+
+        let distFolderNew = path.resolve(__dirname, '../demo-shell/dist', program.baseHref);
+
+        if (!fs.existsSync(distFolderNew)){
+            fs.mkdirSync(distFolder);
+            fs.mkdirSync(distFolderNew);
+        }
+
+        ncp(outputTemp, distFolderNew, (err) => {
+            if (err) {
+                return console.error(err);
+            }
+
+            replaceHrefInIndex(program.baseHref);
+
+            rimraf(outputTemp,  ()=> { console.log('output temp removed'); });
+
+        });
+
     });
 
-    replaceHrefInIndex(program.baseHref);
 }
 
 main();
