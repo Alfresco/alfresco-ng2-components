4f5c38535d6cb88f8903f684928a3a51aec4ac4e
Roxana Gherghelas <Roxana.Gherghelas@ness.com>
2018-12-18 11:06:07 +0200

    modify method

--- e2e/pages/adf/demo-shell/tasksCloudDemoPage.ts
+++ e2e/pages/adf/demo-shell/tasksCloudDemoPage.ts
@@ -78,6 +78,7 @@ export class TasksCloudDemoPage {
         this.clickOnCreateButton();
         this.newTaskButtonIsDisplayed();
         this.newTaskButton.click();
+        return this;
     }
 
     createButtonIsDisplayed() {
