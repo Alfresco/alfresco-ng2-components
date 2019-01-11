ebd291039bae67fb5940b1e7b70c97a547ce0865
davidcanonieto <david.cano.nieto@gmail.com>
2019-01-11 09:31:33 +0000

    [ADF-3783] Create Task List Cloud Demo page

--- demo-shell/src/app/app.routes.ts
+++ demo-shell/src/app/app.routes.ts
@@ -340,6 +340,11 @@ export const appRoutes: Routes = [
                 loadChildren: 'app/components/task-list-demo/task-list.module#AppTaskListModule'
             },
             {
+                path: 'task-list-cloud',
+                canActivate: [AuthGuard],
+                loadChildren: 'app/components/task-list-cloud/task-list-cloud.module#TaskListCloudDemoModule'
+            },
+            {
                 path: 'process-list',
                 canActivate: [AuthGuardBpm],
                 loadChildren: 'app/components/process-list-demo/process-list.module#AppProcessListModule'
