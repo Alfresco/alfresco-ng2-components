61d19db5f6f242c1f4c023935eea5f4304aeda3e
butuflorin <florin.butu@ness.com>
2019-01-14 11:04:10 +0200

    [ADF-3900] Implement tests for Universal Icons component

---
+++ e2e/core/icons-component.e2e.ts
@@ -0,0 +1,62 @@
+/*!
+ * @license
+ * Copyright 2016 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { LoginPage } from '../pages/adf/loginPage';
+import { NavigationBarPage } from '../pages/adf/navigationBarPage';
+import { IconsPage } from '../pages/adf/iconsPage';
+import { AcsUserModel } from '../models/ACS/acsUserModel';
+
+import TestConfig = require('../test.config');
+import AlfrescoApi = require('alfresco-js-api-node');
+
+describe('Universal Icon component', function () {
+
+    let loginPage = new LoginPage();
+    let acsUser = new AcsUserModel();
+    let navigationBarPage = new NavigationBarPage();
+    let iconsPage = new IconsPage();
+
+    beforeAll(async (done) => {
+        this.alfrescoJsApi = new AlfrescoApi({
+            provider: 'ECM',
+            hostEcm: TestConfig.adf.url
+        });
+
+        await this.alfrescoJsApi.login(TestConfig.adf.adminEmail, TestConfig.adf.adminPassword);
+
+        await this.alfrescoJsApi.core.peopleApi.addPerson(acsUser);
+
+        loginPage.loginToContentServicesUsingUserModel(acsUser);
+
+        done();
+    });
+
+    beforeEach(async (done) => {
+        navigationBarPage.navigateToIconsPage();
+        done();
+    });
+
+    it('Should display the icons on the page', () => {
+
+        expect(iconsPage.locateLigatureIcon('alarm').isDisplayed()).toBe(true, 'Ligature icon is not displayed');
+
+        expect(iconsPage.locateCustomIcon('adf:move_file').isDisplayed()).toBe(true, 'Named icon is not displayed');
+
+        expect(iconsPage.locateCustomIcon('image/gif').isDisplayed()).toBe(true, 'Thumbnail service icon is not displayed');
+    });
+
+});
