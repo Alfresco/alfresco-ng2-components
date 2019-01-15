c88b92bb7e98018a4c1e2e2b781e205cf545b8c5
Cilibiu Bogdan <pionnegru@users.noreply.github.com>
2019-01-15 18:28:41 +0200

    [ADF-3897] Upload Dialog - upload file error tooltip (#4148)

--- lib/core/services/upload.service.ts
+++ lib/core/services/upload.service.ts
@@ -250,6 +250,7 @@ export class UploadService {
 
     private onUploadError(file: FileModel, error: any): void {
         if (file) {
+            file.errorCode = ( error || {} ).status;
             file.status = FileUploadStatus.Error;
             this.totalError++;
 
