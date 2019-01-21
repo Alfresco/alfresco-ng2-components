1d71d57a35d64da80ca0f7ac891ef18bdabe20f1
Deepak Paul <deepak.paul@muraai.com>
2019-01-21 16:42:30 +0530

    [ADF-3934] Changed preset user selection logic Added title input

--- lib/process-services-cloud/src/lib/task/start-task/components/people-cloud/people-cloud.component.ts
+++ lib/process-services-cloud/src/lib/task/start-task/components/people-cloud/people-cloud.component.ts
@@ -16,7 +16,7 @@
  */
 
 import { FormControl } from '@angular/forms';
-import { Component, OnInit, Output, EventEmitter, ViewEncapsulation, Input, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewEncapsulation, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { Observable, of, BehaviorSubject } from 'rxjs';
 import { switchMap, debounceTime, distinctUntilChanged, mergeMap, tap, filter } from 'rxjs/operators';
 import { FullNamePipe, IdentityUserModel, IdentityUserService } from '@alfresco/adf-core';
@@ -39,7 +39,7 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
     encapsulation: ViewEncapsulation.None
 })
 
-export class PeopleCloudComponent implements OnInit {
+export class PeopleCloudComponent implements OnInit, AfterViewInit {
 
     static MODE_SINGLE = 'single';
     static MODE_MULTIPLE = 'multiple';
@@ -64,6 +64,10 @@ export class PeopleCloudComponent implements OnInit {
     @Input()
     preSelectUsers: IdentityUserModel[];
 
+    /** Title for input control.  */
+    @Input()
+    title: string
+
     /** Emitted when a user is selected. */
     @Output()
     selectUser: EventEmitter<IdentityUserModel> = new EventEmitter<IdentityUserModel>();
@@ -79,7 +83,8 @@ export class PeopleCloudComponent implements OnInit {
     @ViewChild('userInput')
     private userInput: ElementRef<HTMLInputElement>;
 
-    private _searchUsers: IdentityUserModel[] = [];
+    private selectedUsers: IdentityUserModel[] = [];
+    private searchUsers: IdentityUserModel[] = [];
     private selectedUsersSubject: BehaviorSubject<IdentityUserModel[]>;
     private searchUsersSubject: BehaviorSubject<IdentityUserModel[]>;
     selectedUsers$: Observable<IdentityUserModel[]>;
@@ -94,23 +99,26 @@ export class PeopleCloudComponent implements OnInit {
     isFocused: boolean;
 
     constructor(private identityUserService: IdentityUserService) {
-        this.searchUsersSubject = new BehaviorSubject<IdentityUserModel[]>(this._searchUsers);
+        this.searchUsersSubject = new BehaviorSubject<IdentityUserModel[]>(this.searchUsers);
         this.searchUsers$ = this.searchUsersSubject.asObservable();
+        this.selectedUsersSubject = new BehaviorSubject<IdentityUserModel[]>(this.selectedUsers);
+        this.selectedUsers$ = this.selectedUsersSubject.asObservable();
     }
 
     ngOnInit() {
-        this.selectedUsersSubject = new BehaviorSubject<IdentityUserModel[]>(this.preSelectUsers);
-        this.selectedUsers$ = this.selectedUsersSubject.asObservable();
-
-        if (this.hasPreSelectUsers()) {
-            this.loadPreSelectUsers();
-        }
-
         this.initSearch();
 
         if (this.appName) {
             this.disableSearch();
             this.loadClientId();
+        }
+    }
+
+    ngAfterViewInit() {
+        if (this.hasPreSelectUsers()) {
+            setTimeout(() => {
+                this.loadPreSelectUsers();
+            });
         }
     }
 
@@ -122,6 +130,9 @@ export class PeopleCloudComponent implements OnInit {
             tap((value) => {
                 if (value) {
                     this.setError();
+                    if (this.isSingleMode() && this.hasSelectedUsers()) {
+                        this.resetSingleModeSelection();
+                    }
                 } else {
                     this.clearError();
                 }
@@ -150,8 +161,8 @@ export class PeopleCloudComponent implements OnInit {
                 }
             })
         ).subscribe((user) => {
-            this._searchUsers.push(user);
-            this.searchUsersSubject.next(this._searchUsers);
+            this.searchUsers.push(user);
+            this.searchUsersSubject.next(this.searchUsers);
         });
     }
 
@@ -168,8 +179,8 @@ export class PeopleCloudComponent implements OnInit {
     }
 
     private isUserAlreadySelected(user: IdentityUserModel): boolean {
-        if (this.preSelectUsers && this.preSelectUsers.length > 0) {
-            const result = this.preSelectUsers.find((selectedUser) => {
+        if (this.selectedUsers && this.selectedUsers.length > 0) {
+            const result = this.selectedUsers.find((selectedUser) => {
                 return selectedUser.id === user.id || selectedUser.email  === user.email;
             });
 
@@ -179,9 +190,11 @@ export class PeopleCloudComponent implements OnInit {
     }
 
     private loadPreSelectUsers() {
-        if (!this.isMultipleMode()) {
+        if (this.isSingleMode()) {
             this.searchUserCtrl.setValue(this.preSelectUsers[0]);
-            this.preSelectUsers = [];
+            this.onSelect(this.preSelectUsers[0]);
+        } else {
+            this.preSelectUsers.forEach((user) => this.onSelect(user));
         }
     }
 
@@ -194,18 +207,16 @@ export class PeopleCloudComponent implements OnInit {
     }
 
     onSelect(user: IdentityUserModel) {
+
+        if (!this.isUserAlreadySelected(user)) {
+            this.selectedUsers.push(user);
+            this.selectedUsersSubject.next(this.selectedUsers);
+            this.selectUser.emit(user);
+        }
+
         if (this.isMultipleMode()) {
-
-            if (!this.isUserAlreadySelected(user)) {
-                this.preSelectUsers.push(user);
-                this.selectedUsersSubject.next(this.preSelectUsers);
-                this.selectUser.emit(user);
-            }
-
             this.userInput.nativeElement.value = '';
             this.searchUserCtrl.setValue('');
-        } else {
-            this.selectUser.emit(user);
         }
 
         this.clearError();
@@ -214,9 +225,9 @@ export class PeopleCloudComponent implements OnInit {
 
     onRemove(user: IdentityUserModel) {
         this.removeUser.emit(user);
-        const indexToRemove = this.preSelectUsers.findIndex((selectedUser) => { return selectedUser.id === user.id; });
-        this.preSelectUsers.splice(indexToRemove, 1);
-        this.selectedUsersSubject.next(this.preSelectUsers);
+        const indexToRemove = this.selectedUsers.findIndex((selectedUser) => { return selectedUser.id === user.id; });
+        this.selectedUsers.splice(indexToRemove, 1);
+        this.selectedUsersSubject.next(this.selectedUsers);
     }
 
     getDisplayName(user): string {
@@ -227,13 +238,17 @@ export class PeopleCloudComponent implements OnInit {
         return this.mode === PeopleCloudComponent.MODE_MULTIPLE;
     }
 
+    isSingleMode(): boolean {
+        return this.mode !== PeopleCloudComponent.MODE_MULTIPLE;
+    }
+
     private hasPreSelectUsers(): boolean {
         return this.preSelectUsers && this.preSelectUsers.length > 0;
     }
 
     private resetSearchUsers() {
-        this._searchUsers = [];
-        this.searchUsersSubject.next(this._searchUsers);
+        this.searchUsers = [];
+        this.searchUsersSubject.next(this.searchUsers);
     }
 
     private setError() {
@@ -262,6 +277,20 @@ export class PeopleCloudComponent implements OnInit {
 
     private enableSearch() {
         this.searchUserCtrl.enable();
+    }
+
+    private resetSingleModeSelection() {
+        if (this.hasSelectedUsers()) {
+            this.onRemove(this.searchUsers[0]);
+        } 
+    }
+
+    private hasSelectedUsers(): boolean {
+        return this.selectedUsers && this.selectedUsers.length > 0;
+    }
+
+    getValue(): IdentityUserModel[] {
+        return this.selectedUsers;
     }
 
 }
