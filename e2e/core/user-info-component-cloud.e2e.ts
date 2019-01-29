02947c8479b37b9d96871afd2f2512089cd3e302
Roxana Gherghelas <Roxana.Gherghelas@ness.com>
2019-01-29 14:24:40 +0200

    add hostIdentity parameter

--- e2e/core/user-info-component-cloud.e2e.ts
+++ e2e/core/user-info-component-cloud.e2e.ts
@@ -36,7 +36,7 @@ describe('User Info - SSO', () => {
         await identityService.init(TestConfig.adf.adminEmail, TestConfig.adf.adminPassword);
         identityUser = await identityService.createIdentityUser();
         silentLogin = false;
-        settingsPage.setProviderBpmSso(TestConfig.adf.hostBPM, TestConfig.adf.hostSso, silentLogin);
+        settingsPage.setProviderBpmSso(TestConfig.adf.hostBPM, TestConfig.adf.hostSso, TestConfig.adf.hostIdentity, silentLogin);
         loginSSOPage.clickOnSSOButton();
         browser.ignoreSynchronization = true;
         loginSSOPage.loginAPS(identityUser['0'].username, identityUser['0'].password);
