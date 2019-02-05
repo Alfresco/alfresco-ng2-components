547371130bac016c43cb4bcfeaa8378e31292d7a
Silviu Popa <silviucpopa@gmail.com>
2019-02-05 13:45:56 +0200

    [ADF-4009] - fix PR changes and add people input filter

--- demo-shell/src/app/components/app-layout/cloud/people-groups-cloud-demo.component.ts
+++ demo-shell/src/app/components/app-layout/cloud/people-groups-cloud-demo.component.ts
@@ -17,7 +17,7 @@
 
 import { Component, ViewEncapsulation } from '@angular/core';
 import { PeopleCloudComponent, GroupCloudComponent, GroupModel } from '@alfresco/adf-process-services-cloud';
-import { MatRadioChange } from '@angular/material';
+import { MatRadioChange, MatChipInputEvent } from '@angular/material';
 
 @Component({
     selector: 'app-people-groups-cloud',
@@ -28,35 +28,28 @@ import { MatRadioChange } from '@angular/material';
 export class PeopleGroupCloudDemoComponent {
 
     peopleMode: string = PeopleCloudComponent.MODE_SINGLE;
-    preSelectUsers: any = [];
+    preSelectUsers: string[] = [];
+    peopleRoles: string[] = [];
 
     groupMode: string = GroupCloudComponent.MODE_SINGLE;
-    preSelectGroup: any = [];
-    selectedGroupList: any = [];
-    groupRoles: any;
+    preSelectGroup: string[] = [];
+    selectedGroupList: GroupModel[] = [];
+    groupRoles: string[];
 
-    setPeoplePreselectValue(value: string) {
-        if (this.isStringArray(value)) {
-            this.preSelectUsers = JSON.parse(value);
-        } else if (value.length === 0) {
-            this.preSelectUsers = [];
-        }
+    setPeoplePreselectValue(event: any) {
+        this.preSelectUsers = this.getArrayFromString(event.target.value);
     }
 
-    setGroupsPreselectValue(value: string) {
-        if (this.isStringArray(value)) {
-            this.preSelectGroup = JSON.parse(value);
-        } else if (value.length === 0) {
-            this.preSelectGroup = [];
-        }
+    setGroupsPreselectValue(event: any) {
+        this.preSelectGroup = this.getArrayFromString(event.target.value);
     }
 
-    setGroupRoles(value: string) {
-        if (this.isStringArray(value)) {
-            this.groupRoles = JSON.parse(value);
-        } else {
-            this.groupRoles = [];
-        }
+    setPeopleRoles(event:any) {
+        this.peopleRoles = this.getArrayFromString(event.target.value);
+    }
+
+    setGroupRoles(event: any) {
+        this.groupRoles = this.getArrayFromString(event.target.value);
     }
 
     onChangePeopleMode(event: MatRadioChange) {
@@ -73,6 +66,15 @@ export class PeopleGroupCloudDemoComponent {
             return Array.isArray(result);
         } catch (e) {
             return false;
+        }
+    }
+
+    private getArrayFromString(value: string) {
+
+        if (this.isStringArray(value)) {
+            return JSON.parse(value);
+        } else {
+            return [];
         }
     }
 
