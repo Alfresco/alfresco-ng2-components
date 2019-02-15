c543a56bff9c84cdbd8c26646bc695c5194145d8
gmandakini <geeta.ayyalasomayajula@alfresco.com>
2019-02-15 17:14:55 +0000

    linting fix

--- e2e/pages/adf/dataTablePage.ts
+++ e2e/pages/adf/dataTablePage.ts
@@ -38,7 +38,7 @@ export class DataTablePage {
     tableBody = element.all(by.css(`div[class='adf-datatable-body']`)).first();
     spinner = element(by.css('mat-progress-spinner'));
     rows = by.css(`adf-datatable div[class*='adf-datatable-body'] div[class*='adf-datatable-row']`);
-    allColumns = element.all(by.css('div[data-automation-id*="auto_id_entry."]'))
+    allColumns = element.all(by.css('div[data-automation-id*="auto_id_entry."]'));
 
     constructor(rootElement: ElementFinder = element(by.css('adf-datatable'))) {
         this.rootElement = rootElement;
