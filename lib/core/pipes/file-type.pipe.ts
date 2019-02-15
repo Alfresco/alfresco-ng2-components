bea0e6e4b355be31dfbd441d60a5c57193dff918
Silviu Popa <silviucpopa@gmail.com>
2019-02-15 11:41:06 +0200

    [ADF-3863] DocumentList - change FiletType pipe to pure

--- lib/core/pipes/file-type.pipe.ts
+++ lib/core/pipes/file-type.pipe.ts
@@ -1,6 +1,6 @@
 /*!
  * @license
- * Copyright 2016 Alfresco Software, Ltd.
+ * Copyright 2019 Alfresco Software, Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@ import { PipeTransform, Pipe } from '@angular/core';
 
 @Pipe({
     name: 'fileType',
-    pure: false
+    pure: true
 })
 export class FileTypePipe implements PipeTransform {
 
