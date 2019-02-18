cfdbc8c67113789a7766b7b1da35a8544f040638
Silviu Popa <silviucpopa@gmail.com>
2019-02-18 17:51:34 +0200

    [ADF-4048] PeopleCloudComponent - focus input when preselected user exists

--- lib/process-services-cloud/src/lib/task/start-task/components/people-cloud/people-cloud.component.ts
+++ lib/process-services-cloud/src/lib/task/start-task/components/people-cloud/people-cloud.component.ts
@@ -288,6 +288,7 @@ export class PeopleCloudComponent implements OnInit, OnChanges {
         } else {
             this.searchUserCtrl.setValue(this.preSelectUsers[0]);
         }
+        this.userInput.nativeElement.click();
     }
 
     private async loadSinglePreselectUser() {
@@ -301,6 +302,7 @@ export class PeopleCloudComponent implements OnInit, OnChanges {
         this.checkPreselectValidationErrors();
         this.preSelectUsers = [...users];
         this.selectedUsersSubject.next(users);
+        this.userInput.nativeElement.click();
     }
 
     private checkPreselectValidationErrors() {
