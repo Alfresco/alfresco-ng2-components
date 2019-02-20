6feabe9c60d90c62e4b991722d65bf38ebc6a902
Eugenio Romano <eugenio.romano@alfresco.com>
2019-02-20 10:53:01 +0000

    fix script

--- scripts/affected-libs.sh
+++ scripts/affected-libs.sh
@@ -4,11 +4,18 @@ eval BRANCH_NAME=""
 eval HEAD_SHA_BRANCH=""
 eval SHA_2="HEAD"
 eval DIRECTORY="tmp"
+eval GNU=false
 
 show_help() {
     echo "Usage: affected-libs.sh"
     echo ""
     echo "-b branch name"
+    echo "-gnu for gnu"
+}
+
+gnu_mode() {
+    echo "====== GNU MODE ====="
+    GNU=true
 }
 
 branch_name(){
@@ -18,9 +25,17 @@ branch_name(){
 while [[ $1  == -* ]]; do
     case "$1" in
       -b)  branch_name $2; shift 2;;
+      -gnu) gnu_mode; shift;;
       -*) echo "invalid option: $1" 1>&2; show_help; exit 0;;
     esac
 done
+
+if $GNU; then
+ sedi='-i'
+else
+ sedi=('-i' '')
+fi
+
 
 if [[ "$BRANCH_NAME" == "" ]]
 then
@@ -47,10 +62,10 @@ cat $DIRECTORY/deps.txt
 #echo "extensions" > deps.txt
 
 #clean file
-sed -i '/^$/d'  ./$DIRECTORY/deps.txt
-sed -i '/alfresco-components/d' ./$DIRECTORY/deps.txt
-sed -i '/nx affected:libs/d' ./$DIRECTORY/deps.txt
-sed -i '/^$/d'  ./$DIRECTORY/deps.txt
+sed "${sedi[@]}" '/^$/d'  ./$DIRECTORY/deps.txt
+sed "${sedi[@]}" '/alfresco-components/d' ./$DIRECTORY/deps.txt
+sed "${sedi[@]}" '/nx affected:libs/d' ./$DIRECTORY/deps.txt
+sed "${sedi[@]}" '/^$/d'  ./$DIRECTORY/deps.txt
 
 #read result from file
 while IFS= read -r var
@@ -111,4 +126,4 @@ do
     fi
 done
 
-echo "${AFFECTED_LIBS}"
+echo "AFFECTED LIBS =>  ${AFFECTED_LIBS}"
