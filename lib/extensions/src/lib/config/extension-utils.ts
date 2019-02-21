ba66ffa411dc3ece0fe28f1254e17ac7bf36cbe2
Eugenio Romano <eugenio.romano@alfresco.com>
2019-02-21 12:27:15 +0000

    extension using map

--- lib/extensions/src/lib/config/extension-utils.ts
+++ lib/extensions/src/lib/config/extension-utils.ts
@@ -146,5 +146,5 @@ export function mergeArrays(left: any[], right: any[]): any[] {
         }
     });
 
-    return Object.keys(map).map(key => map[key]).concat(result);
+    return Object.keys(map).map((key) => map[key]).concat(result);
 }
