3bdd5f128bce84b3d7cd24555ba43449e6a67495
davidcanonieto <david.cano.nieto@gmail.com>
2019-02-28 12:16:45 +0100

    [ADF-4122] Fix core unit tests

--- lib/core/datatable/components/datatable/datatable.component.spec.ts
+++ lib/core/datatable/components/datatable/datatable.component.spec.ts
@@ -140,7 +140,7 @@ describe('DataTable', () => {
         fixture.detectChanges();
 
         expect(element.querySelector('.adf-datatable-card')).toBeNull();
-        expect(element.querySelector('.adf-datatable')).not.toBeNull();
+        expect(element.querySelector('.adf-datatable-list')).not.toBeNull();
     });
 
     it('should hide the header if showHeader is false', () => {
@@ -1021,12 +1021,12 @@ describe('Accesibility', () => {
         });
 
         fixture.detectChanges();
-        const datatableAttributes = element.querySelector('.adf-datatable').attributes;
-        const datatableHeaderAttributes = element.querySelector('.adf-datatable .adf-datatable-header').attributes;
-        const datatableHeaderCellAttributes = element.querySelector('.adf-datatable-table-cell-header').attributes;
+        const datatableAttributes = element.querySelector('.adf-datatable-list').attributes;
+        const datatableHeaderAttributes = element.querySelector('.adf-datatable-list .adf-datatable-header').attributes;
+        const datatableHeaderCellAttributes = element.querySelector('.adf-datatable-cell-header').attributes;
         const datatableBodyAttributes = element.querySelector('.adf-datatable-body').attributes;
         const datatableBodyRowAttributes = element.querySelector('.adf-datatable-body .adf-datatable-row').attributes;
-        const datatableBodyCellAttributes = element.querySelector('.adf-datatable-body .adf-datatable-table-cell').attributes;
+        const datatableBodyCellAttributes = element.querySelector('.adf-datatable-body .adf-datatable-cell').attributes;
 
         expect(datatableAttributes.getNamedItem('role').value).toEqual('grid');
         expect(datatableHeaderAttributes.getNamedItem('role').value).toEqual('rowgroup');
