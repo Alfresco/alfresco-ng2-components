11e821a5508f2932f57ab0fccfecdb8f957c06a2
Roxana Gherghelas <Roxana.Gherghelas@ness.com>
2019-03-06 11:05:02 +0200

    update exports

--- lib/testing/src/lib/process-services-cloud/pages/public-api.ts
+++ lib/testing/src/lib/process-services-cloud/pages/public-api.ts
@@ -5,3 +5,6 @@
 export * from './login-sso.page';
 export * from '../../core/pages/user-info.page';
 export * from '../app/app-list-cloud.page';
+export * from './user-info-dialog.page';
+export * from './start-tasks-cloud-component.page';
+export * from './task-details-cloud-component.page';
