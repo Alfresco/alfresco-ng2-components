52c87072b52607123ab336029a78122f80b1c28d
Roxana Gherghelas <Roxana.Gherghelas@ness.com>
2019-03-12 16:34:39 +0200

    remove user-info-dialog

--- lib/testing/src/lib/process-services-cloud/pages/public-api.ts
+++ lib/testing/src/lib/process-services-cloud/pages/public-api.ts
@@ -5,6 +5,5 @@
 export * from './login-sso.page';
 export * from '../../core/pages/user-info.page';
 export * from '../app/app-list-cloud.page';
-export * from './user-info-dialog.page';
 export * from './start-tasks-cloud-component.page';
 export * from './task-header-cloud-component.page';
