5193a4298356f70912dbb503da2b78cbebd18bc6
Marouan Bentaleb <38426175+marouanbentaleb@users.noreply.github.com>
2019-03-13 14:54:04 +0000

    [ADF-4197] Automation test for redirection after page reload (#4412)
    * [ADF-4197] Automation test for redirection after page reload
    
    * Removing browser sleep

---
+++ e2e/pages/adf/demo-shell/logoutPage.ts
@@ -0,0 +1,28 @@
+/*!
+ * @license
+ * Copyright 2019 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { by, element } from 'protractor';
+import { Util } from '../../../util/util';
+
+export class LogoutPage {
+
+    logoutSection = element(by.css('div[data-automation-id="adf-logout-section"]'));
+
+    checkLogoutSectionIsDisplayed() {
+        return Util.waitUntilElementIsVisible(this.logoutSection);
+    }
+}
