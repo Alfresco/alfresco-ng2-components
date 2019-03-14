e1204d7b4471d26b4810c1c01d4b0e5f1945ba6a
Eugenio Romano <eugenio.romano@alfresco.com>
2019-03-14 12:24:18 +0000

    fix configuration test package

---
+++ lib/testing/index.ts
@@ -0,0 +1,52 @@
+/*!
+ * @license
+ * Copyright 2019 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export * from './about/index';
+export * from './viewer/index';
+export * from './userinfo/index';
+export * from './toolbar/index';
+export * from './settings/index';
+export * from './pagination/index';
+export * from './login/index';
+export * from './language-menu/index';
+export * from './info-drawer/index';
+export * from './data-column/index';
+export * from './datatable/index';
+export * from './context-menu/index';
+export * from './card-view/index';
+export * from './app-config/index';
+export * from './form/index';
+export * from './layout/index';
+export * from './comments/index';
+export * from './buttons-menu/index';
+export * from './sorting-picker/index';
+export * from './templates/index';
+export * from './pipes/index';
+export * from './services/index';
+export * from './directives/index';
+export * from './clipboard/index';
+export * from './dialogs/index';
+export * from './icon/index';
+
+export * from './utils/index';
+export * from './interface/index';
+export * from './models/index';
+export * from './events/index';
+export * from './mock/index';
+export * from './testing';
+
+export * from './core.module';
