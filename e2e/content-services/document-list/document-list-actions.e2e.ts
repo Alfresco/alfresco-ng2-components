cb973d61d5b993b0a382be31728022143cca9f1f
gmandakini <geeta.ayyalasomayajula@alfresco.com>
2019-03-20 10:14:59 +0000

    in progress

--- e2e/content-services/document-list/document-list-actions.e2e.ts
+++ e2e/content-services/document-list/document-list-actions.e2e.ts
@@ -50,11 +50,10 @@ describe('Document List Component - Actions', () => {
         'location': resources.Files.ADF_DOCUMENTS.TEST.file_location
     });
 
-    beforeAll(() => {
-        this.alfrescoJsApi = new AlfrescoApi({
-            provider: 'ECM',
-            hostEcm: TestConfig.adf.url
-        });
+    let alfrescoJsApi = new AlfrescoApi({
+        provider: 'ECM',
+        hostEcm: TestConfig.adf.url
+
     });
 
     describe('File Actions', () => {
@@ -65,12 +64,12 @@ describe('Document List Component - Actions', () => {
         beforeEach(async (done) => {
             acsUser = new AcsUserModel();
             folderName = `TATSUMAKY_${Util.generateRandomString(5)}_SENPOUKYAKU`;
-            await this.alfrescoJsApi.login(TestConfig.adf.adminEmail, TestConfig.adf.adminPassword);
-            await this.alfrescoJsApi.core.peopleApi.addPerson(acsUser);
-            await this.alfrescoJsApi.login(acsUser.id, acsUser.password);
-            pdfUploadedNode = await uploadActions.uploadFile(this.alfrescoJsApi, pdfFileModel.location, pdfFileModel.name, '-my-');
-            testFileNode = await uploadActions.uploadFile(this.alfrescoJsApi, testFileModel.location, testFileModel.name, '-my-');
-            uploadedFolder = await uploadActions.createFolder(this.alfrescoJsApi, folderName, '-my-');
+            await alfrescoJsApi.login(TestConfig.adf.adminEmail, TestConfig.adf.adminPassword);
+            await alfrescoJsApi.core.peopleApi.addPerson(acsUser);
+            await alfrescoJsApi.login(acsUser.id, acsUser.password);
+            pdfUploadedNode = await uploadActions.uploadFile(alfrescoJsApi, pdfFileModel.location, pdfFileModel.name, '-my-');
+            testFileNode = await uploadActions.uploadFile(alfrescoJsApi, testFileModel.location, testFileModel.name, '-my-');
+            uploadedFolder = await uploadActions.createFolder(alfrescoJsApi, folderName, '-my-');
 
             loginPage.loginToContentServicesUsingUserModel(acsUser);
             contentServicesPage.goToDocumentList();
@@ -81,9 +80,9 @@ describe('Document List Component - Actions', () => {
         afterEach(async (done) => {
             try {
                 await this.alfrescoJsApi.login(TestConfig.adf.adminEmail, TestConfig.adf.adminPassword);
-                await uploadActions.deleteFilesOrFolder(this.alfrescoJsApi, pdfUploadedNode.entry.id);
-                await uploadActions.deleteFilesOrFolder(this.alfrescoJsApi, testFileNode.entry.id);
-                await uploadActions.deleteFilesOrFolder(this.alfrescoJsApi, uploadedFolder.entry.id);
+                await uploadActions.deleteFilesOrFolder(alfrescoJsApi, pdfUploadedNode.entry.id);
+                await uploadActions.deleteFilesOrFolder(alfrescoJsApi, testFileNode.entry.id);
+                await uploadActions.deleteFilesOrFolder(alfrescoJsApi, uploadedFolder.entry.id);
             } catch (error) {
             }
             done();
@@ -154,11 +153,11 @@ describe('Document List Component - Actions', () => {
             acsUser = new AcsUserModel();
             folderName = `TATSUMAKY_${Util.generateRandomString(5)}_SENPOUKYAKU`;
             secondFolderName = `TATSUMAKY_${Util.generateRandomString(5)}_SENPOUKYAKU`;
-            await this.alfrescoJsApi.login(TestConfig.adf.adminEmail, TestConfig.adf.adminPassword);
-            await this.alfrescoJsApi.core.peopleApi.addPerson(acsUser);
-            await this.alfrescoJsApi.login(acsUser.id, acsUser.password);
-            uploadedFolder = await uploadActions.createFolder(this.alfrescoJsApi, folderName, '-my-');
-            secondUploadedFolder = await uploadActions.createFolder(this.alfrescoJsApi, secondFolderName, '-my-');
+            await alfrescoJsApi.login(TestConfig.adf.adminEmail, TestConfig.adf.adminPassword);
+            await alfrescoJsApi.core.peopleApi.addPerson(acsUser);
+            await alfrescoJsApi.login(acsUser.id, acsUser.password);
+            uploadedFolder = await uploadActions.createFolder(alfrescoJsApi, folderName, '-my-');
+            secondUploadedFolder = await uploadActions.createFolder(alfrescoJsApi, secondFolderName, '-my-');
 
             loginPage.loginToContentServicesUsingUserModel(acsUser);
             contentServicesPage.goToDocumentList();
@@ -169,8 +168,8 @@ describe('Document List Component - Actions', () => {
         afterEach(async (done) => {
             try {
                 await this.alfrescoJsApi.login(TestConfig.adf.adminEmail, TestConfig.adf.adminPassword);
-                await uploadActions.deleteFilesOrFolder(this.alfrescoJsApi, uploadedFolder.entry.id);
-                await uploadActions.deleteFilesOrFolder(this.alfrescoJsApi, secondUploadedFolder.entry.id);
+                await uploadActions.deleteFilesOrFolder(alfrescoJsApi, uploadedFolder.entry.id);
+                await uploadActions.deleteFilesOrFolder(alfrescoJsApi, secondUploadedFolder.entry.id);
             } catch (error) {
             }
             done();
@@ -222,16 +221,17 @@ describe('Document List Component - Actions', () => {
 
         beforeAll(async (done) => {
 
-            await this.alfrescoJsApi.login(TestConfig.adf.adminEmail, TestConfig.adf.adminPassword);
-            await this.alfrescoJsApi.core.peopleApi.addPerson(contentServicesUser);
-            await this.alfrescoJsApi.login(contentServicesUser.id, contentServicesUser.password);
-            folder1 = await uploadActions.createFolder(this.alfrescoJsApi, folderModel1.name, '-my-');
-            folder2 = await uploadActions.createFolder(this.alfrescoJsApi, folderModel2.name, '-my-');
-            folder3 = await uploadActions.createFolder(this.alfrescoJsApi, folderModel3.name, '-my-');
-            folder4 = await uploadActions.createFolder(this.alfrescoJsApi, folderModel4.name, '-my-');
-            folder5 = await uploadActions.createFolder(this.alfrescoJsApi, folderModel5.name, '-my-');
-            folder6 = await uploadActions.createFolder(this.alfrescoJsApi, folderModel6.name, '-my-');
+            await alfrescoJsApi.login(TestConfig.adf.adminEmail, TestConfig.adf.adminPassword);
+            await alfrescoJsApi.core.peopleApi.addPerson(contentServicesUser);
+            await alfrescoJsApi.login(contentServicesUser.id, contentServicesUser.password);
+            folder1 = await uploadActions.createFolder(alfrescoJsApi, 'A' + folderModel1.name, '-my-');
+            folder2 = await uploadActions.createFolder(alfrescoJsApi, 'B' + folderModel2.name, '-my-');
+            folder3 = await uploadActions.createFolder(alfrescoJsApi, 'C' + folderModel3.name, '-my-');
+            folder4 = await uploadActions.createFolder(alfrescoJsApi, 'D' + folderModel4.name, '-my-');
+            folder5 = await uploadActions.createFolder(alfrescoJsApi, 'E' + folderModel5.name, '-my-');
+            folder6 = await uploadActions.createFolder(alfrescoJsApi, 'F' + folderModel6.name, '-my-');
             folders = [folder1, folder2, folder3, folder4, folder5, folder6];
+            done();
         });
 
         beforeEach(async (done) => {
@@ -241,25 +241,26 @@ describe('Document List Component - Actions', () => {
             paginationPage.selectItemsPerPage('5');
             contentServicesPage.checkAcsContainer();
             contentServicesPage.waitForTableBody();
-            expect(paginationPage.getCurrentItemsPerPage()).toEqual(5);
+            expect(paginationPage.getCurrentItemsPerPage()).toEqual('5');
             expect(paginationPage.getPaginationRange()).toEqual('Showing 1-' + 5 + ' of ' + 6);
             done();
         });
 
         afterAll(async (done) => {
-            await this.alfrescoJsApi.login(TestConfig.adf.adminEmail, TestConfig.adf.adminPassword);
+            await alfrescoJsApi.login(TestConfig.adf.adminEmail, TestConfig.adf.adminPassword);
             await folders.forEach(function (folder) {
-                uploadActions.deleteFilesOrFolder(this.alfrescoJsApi, folder.entry.id);
+                uploadActions.deleteFilesOrFolder(alfrescoJsApi, folder.entry.id);
             });
             done();
         });
 
         it('[C260132] Move action on folder with - Load more', () => {
-            contentListPage.rightClickOnRow(folderModel1.name);
+
+            contentListPage.rightClickOnRow('A' + folderModel1.name);
             contentServicesPage.checkContextActionIsVisible('Move');
             contentServicesPage.pressContextMenuActionNamed('Move');
             copyMoveDialog.checkDialogIsDisplayed();
-            expect(copyMoveDialog.getDialogHeaderText()).toBe('Move \'' + folderModel1.name + '\' to...');
+            expect(copyMoveDialog.getDialogHeaderText()).toBe('Move \'' + 'A' + folderModel1.name + '\' to...');
 
         });
 
