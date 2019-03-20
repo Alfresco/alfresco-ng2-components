62238d380aa384c730b3fd19db8e4a68b4c7b427
gmandakini <geeta.ayyalasomayajula@alfresco.com>
2019-03-20 10:33:46 +0000

    export public-api update

--- lib/testing/src/lib/core/pages/public-api.ts
+++ lib/testing/src/lib/core/pages/public-api.ts
@@ -16,5 +16,4 @@
  */
 
 export * from './header.page';
-export * from '../../material/tabs.page';
 export * from './user-info.page';
--- lib/testing/src/lib/process-services-cloud/app/public-api.ts
+++ lib/testing/src/lib/process-services-cloud/app/public-api.ts
@@ -15,4 +15,4 @@
  * limitations under the License.
  */
 
-export * from '../app/app-list-cloud.page';
+export * from './app-list-cloud.page';
