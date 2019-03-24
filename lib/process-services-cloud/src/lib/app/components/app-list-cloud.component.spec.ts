d6e52955f001f28b3fbb88112d5036bb096f3079
Silviu Popa <silviucpopa@gmail.com>
2019-03-24 14:20:22 +0200

    [ADF-4155] -  app- list unit test

--- lib/process-services-cloud/src/lib/app/components/app-list-cloud.component.spec.ts
+++ lib/process-services-cloud/src/lib/app/components/app-list-cloud.component.spec.ts
@@ -18,6 +18,7 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { setupTestBed, CoreModule, AlfrescoApiServiceMock,  AlfrescoApiService } from '@alfresco/adf-core';
+import { of } from 'rxjs';
 
 import { fakeApplicationInstance } from '../mock/app-model.mock';
 import { AppListCloudComponent } from './app-list-cloud.component';
@@ -29,6 +30,8 @@ describe('AppListCloudComponent', () => {
 
     let component: AppListCloudComponent;
     let fixture: ComponentFixture<AppListCloudComponent>;
+    let appsProcessCloudService: AppsProcessCloudService;
+    let getAppsSpy: jasmine.Spy;
     let alfrescoApiService: AlfrescoApiService;
 
     const mock = {
@@ -37,12 +40,11 @@ describe('AppListCloudComponent', () => {
             }
     };
 
-    beforeEach(() => {
+    beforeEach( async(() => {
         TestBed.configureTestingModule({
             imports: [CoreModule.forRoot(), ProcessServiceCloudTestingModule, AppListCloudModule],
             providers: [
-                AppsProcessCloudService,
-                {provide: AlfrescoApiService, useClass: AlfrescoApiServiceMock}
+                AppsProcessCloudService
             ]
           })
           .overrideComponent(AppListCloudComponent, {
@@ -52,18 +54,137 @@ describe('AppListCloudComponent', () => {
               ]
             }
           }).compileComponents();
+    }));
 
+    beforeEach( () => {
         fixture = TestBed.createComponent(AppListCloudComponent);
         component = fixture.componentInstance;
         alfrescoApiService = TestBed.get(AlfrescoApiService);
+        appsProcessCloudService = TestBed.get(AppsProcessCloudService);
 
         spyOn(alfrescoApiService, 'getInstance').and.returnValue(mock);
+        getAppsSpy = spyOn(appsProcessCloudService, 'getDeployedApplicationsByStatus').and.returnValue(of(fakeApplicationInstance));
     });
 
     it('should create AppListCloudComponent ', async(() => {
         expect(component instanceof AppListCloudComponent).toBe(true);
     }));
 
+    it('should define layoutType with the default value', () => {
+        component.layoutType = '';
+        fixture.detectChanges();
+        expect(component.isGrid()).toBe(true);
+    });
+
+    it('Should fetch deployed apps', async(() => {
+        fixture.detectChanges();
+        fixture.whenStable().then(() => {
+            component.apps$.subscribe((response: any[]) => {
+                expect(response).toBeDefined();
+                expect(response.length).toEqual(3);
+                expect(response[0].name).toEqual('application-new-1');
+                expect(response[0].status).toEqual('Running');
+                expect(response[0].icon).toEqual('favorite_border');
+                expect(response[0].theme).toEqual('theme-2');
+                expect(response[1].name).toEqual('application-new-2');
+                expect(response[1].status).toEqual('Pending');
+                expect(response[1].icon).toEqual('favorite_border');
+                expect(response[1].theme).toEqual('theme-2');
+            });
+        });
+        expect(getAppsSpy).toHaveBeenCalled();
+    }));
+
+    it('should display default adf-empty-content template when response empty', () => {
+        getAppsSpy.and.returnValue(of([]));
+        fixture.detectChanges();
+        const defaultEmptyTemplate = fixture.nativeElement.querySelector('.adf-app-list-empty');
+        const emptyContent = fixture.debugElement.nativeElement.querySelector('.adf-empty-content');
+        const emptyTitle = fixture.debugElement.nativeElement.querySelector('.adf-empty-content__title');
+        const emptySubtitle = fixture.debugElement.nativeElement.querySelector('.adf-empty-content__subtitle');
+        expect(defaultEmptyTemplate).toBeDefined();
+        expect(defaultEmptyTemplate).not.toBeNull();
+        expect(emptyContent).not.toBeNull();
+        expect(emptyTitle.innerText).toBe('ADF_CLOUD_TASK_LIST.APPS.TITLE');
+        expect(emptySubtitle.innerText).toBe('ADF_CLOUD_TASK_LIST.APPS.SUBTITLE');
+        expect(getAppsSpy).toHaveBeenCalled();
+    });
+
+    describe('Grid Layout ', () => {
+
+        it('should display a grid by default', () => {
+            fixture.detectChanges();
+            expect(component.isGrid()).toBe(true);
+            expect(component.isList()).toBe(false);
+        });
+
+        it('should defined adf-cloud-app-details when layout type is grid', () => {
+            fixture.detectChanges();
+            const adfCloudDetailsElement = fixture.nativeElement.querySelectorAll('adf-cloud-app-details');
+            const appName = fixture.nativeElement.querySelector('.adf-app-listgrid-item-card-title');
+            expect(adfCloudDetailsElement).toBeDefined();
+            expect(adfCloudDetailsElement).not.toBeNull();
+
+            expect(adfCloudDetailsElement.length).toEqual(3);
+            expect(component.isGrid()).toBe(true);
+            expect(component.isList()).toBe(false);
+
+            expect(appName.innerText.trim()).toEqual(fakeApplicationInstance[0].name);
+        });
+
+        it('should display a grid when configured to', () => {
+            component.layoutType = AppListCloudComponent.LAYOUT_GRID;
+            fixture.detectChanges();
+            expect(component.isGrid()).toBe(true);
+            expect(component.isList()).toBe(false);
+        });
+
+        it('should throw an exception on init if unknown type configured', () => {
+            component.layoutType = 'unknown';
+            expect(component.ngOnInit).toThrowError();
+        });
+    });
+
+    describe('List Layout ', () => {
+
+        beforeEach(() => {
+            component.layoutType = AppListCloudComponent.LAYOUT_LIST;
+        });
+
+        it('should display a LIST when configured to', () => {
+            fixture.detectChanges();
+            expect(component.isGrid()).toBe(false);
+            expect(component.isList()).toBe(true);
+        });
+
+        it('should defined mat-list when layout type is LIST', () => {
+            fixture.detectChanges();
+            const appListElement = fixture.nativeElement.querySelectorAll('mat-list');
+            const appListItemElement = fixture.nativeElement.querySelectorAll('mat-list-item');
+            const appName = fixture.nativeElement.querySelector('.mat-list-text');
+            expect(appListElement).toBeDefined();
+            expect(appListElement).not.toBeNull();
+
+            expect(appListItemElement.length).toEqual(3);
+            expect(component.isGrid()).toBe(false);
+            expect(component.isList()).toBe(true);
+
+            expect(appName.innerText.trim()).toEqual(fakeApplicationInstance[0].name);
+        });
+
+        it('should throw an exception on init if unknown type configured', () => {
+            component.layoutType = 'unknown';
+            expect(component.ngOnInit).toThrowError();
+        });
+    });
+
+    it('should emit a click event when app selected', () => {
+        spyOn(component.appClick, 'emit');
+        fixture.detectChanges();
+        const onAppClick = fixture.nativeElement.querySelector('.mat-card');
+        onAppClick.click();
+        expect(component.appClick.emit).toHaveBeenCalledWith(fakeApplicationInstance[0]);
+    });
 });
 
 @Component({
