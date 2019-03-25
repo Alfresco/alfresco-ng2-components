be604a6360f3ac347d79e423afed2b641448264b
Marouan Bentaleb <38426175+marouanbentaleb@users.noreply.github.com>
2019-03-25 15:23:58 +0100

    [ADF-NO-ISSUE] Removing wrong dependencies (#4458)

--- lib/testing/src/lib/process-services-cloud/pages/public-api.ts
+++ lib/testing/src/lib/process-services-cloud/pages/public-api.ts
@@ -3,7 +3,5 @@
  */
 
 export * from './login-sso.page';
-export * from '../../core/pages/user-info.page';
-export * from '../app/app-list-cloud.page';
 export * from './start-tasks-cloud-component.page';
 export * from './task-header-cloud-component.page';
--- lib/testing/src/public-api.ts
+++ lib/testing/src/public-api.ts
@@ -3,7 +3,7 @@
  */
 
 export * from './lib/core/public-api';
-export * from './lib/material/public-api';
+export * from './lib/core/material/public-api';
 export * from './lib/content-services/public-api';
 export * from './lib/process-services/public-api';
 export * from './lib/process-services-cloud/public-api';
