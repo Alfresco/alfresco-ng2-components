c02ff7d472c1d946cb306290991799251419851c
Silviu Popa <silviucpopa@gmail.com>
2019-04-16 12:52:50 +0300

    [ADF-4394] - add suport for copy clipboard  on JSON cell type

--- lib/core/datatable/components/datatable/json-cell.component.ts
+++ lib/core/datatable/components/datatable/json-cell.component.ts
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
 import { DataTableCellComponent } from './datatable-cell.component';
 
 @Component({
@@ -23,16 +23,30 @@ import { DataTableCellComponent } from './datatable-cell.component';
     changeDetection: ChangeDetectionStrategy.OnPush,
     template: `
         <ng-container>
+            <span *ngIf="copyContent; else defaultJsonTemplate" class="adf-datatable-cell-value">
+                <pre
+                    class="adf-datatable-json-cell"
+                    [adf-clipboard]="'DATATABLE.CLICK_TO_COPY'"
+                    [clipboard-notification]="'DATATABLE.SUCCESS_COPY'">
+                    {{ value$ | async | json }}
+                </pre>
+            </span>
+        </ng-container>
+        <ng-template #defaultJsonTemplate>
             <span class="adf-datatable-cell-value">
                 <pre class="adf-datatable-json-cell">{{ value$ | async | json }}</pre>
             </span>
-        </ng-container>
+        </ng-template>
     `,
     styleUrls: ['./json-cell.component.scss'],
     encapsulation: ViewEncapsulation.None,
     host: { class: 'adf-datatable-cell' }
 })
 export class JsonCellComponent extends DataTableCellComponent implements OnInit {
+
+     /** Enables/disables a Clipboard directive to allow copying of the cell's content. */
+     @Input()
+     copyContent: boolean;
 
     ngOnInit() {
         if (this.column && this.column.key && this.row && this.data) {
