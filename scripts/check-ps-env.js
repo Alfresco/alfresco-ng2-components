0dc28ad9870872a48338d48ffe8722bae75913fc
Eugenio Romano <eromano@users.noreply.github.com>
2019-04-23 14:27:45 +0100

    [NO-ISSUE] add check env is up also for CS ans PS (#4640)
    * add check env is up also for CS ans PS
    
    * remove logs

---
+++ scripts/check-ps-env.js
@@ -0,0 +1,27 @@
+let alfrescoApi = require('@alfresco/js-api');
+let program = require('commander');
+
+async function main() {
+
+    program
+        .version('0.1.0')
+        .option('--host [type]', 'Remote environment host adf.lab.com ')
+        .option('-p, --password [type]', 'password ')
+        .option('-u, --username [type]', 'username ')
+        .parse(process.argv);
+
+    try {
+
+        this.alfrescoJsApi = new alfrescoApi.AlfrescoApiCompatibility({
+            provider: 'ECM',
+            hostEcm: program.host
+        });
+        await this.alfrescoJsApi.login(program.username, program.password);
+    } catch (e) {
+        console.log('Login error environment down or inaccessible');
+        process.exit(1);
+    }
+
+}
+
+main();
