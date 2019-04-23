465f7f836b89f82731e51e2518e010df2e7e0885
Denys Vuika <denys.vuika@gmail.com>
2019-04-23 15:49:30 +0100

    restore typings

--- demo-shell/src/typings.d.ts
+++ demo-shell/src/typings.d.ts
@@ -3,3 +3,11 @@ declare var module: NodeModule;
 interface NodeModule {
     id: string;
 }
+
+interface Window {
+    define: (name: string, deps: string[], definitionFn: () => any) => void;
+
+    System: {
+        import: (path) => Promise<any>;
+    };
+}
