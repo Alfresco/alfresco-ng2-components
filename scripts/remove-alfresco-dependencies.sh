669c64fb46eec47b221b66c1dab9374f06c0cee9
davidcanonieto <david.cano.nieto@gmail.com>
2019-04-26 14:37:30 +0200

    [ADF-4411] Keep js-api dependecy in remove script (#4661)

--- scripts/remove-alfresco-dependencies.sh
+++ scripts/remove-alfresco-dependencies.sh
@@ -2,8 +2,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "====== Removing Alfresco dependencies from package.json ====="
 
-grep -wirn '@alfresco*' $DIR/../package.json
-
-sed -i '' '/@alfresco*/,// d' $DIR/../package.json
+grep -wirn '@alfresco\/adf*' $DIR/../package.json
+sed -i '' 's/"@alfresco\/adf-[^,]*,//' $DIR/../package.json
+sed -i '' '/^[[:space:]]*$/d' $DIR/../package.json
 
 echo "====== Alfresco dependencies removed from package.json ====="
