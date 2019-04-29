ed9bf3f1972630cb70cd71c82bcc4839f6809155
Eugenio Romano <eugenio.romano@alfresco.com>
2019-04-29 17:32:21 +0100

    remove concurrent

--- scripts/travis/e2e/core-e2e.sh
+++ scripts/travis/e2e/core-e2e.sh
@@ -8,7 +8,7 @@ AFFECTED_E2E="$(./affected-folder.sh -b $TRAVIS_BRANCH -f "e2e")";
 AFFECTED_LIBS="$(./affected-libs.sh -gnu -b $TRAVIS_BRANCH)";
 if [[ $AFFECTED_LIBS =~ "core$" || $AFFECTED_E2E = "e2e" || $TRAVIS_PULL_REQUEST == "false"  ]];
 then
-    node ./travis/check-env/check-ps-env.js --host "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" || exit 1;
-    node ./travis/check-env/check-cs-env.js --host "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" || exit 1;
+    node ./check-env/check-ps-env.js --host "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" || exit 1;
+    node ./check-env/check-cs-env.js --host "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" || exit 1;
     ./test-e2e-lib.sh -host localhost:4200 -proxy "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" -e "$E2E_EMAIL" --folder core --skip-lint -save --use-dist || exit 1;
 fi;
