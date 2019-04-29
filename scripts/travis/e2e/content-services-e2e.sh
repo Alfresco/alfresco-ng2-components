2ddb90555b1f3bbefe96831591698cc918549ff8
Eugenio Romano <eugenio.romano@alfresco.com>
2019-04-29 15:39:37 +0100

    fix scripts path

--- scripts/travis/e2e/content-services-e2e.sh
+++ scripts/travis/e2e/content-services-e2e.sh
@@ -8,6 +8,6 @@ AFFECTED_E2E="$(./affected-folder.sh -b $TRAVIS_BRANCH -f "e2e")";
 AFFECTED_LIBS="$(./affected-libs.sh -gnu -b $TRAVIS_BRANCH)";
 if [[ $AFFECTED_LIBS =~ "content-services$" || $AFFECTED_E2E = "e2e" || $TRAVIS_PULL_REQUEST == "false"  ]];
 then
-    node ../../check-cs-env.js --host "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" || exit 1;
-    ../../test-e2e-lib.sh -host localhost:4200 -proxy "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" -e "$E2E_EMAIL" --folder content-services --skip-lint --use-dist  || exit 1;
+    node ./check-cs-env.js --host "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" || exit 1;
+    ./test-e2e-lib.sh -host localhost:4200 -proxy "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" -e "$E2E_EMAIL" --folder content-services --skip-lint --use-dist  || exit 1;
 fi;
