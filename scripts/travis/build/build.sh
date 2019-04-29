3fa6eb7e206c471ca364420adb7abfbc6fc30d63
Eugenio Romano <eugenio.romano@alfresco.com>
2019-04-29 03:07:49 +0100

    fix path

--- scripts/travis/build/build.sh
+++ scripts/travis/build/build.sh
@@ -6,15 +6,15 @@ cd $DIR/../../
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]];
 then
-    ./scripts/lint.sh || exit 1;
-    ./scripts/npm-build-all.sh || exit 1;
+    ./lint.sh || exit 1;
+    ./npm-build-all.sh || exit 1;
 else
-    ./scripts/update-version.sh -gnu -alpha || exit 1;
+    ./update-version.sh -gnu -alpha || exit 1;
     npm install;
-    ./scripts/lint.sh || exit 1;
+    ./lint.sh || exit 1;
     rm -rf tmp && mkdir tmp;
     git merge-base origin/$TRAVIS_BRANCH HEAD > ./tmp/devhead.txt;
-    ./scripts/smart-build.sh -b $TRAVIS_BRANCH  -gnu || exit 1;
+    ./smart-build.sh -b $TRAVIS_BRANCH  -gnu || exit 1;
 fi;
 npm run build:dist || exit 1;
-./scripts/license-list-generator.sh || exit 1;
+./license-list-generator.sh || exit 1;
