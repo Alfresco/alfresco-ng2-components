b945c07e56bf8f666b22e4b9ca1f116737c8674f
Eugenio Romano <eugenio.romano@alfresco.com>
2019-05-01 14:08:08 +0100

    parallel unit

--- scripts/travis/unit-test/process.sh
+++ scripts/travis/unit-test/process.sh
@@ -2,6 +2,8 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+command=""
+
 cd $DIR/../../../
 
 AFFECTED_LIBS="$(./scripts/affected-libs.sh -gnu -b $TRAVIS_BRANCH)";
@@ -9,17 +11,19 @@ AFFECTED_LIBS="$(./scripts/affected-libs.sh -gnu -b $TRAVIS_BRANCH)";
 echo "================== process-services unit ==================="
 if [[ $AFFECTED_LIBS =~ "process-services$" || $TRAVIS_PULL_REQUEST == "false"  ]];
 then
-    ng test process-services --watch=false || exit 1;
+    command=${command}" 'ng test process-services --watch=false || exit 1;'"
 fi;
 
 echo "================== insights unit ==================="
 if [[ $AFFECTED_LIBS =~ "insights$" || $TRAVIS_PULL_REQUEST == "false"  ]];
 then
-    ng test insights --watch=false || exit 1;
+    command=${command}" 'ng test insights --watch=false || exit 1;'"
 fi;
 
 echo "================== process-services-cloud unit ==================="
 if [[ $AFFECTED_LIBS =~ "process-services-cloud$" || $TRAVIS_PULL_REQUEST == "false"  ]];
 then
-    ng test process-services-cloud --watch=false || exit 1;
+    command=${command}" 'ng test process-services-cloud --watch=false || exit 1;'"
 fi;
+
+concurrently ${command}
