0e1a176f3aba13b8b6d16467b179e5d97dfae1c1
Denys Vuika <denys.vuika@gmail.com>
2019-05-03 13:34:18 +0100

    update code docs

--- lib/core/services/notification.service.ts
+++ lib/core/services/notification.service.ts
@@ -95,14 +95,29 @@ export class NotificationService {
         });
     }
 
+    /**
+     * Rase error message
+     * @param message Text message or translation key for the message.
+     * @param action (optional) Action name
+     */
     showError(message: string, action?: string): MatSnackBarRef<any> {
         return this.showMessage(message, 'adf-error-snackbar', action);
     }
 
+    /**
+     * Rase info message
+     * @param message Text message or translation key for the message.
+     * @param action (optional) Action name
+     */
     showInfo(message: string, action?: string): MatSnackBarRef<any> {
         return this.showMessage(message, 'adf-info-snackbar', action);
     }
 
+    /**
+     * Rase warning message
+     * @param message Text message or translation key for the message.
+     * @param action (optional) Action name
+     */
     showWarning(message: string, action?: string): MatSnackBarRef<any> {
         return this.showMessage(message, 'adf-warning-snackbar', action);
     }
