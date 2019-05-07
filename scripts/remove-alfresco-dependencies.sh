b7e50dd9ebf2eb765a2d7e92ddce2160f34492e4
davidcanonieto <david.cano.nieto@gmail.com>
2019-05-07 11:13:15 +0100

    [ADF-4495] Fix Remove dependencies script

--- scripts/remove-alfresco-dependencies.sh
+++ scripts/remove-alfresco-dependencies.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "====== Removing Alfresco dependencies from package.json ====="
 
 grep -wirn '@alfresco\/adf*' $DIR/../package.json
-sed -i '' 's/"@alfresco\/adf-[^,]*,//' $DIR/../package.json
-sed -i '' '/^[[:space:]]*$/d' $DIR/../package.json
+sed -i 's/"@alfresco\/adf-[^,]*,//' $DIR/../package.json
+sed -i '/^[[:space:]]*$/d' $DIR/../package.json
 
 echo "====== Alfresco dependencies removed from package.json ====="
