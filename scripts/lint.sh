088e42cadfb36c57c5a1f0275f5db5a6288a4c11
davidcanonieto <david.cano.nieto@gmail.com>
2019-05-20 12:03:59 +0100

    Run lint concurrently

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -10,27 +10,9 @@ show_help() {
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR/../"
 
-echo "====== lint Lib ====="
+echo "====== Parallel lint ====="
 
-npm run lint-lib || exit 1
-
-echo "====== lint E2E ====="
-
-npm run lint-e2e || exit 1
-
-echo "====== lint Demo shell ====="
-
-ng lint dev || exit 1
-
-echo "====== spellcheck ====="
-
-npm run spellcheck || exit 1
-
-echo "====== styleLint ====="
-
-npm run stylelint || exit 1
-
-echo "====== exclude-word ====="
+npx concurrently -s "all" "npm run lint-lib || exit 1" "npm run stylelint || exit 1" "npm run spellcheck || exit " "ng lint dev || exit 1" "npm run lint-e2e || exit 1" || exit 1
 
 if grep "envalfresco" . -R --exclude-dir={node_modules,.history,.idea,scripts}; then
     echo not permitted word
