280c7b3acfadedf031241d7ecb4137fd20837f56
maurizio vitale <maurizio.vitale@alfresco.com>
2019-05-26 22:15:13 +0100

    Enable the smart e2e on content

--- scripts/travis/e2e/core-e2e.sh
+++ scripts/travis/e2e/core-e2e.sh
@@ -17,12 +17,16 @@ then
     ./scripts/test-e2e-lib.sh -host http://localhost:4200 -proxy "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" -e "$E2E_EMAIL" --folder core -save --use-dist -b || exit 1;
 else [[ $AFFECTED_E2E = "e2e" ]];
     HEAD_SHA_BRANCH="$(git merge-base origin/$TRAVIS_BRANCH HEAD)"
+    LIST_CONTENT_SPECS="$(git diff --name-only $HEAD_SHA_BRANCH HEAD | grep "^e2e/content-services" | paste -sd , -)"
+    if [[ $LIST_CONTENT_SPECS != "" ]];
+    then
+        echo "Run content e2e based on the sha $HEAD_SHA_BRANCH with the specs: "$LIST_CONTENT_SPECS
+        ./scripts/test-e2e-lib.sh -host http://localhost:4200 -proxy "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" -e "$E2E_EMAIL" -ud -s "$LIST_CONTENT_SPECS"
+    fi
     LIST_CLOUD_SPECS="$(git diff --name-only $HEAD_SHA_BRANCH HEAD | grep "^e2e/process-services-cloud" | paste -sd , -)"
     if [[ $LIST_CLOUD_SPECS != "" ]];
     then
         echo "Run cloud e2e based on the sha $HEAD_SHA_BRANCH with the specs: "$LIST_CLOUD_SPECS
         ./scripts/test-e2e-lib.sh -host http://localhost:4200 -u "$E2E_USERNAME" -p "$E2E_PASSWORD" -e "$E2E_EMAIL" -host_sso "$E2E_HOST_SSO" -host_bpm "$E2E_HOST_BPM" -host_identity "$E2E_HOST_IDENTITY" -ud -s "$LIST_CLOUD_SPECS"
-    else
-        echo "no cloud e2e found"
     fi
 fi;
