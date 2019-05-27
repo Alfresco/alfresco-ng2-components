86bcd995575775d8e09a4ab19bf5f2b9eaa0ffe5
maurizio vitale <maurizio.vitale@alfresco.com>
2019-05-27 10:30:39 +0100

    Delegate the e2e run on the specific project

--- scripts/travis/e2e/content-services-e2e.sh
+++ scripts/travis/e2e/content-services-e2e.sh
@@ -4,10 +4,24 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR/../../../
 
+CONTEXT_ENV="content-services"
 ./scripts/check-branch-updated.sh -b $TRAVIS_BRANCH || exit 1;
 AFFECTED_LIBS="$(./scripts/affected-libs.sh -gnu -b $TRAVIS_BRANCH)";
-if [[ $AFFECTED_LIBS =~ "content-services$" || $TRAVIS_PULL_REQUEST == "false"  ]];
+AFFECTED_E2E="$(./scripts/affected-folder.sh -b $TRAVIS_BRANCH -f "e2e/$CONTEXT_ENV")";
+
+RUN_CHECK=$(echo node ./scripts/check-env/check-cs-env.js --host "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" || exit 1)
+RUN_E2E=$(echo ./scripts/test-e2e-lib.sh -host http://localhost:4200 -proxy "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" -e "$E2E_EMAIL" --use-dist -b  || exit 1)
+if [[ $AFFECTED_LIBS =~ "$CONTEXT_ENV$" || $TRAVIS_PULL_REQUEST == "false"  ]];
 then
-    node ./scripts/check-env/check-cs-env.js --host "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" || exit 1;
-    ./scripts/test-e2e-lib.sh -host http://localhost:4200 -proxy "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" -e "$E2E_EMAIL" --folder content-services --use-dist -b  || exit 1;
+    $RUN_CHECK
+    $RUN_E2E --folder $CONTEXT_ENV
+else [[ $AFFECTED_E2E = "e2e/$CONTEXT_ENV" ]];
+    HEAD_SHA_BRANCH="$(git merge-base origin/$TRAVIS_BRANCH HEAD)"
+    LIST_SPECS="$(git diff --name-only $HEAD_SHA_BRANCH HEAD | grep "^e2e/$CONTEXT_ENV" | paste -sd , -)"
+    if [[ $LIST_SPECS != "" ]];
+    then
+        echo "Run $CONTEXT_ENV e2e based on the sha $HEAD_SHA_BRANCH with the specs: "$LIST_SPECS
+        $RUN_CHECK
+        $RUN_E2E --specs -s "$LIST_SPECS"
+    fi
 fi;
--- scripts/travis/e2e/core-e2e.sh
+++ scripts/travis/e2e/core-e2e.sh
@@ -4,29 +4,28 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR/../../../
 
+CONTEXT_ENV="core"
 ./scripts/check-branch-updated.sh -b $TRAVIS_BRANCH || exit 1;
-AFFECTED_E2E="$(./scripts/affected-folder.sh -b $TRAVIS_BRANCH -f "e2e")";
 AFFECTED_LIBS="$(./scripts/affected-libs.sh -gnu -b $TRAVIS_BRANCH)";
-echo "AFFECTED_E2E: "$AFFECTED_E2E
-echo "AFFECTED_LIBS: "$AFFECTED_LIBS
+AFFECTED_E2E="$(./scripts/affected-folder.sh -b $TRAVIS_BRANCH -f "e2e/$CONTEXT_ENV")";
 
-if [[ $AFFECTED_LIBS =~ "core$" || $TRAVIS_PULL_REQUEST == "false"  ]];
+RUN_CHECK_PS=$(echo node ./scripts/check-env/check-ps-env.js --host "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" || exit 1 )
+RUN_CHECK_CS=$(echo node ./scripts/check-env/check-cs-env.js --host "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" || exit 1 )
+RUN_E2E=$(echo ./scripts/test-e2e-lib.sh -host http://localhost:4200 -proxy "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" -e "$E2E_EMAIL" -save --use-dist -b )
+
+
+if [[ $AFFECTED_LIBS =~ "$CONTEXT_ENV$" || $TRAVIS_PULL_REQUEST == "false"  ]];
 then
-    node ./scripts/check-env/check-ps-env.js --host "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" || exit 1;
-    node ./scripts/check-env/check-cs-env.js --host "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" || exit 1;
-    ./scripts/test-e2e-lib.sh -host http://localhost:4200 -proxy "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" -e "$E2E_EMAIL" --folder core -save --use-dist -b || exit 1;
-else [[ $AFFECTED_E2E = "e2e" ]];
+    $RUN_CHECK_PS
+    $RUN_CHECK_CS
+    $RUN_E2E --folder $CONTEXT_ENV
+else [[ $AFFECTED_E2E = "e2e/$CONTEXT_ENV" ]];
     HEAD_SHA_BRANCH="$(git merge-base origin/$TRAVIS_BRANCH HEAD)"
-    LIST_CONTENT_SPECS="$(git diff --name-only $HEAD_SHA_BRANCH HEAD | grep "^e2e/content-services" | paste -sd , -)"
-    if [[ $LIST_CONTENT_SPECS != "" ]];
+    LIST_SPECS="$(git diff --name-only $HEAD_SHA_BRANCH HEAD | grep "^e2e/$CONTEXT_ENV" | paste -sd , -)"
+    if [[ $LIST_SPECS != "" ]];
     then
-        echo "Run content e2e based on the sha $HEAD_SHA_BRANCH with the specs: "$LIST_CONTENT_SPECS
-        ./scripts/test-e2e-lib.sh -host http://localhost:4200 -proxy "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" -e "$E2E_EMAIL" -ud -s "$LIST_CONTENT_SPECS"
-    fi
-    LIST_CLOUD_SPECS="$(git diff --name-only $HEAD_SHA_BRANCH HEAD | grep "^e2e/process-services-cloud" | paste -sd , -)"
-    if [[ $LIST_CLOUD_SPECS != "" ]];
-    then
-        echo "Run cloud e2e based on the sha $HEAD_SHA_BRANCH with the specs: "$LIST_CLOUD_SPECS
-        ./scripts/test-e2e-lib.sh -host http://localhost:4200 -u "$E2E_USERNAME" -p "$E2E_PASSWORD" -e "$E2E_EMAIL" -host_sso "$E2E_HOST_SSO" -host_bpm "$E2E_HOST_BPM" -host_identity "$E2E_HOST_IDENTITY" -ud -s "$LIST_CLOUD_SPECS"
+        echo "Run $CONTEXT_ENV e2e based on the sha $HEAD_SHA_BRANCH with the specs: "$LIST_SPECS
+        $RUN_CHECK
+        $RUN_E2E --specs -s "$LIST_SPECS"
     fi
 fi;
