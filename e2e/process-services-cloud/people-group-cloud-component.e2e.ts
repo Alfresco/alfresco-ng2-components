28ae3a966e5bade7338f4ed9eb00ebe6a0ab1e2c
Cristina Jalba <Cristina.Jalba@ness.com>
2019-05-27 15:51:50 +0300

    no message

--- e2e/process-services-cloud/people-group-cloud-component.e2e.ts
+++ e2e/process-services-cloud/people-group-cloud-component.e2e.ts
@@ -19,7 +19,7 @@ import { NavigationBarPage } from '../pages/adf/navigationBarPage';
 import { PeopleGroupCloudComponentPage } from '../pages/adf/demo-shell/process-services/peopleGroupCloudComponentPage';
 import { GroupCloudComponentPage, PeopleCloudComponentPage, SettingsPage } from '@alfresco/adf-testing';
 import { browser } from 'protractor';
-import { LoginSSOPage, IdentityService, GroupIdentityService, RolesService, ApiService } from '@alfresco/adf-testing';r
+import { LoginSSOPage, IdentityService, GroupIdentityService, RolesService, ApiService } from '@alfresco/adf-testing';
 import CONSTANTS = require('../util/constants');
 import resources = require('../util/resources');
 
