f9520609c12472821f0fedcf17877d8062518f6d
Eugenio Romano <eugenio.romano@alfresco.com>
2019-05-30 09:29:32 +0100

    download browser util fix

--- lib/testing/src/lib/core/utils/file-browser.util.ts
+++ lib/testing/src/lib/core/utils/file-browser.util.ts
@@ -19,7 +19,7 @@ import * as path from 'path';
 import * as fs from 'fs';
 import { browser } from 'protractor';
 
-const DEFAULT_ROOT_PATH = global['TestConfig'] ? global['TestConfig'].main.rootPath : __dirname;
+const DEFAULT_ROOT_PATH = browser.params.testConfig ? browser.params.testConfig.main.rootPath : __dirname;
 
 export class FileBrowserUtil {
 
