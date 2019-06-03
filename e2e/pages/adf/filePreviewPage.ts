071ec3e715b2574f71b591cea75872edb86438b7
suzana_dirla <suzana@dirla.ro>
2019-06-03 10:12:01 +0300

    [ADF-4232] remove unused methods
    - these already exist on viewerPage.ts

--- e2e/pages/adf/filePreviewPage.ts
+++ e2e/pages/adf/filePreviewPage.ts
@@ -15,31 +15,13 @@
  * limitations under the License.
  */
 
-import { browser, by, element, protractor } from 'protractor';
+import { by, element, protractor } from 'protractor';
 import { BrowserVisibility, BrowserActions } from '@alfresco/adf-testing';
 
 export class FilePreviewPage {
 
     pdfTitleFromSearch = element(by.css(`span[id='adf-viewer-display-name']`));
     textLayer = element.all(by.css(`div[class='textLayer'] > div`)).first();
-    canvasLayer = element.all(by.css(`div[class='canvasWrapper'] > canvas`)).first();
-    closeButton = element(by.css('button[data-automation-id="adf-toolbar-back"]'));
-    zoomInButton = element(by.id('viewer-zoom-in-button'));
-    zoomOutButton = element(by.id('viewer-zoom-out-button'));
-    scalePageButton = element(by.id('viewer-scale-page-button'));
-
-    waitForElements() {
-        BrowserVisibility.waitUntilElementIsVisible(this.closeButton);
-
-        BrowserVisibility.waitUntilElementIsVisible(this.canvasLayer);
-        BrowserVisibility.waitUntilElementIsVisible(this.textLayer);
-    }
-
-    viewFile(fileName) {
-        BrowserVisibility.waitUntilElementIsVisible(element(by.cssContainingText(`div[data-automation-id="${fileName}"]`, fileName)));
-        browser.actions().doubleClick(element(by.cssContainingText(`div[data-automation-id="${fileName}"]`, fileName))).perform();
-        this.waitForElements();
-    }
 
     getPDFTitleFromSearch() {
         const deferred = protractor.promise.defer();
@@ -53,29 +35,5 @@ export class FilePreviewPage {
 
     closePreviewWithButton() {
         BrowserActions.clickExecuteScript('button[data-automation-id="adf-toolbar-back"]');
-    }
-
-    clickZoomIn() {
-        BrowserActions.click(this.zoomInButton);
-    }
-
-    clickZoomOut() {
-        BrowserActions.click(this.zoomOutButton);
-    }
-
-    clickActualSize() {
-        BrowserActions.click(this.scalePageButton);
-    }
-
-    getCanvasWidth() {
-        return this.canvasLayer.getAttribute(`width`).then((width) => {
-            return width;
-        });
-    }
-
-    getCanvasHeight() {
-        return this.canvasLayer.getAttribute(`height`).then((height) => {
-            return height;
-        });
     }
 }
