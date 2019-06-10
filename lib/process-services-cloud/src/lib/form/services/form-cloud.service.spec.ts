ad6f0b2ba475d7b61d9d33f7afc085c365d9afff
Mario Romano <dnamario@hotmail.com>
2019-06-10 23:18:14 +0100

    [ACTIVITI-3234] fix tests

--- lib/process-services-cloud/src/lib/form/services/form-cloud.service.spec.ts
+++ lib/process-services-cloud/src/lib/form/services/form-cloud.service.spec.ts
@@ -41,6 +41,7 @@ describe('Form Cloud service', () => {
     let apiService: AlfrescoApiService;
     const appName = 'app-name';
     const taskId = 'task-id';
+    const processId = 'process-id';
 
     setupTestBed({
         imports: [
@@ -167,7 +168,7 @@ describe('Form Cloud service', () => {
             oauth2Auth.callCustomApi.and.returnValue(Promise.resolve(responseBody));
             const formId = 'form-id';
 
-            service.saveTaskForm(appName, taskId, formId, {}).subscribe((result: any) => {
+            service.saveTaskForm(appName, taskId, processId, formId, {}).subscribe((result: any) => {
                 expect(result).toBeDefined();
                 expect(result.id).toBe('id');
                 expect(result.name).toBe('name');
@@ -182,7 +183,7 @@ describe('Form Cloud service', () => {
             oauth2Auth.callCustomApi.and.returnValue(Promise.resolve(responseBody));
             const formId = 'form-id';
 
-            service.completeTaskForm(appName, taskId, formId, {}, '').subscribe((result: any) => {
+            service.completeTaskForm(appName, taskId, processId,  formId, {}, '').subscribe((result: any) => {
                 expect(result).toBeDefined();
                 expect(result.id).toBe('id');
                 expect(result.name).toBe('name');
