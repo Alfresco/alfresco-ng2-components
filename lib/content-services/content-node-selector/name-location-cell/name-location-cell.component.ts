f861b94de4df7875ea475e06899b21f4d98a4f94
Eugenio Romano <eromano@users.noreply.github.com>
2019-06-20 12:32:41 +0100

    [Staging-branch] 3.4.0 (#4839)
    * next 3.4.0
    
    * next 3.4.0
    
    * [ADF-4426] Move CSS overrides from ACA to ADF - 2nd round (#4837)
    
    * [ADF-4426] align toolbar icon color with design
    
    * [ADF-4426] clean-up style - it is never applied
    
    * [ADF-4426] display title for name location cell - useful for long names
    
    * [ADF-4426] move content-node-selector cell fix from ACA
    
    - fixes https://github.com/Alfresco/alfresco-ng2-components/issues/4686
    
    * [ADF-4426] move content-node-selector cell fix from ACA
    
    - fixes https://github.com/Alfresco/alfresco-ng2-components/issues/4686
    
    * [ADF-4426] move image width fix from ACA
    
    * [ADF-4426] allow more customization of data-table borders
    
    * [ADF-4426] move fix from ACA
    
    [ACA-2363] fixes issue with text being visible underneath the info drawer
    
    * [ADF-4426] get adf-datatable-link style from ACA
    
    * [ADF-4426] allow set of DL selected row icon color
    
    * [ADF-4426] allow more document list customization
    
    - code moved from ACA
    - more css variables
    - removed non used and duplicated outline styles
    - some code clean-up...
    
    * [ADF-4635] The Status drop-down is looking different for Processes than for Tasks (#4822)

--- lib/content-services/content-node-selector/name-location-cell/name-location-cell.component.ts
+++ lib/content-services/content-node-selector/name-location-cell/name-location-cell.component.ts
@@ -21,7 +21,7 @@ import { DataRow } from '@alfresco/adf-core';
 @Component({
     selector: 'adf-name-location-cell',
     template: `
-        <div class="adf-name-location-cell-name adf-datatable-cell-value">{{ name }}</div>
+        <div class="adf-name-location-cell-name adf-datatable-cell-value" [title]="name">{{ name }}</div>
         <div class="adf-name-location-cell-location adf-datatable-cell-value" [title]="path">{{ path }}</div>
     `,
     styleUrls: ['./name-location-cell.component.scss'],
