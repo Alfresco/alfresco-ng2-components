275be571d49b02782f857ff51ee7f88e2d41bae6
gmandakini <geeta.ayyalasomayajula@alfresco.com>
2019-07-05 16:42:22 +0100

    C260134 - Move Folder with subfolder and file within it, test added

--- e2e/content-services/document-list/document-list-copy-move-actions.e2e.ts
+++ e2e/content-services/document-list/document-list-copy-move-actions.e2e.ts
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-import { browser } from 'protractor';
+import {browser} from 'protractor';
 import {
     LoginPage,
     UploadActions,
@@ -23,12 +23,12 @@ import {
     ContentNodeSelectorDialogPage,
     NotificationHistoryPage
 } from '@alfresco/adf-testing';
-import { ContentServicesPage } from '../../pages/adf/contentServicesPage';
-import { NavigationBarPage } from '../../pages/adf/navigationBarPage';
-import { AcsUserModel } from '../../models/ACS/acsUserModel';
+import {ContentServicesPage} from '../../pages/adf/contentServicesPage';
+import {NavigationBarPage} from '../../pages/adf/navigationBarPage';
+import {AcsUserModel} from '../../models/ACS/acsUserModel';
 import resources = require('../../util/resources');
-import { AlfrescoApiCompatibility as AlfrescoApi } from '@alfresco/js-api';
-import { FileModel } from '../../models/ACS/fileModel';
+import {AlfrescoApiCompatibility as AlfrescoApi} from '@alfresco/js-api';
+import {FileModel} from '../../models/ACS/fileModel';
 
 describe('Document List Component - Actions Move and Copy', () => {
 
@@ -44,52 +44,80 @@ describe('Document List Component - Actions Move and Copy', () => {
     });
     const uploadActions = new UploadActions(this.alfrescoJsApi);
 
-    let uploadedFolder, uploadedFile;
+    let uploadedFolder, uploadedFile, sourceFolder, destinationFolder, subFolder, subFile;
     let acsUser = null;
     let folderName;
 
     const pdfFileModel = new FileModel({
-            'name': resources.Files.ADF_DOCUMENTS.PDF.file_name,
-            'location': resources.Files.ADF_DOCUMENTS.PDF.file_location
-        });
+        'name': resources.Files.ADF_DOCUMENTS.PDF.file_name,
+        'location': resources.Files.ADF_DOCUMENTS.PDF.file_location
+    });
+
+    const testFileModel = new FileModel({
+        'name': resources.Files.ADF_DOCUMENTS.TEST.file_name,
+        'location': resources.Files.ADF_DOCUMENTS.TEST.file_location
+    });
 
     beforeAll(async (done) => {
-            acsUser = new AcsUserModel();
-            folderName = StringUtil.generateRandomString(5);
-            await this.alfrescoJsApi.login(browser.params.testConfig.adf.adminEmail, browser.params.testConfig.adf.adminPassword);
-            await this.alfrescoJsApi.core.peopleApi.addPerson(acsUser);
-            await this.alfrescoJsApi.login(acsUser.id, acsUser.password);
-            uploadedFolder = await uploadActions.createFolder(folderName, '-my-');
-            await uploadActions.uploadFile(pdfFileModel.location, pdfFileModel.name, uploadedFolder.entry.id);
-            uploadedFile = await uploadActions.uploadFile(pdfFileModel.location, pdfFileModel.name, '-my-');
+        acsUser = new AcsUserModel();
+        folderName = StringUtil.generateRandomString(5);
+        await this.alfrescoJsApi.login(browser.params.testConfig.adf.adminEmail, browser.params.testConfig.adf.adminPassword);
+        await this.alfrescoJsApi.core.peopleApi.addPerson(acsUser);
+        await this.alfrescoJsApi.login(acsUser.id, acsUser.password);
+        uploadedFolder = await uploadActions.createFolder(folderName, '-my-');
+        destinationFolder = await uploadActions.createFolder(StringUtil.generateRandomString(5), '-my-');
+        sourceFolder = await uploadActions.createFolder(StringUtil.generateRandomString(5), '-my-');
+        subFolder = await uploadActions.createFolder(StringUtil.generateRandomString(5), sourceFolder.entry.id);
+        subFile = await uploadActions.uploadFile(testFileModel.location, testFileModel.name, subFolder.entry.id);
+        await uploadActions.uploadFile(pdfFileModel.location, pdfFileModel.name, uploadedFolder.entry.id);
+        uploadedFile = await uploadActions.uploadFile(pdfFileModel.location, pdfFileModel.name, '-my-');
 
-            await loginPage.loginToContentServicesUsingUserModel(acsUser);
-            browser.driver.sleep(10000);
-            done();
-        });
+        await loginPage.loginToContentServicesUsingUserModel(acsUser);
+        browser.driver.sleep(10000);
+        done();
+    });
 
     afterAll(async (done) => {
         await this.alfrescoJsApi.login(browser.params.testConfig.adf.adminEmail, browser.params.testConfig.adf.adminPassword);
         uploadActions.deleteFileOrFolder(uploadedFolder.entry.id);
         uploadActions.deleteFileOrFolder(uploadedFile.entry.id);
+        uploadActions.deleteFileOrFolder(sourceFolder.entry.id);
+        uploadActions.deleteFileOrFolder(destinationFolder.entry.id);
         done();
     });
 
-    beforeEach( (done) => {
-            navigationBarPage.clickContentServicesButton();
-            done();
-        });
+    beforeEach((done) => {
+        navigationBarPage.clickContentServicesButton();
+        done();
+    });
 
     it('[C260128] Move - Same name file', () => {
-                contentServicesPage.checkContentIsDisplayed(pdfFileModel.name);
-                contentServicesPage.getDocumentList().rightClickOnRow(pdfFileModel.name);
-                contentServicesPage.pressContextMenuActionNamed('Move');
-                contentNodeSelector.checkDialogIsDisplayed();
-                contentNodeSelector.typeIntoNodeSelectorSearchField(folderName);
-                contentNodeSelector.clickContentNodeSelectorResult(folderName);
-                contentNodeSelector.clickMoveCopyButton();
-                notificationHistoryPage.clickNotificationButton();
-                notificationHistoryPage.checkNotificationIsPresent('This name is already in use, try a different name.');
-            });
+        contentServicesPage.checkContentIsDisplayed(pdfFileModel.name);
+        contentServicesPage.getDocumentList().rightClickOnRow(pdfFileModel.name);
+        contentServicesPage.pressContextMenuActionNamed('Move');
+        contentNodeSelector.checkDialogIsDisplayed();
+        contentNodeSelector.typeIntoNodeSelectorSearchField(folderName);
+        contentNodeSelector.clickContentNodeSelectorResult(folderName);
+        contentNodeSelector.clickMoveCopyButton();
+        notificationHistoryPage.checkNotifyContains('This name is already in use, try a different name.');
+    });
 
-        });
+    it('[C260134] Move - folder with subfolder and file within it', () => {
+        contentServicesPage.checkContentIsDisplayed(destinationFolder.entry.name);
+        contentServicesPage.checkContentIsDisplayed(sourceFolder.entry.name);
+        contentServicesPage.getDocumentList().rightClickOnRow(sourceFolder.entry.name);
+        contentServicesPage.pressContextMenuActionNamed('Move');
+        contentNodeSelector.checkDialogIsDisplayed();
+        contentNodeSelector.typeIntoNodeSelectorSearchField(destinationFolder.entry.name);
+        contentNodeSelector.clickContentNodeSelectorResult(destinationFolder.entry.name);
+        contentNodeSelector.clickMoveCopyButton();
+        contentServicesPage.checkContentIsNotDisplayed(sourceFolder.entry.name);
+        contentServicesPage.doubleClickRow(destinationFolder.entry.name);
+        contentServicesPage.checkContentIsDisplayed(sourceFolder.entry.name);
+        contentServicesPage.doubleClickRow(sourceFolder.entry.name);
+        contentServicesPage.checkContentIsDisplayed(subFolder.entry.name);
+        contentServicesPage.doubleClickRow(subFolder.entry.name);
+        contentServicesPage.checkContentIsDisplayed(subFile.entry.name);
+    });
+
+});
