fdd5c0f1dbece76d0e088a1182e183e6c8b67997
Silviu Popa <silviucpopa@gmail.com>
2019-07-11 16:35:54 +0300

    [ADF-4735] - Automation for visiblity condition on APS1

---
+++ e2e/pages/adf/demo-shell/process-services/formRenderDemoPage.ts
@@ -0,0 +1,45 @@
+/*!
+ * @license
+ * Copyright 2019 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { ConfigEditorPage } from '../../configEditorPage';
+import { BrowserVisibility } from '@alfresco/adf-testing';
+import { by, element } from 'protractor';
+
+export class FormRenderDemoPage {
+
+    formTab = element(by.css('mat-tab-list'));
+    formEditor = element.all(by.css('.mat-tab-labels .mat-tab-label')).get(1);
+    formRender = element.all(by.css('.mat-tab-labels .mat-tab-label')).get(0);
+
+    configEditorPage = new ConfigEditorPage();
+
+    goToEditor() {
+        BrowserVisibility.waitUntilElementIsVisible(this.formEditor);
+        this.formEditor.click();
+    }
+
+    goToRenderedForm() {
+        BrowserVisibility.waitUntilElementIsVisible(this.formRender);
+        this.formRender.click();
+    }
+
+    setConfigToEditor(text) {
+        this.goToEditor();
+        this.configEditorPage.enterBulkConfiguration(text);
+        this.goToRenderedForm();
+    }
+}
