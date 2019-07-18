909307e75d5ebf428fd9a99fc6299721730f2e40
gmandakini <geeta.ayyalasomayajula@alfresco.com>
2019-07-18 10:22:31 +0100

    removed the rightclick from here

--- lib/testing/src/lib/core/pages/data-table-component.page.ts
+++ lib/testing/src/lib/core/pages/data-table-component.page.ts
@@ -166,13 +166,6 @@ export class DataTableComponentPage {
         BrowserVisibility.waitUntilElementIsVisible(element(by.id('adf-context-menu-content')));
     }
 
-    rightClickOnRowByIndex(index: number) {
-        BrowserActions.closeMenuAndDialogs();
-        const row = this.getRowByIndex(index);
-        BrowserActions.rightClick(row);
-        BrowserVisibility.waitUntilElementIsVisible(element(by.id('adf-context-menu-content')));
-    }
-
     getTooltip(columnName, columnValue) {
         return this.getCellElementByValue(columnName, columnValue).getAttribute('title');
     }
