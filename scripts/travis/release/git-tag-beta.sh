31f6cdc67af1bb603d0a07ae6ab180c60c5b6ae6
maurizio vitale <maurizio.vitale@alfresco.com>
2019-09-02 11:12:37 +0100

    add the name and email values and push with the token

--- scripts/travis/release/git-tag-beta.sh
+++ scripts/travis/release/git-tag-beta.sh
@@ -3,7 +3,9 @@
 VERSION=$(npm view @alfresco/adf-core@beta version)
 
 echo "git tag -a ${VERSION} -m ${VERSION}"
+git config --local user.name "alfresco-build"
+git config --local user.email "build@alfresco.com"
 git tag -a ${VERSION} -m "${VERSION} [ci skip] "
 git remote rm origin
-git remote add origin 'https://$GITHUB_TOKEN:x-oauth-basic@github.com:Alfresco/alfresco-ng2-components.git'
+git remote add origin `https://$GITHUB_TOKEN:x-oauth-basic@github.com/Alfresco/alfresco-ng2-components.git`
 git push origin --tags
