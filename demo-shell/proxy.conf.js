53078c74f7e402b7410f53b5b2e80dd9f9c87cb9
davidcanonieto <david.cano.nieto@gmail.com>
2019-10-20 20:40:43 +0100

    fix linting

--- demo-shell/proxy.conf.js
+++ demo-shell/proxy.conf.js
@@ -2,7 +2,7 @@ var PROXY_HOST_ADF = process.env.PROXY_HOST_ADF;
 
 module.exports = {
     "/alfresco": {
-        "target": (PROXY_HOST_ADF || "http://adfdev.envalfresco.com"),
+        "target": (PROXY_HOST_ADF || "http://localhost:8080"),
         "secure": false,
         "pathRewrite": {
             "^/alfresco/alfresco": ""
@@ -17,7 +17,7 @@ module.exports = {
         }
     },
     "/activiti-app": {
-        "target": (PROXY_HOST_ADF || "http://adfdev.envalfresco.com"),
+        "target": (PROXY_HOST_ADF || "http://localhost:8080"),
         "secure": false,
         "pathRewrite": {
             "^/activiti-app/activiti-app": ""
