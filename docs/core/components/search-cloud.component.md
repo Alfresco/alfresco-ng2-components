e0cfb991383c9573f6ec752a694279d5d4ef2215
Silviu Popa <silviucpopa@gmail.com>
2019-10-25 13:27:51 +0300

    [AAE-534] - add search cloud component

---
+++ docs/core/components/search-cloud.component.md
@@ -0,0 +1,36 @@
+---
+Title: Search Cloud Component
+Added: v3.5.0
+Status: Active
+Last reviewed: 2019-10-24
+---
+
+# [Serch Cloud Component](../../../lib/core/search-cloud/search-cloud.component.ts "Defined in pagination.component.ts")
+
+Should manage search for cloud components
+
+## Basic Usage
+
+```html
+<adf-search-cloud>
+    [placeholder]="'placeholder'"
+    [debounceTime]="200"
+    (change)="onSearchValueChanged($event)"
+</adf-search-cloud>
+```
+
+## Class members
+
+### Properties
+
+| Name | Type | Default value | Description |
+| ---- | ---- | ------------- | ----------- |
+| type | [`SearchCloudTypesEnum`](../../../lib/core/models/search-cloud.model.ts) |  | search type ('text'). |
+| placeholder | `string` |  | placeholder content. |
+| debounceTime | `number` |  | An array of page sizes. |
+
+### Events
+
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| change | [`EventEmitter`](https://angular.io/api/core/EventEmitter)`<string>` | Emitted when search widget value is changed. |
---
+++ lib/core/search-cloud/components/search-text-cloud/search-text-cloud.component.ts
@@ -0,0 +1,51 @@
+/*!
+ * @license
+ * Copyright 2019 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { ViewEncapsulation, Component, OnInit, OnDestroy } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { SearchCloudService } from '../../../services/search-cloud.service';
+
+@Component({
+    selector: 'adf-search-text-cloud',
+    templateUrl: './search-text-cloud.component.html',
+    styleUrls: ['./search-text-cloud.component.scss'],
+    encapsulation: ViewEncapsulation.None
+})
+export class SearchTextCloudComponent implements OnInit, OnDestroy {
+
+    placeholder: string;
+
+    searchField: FormControl = new FormControl();
+    onDestroy$: Subject<void> = new Subject<void>();
+
+    constructor(private searchCloudService: SearchCloudService) {}
+
+    ngOnInit() {
+        this.searchField.valueChanges
+        .pipe(takeUntil(this.onDestroy$))
+        .subscribe( (value: string) => {
+            this.searchCloudService.value.next(value);
+        });
+    }
+
+    ngOnDestroy() {
+        this.onDestroy$.next();
+        this.onDestroy$.complete();
+    }
+}
