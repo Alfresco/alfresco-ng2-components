d5a2d4e3a779a75fda44b3f6e5833bf494033d2d
Silviu Popa <silviucpopa@gmail.com>
2019-11-08 17:34:00 +0200

    rebase and add test

--- lib/process-services-cloud/src/lib/task/task-header/components/task-header-cloud.component.spec.ts
+++ lib/process-services-cloud/src/lib/task/task-header/components/task-header-cloud.component.spec.ts
@@ -139,7 +139,7 @@ describe('TaskHeaderCloudComponent', () => {
 
             fixture.whenStable().then(() => {
                 const valueEl = fixture.debugElement.query(By.css('[data-automation-id="header-dueDate"] .adf-property-value'));
-                expect(valueEl.nativeElement.innerText.trim()).toBe('Dec 18, 2018');
+                expect(valueEl.nativeElement.innerText.trim()).toBe(moment(assignedTaskDetailsCloudMock.dueDate, 'x').format('MMM D, Y, H:mm'));
             });
         }));
 
