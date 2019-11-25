27934b847f4e0f10df4591ecea9ba52f5d05e582
Eugenio Romano <eugenioromano16@gmail.com>
2019-11-25 12:29:51 +0000

    improve tag script

--- scripts/travis/release/git-tag.sh
+++ scripts/travis/release/git-tag.sh
@@ -11,6 +11,6 @@ git config --local user.name "alfresco-build"
 git config --local user.email "build@alfresco.com"
 git tag -a ${VERSION} -m "${VERSION} [ci skip] "
 git remote rm origin
-GITHUB_REPO=https://$GITHUB_TOKEN:x-oauth-basic@github.com/Alfresco/alfresco-js-api.git
+GITHUB_REPO=https://$GITHUB_TOKEN:x-oauth-basic@github.com/Alfresco/alfresco-ng2-components.git
 git remote add origin $GITHUB_REPO
 git push origin --tags
