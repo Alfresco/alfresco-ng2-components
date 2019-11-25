0fb954dd115ca29cad3635720a7f0d9102eb4e05
Eugenio Romano <eugenio.romano@alfresco.com>
2019-11-25 13:11:23 +0000

    gix tag script

--- scripts/travis/release/git-tag.sh
+++ scripts/travis/release/git-tag.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[  $TRAVIS_BRANCH == "master" ]]; then
-    VERSION=$(npm view @alfresco/adf-core@beta version)
-else
     VERSION=$(grep -m1 version package.json | awk '{ print $2 }' | sed 's/[", ]//g')
+else
+    VERSION=$(npm view @alfresco/adf-core@beta version)
 fi;
 
 echo "git tag -a ${VERSION} -m ${VERSION}"
@@ -12,5 +12,5 @@ git config --local user.email "build@alfresco.com"
 git tag -a ${VERSION} -m "${VERSION} [ci skip] "
 git remote rm origin
 GITHUB_REPO=https://$GITHUB_TOKEN:x-oauth-basic@github.com/Alfresco/alfresco-ng2-components.git
-git remote add origin $GITHUB_REPO
+git-tag.shgit remote add origin $GITHUB_REPO
 git push origin --tags
