a1a9f33daeb486e93ef63a22288df3c10f1681eb
Eugenio Romano <eugenioromano16@gmail.com>
2019-11-25 12:24:33 +0000

    improve tag script

--- scripts/travis/release/git-tag.sh
+++ scripts/travis/release/git-tag.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[  $TRAVIS_BRANCH == "master" ]]; then
-    TAG_VERSION=$(grep -m1 version package.json | awk '{ print $2 }' | sed 's/[", ]//g')
+    VERSION=$(npm view @alfresco/adf-core@beta version)
 else
-    TAG_VERSION=$(npm view @alfresco/adf-core@beta version)
+    VERSION=$(grep -m1 version package.json | awk '{ print $2 }' | sed 's/[", ]//g')
 fi;
 
 echo "git tag -a ${VERSION} -m ${VERSION}"
@@ -11,6 +11,6 @@ git config --local user.name "alfresco-build"
 git config --local user.email "build@alfresco.com"
 git tag -a ${VERSION} -m "${VERSION} [ci skip] "
 git remote rm origin
-GITHUB_REPO=https://$GITHUB_TOKEN:x-oauth-basic@github.com/Alfresco/alfresco-ng2-components.git
+GITHUB_REPO=https://$GITHUB_TOKEN:x-oauth-basic@github.com/Alfresco/alfresco-js-api.git
 git remote add origin $GITHUB_REPO
 git push origin --tags
