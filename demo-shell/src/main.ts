efc5f8e31c14e05092887f23ba439bfbda3ca9a5
Denys Vuika <denys.vuika@gmail.com>
2019-11-26 12:40:59 +0000

    restore hammer

--- demo-shell/src/main.ts
+++ demo-shell/src/main.ts
@@ -21,6 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import 'hammerjs';
 import 'chart.js';
 import 'ng2-charts';
 
