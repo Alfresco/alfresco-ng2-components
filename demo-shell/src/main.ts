782a8f1c9ff6a0113ba493c5a064e89a0452e1e5
Denys Vuika <denys.vuika@gmail.com>
2019-11-26 11:03:06 +0000

    remove hammerjs dependency

--- demo-shell/src/main.ts
+++ demo-shell/src/main.ts
@@ -21,7 +21,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import 'hammerjs';
 import 'chart.js';
 import 'ng2-charts';
 
