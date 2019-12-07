ffca6334b0aa53d07329cf199a44618450ad576e
Eugenio Romano <eugenio.romano@alfresco.com>
2019-12-07 11:33:03 +0000

    fix compilation issue

--- lib/process-services-cloud/src/lib/people/people-cloud.module.ts
+++ lib/process-services-cloud/src/lib/people/people-cloud.module.ts
@@ -19,13 +19,20 @@ import { NgModule } from '@angular/core';
 import { PeopleCloudComponent } from './components/people-cloud.component';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material.module';
-import { PipeModule } from '@alfresco/adf-core';
+import { TemplateModule, CoreModule, PipeModule } from '@alfresco/adf-core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     imports: [
-        PipeModule,
         CommonModule,
-        MaterialModule
+        PipeModule,
+        TemplateModule,
+        FlexLayoutModule,
+        MaterialModule,
+        FormsModule,
+        ReactiveFormsModule,
+        CoreModule
     ],
     declarations: [PeopleCloudComponent],
     exports: [
