c7839a3a9cd14ef6a74b91897ed51dffec9175f1
Martin Mueller <damadden88@googlemail.com>
2019-12-13 16:06:42 +0100

    Try to set a sleep for rendition

--- lib/content-services/src/lib/content-node-share/content-node-share.settings.ts
+++ lib/content-services/src/lib/content-node-share/content-node-share.settings.ts
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// trigger build
+// trigger build again
 
 import { NodeEntry } from '@alfresco/js-api';
 
