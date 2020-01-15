eeb161f2720e5cf38d56cb723d8108d15333de67
Cristina Jalba <cristina.jalba@ness.com>
2020-01-15 12:18:00 +0200

    Renamed the method

--- lib/testing/src/lib/core/pages/pagination.page.ts
+++ lib/testing/src/lib/core/pages/pagination.page.ts
@@ -128,7 +128,7 @@ export class PaginationPage {
     /*
      * Wait until the total number of items is less then specified value
      */
-    async waitUntilNrOfItemsIsLessThenValue(expectedValue: number): Promise<any> {
+    async waitUntilNoOfItemsIsLessThenValue(expectedValue: number): Promise<any> {
         await BrowserVisibility.waitUntilElementIsVisible(this.totalFiles);
         const condition = () => this.totalFiles.getText().then(value => value && +value.split('of ')[1] < expectedValue);
         return browser.wait(condition, 10000);
