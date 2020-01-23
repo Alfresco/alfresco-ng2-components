5fbd304dde59ab85a8fafa8516b164a5f1d02206
Martin Mueller <damadden88@googlemail.com>
2020-01-23 20:47:12 +0100

    Check if oauth is enabled

--- lib/core/services/auth-guard-ecm.service.ts
+++ lib/core/services/auth-guard-ecm.service.ts
@@ -41,11 +41,11 @@ export class AuthGuardEcm extends AuthGuardBase {
         if (this.authenticationService.isEcmLoggedIn() || this.withCredentials) {
             return true;
         }
+        this.redirectToUrl('ECM', redirectUrl);
         if (!this.authenticationService.isEcmLoggedIn() && this.isSilentLogin() && !this.authenticationService.isPublicUrl()) {
             this.authenticationService.ssoImplicitLogin();
             return true;
         }
-        this.redirectToUrl('ECM', redirectUrl);
         return false;
     }
 }
