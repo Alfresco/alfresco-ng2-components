81efb072832d95865ed3bd38a1504b5114b69754
Maurizio Vitale <maurizio.vitale@alfresco.com>
2020-02-24 17:10:40 +0000

    adf-cli Provide a way to pass buildArgs to the docker build (#5507)
    * Provide a way to pass buildArgs to the docker build
    
    * Create folder in case does not exist

--- scripts/build/build-cli.sh
+++ scripts/build/build-cli.sh
@@ -11,9 +11,17 @@ echo "====== Build ======"
 npm run dist
 
 cd $DIR/../../
-rm -rf ./lib/dist/cli/ && \
-cp -R ./lib/cli/dist lib/dist/cli/
+if [ -e "./lib/dist/cli" ];
+then
+    rm -rf "./lib/dist/cli" ;
+else if [ ! -d "./lib/dist" ];
+    then
+    mkdir ./lib/dist
+    fi
+fi
 
+cp -R ./lib/cli/dist lib/dist/cli/
+exit 0
 echo "====== Move to node_modules ======"
 rm -rf ./node_modules/@alfresco/adf-cli/ && \
 mkdir -p ./node_modules/@alfresco/adf-cli/ && \
