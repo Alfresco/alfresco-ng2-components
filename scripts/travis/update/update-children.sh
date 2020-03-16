becf45d1500082c088b9541edefb9ec26e454ea2
Eugenio Romano <eromano@users.noreply.github.com>
2020-03-16 14:52:11 +0000

    Update update-children.sh

--- scripts/travis/update/update-children.sh
+++ scripts/travis/update/update-children.sh
@@ -19,5 +19,4 @@ echo "Update AMA"
 ./scripts/travis/update/update-project.sh -gnu -t $GITHUB_ENTERPRISE_TOKEN -n 'Alfresco/alfresco-modeler-app' -v $VERSION -vjs $JS_VERSION
 echo "Update Workspace"
 ./scripts/travis/update/update-project.sh -gnu -t $GITHUB_ENTERPRISE_TOKEN -n 'Alfresco/alfresco-process-workspace-app' -v $VERSION -vjs $JS_VERSION
-echo "Update APA Activiti"
-./scripts/travis/update/update-project.sh -gnu -t $GITHUB_ENTERPRISE_TOKEN -n 'Alfresco/alfresco-admin-app' -v $VERSION -vjs $JS_VERSION
+
