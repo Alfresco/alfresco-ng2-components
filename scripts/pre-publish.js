672ef3d45d467aafa68663dc7e7c0ca81c9da341
Eugenio Romano <eugenio.romano@alfresco.com>
2020-05-04 17:46:22 +0100

    fix pre-publish.sh

--- scripts/pre-publish.js
+++ scripts/pre-publish.js
@@ -9,6 +9,14 @@ Object.keys(file.dependencies).forEach((currentDep)=>{
     }
 });
 
+Object.keys(file.devDependencies).forEach((currentDep)=>{
+
+    if(currentDep.indexOf("@alfresco/adf-")>=0) {
+        delete file.devDependencies[currentDep];
+    }
+});
+
+
 fs.writeFileSync('package.json', JSON.stringify(file), function (err) {
     if (err) return console.log(err);
 });
