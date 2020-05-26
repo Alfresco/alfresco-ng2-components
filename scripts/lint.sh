f2c5bfc7f3faf6c0c198d9660b899648f35c31a0
Denys Vuika <denys.vuika@gmail.com>
2020-05-26 18:22:21 +0100

    lint fixes

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -10,14 +10,20 @@ show_help() {
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR/../"
 
-echo "====== Parallel lint ====="
-
-npx concurrently -s "all" "npm run lint-lib || exit 1" "npm run stylelint || exit 1" "npm run spellcheck || exit " "ng lint dev || exit 1" "npm run lint-e2e || exit 1" || exit 1
-
 if grep "envalfresco" . -R --exclude-dir={node_modules,.history,.idea,scripts,dist,e2e-output} --exclude=".env.cloud"; then
     echo not permitted word
     exit 1
 fi
 
-echo "====== License Check ====="
-npm run license-checker || exit 1
+npm run ng -- lint extensions && \
+npm run ng -- lint core && \
+npm run ng -- lint content-services && \
+npm run ng -- lint process-services && \
+npm run ng -- lint process-services-cloud && \
+npm run ng -- lint insights && \
+npm run ng -- lint testing && \
+npm run ng -- lint dev && \
+npm run lint-e2e && \
+npm run stylelint && \
+npm run spellcheck && \
+npm run license-checker
