69ed3a86f19e32d36b8efe2e6e488e35f2972349
Eugenio Romano <eugenio.romano@alfresco.com>
2020-06-04 03:04:56 +0100

    not use npx

--- scripts/app-config-replace.js
+++ scripts/app-config-replace.js
@@ -12,8 +12,8 @@ fs.readFile(configPath, (err, appConfigString) => {
     appConfig.providers = process.env.PROVIDERS || 'ALL';
     appConfig.bpmHost = (process.env.PROXY_HOST_ADF || process.env.URL_HOST_ADF);
     appConfig.ecmHost = (process.env.PROXY_HOST_ADF || process.env.URL_HOST_ADF);
-    appConfig.identityHost = process.env.URL_HOST_IDENTITY + '/auth/admin/realms/alfresco';
-    appConfig.oauth2.host = process.env.HOST_SSO + '/auth/realms/alfresco';
+    appConfig.identityHost = (process.env.HOST_SSO + '/auth/admin/realms/alfresco');
+    appConfig.oauth2.host = (process.env.HOST_SSO + '/auth/realms/alfresco');
     appConfig.notificationDefaultDuration = process.env.NOTIFICATION_LAST || 8000;
     appConfig.authType = process.env.AUTH_TYPE || 'BASIC';
 
