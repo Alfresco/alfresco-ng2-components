0a49d43c8dde1468109836c2b860c8f7bfb0b6d4
Silviu Popa <p3701014@L3700101120.ness.com>
2020-06-05 18:31:00 +0300

    fix unit test

--- lib/process-services-cloud/src/lib/task/task-filters/services/task-filter-cloud.service.spec.ts
+++ lib/process-services-cloud/src/lib/task/task-filters/services/task-filter-cloud.service.spec.ts
@@ -259,23 +259,24 @@ describe('Inject [LocalPreferenceCloudService] into the TaskFilterCloudService',
             expect(res[0].status).toEqual('ASSIGNED');
             expect(res[0].assignee).toEqual(identityUserMock.username);
 
-            expect(res[1].name).toEqual('ADF_CLOUD_TASK_FILTERS.COMPLETED_TASKS');
-            expect(res[1].key).toEqual('completed-tasks');
-            expect(res[1].appName).toEqual(appName);
-            expect(res[1].icon).toEqual('done');
-            expect(res[1].status).toEqual('COMPLETED');
-
             expect(res[1].name).toEqual('ADF_CLOUD_TASK_FILTERS.QUEUED_TASKS');
             expect(res[1].key).toEqual('queued-tasks');
             expect(res[1].appName).toEqual(appName);
-            expect(res[1].icon).toEqual('done');
+            expect(res[1].icon).toEqual('queue');
             expect(res[1].status).toEqual('CREATED');
+
+            expect(res[2].name).toEqual('ADF_CLOUD_TASK_FILTERS.COMPLETED_TASKS');
+            expect(res[2].key).toEqual('completed-tasks');
+            expect(res[2].appName).toEqual(appName);
+            expect(res[2].icon).toEqual('done');
+            expect(res[2].status).toEqual('COMPLETED');
+
             done();
         });
         expect(getPreferencesSpy).toHaveBeenCalled();
 
         const localData = JSON.parse(localStorage.getItem(`task-filters-${appName}-${identityUserMock.username}`));
-        expect(localData.length).toEqual(2);
+        expect(localData.length).toEqual(3);
 
         expect(localData[0].name).toEqual('ADF_CLOUD_TASK_FILTERS.MY_TASKS');
         expect(localData[0].key).toEqual('my-tasks');
@@ -284,10 +285,16 @@ describe('Inject [LocalPreferenceCloudService] into the TaskFilterCloudService',
         expect(localData[0].status).toEqual('ASSIGNED');
         expect(localData[0].assignee).toEqual(identityUserMock.username);
 
-        expect(localData[1].name).toEqual('ADF_CLOUD_TASK_FILTERS.COMPLETED_TASKS');
-        expect(localData[1].key).toEqual('completed-tasks');
+        expect(localData[1].name).toEqual('ADF_CLOUD_TASK_FILTERS.QUEUED_TASKS');
+        expect(localData[1].key).toEqual('queued-tasks');
         expect(localData[1].appName).toEqual(appName);
-        expect(localData[1].icon).toEqual('done');
-        expect(localData[1].status).toEqual('COMPLETED');
+        expect(localData[1].icon).toEqual('queue');
+        expect(localData[1].status).toEqual('CREATED');
+
+        expect(localData[2].name).toEqual('ADF_CLOUD_TASK_FILTERS.COMPLETED_TASKS');
+        expect(localData[2].key).toEqual('completed-tasks');
+        expect(localData[2].appName).toEqual(appName);
+        expect(localData[2].icon).toEqual('done');
+        expect(localData[2].status).toEqual('COMPLETED');
     });
 });
