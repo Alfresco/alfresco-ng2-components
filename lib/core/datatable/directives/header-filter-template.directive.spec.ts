aa07d105e3af38e4c95d5adbeaf0b459e9a6242a
BaptisteMahe <mahe.baptiste.19@gmail.com>
2020-06-09 11:16:22 +0200

    [ACA-3426] Allow custom header filters inside document-list

---
+++ lib/core/datatable/directives/header-filter-template.directive.spec.ts
@@ -0,0 +1,54 @@
+/*!
+ * @license
+ * Copyright 2019 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { DataTableComponent } from '../components/datatable/datatable.component';
+import { HeaderFilterTemplateDirective } from './header-filter-template.directive';
+import { setupTestBed } from '../../testing/setup-test-bed';
+import { CoreTestingModule } from '../../testing/core.testing.module';
+import { TranslateModule } from '@ngx-translate/core';
+
+describe('HeaderFilterTemplateDirective', () => {
+
+    let fixture: ComponentFixture<DataTableComponent>;
+    let dataTable: DataTableComponent;
+    let directive: HeaderFilterTemplateDirective;
+
+    setupTestBed({
+        imports: [
+            TranslateModule.forRoot(),
+            CoreTestingModule
+        ]
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(DataTableComponent);
+        dataTable = fixture.componentInstance;
+        directive = new HeaderFilterTemplateDirective(dataTable);
+    });
+
+    afterEach(() => {
+        fixture.destroy();
+    });
+
+    it('applies template to the datatable', () => {
+        const template: any = 'test template';
+        directive.template = template;
+        directive.ngAfterContentInit();
+        expect(dataTable.headerFilterTemplate).toBe(template);
+    });
+});
