f76e0cd0bb883b3084d5d479955c2e11144b1784
arditdomi <32884230+arditdomi@users.noreply.github.com>
2020-06-24 13:35:47 +0100

    [AAE-2840] Not able to complete a task with attach file widget with mappings (#5809)
    * [AAE-2840] Check pipeline with possible fix
    
    * Remove update form loop causing overwriting of the form values
    
    * Revert ACA-3235 causing form values regression

--- lib/core/form/components/form-field/form-field.component.ts
+++ lib/core/form/components/form-field/form-field.component.ts
@@ -98,7 +98,6 @@ export class FormFieldComponent implements OnInit, OnDestroy {
                     instance.fieldChanged.subscribe((field) => {
                         if (field && this.field.form) {
                             this.visibilityService.refreshVisibility(field.form);
-                            this.visibilityService.removeInvisibleFormValues(field.form);
                             field.form.onFormFieldChanged(field);
                         }
                     });
