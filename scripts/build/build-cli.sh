00a2a300983b95f146b783085c2f9f7d0cac923f
maurizio vitale <maurizio.vitale@alfresco.com>
2020-07-06 13:44:10 +0100

    return the release project

--- scripts/build/build-cli.sh
+++ scripts/build/build-cli.sh
@@ -20,3 +20,8 @@ else if [ ! -d "./lib/dist" ];
 fi
 
 cp -R ./lib/cli/dist lib/dist/cli/
+
+echo "====== Move to node_modules ======"
+rm -rf ./node_modules/@alfresco/adf-cli/ && \
+mkdir -p ./node_modules/@alfresco/adf-cli/ && \
+cp -R ./lib/dist/cli/* ./node_modules/@alfresco/adf-cli/
