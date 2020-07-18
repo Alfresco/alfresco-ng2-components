6358f4b318f2cf8be2744ed764bbe95882b656d8
Eugenio Romano <eugenio.romano@alfresco.com>
2020-07-18 22:08:09 +0100

    fix build

--- scripts/build/build-testing.sh
+++ scripts/build/build-testing.sh
@@ -7,18 +7,10 @@ cd $DIR/../..
 echo "====== Testing ======"
 echo "====== Build ======"
 
-npm run ng-packagr -- -p ./lib/testing/
-if [ "$CI" = "true" ]; then
-    echo "Building testing for production"
-    nx -- build testing --prod || exit 1
-else
-    echo "Building testing for development"
-    nx build testing || exit 1
-fi
+nx build testing || exit 1
 
 echo "====== Move to node_modules ======"
 rm -rf ./node_modules/@alfresco/adf-testing/ && \
 mkdir -p ./node_modules/@alfresco/adf-testing/ && \
 cp -R ./lib/dist/testing/* ./node_modules/@alfresco/adf-testing/
-nx build testing || exit 1
 
