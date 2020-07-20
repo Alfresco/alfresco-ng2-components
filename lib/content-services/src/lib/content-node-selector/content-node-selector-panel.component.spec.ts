35142620fa4eae0463942cbfe052866cbc7831df
maurizio vitale <maurizio.vitale@alfresco.com>
2020-07-20 16:15:55 +0100

    Add unit test

--- lib/content-services/src/lib/content-node-selector/content-node-selector-panel.component.spec.ts
+++ lib/content-services/src/lib/content-node-selector/content-node-selector-panel.component.spec.ts
@@ -634,6 +634,19 @@ describe('ContentNodeSelectorComponent', () => {
                 expect(component.showingSearchResults).toBeFalsy();
             });
 
+            it('should restrict the search to the root in case is defined', () => {
+                component.root = 'my-root-id';
+                component.search('search');
+                expect(cnSearchSpy).toHaveBeenCalledWith('search', 'my-root-id', 0, 25, [], false);
+            });
+
+            it('should restrict the search to the site and not to the root in case is defined', () => {
+                component.root = 'my-root-id';
+                component.siteId = 'my-site-id';
+                component.search('search');
+                expect(cnSearchSpy).toHaveBeenCalledWith('search', 'my-site-id', 0, 25, [], false);
+            });
+
             it('should clear the search field, nodes and chosenNode when deleting the search input', fakeAsync(() => {
                 spyOn(component, 'clear').and.callThrough();
                 typeToSearchBox('a');
