23852721a9e5ead9c89ac47b92b52362e8d175d6
Eugenio Romano <eugenio.romano@alfresco.com>
2020-07-22 22:22:13 +0100

    logout if not login

--- lib/core/services/authentication.service.ts
+++ lib/core/services/authentication.service.ts
@@ -164,15 +164,17 @@ export class AuthenticationService {
      * Logs the user out.
      * @returns Response event called when logout is complete
      */
-    logout() {
-        return from(this.callApiLogout())
-            .pipe(
-                tap((response) => {
-                    this.onLogout.next(response);
-                    return response;
-                }),
-                catchError((err) => this.handleError(err))
-            );
+    logout(): Promise<any> {
+        if (this.isLoggedIn) {
+            return from(this.callApiLogout())
+                .pipe(
+                    tap((response) => {
+                        this.onLogout.next(response);
+                        return response;
+                    }),
+                    catchError((err) => this.handleError(err))
+                );
+        }
     }
 
     private callApiLogout(): Promise<any> {
@@ -265,7 +267,7 @@ export class AuthenticationService {
      * @returns The redirect URL
      */
     getRedirect(): string {
-        const provider = <string> this.appConfig.get(AppConfigValues.PROVIDERS);
+        const provider = <string>this.appConfig.get(AppConfigValues.PROVIDERS);
         return this.hasValidRedirection(provider) ? this.redirectUrl.url : null;
     }
 
