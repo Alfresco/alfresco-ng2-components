de3ba4765dafef59877ca33fc8aad95697d48069
Eugenio Romano <eugenio.romano@alfresco.com>
2020-08-30 20:36:55 +0100

    fix

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -222,7 +222,7 @@ exports.config = {
         jasmine.getEnv().addReporter(
             new SpecReporter({
                 spec: {
-                    displayStacktrace: 'pretty',
+                    displayStacktrace: 'raw',
                     displayDuration: true
                 }
             })
