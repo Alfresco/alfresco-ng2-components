296d682c9f22514105f88abcc4ab97be42f776e0
AleAb25 <abrudanalexandra31@gmail.com>
2020-09-18 14:49:53 +0100

    [ADF-4481] e2e test for uploading an unsupported file

--- e2e/core/viewer/viewer-content-services-component.e2e.ts
+++ e2e/core/viewer/viewer-content-services-component.e2e.ts
@@ -28,6 +28,8 @@ import {
 import { ContentServicesPage } from '../../core/pages/content-services.page';
 import { FileModel } from '../../models/ACS/file.model';
 import { NavigationBarPage } from '../../core/pages/navigation-bar.page';
+import { VersionManagePage } from '../pages/version-manager.page';
+import { MetadataViewPage } from '../pages/metadata-view.page';
 
 describe('Content Services Viewer', () => {
     const acsUser = new UserModel();
@@ -35,6 +37,8 @@ describe('Content Services Viewer', () => {
     const contentServicesPage = new ContentServicesPage();
     const loginPage = new LoginPage();
     const navigationBarPage = new NavigationBarPage();
+    const versionManagePage = new VersionManagePage();
+    const metadataViewPage = new MetadataViewPage();
 
     let zoom;
 
@@ -67,6 +71,10 @@ describe('Content Services Viewer', () => {
     });
     const unsupportedFile = new FileModel({
         'location': browser.params.resources.Files.ADF_DOCUMENTS.UNSUPPORTED.file_path,
+        'name': browser.params.resources.Files.ADF_DOCUMENTS.UNSUPPORTED.file_name
+    });
+    const unsupportedFileByLocation = new FileModel({
+        'location': browser.params.resources.Files.ADF_DOCUMENTS.UNSUPPORTED.file_location,
         'name': browser.params.resources.Files.ADF_DOCUMENTS.UNSUPPORTED.file_name
     });
     const pptFile = new FileModel({
@@ -410,4 +418,78 @@ describe('Content Services Viewer', () => {
         await viewerPage.clickClosePasswordDialog();
         await contentServicesPage.checkContentIsDisplayed(protectedFile.name);
     });
+});
+
+    describe('Viewer - version update with unsupported file', () => {
+        beforeAll(async () => {
+            await apiService.getInstance().login(browser.params.testConfig.admin.email, browser.params.testConfig.admin.password);
+            await usersActions.createUser(acsUser);
+            await apiService.getInstance().login(acsUser.email, acsUser.password);
+
+            const pdfFileUploaded = await uploadActions.uploadFile(pdfFile.location, pdfFile.name, '-my-');
+            Object.assign(pdfFile, pdfFileUploaded.entry);
+
+            const jpgFileUploaded = await uploadActions.uploadFile(jpgFile.location, jpgFile.name, '-my-');
+            Object.assign(jpgFile, jpgFileUploaded.entry);
+
+            const mp4FileUploaded = await uploadActions.uploadFile(mp4File.location, mp4File.name, '-my-');
+            Object.assign(mp4File, mp4FileUploaded.entry);
+
+            await loginPage.login(acsUser.email, acsUser.password);
+            await contentServicesPage.goToDocumentList();
+        });
+
+        afterAll(async () => {
+            try {
+                await uploadActions.deleteFileOrFolder(pdfFile.getId());
+                await uploadActions.deleteFileOrFolder(jpgFile.getId());
+                await uploadActions.deleteFileOrFolder(mp4File.getId());
+                await navigationBarPage.clickLogoutButton();
+            } catch (error) {
+                throw new Error(`cleanup afterAll call failed with error ${error}`);
+            }
+        });
+
+        it('[C587084] Should display the preview for an unsupported file', async () => {
+            await uploadNewVersion(jpgFile.name, unsupportedFileByLocation.location);
+            await previewUnsupportedFile(unsupportedFileByLocation.name);
+
+            await changeFileNameInViewer(unsupportedFileByLocation.name, 'generic-unsupported-file-2nd.3DS');
+            await uploadNewVersion(pdfFile.name, unsupportedFileByLocation.location);
+            await previewUnsupportedFile(unsupportedFileByLocation.name);
+
+            await changeFileNameInViewer(unsupportedFileByLocation.name, 'generic-unsupported-file-3rd.3DS');
+            await uploadNewVersion(mp4File.name, unsupportedFileByLocation.location);
+            await previewUnsupportedFile(unsupportedFileByLocation.name);
+        });
+
+        async function uploadNewVersion(originalFileName: string, newVersionLocation: string): Promise<void> {
+            await viewerPage.viewFile(originalFileName);
+            await viewerPage.clickCloseButton();
+            await contentServicesPage.versionManagerContent(originalFileName);
+            await BrowserActions.click(versionManagePage.showNewVersionButton);
+            await versionManagePage.uploadNewVersionFile(newVersionLocation);
+            await versionManagePage.clickActionButton('1.1');
+            await versionManagePage.checkActionsArePresent('1.1');
+            await versionManagePage.closeActionsMenu();
+            await versionManagePage.closeVersionDialog();
+            await browser.refresh();
+        }
+        async function previewUnsupportedFile(unsupportedFileName: string): Promise<void> {
+            await viewerPage.viewFile(unsupportedFileName);
+            await viewerPage.checkUnknownFormatIsDisplayed();
+            await expect(await viewerPage.getUnknownFormatMessage()).toBe('Couldn\'t load preview. Unknown format.');
+            await viewerPage.clickCloseButton();
+        }
+        async function changeFileNameInViewer(fileName: string, newName: string): Promise<void> {
+            await viewerPage.viewFile(fileName);
+            await viewerPage.clickInfoButton();
+            await viewerPage.checkInfoSideBarIsDisplayed();
+            await viewerPage.clickOnTab('Properties');
+            await viewerPage.checkTabIsActive('Properties');
+            await metadataViewPage.editIconClick();
+            await  metadataViewPage.enterPropertyText('name', newName);
+            await metadataViewPage.clickSaveMetadata();
+            await viewerPage.clickCloseButton();
+        }
 });
