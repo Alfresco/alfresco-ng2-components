9fd5a32f0b33e3c725c510feb7184a4700d385b6
Eugenio Romano <eugenio.romano@alfresco.com>
2020-09-30 10:54:33 +0100

    fix beta release script

--- scripts/travis/release/release-npm.sh
+++ scripts/travis/release/release-npm.sh
@@ -4,20 +4,21 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR/../../../
 
-if [[ "${TRAVIS_EVENT_TYPE}" == "push" ]];
+if [ $TRAVIS_EVENT_TYPE == "push"  ] || [ $TRAVIS_EVENT_TYPE == "cron" ]
 then
     TAG_NPM=latest
     if [[ $TRAVIS_BRANCH == "develop" ]];
     then
         TAG_NPM=alpha
-        if [[ $TRAVIS_EVENT_TYPE == "cron" ]];
-        then
-            TAG_NPM=beta
-        fi
+    fi
+
+    if [[ $TRAVIS_EVENT_TYPE == "cron" ]];
+    then
+        TAG_NPM=beta
     fi
 
     echo "Publishing on npm with tag $TAG_NPM"
     npx @alfresco/adf-cli@alpha npm-publish --npmRegistry $NPM_REGISTRY_ADDRESS --tokenRegistry $NPM_REGISTRY_TOKEN --tag $TAG_NPM --pathProject "$(pwd)"
 else
-     echo "PR No need to release in NPM"
+    echo "PR No need to release in NPM"
 fi;
