a3b49fda31d6e8b11ec7596c6e8b18303d426321
iuliaib <iulia.burca@ness.com>
2020-11-04 17:01:22 +0200

    add more options for filtering the list

--- lib/testing/src/lib/process-services/actions/process.util.ts
+++ lib/testing/src/lib/process-services/actions/process.util.ts
@@ -74,11 +74,11 @@ export class ProcessUtil {
         }
     }
 
-    async getProcessInstanceByName(processInstanceName: string): Promise<any> {
+    async getProcessInstanceByName(processInstanceName: string, processInstanceStatus?: string, maxNumberOfResults?: number): Promise<any> {
         try {
-            return await this.api.apiService.activiti.processApi.filterProcessInstances({filter: {name: processInstanceName}});
+            return await this.api.apiService.activiti.processApi.filterProcessInstances({filter: {name: processInstanceName, state: processInstanceStatus}, size: maxNumberOfResults});
         } catch (error) {
-            Logger.error('Get Process Instance by Name - Service error, Response: ', JSON.parse(JSON.stringify(error)));
+            Logger.error('List process instances using a filter - Service error, Response: ', JSON.parse(JSON.stringify(error)));
         }
     }
 
