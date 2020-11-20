94f07fa123f40b578d5d0888480054332702810e
Eugenio Romano <eugenio.romano@alfresco.com>
2020-11-20 09:18:50 +0000

    log

--- scripts/ci/check-env/check-cs-env.sh
+++ scripts/ci/check-env/check-cs-env.sh
@@ -4,4 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR/../../../
 
+echo "====== Check content UP ====="
+
 ./node_modules/@alfresco/adf-cli/bin/adf-cli check-cs-env --host "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" || exit 1
--- scripts/ci/check-env/check-ps-env.sh
+++ scripts/ci/check-env/check-ps-env.sh
@@ -4,4 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR/../../../
 
+echo "====== Check PS UP ====="
+
 #./node_modules/@alfresco/adf-cli/bin/adf-cli check-ps-env --host "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" || exit 1 TODO chanvge login check SSO
