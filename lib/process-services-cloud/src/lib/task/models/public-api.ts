bb45d9793c78b8a40f392d120292c23188964e4c
silviu popa <silviucpopa@gmail.com>
2020-11-25 16:04:08 +0200

    export default priority values

--- lib/process-services-cloud/src/lib/task/models/public-api.ts
+++ lib/process-services-cloud/src/lib/task/models/public-api.ts
@@ -15,15 +15,4 @@
  * limitations under the License.
  */
 
-export * from './task-list/public-api';
-export * from './task-filters/public-api';
-export * from './start-task/public-api';
-export * from './task-header/public-api';
-export * from './task-form/public-api';
-export * from './directives/public-api';
-export * from './models/public-api';
-
-export * from './services/task-cloud.service';
-export * from './services/start-task-cloud.service';
-
-export * from './task-cloud.module';
+ export * from './task.model';
