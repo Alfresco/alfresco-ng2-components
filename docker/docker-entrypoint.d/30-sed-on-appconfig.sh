419c47b87ea1b081bc1679812c8a62547aec8baa
Marcello Teodori <151025+mteodori@users.noreply.github.com>
2020-11-30 22:59:31 +0100

    AAE-4023 fix BASE_PATH as / (#6397)

--- docker/docker-entrypoint.d/30-sed-on-appconfig.sh
+++ docker/docker-entrypoint.d/30-sed-on-appconfig.sh
@@ -1,81 +1,79 @@
 #!/bin/sh
 
-if [ -n "${APP_CONFIG_AUTH_TYPE}" ];then
+set -e
+
+echo Running sed on "${NGINX_ENVSUBST_OUTPUT_DIR}/app.config.json"
+
+if [ -n "${APP_CONFIG_AUTH_TYPE}" ]; then
   sed -e "s/\"authType\": \".*\"/\"authType\": \"${APP_CONFIG_AUTH_TYPE}\"/g" \
-    -i ./app.config.json
+    -i "${NGINX_ENVSUBST_OUTPUT_DIR}/app.config.json"
 fi
 
 if [ -n "${APP_CONFIG_IDENTITY_HOST}" ]; then
   replace="\/"
   encodedIdentity=${APP_CONFIG_IDENTITY_HOST//\//$replace}
   sed -e "s/\"identityHost\": \".*\"/\"identityHost\": \"$encodedIdentity\"/g" \
-    -i ./app.config.json
+    -i "${NGINX_ENVSUBST_OUTPUT_DIR}/app.config.json"
 fi
 
-if [ -n "${APP_CONFIG_OAUTH2_HOST}" ];then
+if [ -n "${APP_CONFIG_OAUTH2_HOST}" ]; then
   replace="\/"
   encoded=${APP_CONFIG_OAUTH2_HOST//\//$replace}
   sed -e "s/\"host\": \".*\"/\"host\": \"${encoded}\"/g" \
-    -i ./app.config.json
+    -i "${NGINX_ENVSUBST_OUTPUT_DIR}/app.config.json"
 fi
 
-if [ -n "${APP_CONFIG_OAUTH2_CLIENTID}" ];then
+if [ -n "${APP_CONFIG_OAUTH2_CLIENTID}" ]; then
   sed -e "s/\"clientId\": \".*\"/\"clientId\": \"${APP_CONFIG_OAUTH2_CLIENTID}\"/g" \
-    -i ./app.config.json
+    -i "${NGINX_ENVSUBST_OUTPUT_DIR}/app.config.json"
 fi
 
-if [ -n "${APP_CONFIG_OAUTH2_IMPLICIT_FLOW}" ];then
+if [ -n "${APP_CONFIG_OAUTH2_IMPLICIT_FLOW}" ]; then
   sed -e "s/\"implicitFlow\": [^,]*/\"implicitFlow\": ${APP_CONFIG_OAUTH2_IMPLICIT_FLOW}/g" \
-    -i ./app.config.json
+    -i "${NGINX_ENVSUBST_OUTPUT_DIR}/app.config.json"
 fi
 
-if [ -n "${APP_CONFIG_OAUTH2_SILENT_LOGIN}" ];then
+if [ -n "${APP_CONFIG_OAUTH2_SILENT_LOGIN}" ]; then
   sed -e "s/\"silentLogin\": [^,]*/\"silentLogin\": ${APP_CONFIG_OAUTH2_SILENT_LOGIN}/g" \
-    -i ./app.config.json
+    -i "${NGINX_ENVSUBST_OUTPUT_DIR}/app.config.json"
 fi
 
-if [ -n "${APP_CONFIG_OAUTH2_REDIRECT_SILENT_IFRAME_URI}" ];then
+if [ -n "${APP_CONFIG_OAUTH2_REDIRECT_SILENT_IFRAME_URI}" ]; then
   replace="\/"
   encoded=${APP_CONFIG_OAUTH2_REDIRECT_SILENT_IFRAME_URI//\//$replace}
   sed -e "s/\"redirectSilentIframeUri\": \".*\"/\"redirectSilentIframeUri\": \"${encoded}\"/g" \
-    -i ./app.config.json
+    -i "${NGINX_ENVSUBST_OUTPUT_DIR}/app.config.json"
 fi
 
-if [ -n "${APP_CONFIG_OAUTH2_REDIRECT_LOGIN}" ];then
+if [ -n "${APP_CONFIG_OAUTH2_REDIRECT_LOGIN}" ]; then
   replace="\/"
   encoded=${APP_CONFIG_OAUTH2_REDIRECT_LOGIN//\//$replace}
   sed -e "s/\"redirectUri\": \".*\"/\"redirectUri\": \"${encoded}\"/g" \
-    -i ./app.config.json
+    -i "${NGINX_ENVSUBST_OUTPUT_DIR}/app.config.json"
 fi
 
-if [ -n "${APP_CONFIG_OAUTH2_REDIRECT_LOGOUT}" ];then
+if [ -n "${APP_CONFIG_OAUTH2_REDIRECT_LOGOUT}" ]; then
   replace="\/"
   encoded=${APP_CONFIG_OAUTH2_REDIRECT_LOGOUT//\//$replace}
   sed -e "s/\"redirectUriLogout\": \".*\"/\"redirectUriLogout\": \"${encoded}\"/g" \
-    -i ./app.config.json
+    -i "${NGINX_ENVSUBST_OUTPUT_DIR}/app.config.json"
 fi
 
-if [[ -n "${APP_CONFIG_BPM_HOST}" ]]
-then
+if [[ -n "${APP_CONFIG_BPM_HOST}" ]]; then
   replace="\/"
   encoded=${APP_CONFIG_BPM_HOST//\//$replace}
   sed -e "s/\"bpmHost\": \".*\"/\"bpmHost\": \"${encoded}\"/g" \
-    -i ./app.config.json
+    -i "${NGINX_ENVSUBST_OUTPUT_DIR}/app.config.json"
 fi
 
-if [[ -n "${APP_CONFIG_ECM_HOST}" ]]
-then
+if [[ -n "${APP_CONFIG_ECM_HOST}" ]]; then
   replace="\/"
   encoded=${APP_CONFIG_ECM_HOST//\//$replace}
   sed -e "s/\"ecmHost\": \".*\"/\"ecmHost\": \"${encoded}\"/g" \
-    -i ./app.config.json
+    -i "${NGINX_ENVSUBST_OUTPUT_DIR}/app.config.json"
 fi
 
-if [[ -n "${BASE_PATH}" ]]
-then
-  sed s%href=\"/\"%href=\""${BASE_PATH}"\"%g \
-    -i ./index.html
+if [ -n "${APP_CONFIG_APPS_DEPLOYED}" ]; then
+  sed -e "s/\"alfresco-deployed-apps\": \[.*\]/\"alfresco-deployed-apps\": ${APP_CONFIG_APPS_DEPLOYED}/g" \
+    -i "${NGINX_ENVSUBST_OUTPUT_DIR}/app.config.json"
 fi
-
-
-nginx -g "daemon off;"
