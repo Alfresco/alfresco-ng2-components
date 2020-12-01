59cf157aaca90b60ec4bf68f309c906f2aeb70d9
Eugenio Romano <eugenio.romano@alfresco.com>
2020-12-01 15:49:00 +0000

    hash startegy

--- demo-shell/src/app/app.module.ts
+++ demo-shell/src/app/app.module.ts
@@ -128,7 +128,7 @@ registerLocaleData(localeSv);
         BrowserModule,
         environment.e2e ? NoopAnimationsModule : BrowserAnimationsModule,
         ReactiveFormsModule,
-        RouterModule.forRoot(appRoutes, { initialNavigation: true }),
+        RouterModule.forRoot(appRoutes, { initialNavigation: true,  useHash: true  }),
         FormsModule,
         HttpClientModule,
         MaterialModule,
