1998a5118dd054d481b25e0d5e571a47eb9c4ce0
Alexandra Abrudan <Alexandra.Abrudan@ness.com>
2020-12-15 15:33:48 +0200

    [AAE-3472-adf] added e2e service task test
        fixed spell-check & re-included C311290

---
+++ e2e/process-services-cloud/pages/service-task-list.page.ts
@@ -0,0 +1,69 @@
+/*!
+ * @license
+ * Copyright 2019 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { element, by } from 'protractor';
+import {
+    BrowserVisibility,
+    BrowserActions
+} from '@alfresco/adf-testing';
+
+export class ServiceTaskListPage {
+
+    allServiceTaskButton = element(by.css('button[data-automation-id="my-service-tasks_filter"]'));
+    completedServiceTaskButton = element(by.css('button[data-automation-id="completed-tasks_filter"]'));
+    errorServiceTaskButton = element(by.css('button[data-automation-id="errored-service-tasks_filter"]'));
+    searchHeader = element(by.css('adf-cloud-edit-service-task-filter mat-expansion-panel-header'));
+    serviceTaskList = element(by.css('adf-cloud-service-task-list'));
+    activityNameField = element(by.css('input[data-automation-id="adf-cloud-edit-task-property-activityName"]'));
+    activityStatus = element(by.css('[data-automation-id="datatable-row-0"] div[aria-label="Status"]'));
+    activityName = element(by.css('[data-automation-id="datatable-row-0"] div[aria-label="Activity name"]'));
+
+    async checkServiceTaskFiltersDisplayed(): Promise<void> {
+        await BrowserVisibility.waitUntilElementIsVisible(this.allServiceTaskButton);
+        await BrowserVisibility.waitUntilElementIsVisible(this.completedServiceTaskButton);
+        await BrowserVisibility.waitUntilElementIsVisible(this.errorServiceTaskButton);
+    }
+
+    async checkSearchServiceTaskFiltersDisplayed(): Promise<void> {
+        await BrowserVisibility.waitUntilElementIsVisible(this.searchHeader);
+    }
+
+    async checkServiceTaskListDisplayed(): Promise<void> {
+        await BrowserVisibility.waitUntilElementIsVisible(this.serviceTaskList);
+    }
+
+    async clickCompletedServiceTask(): Promise<void> {
+        await BrowserActions.click(this.completedServiceTaskButton);
+    }
+
+    async clickSearchHeaderServiceTask(): Promise<void> {
+        await BrowserActions.click(this.searchHeader);
+    }
+
+    async searchByActivityName(text: string): Promise<void> {
+        await BrowserActions.clearSendKeys(this.activityNameField, text);
+    }
+
+    async getActivityNameText(): Promise<string> {
+        return BrowserActions.getText(this.activityName);
+    }
+
+    async getStatusText(): Promise<string> {
+        return BrowserActions.getText(this.activityStatus);
+    }
+
+}
---
+++ e2e/process-services-cloud/service-task-list-cloud-component.e2e.ts
@@ -0,0 +1,102 @@
+/*!
+ * @license
+ * Copyright 2019 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { browser } from 'protractor';
+import {
+    LoginPage,
+    ApiService,
+    AppListCloudPage,
+    StringUtil,
+    IdentityService,
+    GroupIdentityService,
+    StartProcessCloudPage
+} from '@alfresco/adf-testing';
+import { NavigationBarPage } from '../core/pages/navigation-bar.page';
+import { ProcessCloudDemoPage } from './pages/process-cloud-demo.page';
+import { ServiceTaskListPage } from './pages/service-task-list.page';
+import CONSTANTS = require('../util/constants');
+
+describe('Service task list cloud', () => {
+
+    describe('Service Task Filters', () => {
+
+        const loginSSOPage = new LoginPage();
+        const navigationBarPage = new NavigationBarPage();
+        const appListCloudComponent = new AppListCloudPage();
+        const processCloudDemoPage = new ProcessCloudDemoPage();
+        const startProcessPage = new StartProcessCloudPage();
+        const serviceTaskListPage = new ServiceTaskListPage();
+
+        const apiService = new ApiService();
+        const identityService = new IdentityService(apiService);
+        const groupIdentityService = new GroupIdentityService(apiService);
+
+        const simpleApp = browser.params.resources.ACTIVITI_CLOUD_APPS.SIMPLE_APP.name;
+        /* cspell:disable-next-line */
+        const activityNameSimpleApp = browser.params.resources.ACTIVITI_CLOUD_APPS.SIMPLE_APP.processes.multiinstanceservicetask;
+
+        const processName = StringUtil.generateRandomString(5);
+        let testUser, groupInfo;
+
+        beforeAll(async () => {
+            await apiService.loginWithProfile('identityAdmin');
+
+            testUser = await identityService.createIdentityUserWithRole([identityService.ROLES.ACTIVITI_USER]);
+            groupInfo = await groupIdentityService.getGroupInfoByGroupName('hr');
+            await identityService.addUserToGroup(testUser.idIdentityService, groupInfo.id);
+
+            await apiService.login(testUser.email, testUser.password);
+            await loginSSOPage.login(testUser.email, testUser.password);
+
+            await navigationBarPage.navigateToProcessServicesCloudPage();
+            await appListCloudComponent.checkApsContainer();
+            await appListCloudComponent.goToApp(simpleApp);
+
+            await processCloudDemoPage.openNewProcessForm();
+            await startProcessPage.clearField(startProcessPage.processNameInput);
+            await startProcessPage.selectFromProcessDropdown(activityNameSimpleApp);
+            await startProcessPage.enterProcessName(processName);
+            await startProcessPage.checkStartProcessButtonIsEnabled();
+            await startProcessPage.clickStartProcessButton();
+
+            await navigationBarPage.clickLogoutButton();
+            await loginSSOPage.login(browser.params.testConfig.users.admin.username, browser.params.testConfig.users.admin.password);
+
+        });
+
+        afterAll(async () => {
+            await apiService.loginWithProfile('identityAdmin');
+            await identityService.deleteIdentityUser(testUser.idIdentityService);
+        });
+
+        it('[C587515] Should be able to select a filter service task and see only the service task related to the selected app', async () => {
+            await navigationBarPage.navigateToServiceTaskListCloudPage();
+
+            await serviceTaskListPage.checkServiceTaskFiltersDisplayed();
+            await serviceTaskListPage.checkSearchServiceTaskFiltersDisplayed();
+            await serviceTaskListPage.checkServiceTaskListDisplayed();
+
+            await serviceTaskListPage.clickCompletedServiceTask();
+            await serviceTaskListPage.clickSearchHeaderServiceTask();
+            await serviceTaskListPage.searchByActivityName(activityNameSimpleApp);
+
+            await expect(await serviceTaskListPage.getActivityNameText()).toBe(activityNameSimpleApp);
+            await expect(await serviceTaskListPage.getStatusText()).toBe(CONSTANTS.SERVICE_TASK_STATUS.COMPLETED);
+
+        });
+    });
+});
