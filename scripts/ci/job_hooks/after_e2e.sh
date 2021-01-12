a71f43f0383fd500ab791a692cc8fa78812a8d89
Thomas Hunter <thomas.hunter@alfresco.com>
2021-01-12 10:13:24 +0000

    [ACA-4233] Changed environment variables

--- scripts/ci/job_hooks/after_e2e.sh
+++ scripts/ci/job_hooks/after_e2e.sh
@@ -3,4 +3,4 @@
 # Upload protractor-smartrunner artifact related to this particular job to S3
 ./scripts/ci/utils/artifact-to-s3.sh -a "$SMART_RUNNER_DIRECTORY" -o "$S3_DBP_FOLDER/protractor-smartrunner-$TRAVIS_JOB_ID.tar.bz2"
 
-npx @alfresco/adf-cli scan-env --host "$API_HOST" -u "$ADMIN_EMAIL" -p "$ADMIN_PASSWORD"
+npx @alfresco/adf-cli scan-env --host "$E2E_HOST" -u "$E2E_ADMIN_EMAIL_IDENTITY" -p "$E2E_ADMIN_PASSWORD_IDENTITY"
