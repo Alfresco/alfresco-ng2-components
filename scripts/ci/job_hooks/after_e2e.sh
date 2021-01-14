6f3ce8b6f30ac4e3a5a00eca0093471bbac6b8be
Thomas Hunter <thomas.hunter@alfresco.com>
2021-01-14 16:22:46 +0000

    [ACA-4233] Call the scan-env adf-cli command before and after each E2E job (#6519)
    * [ACA-4233] Call the scan-env adf-cli command before and after each E2E job
    
    * [ACA-4233] Changed environment variables
    
    * [ACA-4233] Call scan-env command from node_modules
    
    * [ACA-4233] Readd package-lock.json

--- scripts/ci/job_hooks/after_e2e.sh
+++ scripts/ci/job_hooks/after_e2e.sh
@@ -2,3 +2,5 @@
 
 # Upload protractor-smartrunner artifact related to this particular job to S3
 ./scripts/ci/utils/artifact-to-s3.sh -a "$SMART_RUNNER_DIRECTORY" -o "$S3_DBP_FOLDER/protractor-smartrunner-$TRAVIS_JOB_ID.tar.bz2"
+
+./node_modules/@alfresco/adf-cli/bin/adf-cli scan-env --host "$E2E_HOST" -u "$E2E_ADMIN_EMAIL_IDENTITY" -p "$E2E_ADMIN_PASSWORD_IDENTITY"
--- scripts/ci/job_hooks/before_e2e.sh
+++ scripts/ci/job_hooks/before_e2e.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+./node_modules/@alfresco/adf-cli/bin/adf-cli scan-env --host "$E2E_HOST" -u "$E2E_ADMIN_EMAIL_IDENTITY" -p "$E2E_ADMIN_PASSWORD_IDENTITY"
+
 # Download built application artifact from S3
 ./scripts/ci/utils/artifact-from-s3.sh -a "$S3_DBP_FOLDER/alfresco-demoshell.tar.bz2" -o "./dist/demo-shell"
 ./scripts/ci/utils/artifact-from-s3.sh -a "$S3_DBP_FOLDER/alfresco-libs.tar.bz2" -o "./lib/dist"
