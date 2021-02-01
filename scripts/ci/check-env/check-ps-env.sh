5e895859ad883642018557cb3b7aeb3cc1a72d50
Denys Vuika <denys.vuika@gmail.com>
2021-02-01 15:20:48 +0000

    travis workaround

--- scripts/ci/check-env/check-ps-env.sh
+++ scripts/ci/check-env/check-ps-env.sh
@@ -6,4 +6,4 @@ cd $DIR/../../../
 
 echo "====== Check PS UP ====="
 
-./node_modules/@alfresco/adf-cli/bin/adf-cli init-aps-env --host "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" --license "$AWS_S3_BUCKET_ACTIVITI_LICENSE"
+npx @alfresco/adf-cli@alpha init-aps-env --host "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" --license "$AWS_S3_BUCKET_ACTIVITI_LICENSE"
