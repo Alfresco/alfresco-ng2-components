f1fb259ab49fb416cced4790867b2c665cf73f14
Denys Vuika <denys.vuika@gmail.com>
2021-02-01 14:41:54 +0000

    travis workaround

--- scripts/ci/check-env/check-cs-env.sh
+++ scripts/ci/check-env/check-cs-env.sh
@@ -6,4 +6,4 @@ cd $DIR/../../../
 
 echo "====== Check content UP ====="
 
-./node_modules/@alfresco/adf-cli/bin/adf-cli check-cs-env --host "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" || exit 1
+npx @alfresco/adf-cli@alpha check-cs-env --host "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" || exit 1
