e39b8a33f38480c042d42309565976a6cad0e0db
Denys Vuika <denys.vuika@gmail.com>
2021-02-02 14:52:22 +0000

    Revert "travis workaround"
    This reverts commit 542fae649c0501a9150ccac2e5a2cd54ee39a690.

--- scripts/ci/check-env/check-ps-cloud-env.sh
+++ scripts/ci/check-env/check-ps-cloud-env.sh
@@ -6,4 +6,4 @@ cd $DIR/../../../
 
 echo "====== Check PS Cloud UP ====="
 
-npx @alfresco/adf-cli@alpha init-aae-env --host "$E2E_HOST" --oauth "$HOST_SSO" --modelerUsername "$E2E_MODELER_USERNAME" --modelerPassword "$E2E_MODELER_PASSWORD" --devopsUsername "$E2E_DEVOPS_USERNAME" --devopsPassword "$E2E_DEVOPS_PASSWORD" --clientId 'alfresco' || exit 1
+./node_modules/@alfresco/adf-cli/bin/adf-cli init-aae-env --host "$E2E_HOST" --oauth "$HOST_SSO" --modelerUsername "$E2E_MODELER_USERNAME" --modelerPassword "$E2E_MODELER_PASSWORD" --devopsUsername "$E2E_DEVOPS_USERNAME" --devopsPassword "$E2E_DEVOPS_PASSWORD" --clientId 'alfresco' || exit 1
