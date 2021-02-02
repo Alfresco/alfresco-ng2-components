0f7e70a3dcc9e3f3bcc841ca9013ba13690c656a
sivakumar414ram <siva.kumar@muraai.com>
2021-02-02 12:51:42 +0530

    [ACA-4266] Improve governance plugin check logs

--- lib/cli/scripts/plugins/governance-check-plugin.ts
+++ lib/cli/scripts/plugins/governance-check-plugin.ts
@@ -7,20 +7,13 @@ export class GovernanceCheckPlugin {
         private pluginInfo: PluginInterface,
         private alfrescoJsApi: any
     ) {
-        this.governanceHealth = new GovernanceHealth(this.alfrescoJsApi);
+        this.governanceHealth = new GovernanceHealth(this.pluginInfo, this.alfrescoJsApi);
     }
 
     async checkRecordManagement() {
-        let pluginStatus;
-
         const isAvailable = await this.governanceHealth.isRecordManagementAvailable();
         if (!isAvailable) {
             await this.governanceHealth.createRecordManagementSite();
-            pluginStatus = [{ PluginName: this.pluginInfo.name, Status: 'Active', RecordManagement: 'Created'}];
-            console.table(pluginStatus);
-        } else {
-            pluginStatus = [{ PluginName: this.pluginInfo.name, Status: 'Active', RecordManagement: 'Available' }];
-            console.table(pluginStatus);
         }
     }
 }
