6a2e51c5a2e4c97468053943f3271e67665e1109
Maurizio Vitale <maurizio.vitale@alfresco.com>
2021-02-04 18:45:14 +0000

    Fetch the appName context from window instead of hardcode it

--- lib/core/services/alfresco-api.service.ts
+++ lib/core/services/alfresco-api.service.ts
@@ -124,8 +124,8 @@ export class AlfrescoApiService {
     protected initAlfrescoApi() {
         const oauth: OauthConfigModel = Object.assign({}, this.appConfig.get<OauthConfigModel>(AppConfigValues.OAUTHCONFIG, null));
         if (oauth) {
-            oauth.redirectUri = window.location.origin + (oauth.redirectUri || '/');
-            oauth.redirectUriLogout = window.location.origin + (oauth.redirectUriLogout || '/');
+            oauth.redirectUri = window.location.origin + window.location.pathname + (oauth.redirectUri || '/');
+            oauth.redirectUriLogout = window.location.origin + window.location.pathname + (oauth.redirectUriLogout || '/');
         }
 
         const config = new AlfrescoApiConfig({
