710ec784aa79f007ab06422a59974091d6d922ff
Pablo Martinez Garcia <pmartinezga@gmail.com>
2021-02-05 16:11:41 +0100

    ATS-854 Move logic to media player

--- lib/core/viewer/components/media-player.component.ts
+++ lib/core/viewer/components/media-player.component.ts
@@ -18,6 +18,7 @@
 import { Component, Input, OnChanges, SimpleChanges, ViewEncapsulation, Output, EventEmitter } from '@angular/core';
 import { ContentService } from '../../services/content.service';
 import { Track } from '../models/viewer.model';
+import { ViewUtilService } from '../services/view-util.service';
 
 @Component({
     selector: 'adf-media-player',
@@ -41,20 +42,28 @@ export class MediaPlayerComponent implements OnChanges {
     nameFile: string;
 
     @Input()
+    nodeId: string;
+
+    @Input()
     tracks: Track[] = [];
 
     @Output()
     error = new EventEmitter<any>();
 
-    constructor(private contentService: ContentService) {
+    constructor(private contentService: ContentService, private viewUtils: ViewUtilService) {
     }
 
     ngOnChanges(changes: SimpleChanges) {
         const blobFile = changes['blobFile'];
+        const nodeId = changes['nodeId'];
 
         if (blobFile && blobFile.currentValue) {
             this.urlFile = this.contentService.createTrustedUrl(this.blobFile);
             return;
+        }
+
+        if (nodeId && nodeId.currentValue) {
+            this.viewUtils.generateMediaTracks(this.nodeId).then((tracks) => this.tracks = tracks);
         }
 
         if (!this.urlFile && !this.blobFile) {
--- lib/core/viewer/components/viewer.component.ts
+++ lib/core/viewer/components/viewer.component.ts
@@ -32,7 +32,6 @@ import { Subscription } from 'rxjs';
 import { ViewUtilService } from '../services/view-util.service';
 import { AppExtensionService, ViewerExtensionRef } from '@alfresco/adf-extensions';
 import { filter } from 'rxjs/operators';
-import { Track } from '../models/viewer.model';
 
 @Component({
     selector: 'adf-viewer',
@@ -222,8 +221,6 @@ export class ViewerComponent implements OnChanges, OnInit, OnDestroy {
     sidebarLeftTemplateContext: { node: Node } = { node: null };
     fileTitle: string;
     viewerExtensions: Array<ViewerExtensionRef> = [];
-
-    mediaTracks: Track[] = [];
 
     private cacheBusterNumber;
 
@@ -417,10 +414,6 @@ export class ViewerComponent implements OnChanges, OnInit, OnDestroy {
             } else {
                 setupNode = this.viewUtils.displayNodeRendition(nodeData.id);
             }
-        }
-
-        if (this.viewerType === 'media') {
-            this.mediaTracks = await this.viewUtils.generateMediaTracks(nodeData.id);
         }
 
         this.extensionChange.emit(this.extension);
