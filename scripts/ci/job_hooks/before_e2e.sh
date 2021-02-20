c21c2d3b010fe2547f77058fac4ee98b88043ca0
Andras Popovics <popovics@ndras.hu>
2021-02-20 15:03:01 +0100

    Remove debug information

--- scripts/ci/job_hooks/before_e2e.sh
+++ scripts/ci/job_hooks/before_e2e.sh
@@ -5,10 +5,16 @@
 ./scripts/ci/utils/artifact-from-s3.sh -a "$S3_DBP_FOLDER/alfresco-libs.tar.bz2" -o "./lib/dist"
 
 # TODO: This one needs to be cleaned up... Only fixing it like this, because for the current PR it is out of scope
+# =======================================
 rm -rf ./node_modules/@alfresco/adf-cli/ && \
 mkdir -p ./node_modules/@alfresco/adf-cli/ && \
 cp -R ./lib/dist/cli/* ./node_modules/@alfresco/adf-cli/
-ls -liaR ./node_modules/@alfresco/adf-cli/
+
+rm -rf ./node_modules/@alfresco/adf-testing/ && \
+mkdir -p ./node_modules/@alfresco/adf-testing/ && \
+cp -R ./lib/dist/testing/* ./node_modules/@alfresco/adf-testing/
+# =======================================
+
 
 ./node_modules/@alfresco/adf-cli/bin/adf-cli scan-env --host "$E2E_HOST" -u "$E2E_ADMIN_EMAIL_IDENTITY" -p "$E2E_ADMIN_PASSWORD_IDENTITY"
 
