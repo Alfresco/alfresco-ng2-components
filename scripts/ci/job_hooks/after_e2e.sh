335aa4861ef098bf7f0dcf93791bdedd1f9c2ab5
Silviu Popa <silviucpopa@gmail.com>
2021-03-11 00:06:45 +0200

    [ACA-4344] - increase debounce time on process filters (#6805)
    * [ACA-4344] - increase debounce time on process filters
    
    * Put back S3 for smartrunner cache
    
    * Remove useless npm caching
    
    Co-authored-by: Silviu Constantin Popa <silviucpopa@L3700101120.ness.com>
    Co-authored-by: Andras Popovics <popovics@ndras.hu>

--- scripts/ci/job_hooks/after_e2e.sh
+++ scripts/ci/job_hooks/after_e2e.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+# Upload protractor-smartrunner artifact related to this particular job to S3
+./scripts/ci/utils/artifact-to-s3.sh -a "$SMART_RUNNER_DIRECTORY" -o "$S3_SMART_RUNNER_PATH/$TRAVIS_JOB_ID.tar.bz2"
+
 ./node_modules/@alfresco/adf-cli/bin/adf-cli scan-env --host "$E2E_HOST" -u "$E2E_ADMIN_EMAIL_IDENTITY" -p "$E2E_ADMIN_PASSWORD_IDENTITY"
