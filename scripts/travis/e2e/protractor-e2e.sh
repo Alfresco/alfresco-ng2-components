8ba64596fcaf070c06484b49e8116d76b9e738d4
Roxana Diacenco <roxana.diacenco@ness.com>
2021-03-18 17:49:16 +0200

    Update protractor test execution stage

--- scripts/travis/e2e/protractor-e2e.sh
+++ scripts/travis/e2e/protractor-e2e.sh
@@ -7,6 +7,6 @@ echo "Start search e2e"
 cd $DIR/../../../
 
 # RUN_E2E=$(echo ./scripts/test-e2e-lib.sh -host http://localhost:4200 -proxy "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" --use-dist || exit 1)
-RUN_E2E=$(echo ./scripts/test-e2e-lib.sh -host "$URL_HOST_ADF" -proxy "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" --use-dist || exit 1)
+# RUN_E2E=$(echo ./scripts/test-e2e-lib.sh -host "$URL_HOST_ADF" -proxy "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" --use-dist || exit 1)
 
-$RUN_E2E --specs "e2e/core/user-info-component-cloud.e2e.ts" || exit 1
+./node_modules/protractor/bin/protractor ./e2e/protractor.conf.js --specs "e2e/core/user-info-component-cloud.e2e.ts" || exit 1
