fdf06b16b3c868fd992b43bebc5f207d3b61c1ea
Sushmitha100796 <sushmitha.v@muraai.com>
2021-03-18 22:17:58 +0530

    Resolve protractor-e2e failure

--- scripts/travis/e2e/protractor-e2e.sh
+++ scripts/travis/e2e/protractor-e2e.sh
@@ -8,5 +8,5 @@ cd $DIR/../../../
 
 # RUN_E2E=$(echo ./scripts/test-e2e-lib.sh -host http://localhost:4200 -proxy "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" --use-dist || exit 1)
 # RUN_E2E=$(echo ./scripts/test-e2e-lib.sh -host "$URL_HOST_ADF" -proxy "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" --use-dist || exit 1)
-
+./scripts/test-e2e-lib.sh
 ./node_modules/protractor/bin/protractor ./e2e/protractor.conf.js --specs "e2e/core/user-info-component-cloud.e2e.ts" || exit 1
