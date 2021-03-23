49c0e17aea30b9d56e84ec05852c700c11ded68b
Denys Vuika <denys.vuika@gmail.com>
2021-03-23 09:59:11 +0000

    update module settings

--- lib/content-services/src/lib/permission-manager/permission-manager.module.ts
+++ lib/content-services/src/lib/permission-manager/permission-manager.module.ts
@@ -22,7 +22,7 @@ import { MaterialModule } from '../material.module';
 import { PermissionListComponent } from './components/permission-list/permission-list.component';
 import { AddPermissionComponent } from './components/add-permission/add-permission.component';
 import { AddPermissionDialogComponent } from './components/add-permission/add-permission-dialog.component';
-import { CoreModule, DirectiveModule } from '@alfresco/adf-core';
+import { CoreModule, PipeModule } from '@alfresco/adf-core';
 import { InheritPermissionDirective } from './components/inherited-button.directive';
 import { NoPermissionTemplateComponent } from './components/permission-list/no-permission.component';
 import { AddPermissionPanelComponent } from './components/add-permission/add-permission-panel.component';
@@ -36,7 +36,7 @@ import { SearchModule } from '../search/search.module';
         ReactiveFormsModule,
         MaterialModule,
         SearchModule,
-        DirectiveModule
+        PipeModule
     ],
     declarations: [
         PermissionListComponent,
