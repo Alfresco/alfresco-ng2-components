1e22d43a02b7f05e7368c9e75274305adda26f99
Eugenio Romano <eromano@users.noreply.github.com>
2021-03-29 16:43:14 +0100

    Update protractor.conf.js

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -104,17 +104,22 @@ exports.config = {
     specs: arraySpecs,
 
     suites: {
-        smokeTest: [
+        smokeTestCore: [
             "e2e/core/login/**/**/*e2e.ts",
-            "e2e/core/viewer/**/**/*e2e.ts",
+            "e2e/core/viewer/**/**/*e2e.ts"
+        ],
+        smokeTestCsSearch: [
             "e2e/content-services/document-list/**/**/*e2e.ts",
             "e2e/content-services/metadata/**/**/*e2e.ts",
+            "e2e/search/components/**/**/*e2e.ts"
+        ],
+        smokeTestPs: [
             "e2e/process-services/process/**/**/*e2e.ts",
             "e2e/process-services/form/**/**/*e2e.ts",
             "e2e/process-services-cloud/process/**/**/*e2e.ts",
             "e2e/process-services-cloud/form-field/**/**/*e2e.ts"
         ]
-    },
+    },t bvcxa
 
     useAllAngular2AppRoots: true,
 
