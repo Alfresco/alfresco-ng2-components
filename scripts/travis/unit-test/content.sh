9c165b230733f5accac19c3135baab03bd8a3191
arditdomi <32884230+arditdomi@users.noreply.github.com>
2021-04-26 18:40:46 +0100

    Fix content unit tests executing twice (#6950)
    * Run all unit test for cron jobs, fix content unit tests executing twice
    
    * Revert execution of all unit tests on cron
    
    * revert identation

--- scripts/travis/unit-test/content.sh
+++ scripts/travis/unit-test/content.sh
@@ -4,14 +4,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR/../../../
 
-ng test content-services --watch=false || exit 1;
+AFFECTED_LIBS="$(nx affected:libs --base=$BASE_HASH --head=$HEAD_HASH --plain || exit 1)"
 
 echo "================== AFFECTED_LIBS  ${AFFECTED_LIBS} ==================="
 
 echo "================== content-services unit ==================="
 
-AFFECTED_LIBS="$(nx affected:libs --base=$BASE_HASH --head=$HEAD_HASH --plain || exit 1)"
-if [[ $AFFECTED_LIBS =~ "content-services" ||  "${TRAVIS_EVENT_TYPE}" == "push"  ]];
+if [[ $AFFECTED_LIBS =~ "content-services" ||  "${TRAVIS_EVENT_TYPE}" == "push" ]];
 then
     ng test content-services --watch=false || exit 1;
 fi;
