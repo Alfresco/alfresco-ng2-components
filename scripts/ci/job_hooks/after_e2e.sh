69ba2f5147c215298dddef72a2b64384cab392b3
Thomas Hunter <thomas.hunter@alfresco.com>
2021-04-29 16:15:04 +0100

    [ACS-1525] Remove environment scan

--- scripts/ci/job_hooks/after_e2e.sh
+++ scripts/ci/job_hooks/after_e2e.sh
@@ -2,8 +2,3 @@
 
 # Upload protractor-smartrunner artifact related to this particular job to S3
 ./scripts/ci/utils/artifact-to-s3.sh -a "$SMART_RUNNER_DIRECTORY" -o "$S3_SMART_RUNNER_PATH/$TRAVIS_JOB_ID.tar.bz2"
-
-if [ "$TRAVIS_EVENT_TYPE" = "cron" ]
-then
-    ./node_modules/@alfresco/adf-cli/bin/adf-cli scan-env --host "$E2E_HOST" -u "$E2E_ADMIN_EMAIL_IDENTITY" -p "$E2E_ADMIN_PASSWORD_IDENTITY"
-fi
