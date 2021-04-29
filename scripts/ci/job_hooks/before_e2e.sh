0f5fa1f90cd4e4cf9895e43a1e6f885914557bb8
Thomas Hunter <thomas.hunter@alfresco.com>
2021-04-29 14:58:19 +0100

    [ACS-1525] Only run scan environment on cron

--- scripts/ci/job_hooks/before_e2e.sh
+++ scripts/ci/job_hooks/before_e2e.sh
@@ -14,4 +14,7 @@ cp -R ./lib/dist/testing/* ./node_modules/@alfresco/adf-testing/
 # Download protractor-smartrunner artifact related to this particular job from S3, if exists
 ./scripts/ci/utils/artifact-from-s3.sh -a "$S3_SMART_RUNNER_PATH/$TRAVIS_JOB_ID.tar.bz2" -o "$SMART_RUNNER_DIRECTORY"
 
-./node_modules/@alfresco/adf-cli/bin/adf-cli scan-env --host "$E2E_HOST" -u "$E2E_ADMIN_EMAIL_IDENTITY" -p "$E2E_ADMIN_PASSWORD_IDENTITY"
+if [ "$TRAVIS_EVENT_TYPE" = "cron" ]
+then
+    ./node_modules/@alfresco/adf-cli/bin/adf-cli scan-env --host "$E2E_HOST" -u "$E2E_ADMIN_EMAIL_IDENTITY" -p "$E2E_ADMIN_PASSWORD_IDENTITY"
+fi
