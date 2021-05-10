0e1c0e87f45ac0c4f18510a7425662cf00d4ad61
Thomas Hunter <thomas.hunter@alfresco.com>
2021-05-10 15:05:09 +0100

    Add exit 0

--- scripts/ci/job_hooks/before_e2e.sh
+++ scripts/ci/job_hooks/before_e2e.sh
@@ -13,3 +13,5 @@ cp -R ./lib/dist/testing/* ./node_modules/@alfresco/adf-testing/
 
 # Download protractor-smartrunner artifact related to this particular job from S3, if exists
 ./scripts/ci/utils/artifact-from-s3.sh -a "$S3_SMART_RUNNER_PATH/$TRAVIS_JOB_ID.tar.bz2" -o "$SMART_RUNNER_DIRECTORY"
+
+exit 0
