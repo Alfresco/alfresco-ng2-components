1c1f04c2542710a648c61be4cfe7610250069af1
Sushmitha100796 <sushmitha.v@muraai.com>
2021-05-13 16:49:17 +0530

    [ACA-4422] Resolved unit test errors

--- lib/content-services/src/lib/aspect-list/aspect-list-dialog.component.spec.ts
+++ lib/content-services/src/lib/aspect-list/aspect-list-dialog.component.spec.ts
@@ -94,7 +94,10 @@ describe('AspectListDialogComponent', () => {
     let aspectListService: AspectListService;
     let nodeService: NodesApiService;
     let data: AspectListDialogComponentData;
-    const event = new KeyboardEvent('keydown', {'key': 'Escape'});
+    const event = new KeyboardEvent('keydown', {
+        bubbles: true,
+        keyCode: 27
+    } as KeyboardEventInit );
 
     describe('Without passing node id', () => {
 
@@ -256,7 +259,7 @@ describe('AspectListDialogComponent', () => {
                         provide: MatDialogRef,
                         useValue: {
                             close: jasmine.createSpy('close'),
-                            keydownEvents: () => of(null),
+                            keydownEvents: () => of(event),
                             backdropClick: () => of(null)
                         }
                     }
