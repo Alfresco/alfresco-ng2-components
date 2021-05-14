ebfd464c62ace3a0ce80c0e0d24ced37c690b2ac
ursedaniel <daniel.urse@hotmail.com>
2021-05-14 14:14:09 +0300

    improve test

--- lib/core/viewer/components/viewer.component.spec.ts
+++ lib/core/viewer/components/viewer.component.spec.ts
@@ -32,6 +32,7 @@ import { TranslateModule } from '@ngx-translate/core';
 import { MatDialog } from '@angular/material/dialog';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { UploadService } from '../../services/upload.service';
+import { FileModel } from '../../models';
 
 @Component({
     selector: 'adf-viewer-container-toolbar',
@@ -966,20 +967,33 @@ describe('ViewerComponent', () => {
 
             it('should update version when emitted by image-viewer and user has update permissions', () => {
                 spyOn(uploadService, 'uploadFilesInTheQueue').and.callFake(() => {});
+                spyOn(uploadService, 'addToQueue');
                 component.readOnly = false;
-                component.nodeEntry = new NodeEntry({ entry: { name: 'fakeImage.png', id: '12', content: { mimeType: 'txt' } } });
+                component.nodeEntry = new NodeEntry({ entry: { name: 'fakeImage.png', id: '12', content: { mimeType: 'img/png' } } });
                 const data = atob('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg==');
                 const fakeBlob = new Blob([data], { type: 'image/png' });
+                const newImageFile: File = new File([fakeBlob], component?.nodeEntry?.entry?.name, { type: component?.nodeEntry?.entry?.content?.mimeType });
+                const newFile = new FileModel(
+                    newImageFile,
+                    {
+                        majorVersion: false,
+                        newVersion: true,
+                        parentId: component?.nodeEntry?.entry?.parentId,
+                        nodeType: component?.nodeEntry?.entry?.content?.mimeType
+                    },
+                    component.nodeEntry.entry?.id
+                );
                 component.onSubmitFile(fakeBlob);
                 fixture.detectChanges();
 
+                expect(uploadService.addToQueue).toHaveBeenCalledWith(...[newFile]);
                 expect(uploadService.uploadFilesInTheQueue).toHaveBeenCalled();
             });
 
             it('should not update version when emitted by image-viewer and user doesn`t have update permissions', () => {
                 spyOn(uploadService, 'uploadFilesInTheQueue').and.callFake(() => {});
                 component.readOnly = true;
-                component.nodeEntry = new NodeEntry({ entry: { name: 'fakeImage.png', id: '12', content: { mimeType: 'txt' } } });
+                component.nodeEntry = new NodeEntry({ entry: { name: 'fakeImage.png', id: '12', content: { mimeType: 'img/png' } } });
                 const data = atob('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg==');
                 const fakeBlob = new Blob([data], { type: 'image/png' });
                 component.onSubmitFile(fakeBlob);
