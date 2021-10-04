1e50db3523afb53dbef358b4b8eaf6e2959b7740
Pablo Martinez Garcia <pmartinezga@gmail.com>
2021-10-04 16:46:22 +0200

    [MNT-22606] Add properties viewer as form widget

---
+++ lib/process-services-cloud/src/lib/form/components/widgets/properties-viewer/properties-viewer-wrapper/properties-viewer-wrapper.component.ts
@@ -0,0 +1,96 @@
+/*!
+ * @license
+ * Copyright 2019 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component, Input, OnChanges, OnInit, SimpleChanges, ViewEncapsulation } from '@angular/core';
+import { NodesApiService } from '@alfresco/adf-core';
+import { Node } from '@alfresco/js-api';
+
+/* tslint:disable:component-selector  */
+
+@Component({
+    selector: 'properties-viewer-wrapper',
+    templateUrl: './properties-viewer-wrapper.component.html',
+    encapsulation: ViewEncapsulation.None
+})
+export class PropertiesViewerWrapperComponent implements OnInit, OnChanges {
+
+    node: Node;
+    loading = true;
+
+    @Input()
+    nodeId: string;
+
+    /** Toggles whether the edit button should be shown */
+    @Input()
+    editable;
+
+    /** Toggles whether to display empty values in the card view */
+    @Input()
+    displayEmpty;
+
+    /** Toggles between expanded (ie, full information) and collapsed
+     * (ie, reduced information) in the display
+     */
+    @Input()
+    expanded;
+
+    /** The multi parameter of the underlying material expansion panel, set to true to allow multi accordion to be expanded at the same time */
+    @Input()
+    multi;
+
+    /** Name of the metadata preset, which defines aspects and their properties */
+    @Input()
+    preset: string;
+
+    /** Toggles whether the metadata properties should be shown */
+    @Input()
+    displayDefaultProperties;
+
+    /** (optional) shows the given aspect in the expanded  card */
+    @Input()
+    displayAspect: string = null;
+
+    /** Toggles whether or not to enable copy to clipboard action. */
+    @Input()
+    copyToClipboardAction;
+
+    /** Toggles whether or not to enable chips for multivalued properties. */
+    @Input()
+    useChipsForMultiValueProperty;
+
+    constructor(private nodesApiService: NodesApiService) { }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes && changes['nodeId'] && changes['nodeId'].currentValue && !changes['nodeId'].isFirstChange()) {
+            this.getNode(changes['nodeId'].currentValue);
+        }
+    }
+
+    ngOnInit(): void {
+        if (this.nodeId) {
+            this.getNode(this.nodeId);
+        }
+    }
+
+    private getNode(nodeId: string) {
+        this.loading = true;
+        this.nodesApiService.getNode(nodeId).subscribe(retrievedNode => {
+            this.node = retrievedNode;
+            this.loading = false;
+        });
+    }
+}
---
+++ lib/process-services-cloud/src/lib/form/components/widgets/properties-viewer/properties-viewer.widget.spec.ts
@@ -0,0 +1,71 @@
+/*!
+ * @license
+ * Copyright 2019 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+    FormFieldModel,
+    FormModel,
+    setupTestBed
+} from '@alfresco/adf-core';
+import { TranslateModule } from '@ngx-translate/core';
+import { PropertiesViewerWidgetComponent } from './properties-viewer.widget';
+import { ProcessServiceCloudTestingModule } from 'process-services-cloud/src/lib/testing/process-service-cloud.testing.module';
+
+describe('PropertiesViewerWidgetComponent', () => {
+    let widget: PropertiesViewerWidgetComponent;
+    let fixture: ComponentFixture<PropertiesViewerWidgetComponent>;
+    let element: HTMLElement;
+
+    setupTestBed({
+        imports: [
+            TranslateModule.forRoot(),
+            ProcessServiceCloudTestingModule
+        ]
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(PropertiesViewerWidgetComponent);
+        widget = fixture.componentInstance;
+        element = fixture.nativeElement;
+
+        widget.field = new FormFieldModel(new FormModel());
+    });
+
+    afterEach(() => fixture.destroy());
+
+    it('should not display properties viewer when value is not set', async () => {
+        widget.field.value = undefined;
+
+        fixture.detectChanges();
+        await fixture.whenStable();
+
+        const propertiesViewer = element.querySelector('properties-viewer-wrapper');
+
+        expect(propertiesViewer).toBeNull();
+    });
+
+    it('should display properties viewer when value is set', async () => {
+        widget.field.value = '1234';
+
+        fixture.detectChanges();
+        await fixture.whenStable();
+
+        const propertiesViewer = element.querySelector('properties-viewer-wrapper');
+
+        expect(propertiesViewer).not.toBeNull();
+    });
+});
