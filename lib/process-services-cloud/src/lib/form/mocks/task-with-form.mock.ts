99916d251a5ec6200b7c4809298851560093aadc
Tomasz Gnyp <wirkod@gmail.com>
2021-10-08 02:25:08 +0200

    [AAE-5953] solved rebase conflict
    :wq

---
+++ lib/process-services-cloud/src/lib/form/mocks/task-with-form.mock.ts
@@ -0,0 +1,18 @@
+import { TaskDetailsCloudModel } from "../../task/public-api";
+
+export const taskWithFormDetails: TaskDetailsCloudModel = {
+    appName: 'simple-app',
+    assignee: 'admin.adf',
+    completedDate: null,
+    createdDate: new Date(1555419255340),
+    dueDate: new Date(1558419255340),
+    description: null,
+    formKey: '4',
+    priority: 1,
+    parentTaskId: 'bd6b1741-6046-11e9-80f0-0a586460040d',
+    id: 'bd6b1741-6046-11e9-80f0-0a586460040d',
+    name: 'Task1',
+    owner: 'admin.adf',
+    standalone: true,
+    status: 'ASSIGNED'
+  };
---
+++ lib/process-services-cloud/src/lib/task/task-form/components/task-form-cloud.component.stories.ts
@@ -0,0 +1,104 @@
+/*!
+ * @license
+ * Copyright 2019 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
+import { TRANSLATION_PROVIDER } from '@alfresco/adf-core';
+import { FormCloudService } from '../../../form/public-api';
+import { TaskCloudService } from '../../services/task-cloud.service';
+import { TaskFormModule } from '../task-form.module';
+import { TaskFormCloudComponent } from './task-form-cloud.component';
+import { FormCloudServiceMock } from '../../../form/services/form-cloud.service.mock';
+
+export default {
+    component: TaskFormCloudComponent,
+    title: 'Process Services Cloud/Components/Task Form',
+    decorators: [
+        moduleMetadata({
+            declarations: [],
+            imports: [TaskFormModule, BrowserAnimationsModule],
+            providers: [
+                {
+                    provide: TRANSLATION_PROVIDER,
+                    multi: true,
+                    useValue: {
+                        name: 'adf-process-services-cloud',
+                        source: 'assets/adf-process-services-cloud'
+                    }
+                },
+                { provide: TaskCloudService, useClass: TaskCloudService },
+                { provide: FormCloudService, useClass: FormCloudServiceMock }
+            ]
+        })
+    ],
+    argTypes: {
+        appName: { table: { disable: true } },
+        taskId: { table: { disable: true } }
+    }
+} as Meta;
+
+const template: Story<TaskFormCloudComponent> = (args: TaskFormCloudComponent) => ({
+    props: args
+});
+
+export const primary = template.bind({});
+primary.args = {
+    appName: 'taskAssigned',
+    readOnly: false,
+    showCancelButton: true,
+    showCompleteButton: true,
+    showRefreshButton: false,
+    showTitle: true,
+    showValidationIcon: true,
+    taskId: 'task'
+};
+
+export const readOnly = template.bind({});
+readOnly.args = {
+    ...primary.args,
+    readOnly: true
+};
+
+export const hideCancelButton = template.bind({});
+hideCancelButton.args = {
+    ...primary.args,
+    showCancelButton: false
+};
+
+export const hideCompleteButton = template.bind({});
+hideCompleteButton.args = {
+    ...primary.args,
+    showCompleteButton: false
+};
+
+export const showRefreshButton = template.bind({});
+showRefreshButton.args = {
+    ...primary.args,
+    showRefreshButton: true
+};
+
+export const hideTitle = template.bind({});
+hideTitle.args = {
+    ...primary.args,
+    showTitle: false
+};
+
+export const hideValidationIcon = template.bind({});
+hideValidationIcon.args = {
+    ...primary.args,
+    showValidationIcon: false
+};
