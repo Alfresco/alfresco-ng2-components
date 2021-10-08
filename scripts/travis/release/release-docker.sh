db6a638a2d834cc628ae7a3ea60e92035b022ef1
Maurizio Vitale <maurizio.vitale@alfresco.com>
2021-10-08 14:52:29 +0100

    [AAE-6099] Storybook deploy image on quay.io (#7288)
    * Try to build on travis
    
    * Use the new quay repository called storybook
    
    * Build and release demoshell and storybook toghether

--- scripts/travis/release/release-docker.sh
+++ scripts/travis/release/release-docker.sh
@@ -31,12 +31,19 @@ then
 
         fi;
 
-        echo "ℹ️ Running the docker with tag" $TAGS
+        echo "ℹ️ demo-shell: Running the docker with tag" $TAGS
 
         DOCKER_PROJECT_ARGS="PROJECT_NAME=demo-shell"
 
         # Publish Image to docker
         ./node_modules/@alfresco/adf-cli/bin/adf-cli docker --loginCheck --loginUsername "$DOCKER_REPOSITORY_USER" --loginPassword "$DOCKER_REPOSITORY_PASSWORD" --loginRepo "$DOCKER_REPOSITORY_DOMAIN" --dockerRepo "$DOCKER_REPOSITORY" --buildArgs "$DOCKER_PROJECT_ARGS" --dockerTags "$TAGS" --pathProject "$(pwd)"
+
+        echo "ℹ️ storybook-shell: Running the docker with tag" $TAGS
+
+        DOCKER_PROJECT_ARGS="PROJECT_NAME=storybook/process-services-cloud"
+
+        # Publish Image to docker
+        ./node_modules/@alfresco/adf-cli/bin/adf-cli docker --loginCheck --loginUsername "$DOCKER_REPOSITORY_USER" --loginPassword "$DOCKER_REPOSITORY_PASSWORD" --loginRepo "$DOCKER_REPOSITORY_DOMAIN" --dockerRepo "$DOCKER_REPOSITORY_STORYBOOK" --buildArgs "$DOCKER_PROJECT_ARGS" --dockerTags "$TAGS" --pathProject "$(pwd)"
 
     fi;
 else
