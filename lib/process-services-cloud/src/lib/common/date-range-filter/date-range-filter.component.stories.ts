ccf16afc766f0aab68a9b66a5635a799f389aeac
Tomasz Gnyp <49343696+tomgny@users.noreply.github.com>
2021-12-07 12:00:03 +0100

    [AAE-5964] added primary story with mocks (#7401)

---
+++ lib/process-services-cloud/src/lib/common/date-range-filter/date-range-filter.component.stories.ts
@@ -0,0 +1,41 @@
+/*!
+ * @license
+ * Copyright 2019 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
+import { ProcessServicesCloudStoryModule } from '../../testing/process-services-cloud-story.module';
+import { mockFilterProperty } from '../mock/date-range-filter.mock';
+import { ProcessCommonModule } from '../process-common.module';
+import { DateRangeFilterComponent } from './date-range-filter.component';
+
+export default {
+    component: DateRangeFilterComponent,
+    title: 'Process Services Cloud/Components/Date Range Filter',
+    decorators: [
+        moduleMetadata({
+            imports: [ProcessServicesCloudStoryModule, ProcessCommonModule]
+        })
+    ]
+} as Meta;
+
+const template: Story<DateRangeFilterComponent> = (args: DateRangeFilterComponent) => ({
+    props: args
+});
+
+export const primary = template.bind({});
+primary.args = {
+    processFilterProperty: mockFilterProperty
+};
