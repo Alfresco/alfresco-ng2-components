dfc0e05890fb08f05266dc8971bbd025f78d134d
eromano <eugenioromano16@gmail.com>
2022-03-03 16:10:15 +0000

    fix
    fix lint
    
    workaround to remove
    remove some unused dependencies
    
    fix flex import
    
    Signed-off-by: eromano <eugenioromano16@gmail.com>

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,13 +3,15 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR/../"
 
-if grep "envalfresco" . -R --exclude-dir={node_modules,.history,.idea,scripts,dist,e2e-output,.git} --exclude={.env,.env.*}; then
+echo "Check envs"
+
+if grep "envalfresco" . -R --exclude-dir={.angular,.lib,.coverage,node_modules,.history,.idea,scripts,dist,e2e-output,.git} --exclude={.env,.env.*}; then
     echo not permitted word
     exit 1
 fi
 
 echo "Lint"
-nx affected:lint --parallel --all --maxParallel=9 || exit 1
+nx affected:lint --parallel=5 --all || exit 1
 
 echo "Style Lint"
 npm run stylelint || exit 1
