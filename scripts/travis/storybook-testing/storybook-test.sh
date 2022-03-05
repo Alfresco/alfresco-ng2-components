abc7382444fea5cf000995cb4a4961a7c43981ae
MichalFidor <michal.fidor@hyland.com>
2022-03-05 19:50:07 +0100

    Update travis.yml

--- scripts/travis/storybook-testing/storybook-test.sh
+++ scripts/travis/storybook-testing/storybook-test.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-npx playwright test --config='e2e-playwright/playwright.config.ts'
+# Increase the max_user_watches
+sudo sysctl -w fs.inotify.max_user_watches=524288
+
+# Run Playwright Storybook Tests
+npx playwright test --config='e2e-playwright/playwright.config.ts' || exit 1
