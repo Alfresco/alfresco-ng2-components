87ced219a03c659197b2dbf733c9299ea7197e9d
Mikołaj Serwicki <Mikolaj.Serwicki@hyland.com>
2022-05-13 12:50:31 +0200

    feat: remove circular dependency in api

--- lib/core/api/legacy-api-client.factory.ts
+++ lib/core/api/legacy-api-client.factory.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Constructor } from '../interface';
-import { AlfrescoApiService } from '../services';
+import { Constructor } from '../interface/common';
+import { AlfrescoApiService } from '../services/alfresco-api.service';
 import { ApiClientFactory } from './api-client.factory';
 
 @Injectable()
 export class LegacyClientFactory implements ApiClientFactory {
-    constructor(private alfrescoApiService: AlfrescoApiService) {}
+    constructor(private alfrescoApiService: AlfrescoApiService) { }
 
     create<T>(apiClass: Constructor<T>): T {
         return new apiClass(this.alfrescoApiService.getInstance());
