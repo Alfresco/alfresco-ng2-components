7949adb8065aec210394b4cf57b59d039447c296
Mikolaj Serwicki <mikolaj.serwicki@hyland.com>
2022-06-29 14:13:05 +0200

    feat: add api providers in core module

--- lib/core/core.module.ts
+++ lib/core/core.module.ts
@@ -48,8 +48,8 @@ import { DialogModule } from './dialogs/dialog.module';
 import { DirectiveModule } from './directives/directive.module';
 import { PipeModule } from './pipes/pipe.module';
 
-import { AlfrescoJsClientsModule } from '@alfresco/adf-core/api';
-import { BaseAuthenticationService, AUTH_CONFIG } from '@alfresco/adf-core/auth';
+import { AlfrescoApiV2, AlfrescoJsClientsModule, API_CLIENT_FACTORY_TOKEN, LegacyAlfrescoApiServiceFacade } from '@alfresco/adf-core/api';
+import { BaseAuthenticationService, AUTH_CONFIG, AuthBearerInterceptor } from '@alfresco/adf-core/auth';
 import { ExtensionsModule } from '@alfresco/adf-extensions';
 import { OAuthStorage } from 'angular-oauth2-oidc';
 import { LegacyApiClientModule } from './api-factories/legacy-api-client.module';
@@ -68,6 +68,9 @@ import { TranslationService } from './services/translation.service';
 import { versionCompatibilityFactory } from './services/version-compatibility-factory';
 import { VersionCompatibilityService } from './services/version-compatibility.service';
 import { SortingPickerModule } from './sorting-picker/sorting-picker.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { createAlfrescoApiV2Service, AlfrescoApiV2LoaderService } from './api-factories/alfresco-api-v2-loader.service';
+import { AngularClientFactory } from './api-factories/angular-api-client.factory';
 
 const defaultConfig: CoreModuleConfig = { useLegacy: true };
 
@@ -180,6 +183,22 @@ export class CoreModule {
                             multi: true
                         }
                     ] : [
+                        AlfrescoApiV2,
+                        LegacyAlfrescoApiServiceFacade,
+                        { provide: HTTP_INTERCEPTORS, useClass: AuthBearerInterceptor, multi: true },
+                        { provide: API_CLIENT_FACTORY_TOKEN, useClass: AngularClientFactory },
+                        {
+                            provide: APP_INITIALIZER,
+                            useFactory: createAlfrescoApiV2Service,
+                            deps: [
+                                AlfrescoApiV2LoaderService
+                            ],
+                            multi: true
+                        },
+                        {
+                            provide: AlfrescoApiService,
+                            useExisting: LegacyAlfrescoApiServiceFacade
+                        },
                         { provide: BaseAuthenticationService, useClass: OIDCAuthenticationService },
                         { provide: OAuthStorage, useExisting: StorageService },
                         {
