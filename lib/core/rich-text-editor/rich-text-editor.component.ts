55dc9c2dae75f0029ee0e627d56e3de3c8e2a270
Amedeo Lepore <amedeo.lepore@hyland.com>
2022-06-30 16:02:09 +0200

    [AAE-9376] Install text color plugin to change text color and highlight text

--- lib/core/rich-text-editor/rich-text-editor.component.ts
+++ lib/core/rich-text-editor/rich-text-editor.component.ts
@@ -5,6 +5,7 @@ import EditorJS from '@editorjs/editorjs';
 /** Plugin import */
 import Header from '@editorjs/header';
 import List from '@editorjs/list';
+import * as ColorPlugin from 'editorjs-text-color-plugin';
 
 @Component({
     selector: 'adf-rich-text-editor',
@@ -30,6 +31,21 @@ export class RichTextEditorComponent implements OnInit, AfterViewInit {
                     inlineToolbar: true,
                     config: {
                         defaultStyle: 'unordered'
+                    }
+                },
+                Color: {
+                    class: ColorPlugin,
+                    config: {
+                        colorCollections: ['#EC7878', '#9C27B0', '#673AB7', '#3F51B5', '#0070FF', '#03A9F4', '#00BCD4', '#4CAF50', '#8BC34A', '#CDDC39', '#FFF'],
+                        defaultColor: '#FF1300',
+                        type: 'text'
+                    }
+                },
+                Marker: {
+                    class: ColorPlugin,
+                    config: {
+                        defaultColor: '#FFBF00',
+                        type: 'marker'
                     }
                 }
             }
