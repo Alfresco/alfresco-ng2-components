f444f84bdf8f579b7ff24cfd634592a5773ce8eb
Amedeo Lepore <amedeo.lepore@hyland.com>
2022-07-19 17:13:19 +0200

    [AAE-9480] Add parse editorjs data to html Test

--- lib/core/form/components/widgets/display-rich-text/display-rich-text.widget.spec.ts
+++ lib/core/form/components/widgets/display-rich-text/display-rich-text.widget.spec.ts
@@ -1,25 +1,73 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { CoreTestingModule, setupTestBed } from 'core/testing';
 
 import { DisplayRichTextComponent } from './display-rich-text.widget';
 
 describe('DisplayRichTextComponent', () => {
-  let component: DisplayRichTextComponent;
-  let fixture: ComponentFixture<DisplayRichTextComponent>;
+    let widget: DisplayRichTextComponent;
+    let fixture: ComponentFixture<DisplayRichTextComponent>;
+    let debugEl: DebugElement;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DisplayRichTextComponent ]
-    })
-    .compileComponents();
-  });
+    const cssSelector = {
+        parsedHTML: '.adf-display-rich-text-widget-parsed-html'
+    };
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DisplayRichTextComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    const fakeFormField: any = {
+        id: 'fake-form-field',
+        name: 'fake-label',
+        value: {
+            time: 1658154611110,
+            blocks: [
+                {
+                    id: '1',
+                    type: 'header',
+                    data: {
+                        text: 'Editor.js',
+                        level: 1
+                    }
+                }
+            ],
+            version: 1
+        },
+        required: false,
+        readOnly: false,
+        overrideId: false,
+        colspan: 1,
+        placeholder: null,
+        minLength: 0,
+        maxLength: 0,
+        params: {
+            existingColspan: 1,
+            maxColspan: 1
+        }
+    };
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    setupTestBed({
+        imports: [
+            CoreTestingModule
+        ]
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(DisplayRichTextComponent);
+        widget = fixture.componentInstance;
+        debugEl = fixture.debugElement;
+        widget.field = fakeFormField;
+    });
+
+    it('should create', () => {
+        fixture.detectChanges();
+        expect(widget).toBeTruthy();
+    });
+
+    it('should parse editorjs data to html', async () => {
+        const expectedHtml = '<h1>Editor.js</h1>';
+        fixture.detectChanges();
+        await fixture.whenStable();
+        const parsedHtmlEl = debugEl.query(By.css(cssSelector.parsedHTML));
+        expect(parsedHtmlEl.nativeElement.innerHTML).toEqual(expectedHtml);
+    });
+
 });
