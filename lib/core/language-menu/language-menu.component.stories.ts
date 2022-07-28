c52203aefe2ce21be34cd85f0da72442e95e90a2
Robert Duda <Robert.Duda@Hyland.com>
2022-07-28 13:39:20 +0200

    created mock of LanguageService

--- lib/core/language-menu/language-menu.component.stories.ts
+++ lib/core/language-menu/language-menu.component.stories.ts
@@ -1,66 +1,83 @@
+import { LanguageService } from '../services/language.service';
 import { LanguageMenuModule } from './language-menu.module';
-import { CoreStoryModule } from './../testing/core.story.module';
+import { CoreStoryModule } from '../testing/core.story.module';
 import { componentWrapperDecorator, Meta, moduleMetadata, Story } from '@storybook/angular';
 import { LanguageMenuComponent } from './language-menu.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { TranslateModule } from '@ngx-translate/core';
+import { action } from '@storybook/addon-actions';
+import { LanguageServiceMock } from '../mock/language.service.mock';
 
 export default {
     component: LanguageMenuComponent,
     title: 'Core/Components/Language Menu/Language Menu',
     decorators: [
         moduleMetadata({
-            imports: [CoreStoryModule, LanguageMenuModule, MatMenuModule, MatIconModule, TranslateModule]
+            imports: [CoreStoryModule, LanguageMenuModule, MatMenuModule, MatIconModule, TranslateModule],
+            providers: [
+                { provide: LanguageService, useClass: LanguageServiceMock }
+            ]
         })
-    ],
-    argTypes: {
-    }
+    ]
 } as Meta;
 
 const Template: Story<LanguageMenuComponent> = (args) => ({
-    props: args
+    props: {
+        ...args,
+        changedLanguage: action('changedLanguage')
+    }
 });
 
-export const LanguageMenuAsMainMenu = Template.bind({});
-LanguageMenuAsMainMenu.args = {
-    key1 : 'CORE.HOST_SETTINGS.TITLE',
-    key2 : 'ADF.LANGUAGE',
-    key3 : 'CORE.METADATA.ACTIONS.SAVE'
-};
-LanguageMenuAsMainMenu.decorators = [
-    componentWrapperDecorator(story => `<button mat-icon-button [matMenuTriggerFor]="langMenu">
+export const AsMainMenu = Template.bind({});
+AsMainMenu.decorators = [
+    componentWrapperDecorator(story => `
+      <style>
+        table {font-family: arial, sans-serif;border-collapse: collapse;margin-top:10px;}
+        td, th {border: 1px solid #dddddd;text-align: left;padding: 8px;}
+      </style>
+      <button mat-icon-button [matMenuTriggerFor]="langMenu">
         <mat-icon>
           language
         </mat-icon>
       </button>
       <mat-menu #langMenu="matMenu">
         ${story}
-    </mat-menu>`)
+      </mat-menu>
+        <table>
+          <tr><th>Example key</th><th>Value</th></tr>
+          <tr><td>CORE.HOST_SETTINGS.TITLE</td><td>{{ 'CORE.HOST_SETTINGS.TITLE' | translate }}</td></tr>
+          <tr><td>CORE.METADATA.ACTIONS.SAVE</td><td>{{ 'CORE.METADATA.ACTIONS.SAVE' | translate }}</td></tr>
+          <tr><td>ADF.LANGUAGE</td><td>{{ 'ADF.LANGUAGE' | translate }}</td></tr>
+        </table>
+    `)
 ];
 
-export const LanguageMenuAsNestedMenu = Template.bind({});
-LanguageMenuAsNestedMenu.args = {
-    ...LanguageMenuAsMainMenu.args
-};
-LanguageMenuAsNestedMenu.decorators = [
-    componentWrapperDecorator(story => `<button mat-icon-button [matMenuTriggerFor]="profileMenu">
+export const AsNestedMenu = Template.bind({});
+AsNestedMenu.decorators = [
+    componentWrapperDecorator(story => `
+      <style>
+        table {font-family: arial, sans-serif;border-collapse: collapse;margin-top:10px;}
+        td, th {border: 1px solid #dddddd;text-align: left;padding: 8px;}
+      </style>
+      <button mat-icon-button [matMenuTriggerFor]="profileMenu">
         <mat-icon>
           more_vert
         </mat-icon>
       </button>
       <mat-menu #profileMenu="matMenu">
-        <button mat-menu-item>
-        {{ '${LanguageMenuAsNestedMenu.args.key1}' | translate }}
-        </button>
         <button mat-menu-item [matMenuTriggerFor]="langMenu">
         {{ 'ADF.LANGUAGE' | translate }}
-        </button>
-        <button mat-menu-item>
-        {{ 'CORE.METADATA.ACTIONS.SAVE' | translate }}
         </button>
       </mat-menu>
       <mat-menu #langMenu="matMenu">
         ${story}
-      </mat-menu>`)
+      </mat-menu>
+      <table>
+        <tr><th>Example key</th><th>Value</th></tr>
+        <tr><td>CORE.HOST_SETTINGS.TITLE</td><td>{{ 'CORE.HOST_SETTINGS.TITLE' | translate }}</td></tr>
+        <tr><td>CORE.METADATA.ACTIONS.SAVE</td><td>{{ 'CORE.METADATA.ACTIONS.SAVE' | translate }}</td></tr>
+        <tr><td>ADF.LANGUAGE</td><td>{{ 'ADF.LANGUAGE' | translate }}</td></tr>
+      </table>
+      `)
 ];
--- lib/core/language-menu/language-picker.component.stories.ts
+++ lib/core/language-menu/language-picker.component.stories.ts
@@ -5,17 +5,16 @@ import { MatMenuModule } from '@angular/material/menu';
 import { CoreStoryModule } from './../testing/core.story.module';
 import { componentWrapperDecorator, Meta, moduleMetadata, Story } from '@storybook/angular';
 import { MatIconModule } from '@angular/material/icon';
+import { TranslateModule } from '@ngx-translate/core';
 
 export default {
     component: LanguagePickerComponent,
     title: 'Core/Components/Language Menu/Language Picker',
     decorators: [
         moduleMetadata({
-            imports: [CoreStoryModule, LanguageMenuModule, MatMenuModule, MatIconModule]
+            imports: [CoreStoryModule, LanguageMenuModule, MatMenuModule, MatIconModule, TranslateModule]
         })
-    ],
-    argTypes: {
-    }
+    ]
 } as Meta;
 
 const Template: Story<LanguageMenuComponent> = (args) => ({
@@ -23,20 +22,40 @@ const Template: Story<LanguageMenuComponent> = (args) => ({
 });
 
 export const Default = Template.bind({});
-Default.args = {
-  };
+Default.decorators = [
+    componentWrapperDecorator(story =>`
+      <style>
+        table {font-family: arial, sans-serif;border-collapse: collapse;margin-top:10px;}
+        td, th {border: 1px solid #dddddd;text-align: left;padding: 8px;}
+      </style>
+      ${story}
+      <table>
+        <tr><th>Example key</th><th>Value</th></tr>
+        <tr><td>CORE.HOST_SETTINGS.TITLE</td><td>{{ 'CORE.HOST_SETTINGS.TITLE' | translate }}</td></tr>
+        <tr><td>CORE.METADATA.ACTIONS.SAVE</td><td>{{ 'CORE.METADATA.ACTIONS.SAVE' | translate }}</td></tr>
+        <tr><td>ADF.LANGUAGE</td><td>{{ 'ADF.LANGUAGE' | translate }}</td></tr>
+      </table>
+    `)
+];
 
-export const InsideNestedMenu = Template.bind({});
-InsideNestedMenu.args = {
-  ...Default.args
-};
-InsideNestedMenu.decorators = [
-    componentWrapperDecorator(story =>
-        `<button mat-icon-button class="dw-profile-menu" [matMenuTriggerFor]="menu">
-            <mat-icon>more_vert</mat-icon>
-         </button>
-         <mat-menu #menu="matMenu">
-            ${story}
-         </mat-menu>`
-        )
+export const AsNestedMenu = Template.bind({});
+AsNestedMenu.decorators = [
+    componentWrapperDecorator(story => `
+      <style>
+        table {font-family: arial, sans-serif;border-collapse: collapse;margin-top:10px;}
+        td, th {border: 1px solid #dddddd;text-align: left;padding: 8px;}
+      </style>
+      <button mat-icon-button class="dw-profile-menu" [matMenuTriggerFor]="menu">
+        <mat-icon>more_vert</mat-icon>
+      </button>
+      <mat-menu #menu="matMenu">
+        ${story}
+      </mat-menu>
+      <table>
+        <tr><th>Example key</th><th>Value</th></tr>
+        <tr><td>CORE.HOST_SETTINGS.TITLE</td><td>{{ 'CORE.HOST_SETTINGS.TITLE' | translate }}</td></tr>
+        <tr><td>CORE.METADATA.ACTIONS.SAVE</td><td>{{ 'CORE.METADATA.ACTIONS.SAVE' | translate }}</td></tr>
+        <tr><td>ADF.LANGUAGE</td><td>{{ 'ADF.LANGUAGE' | translate }}</td></tr>
+      </table>
+      `)
 ];
