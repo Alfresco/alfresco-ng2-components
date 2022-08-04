e4a536aa18abdb3097f23303589bc4c3b71b9170
Marcin Krueger <Marcin.Krueger@hyland.com>
2022-08-04 13:35:30 +0200

    [AAE-10202] Storybook add EmptyContent component stories

---
+++ lib/core/templates/empty-content/empty-content.component.stories.ts
@@ -0,0 +1,72 @@
+/*!
+ * @license
+ * Copyright 2022 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
+import { EmptyContentComponent } from './empty-content.component';
+import { CoreStoryModule } from '../../testing/core.story.module';
+import { TemplateModule } from '../template.module';
+
+export default {
+    component: EmptyContentComponent,
+    title: 'Core/Components/Empty Content',
+    decorators: [
+        moduleMetadata({
+            imports: [CoreStoryModule, TemplateModule]
+        })
+    ],
+    argTypes: {
+        lines: {
+            name: 'lines',
+            type: { name: 'object', required: false },
+            defaultValue: [
+                'Items you removed are moved to the Trash',
+                'Empty Trash to permanently delete items'
+            ]
+        }
+    }
+} as Meta;
+
+const template: Story = args => ({
+    props: args
+});
+
+export const defaultStory = template.bind({});
+defaultStory.argTypes = {
+    lines: {
+        control: { disable: true }
+    }
+};
+defaultStory.args = {
+    icon: 'star_rate',
+    title: 'No favourite files or folders',
+    subtitle: 'Favourite items that you want to easily find later'
+};
+defaultStory.storyName = 'Default';
+
+export const multipleLines: Story = args => ({
+    props: {
+        ...args
+    },
+    template: `
+    <adf-empty-content icon="delete" title="Trash is empty">
+        <p class="adf-empty-content__text" *ngFor="let line of ${JSON.stringify(
+            args.lines
+        ).replace(/\"/g, '\'')}">
+            {{ line }}
+        </p>
+    </adf-empty-content>`
+});
