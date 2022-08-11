b8e76a97a9487fbd2e335951c6280c1369e0210d
Tomasz <tomasz.gnyp@hyland.com>
2022-08-11 17:35:56 +0200

    [AAE-10266] Fix storybook config

--- scripts/travis/storybook-testing/storybook-test.sh
+++ scripts/travis/storybook-testing/storybook-test.sh
@@ -4,5 +4,4 @@
 sudo sysctl -w fs.inotify.max_user_watches=524288
 
 # Run Playwright Storybook Tests
-# NOTE: Test excluded due to Storybook not generating properly - https://alfresco.atlassian.net/browse/AAE-8048
-#npx playwright test --config='e2e-playwright/playwright.config.ts' || exit 1
+npx playwright test --config='e2e-playwright/playwright.config.ts' || exit 1
