84edf3ef864bd81580a092e51bb2bbd6c7f5a493
Tomasz <tomasz.gnyp@hyland.com>
2022-08-11 17:35:56 +0200

    [AAE-10266] Fix storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,17 +2,6 @@ module.exports = {
     stories: [],
     addons: ['@storybook/addon-essentials'],
     framework: "@storybook/angular",
-    core: {
-        builder: "@storybook/builder-webpack5",
-    },
-    features: {
-        interactionsDebugger: true,
-    },
-    // uncomment the property below if you want to apply some webpack config globally
-    // webpackFinal: async (config, { configType }) => {
-    //   // Make whatever fine-grained changes you need that should apply to all storybook configs
-
-    //   // Return the altered config
-    //   return config;
-    // },
+    staticDirs: [ { from: "../../../demo-shell/src/app.config.json", to: 'app.config.json' } ],
+    core: { builder: "webpack5" }
 };
