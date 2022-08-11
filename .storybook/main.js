cd3023cb362ff4d9b5e93ebd5b6fffd8001d73ec
Tomasz <tomasz.gnyp@hyland.com>
2022-08-11 11:25:59 +0200

    [AAE-10266] test storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,13 @@
 module.exports = {
     stories: [],
     addons: ['@storybook/addon-essentials'],
+    framework: "@storybook/angular",
+    core: {
+        builder: "@storybook/builder-webpack5",
+    },
+    features: {
+        interactionsDebugger: true,
+    },
     // uncomment the property below if you want to apply some webpack config globally
     // webpackFinal: async (config, { configType }) => {
     //   // Make whatever fine-grained changes you need that should apply to all storybook configs
