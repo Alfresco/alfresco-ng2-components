dcf2550aa4eeadb5a78bf2f609d56bd08586ee11
Tomasz <tomasz.gnyp@hyland.com>
2022-08-12 14:52:31 +0200

    downgrade nrwl packages

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
     stories: [],
     addons: ['@storybook/addon-essentials'],
-    framework: "@storybook/angular",
-    staticDirs: [ { from: "../../../demo-shell/src/app.config.json", to: 'app.config.json' } ],
-    core: { builder: "webpack5" }
+    framework: '@storybook/angular',
+    staticDirs: [ { from: '../../../demo-shell/src/app.config.json', to: 'app.config.json' } ],
+    core: { builder: 'webpack5' }
 };
