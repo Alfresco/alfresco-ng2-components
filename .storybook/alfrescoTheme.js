be0545801d48e98e7f61a4c4ec656982826b9679
Tomasz Gnyp <49343696+tomgny@users.noreply.github.com>
2022-08-16 14:14:02 +0200

    [AAE-10105] Update Storybook to 6.5.10v (#7765)
    * [AAE-10266] storybook 6.4.19 fix
    
    * [AAE-10266] test storybook config
    
    * [AAE-10266] Fix storybook config
    
    * downgrade nrwl workspace to previous version
    
    * remove duplicated logo
    
    * downgrade nrwl version
    
    * downgrade nrwl packages
    
    * Trigger travis
    
    * fix playwright config
    
    * Fix storybook execute command
    
    * Fix Playwright config v2
    
    * Remove useless command
    
    Co-authored-by: MichalFidor <michal.fidor@hyland.com>

--- .storybook/alfrescoTheme.js
+++ .storybook/alfrescoTheme.js
@@ -1,8 +1,9 @@
 import { create } from '@storybook/theming';
+import alfrescoLogo from '../lib/core/assets/images/alfresco-logo.svg';
 
 export default create({
   base: 'light',
   brandTitle: 'Hyland | Alfresco Storybook App',
   brandUrl: 'https://www.alfresco.com/',
-  brandImage: '/assets/storybook-logo.png',
+  brandImage: alfrescoLogo,
 });
