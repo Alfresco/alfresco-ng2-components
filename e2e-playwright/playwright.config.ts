dbb36392b8660c9ca0603b4d7d48875ea9dc9e1c
MichalFidor <michal.fidor@hyland.com>
2022-08-16 09:32:03 +0200

    Fix storybook execute command

--- e2e-playwright/playwright.config.ts
+++ e2e-playwright/playwright.config.ts
@@ -9,33 +9,13 @@
 
 import { PlaywrightTestConfig, ReporterDescription } from '@playwright/test';
 import { dotenvConfig } from '../lib/cli/tooling';
-import { paths } from './utils/paths';
 import { timeouts } from './utils/timeouts';
-import path from 'path';
 
 export const getGlobalConfig = (): PlaywrightTestConfig => {
     dotenvConfig();
     const env = process.env;
     const baseUrl = `${env.PLAYWRIGHT_STORYBOOK_E2E_HOST}:${env.PLAYWRIGHT_STORYBOOK_E2E_PORT}`;
-    let startCommand: string;
     let report: ReporterDescription;
-
-    if (!!env.CI) {
-        startCommand = 'nx run stories:storybook';
-        report = ['html', { outputFolder: path.resolve(`../${paths.report}`), open: 'never' }];
-    } else {
-        startCommand = 'nx run stories:build-storybook && nx run stories:storybook';
-        report = ['html', { outputFolder: path.resolve(`../${paths.report}`), open: 'on-failure' }];
-    }
-
-    const webServer = {
-        command: startCommand,
-        // It's true, but watch on on localhost! If you'll have other app up and running then it'll use this app to run the tests.
-        // It won't check what application is currently running.
-        reuseExistingServer: true,
-        timeout: timeouts.webServer,
-        url: baseUrl
-    };
 
     return {
         timeout: timeouts.globalTest,
@@ -93,7 +73,6 @@ export const getGlobalConfig = (): PlaywrightTestConfig => {
             }
         ],
 
-        webServer
     };
 
 };
