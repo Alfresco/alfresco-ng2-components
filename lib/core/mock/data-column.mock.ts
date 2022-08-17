969dd7f8359fab6c5dada6206a9ef711611d6d04
Marcin Krueger <Marcin.Krueger@hyland.com>
2022-08-17 09:34:56 +0200

    [AAE-10282] Storybook adjust mock data for stories

--- lib/core/mock/data-column.mock.ts
+++ lib/core/mock/data-column.mock.ts
@@ -1,7 +1,9 @@
 import { ObjectDataTableAdapter } from '@alfresco/adf-core';
 import { DataColumn } from '../datatable/data/data-column.model';
 
-export const getDataColumnMock = <T = unknown>(column: Partial<DataColumn<T>> = {}): DataColumn<T> => ({
+export const getDataColumnMock = <T = unknown>(
+    column: Partial<DataColumn<T>> = {}
+): DataColumn<T> => ({
     id: 'columnId',
     key: 'key',
     type: 'text',
@@ -22,32 +24,46 @@ export const getDataColumnMock = <T = unknown>(column: Partial<DataColumn<T>> = 
     ...column
 });
 
-export const dataText = new ObjectDataTableAdapter([{ id: '1 first' }, { id: '2 second' }, { id: '3 third' }], []);
+export const dataText = new ObjectDataTableAdapter(
+    [{ id: '1 first' }, { id: '2 second' }, { id: '3 third' }],
+    []
+);
 
 export const dateRows = [
-  { createdOn: new Date(2016, 6, 1, 11, 8, 4) },
-  { createdOn: new Date(2018, 4, 3, 12, 8, 4) },
-  { createdOn: new Date(2021, 2, 3, 9, 8, 4) }
+    { createdOn: new Date(2016, 6, 1, 11, 8, 4) },
+    { createdOn: new Date(2018, 4, 3, 12, 8, 4) },
+    { createdOn: new Date(2021, 2, 3, 9, 8, 4) }
 ];
 
-export const dateColumns = [{ type: 'date', key: 'createdOn', title: 'Created On', format: 'medium' }];
+export const dateColumns = {
+    type: 'date',
+    key: 'createdOn',
+    title: 'Created On'
+};
 
 export const locationRows = [
-  {
-    path: {
-      elements: [
-        { id: '1', name: 'path' },
-        { id: '2', name: 'to' },
-        { id: '3', name: 'location' }
-      ],
-      name: '/path/to/location-link'
+    {
+        path: {
+            elements: [
+                { id: '1', name: 'path' },
+                { id: '2', name: 'to' },
+                { id: '3', name: 'location' }
+            ],
+            name: '/path/to/location-link'
+        }
     }
-  }
 ];
 
-export const locationColumns = [{ format: '/somewhere', type: 'location', key: 'path', title: 'Location' }];
+export const locationColumns = [
+    { format: '/somewhere', type: 'location', key: 'path', title: 'Location' }
+];
 
-export const dataIcon = new ObjectDataTableAdapter([{ icon: 'alarm' }, { icon: 'folder_open' }, { icon: 'accessibility' }], []);
+export const dataIcon = new ObjectDataTableAdapter(
+    [{ icon: 'alarm' }, { icon: 'folder_open' }, { icon: 'accessibility' }],
+    []
+);
 
-export const dataSizeInBytes = new ObjectDataTableAdapter([{ size: 12313 }, { size: 23 }, { size: 42421412421 }], []);
-
+export const dataSizeInBytes = new ObjectDataTableAdapter(
+    [{ size: 12313 }, { size: 23 }, { size: 42421412421 }],
+    []
+);
