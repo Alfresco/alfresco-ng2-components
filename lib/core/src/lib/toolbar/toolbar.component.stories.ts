9fa5fbc0ea1cb4dbd597f9bf1304ca4ff04ff5cc
Robert Duda <Robert.Duda@Hyland.com>
2022-08-22 17:01:36 +0200

    Toolbar Module

--- lib/core/src/lib/toolbar/toolbar.component.stories.ts
+++ lib/core/src/lib/toolbar/toolbar.component.stories.ts
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-import { Meta, moduleMetadata, Story } from '@storybook/angular';
+import { Meta, moduleMetadata, Story, componentWrapperDecorator } from '@storybook/angular';
 import { CoreStoryModule } from '../testing/core.story.module';
 import { ToolbarComponent } from './toolbar.component';
 import { ToolbarModule } from './toolbar.module';
@@ -31,7 +31,22 @@ export default {
     argTypes: {
         color: {
             control: 'radio',
-            options: ['primary', 'accent', 'warn']
+            options: ['primary', 'accent', 'warn', undefined],
+            description: 'Toolbar color.',
+            defaultValue: undefined,
+            table: {
+                type: { summary: 'ThemePalette' },
+                defaultValue: { summary: 'undefined' }
+            }
+        },
+        title: {
+            control: 'text',
+            description: 'Toolbar title.',
+            defaultValue: '',
+            table: {
+                type: { summary: 'string' },
+                defaultValue: { summary: '' }
+            }
         }
     }
 } as Meta;
@@ -40,14 +55,32 @@ const template: Story<ToolbarComponent> = (args: ToolbarComponent) => ({
     props: args
 });
 
-export const primary = template.bind({});
-primary.args = {
-    title: 'Alfresco Storybook App',
-    color: 'primary'
-};
+export const defaultToolbar = template.bind({});
 
-export const invalidOrMissingColor = template.bind({});
-invalidOrMissingColor.args = {
-    title: 'Alfresco Storybook App',
-    color: undefined
-};
+const wrap = (story: string, title?: string, content?:  string): string => `<h3>${title ? title : ''}</h3>${story}`.replace('></adf-toolbar>',`>${content ? content : ''}</adf-toolbar>`);
+export const withToolbarTitleComponent = template.bind({});
+withToolbarTitleComponent.decorators = [
+    componentWrapperDecorator(story => wrap(
+        story,
+        `This story supplies the Toolbar component with an adf-toolbar-title element:`,
+        `<div adf-toolbar-title>Toolbar Title</div>`
+    ))
+];
+
+export const withProjectedContent = template.bind({});
+withProjectedContent.decorators = [
+    componentWrapperDecorator(story => wrap(
+        story,
+        `This story supplies the Toolbar component with a div with red background:`,
+        `<div style="backgroundColor:red;">Projected content</div>`
+    ))
+];
+
+export const withToolbarDivider = template.bind({});
+withToolbarDivider.decorators = [
+    componentWrapperDecorator(story => wrap(
+        story,
+        `This story supplies the Toolbar component with two Toolbar Divider components:`,
+        `<div>Text One</div><adf-toolbar-divider></adf-toolbar-divider><div>Text Two</div><adf-toolbar-divider></adf-toolbar-divider><div>Text Three</div>`
+    ))
+];
