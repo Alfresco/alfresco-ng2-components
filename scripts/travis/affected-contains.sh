35596bf04403eb9eea3ae898a4d2f3d3a45fd3e9
Maurizio Vitale <maurizio.vitale@alfresco.com>
2022-08-30 16:06:56 +0100

    Run everything on cron

--- scripts/travis/affected-contains.sh
+++ scripts/travis/affected-contains.sh
@@ -6,7 +6,7 @@ cd $DIR/../../
 
 if [ "${TRAVIS_EVENT_TYPE}" == "cron" ]; then
   #echo "Affected not check in case of cron"
-  echo false
+  echo true
 fi
 AFFECTED_LIBS="$(nx print-affected --type=lib --select=projects --base=$BASE_HASH --head=$HEAD_HASH --plain)"
 #echo "Verify if affected build contains $1"
