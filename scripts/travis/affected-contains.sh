0754185e84a51ef8b4a307a52b9f687085fc96a3
Maurizio Vitale <maurizio.vitale@alfresco.com>
2022-08-30 14:18:22 +0100

    Short circuit e2e in case lib not affected

--- scripts/travis/affected-contains.sh
+++ scripts/travis/affected-contains.sh
@@ -5,18 +5,18 @@ verifyLib=$1;
 cd $DIR/../../
 
 if [ "${TRAVIS_EVENT_TYPE}" == "cron" ]; then
-  echo "Affected not check in case of cron"
-  exit 0
+  #echo "Affected not check in case of cron"
+  echo false
 fi
-AFFECTED_LIBS="$(nx print-affected --type=lib --select=projects --base=$BASE_HASH --head=$HEAD_HASH --plain || exit 1)"
-echo "Verify if affected build contains $1"
+AFFECTED_LIBS="$(nx print-affected --type=lib --select=projects --uncommitted --plain)"
+#echo "Verify if affected build contains $1"
 
 
-echo "Affected libs:$AFFECTED_LIBS"
+#echo "Affected libs:$AFFECTED_LIBS"
 if [[  $AFFECTED_LIBS =~ $verifyLib ]]; then
-    echo "Yep project:$verifyLib is affected carry on"
-    exit 0
+    #echo "Yep project:$verifyLib is affected carry on"
+    echo true
 else
-    echo "Nope project NOT affected save time"
-     exit 1
+    #echo "Nope project NOT affected save time"
+    echo false
 fi;
