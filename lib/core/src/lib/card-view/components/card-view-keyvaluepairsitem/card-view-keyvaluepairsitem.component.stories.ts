4b2c3754678ec1b884c9b51cf75daa025d56f94b
Marcin Krueger <Marcin.Krueger@hyland.com>
2022-09-20 14:46:10 +0200

    [AAE-10446] Storybook create CardView KeyValuePairs Item component stories

---
+++ lib/core/src/lib/card-view/components/card-view-keyvaluepairsitem/card-view-keyvaluepairsitem.component.stories.ts
@@ -0,0 +1,64 @@
+/*!
+ * @license
+ * Copyright 2019 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
+import { CardViewKeyValuePairsItemComponent } from './card-view-keyvaluepairsitem.component';
+import { CoreStoryModule } from './../../../testing/core.story.module';
+import { CardViewModule, CardViewKeyValuePairsItemModel } from '../../../../..';
+
+export default {
+    component: CardViewKeyValuePairsItemComponent,
+    title: 'Core/Card View/Card View Key Value Pairs Item',
+    decorators: [
+        moduleMetadata({
+            imports: [CoreStoryModule, CardViewModule]
+        })
+    ],
+    argTypes: {
+        editable: {
+            control: 'boolean',
+            description: 'Defines if CardView item is editable',
+            defaultValue: false,
+            table: {
+                type: { summary: 'boolean' },
+                defaultValue: { summary: false }
+            }
+        },
+        property: {
+            description: 'Card View Item Model with data',
+            table: {
+                type: { summary: 'CardViewKeyValuePairsItemModel' }
+            }
+        }
+    }
+} as Meta;
+
+export const CardViewKeyValuePairsItem: Story<CardViewKeyValuePairsItemComponent> =
+    (args: CardViewKeyValuePairsItemComponent) => ({
+        props: args
+    });
+CardViewKeyValuePairsItem.args = {
+    property: new CardViewKeyValuePairsItemModel({
+        label: 'CardView Key-Value Pairs Item',
+        value: [
+            { name: 'hey', value: 'you' },
+            { name: 'hey', value: 'you' }
+        ],
+        key: 'key-value-pairs',
+        editable: true
+    })
+};
