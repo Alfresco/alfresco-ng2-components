cafc4a6d483798d45981d22ece097a6456892d80
Mikolaj Serwicki <mikolaj.serwicki@gmail.com>
2022-09-21 22:55:32 +0000

    feat: move api to follow second entry point structure

--- lib/core/api/src/index.ts
+++ lib/core/api/src/index.ts
@@ -15,7 +15,8 @@
  * limitations under the License.
  */
 
-export * from './activiti/activiti-client.types';
-export * from './alfresco-js-clients.module';
-export * from './discovery/discovery-client.types';
-
+export * from './lib/api-client.factory';
+export * from './lib/api-clients.service';
+export * from './lib/clients';
+export * from './lib/types';
+export * from './lib/alfresco-api/alfresco-api.http-client';
