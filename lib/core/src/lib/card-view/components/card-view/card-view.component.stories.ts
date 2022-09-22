f5b24e0edadf13a3dc5f18cc561e292ab81a3e09
Marcin Krueger <Marcin.Krueger@hyland.com>
2022-09-22 14:35:42 +0200

    [AAE-10446] Storybook move all objects from card-view story into separate mock file

--- lib/core/src/lib/card-view/components/card-view/card-view.component.stories.ts
+++ lib/core/src/lib/card-view/components/card-view/card-view.component.stories.ts
@@ -18,19 +18,8 @@
 import { Meta, moduleMetadata, Story } from '@storybook/angular';
 import { CardViewComponent } from './card-view.component';
 import { CoreStoryModule } from './../../../testing/core.story.module';
-import {
-    CardViewArrayItemModel,
-    CardViewBoolItemModel,
-    CardViewDateItemModel,
-    CardViewDatetimeItemModel,
-    CardViewIntItemModel,
-    CardViewKeyValuePairsItemModel,
-    CardViewMapItemModel,
-    CardViewModule,
-    CardViewSelectItemModel,
-    CardViewTextItemModel
-} from '../../public-api';
-import { of } from 'rxjs';
+import { CardViewModule } from '../../public-api';
+import { cardViewDataSource, cardViewUndefinedValues } from 'lib/mock/card-view-content.mock';
 
 export default {
     component: CardViewComponent,
@@ -72,173 +61,17 @@ export default {
     }
 } as Meta;
 
-const dataSource = [
-    new CardViewTextItemModel({
-        label: 'CardView Text Item - Multivalue (chips)',
-        value: [1, 2, 3, 4],
-        key: 'name',
-        default: 'default bar',
-        multiline: true,
-        multivalued: true,
-        icon: 'icon',
-        editable: true
-    }),
-    new CardViewDateItemModel({
-        label: 'CardView Date Item - Multivalue (chips)',
-        value: [new Date(1983, 11, 24, 10, 0, 30)],
-        key: 'date',
-        default: new Date(1983, 11, 24, 10, 0, 30),
-        format: 'shortDate',
-        editable: true,
-        multivalued: true
-    }),
-    new CardViewDatetimeItemModel({
-        label: 'CardView Datetime Item - Multivalue (chips)',
-        value: [new Date(1983, 11, 24, 10, 0, 0)],
-        key: 'datetime',
-        default: new Date(1983, 11, 24, 10, 0, 0),
-        format: 'short',
-        editable: true,
-        multivalued: true
-    }),
-    new CardViewBoolItemModel({
-        label: 'Agree to all terms and conditions',
-        value: true,
-        key: 'boolean',
-        default: false,
-        editable: true
-    }),
-    new CardViewIntItemModel({
-        label: 'CardView Int Item',
-        value: 213,
-        key: 'int',
-        default: 1,
-        editable: true
-    }),
-    new CardViewKeyValuePairsItemModel({
-        label: 'CardView Key-Value Pairs Item',
-        value: [
-            { name: 'hey', value: 'you' },
-            { name: 'hey', value: 'you' }
-        ],
-        key: 'key-value-pairs',
-        editable: true
-    }),
-    new CardViewSelectItemModel({
-        label: 'CardView Select Item',
-        value: 'one',
-        options$: of([
-            { key: 'one', label: 'One' },
-            { key: 'two', label: 'Two' }
-        ]),
-        key: 'select',
-        editable: true
-    }),
-    new CardViewMapItemModel({
-        label: 'My map',
-        value: new Map([['999', 'My Value']]),
-        key: 'map',
-        default: 'default map value'
-    }),
-    new CardViewTextItemModel({
-        label: 'This is clickable ',
-        value: 'click here',
-        key: 'click',
-        default: 'click here',
-        editable: true,
-        clickable: true,
-        icon: 'close'
-    }),
-    new CardViewArrayItemModel({
-        label: 'CardView Array of items',
-        value: of([
-            { icon: 'directions_bike', value: 'Zlatan' },
-            { icon: 'directions_bike', value: 'Lionel Messi' },
-            { value: 'Mohamed', directions_bike: 'save' },
-            { value: 'Ronaldo' }
-        ]),
-        key: 'array',
-        icon: 'edit',
-        default: 'Empty',
-        noOfItemsToDisplay: 2,
-        editable: true
-    })
-];
-
-const valueAndDefaultUndefinedItems = [
-    new CardViewTextItemModel({
-        label: 'CardView Text Item - Multivalue (chips)',
-        value: undefined,
-        key: 'name',
-        default: undefined,
-        multiline: true,
-        multivalued: true,
-        icon: 'icon',
-        editable: true
-    }),
-    new CardViewDateItemModel({
-        label: 'CardView Date Item - Multivalue (chips)',
-        value: undefined,
-        key: 'date',
-        default: undefined,
-        format: 'shortDate',
-        editable: true,
-        multivalued: true
-    }),
-    new CardViewDatetimeItemModel({
-        label: 'CardView Datetime Item - Multivalue (chips)',
-        value: undefined,
-        key: 'datetime',
-        default: undefined,
-        format: 'short',
-        editable: true,
-        multivalued: true
-    }),
-    new CardViewIntItemModel({
-        label: 'CardView Int Item',
-        value: undefined,
-        key: 'int',
-        default: undefined,
-        editable: true
-    }),
-    new CardViewSelectItemModel({
-        label: 'CardView Select Item',
-        value: undefined,
-        options$: of([
-            { key: 'one', label: 'One' },
-            { key: 'two', label: 'Two' }
-        ]),
-        key: 'select',
-        editable: true
-    }),
-    new CardViewMapItemModel({
-        label: 'My map',
-        value: undefined,
-        key: 'map',
-        default: undefined
-    }),
-    new CardViewTextItemModel({
-        label: 'This is clickable ',
-        value: undefined,
-        key: 'click',
-        default: undefined,
-        editable: true,
-        clickable: true,
-        icon: 'close'
-    })
-];
-
 const template: Story<CardViewComponent> = (args: CardViewComponent) => ({
     props: args
 });
 
 export const defaultStory = template.bind({});
 defaultStory.args = {
-    properties: dataSource
+    properties: cardViewDataSource
 };
 
 export const emptyStory = template.bind({});
 emptyStory.args = {
-    properties: valueAndDefaultUndefinedItems,
+    properties: cardViewUndefinedValues,
     editable: false
 };
