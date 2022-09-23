2ac0473bccd180026153e61c85756100b430de9f
Mikolaj Serwicki <mikolaj.serwicki@hyland.com>
2022-09-23 10:21:09 +0000

    feat: enable custom http client [ci:force]

--- lib/core/src/lib/core.module.ts
+++ lib/core/src/lib/core.module.ts
@@ -16,59 +16,59 @@
  */
 
 import { CommonModule } from '@angular/common';
-import { APP_INITIALIZER, NgModule, ModuleWithProviders } from '@angular/core';
+import { APP_INITIALIZER, ModuleWithProviders, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TranslateModule, TranslateLoader, TranslateStore, TranslateService } from '@ngx-translate/core';
+import { TranslateLoader, TranslateModule, TranslateService, TranslateStore } from '@ngx-translate/core';
 
-import { MaterialModule } from './material.module';
 import { AboutModule } from './about/about.module';
 import { AppConfigModule } from './app-config/app-config.module';
+import { BlankPageModule } from './blank-page/blank-page.module';
+import { ButtonsMenuModule } from './buttons-menu/buttons-menu.module';
 import { CardViewModule } from './card-view/card-view.module';
+import { ClipboardModule } from './clipboard/clipboard.module';
+import { CommentsModule } from './comments/comments.module';
 import { ContextMenuModule } from './context-menu/context-menu.module';
 import { DataColumnModule } from './data-column/data-column.module';
 import { DataTableModule } from './datatable/datatable.module';
+import { FormBaseModule } from './form/form-base.module';
 import { InfoDrawerModule } from './info-drawer/info-drawer.module';
 import { LanguageMenuModule } from './language-menu/language-menu.module';
+import { SidenavLayoutModule } from './layout/layout.module';
 import { LoginModule } from './login/login.module';
+import { MaterialModule } from './material.module';
+import { NotificationHistoryModule } from './notifications/notification-history.module';
 import { PaginationModule } from './pagination/pagination.module';
 import { HostSettingsModule } from './settings/host-settings.module';
+import { TemplateModule } from './templates/template.module';
 import { ToolbarModule } from './toolbar/toolbar.module';
 import { UserInfoModule } from './userinfo/userinfo.module';
 import { ViewerModule } from './viewer/viewer.module';
-import { FormBaseModule } from './form/form-base.module';
-import { SidenavLayoutModule } from './layout/layout.module';
-import { CommentsModule } from './comments/comments.module';
-import { ButtonsMenuModule } from './buttons-menu/buttons-menu.module';
-import { TemplateModule } from './templates/template.module';
-import { ClipboardModule } from './clipboard/clipboard.module';
-import { NotificationHistoryModule } from './notifications/notification-history.module';
-import { BlankPageModule } from './blank-page/blank-page.module';
 
-import { DirectiveModule } from './directives/directive.module';
 import { DownloadZipDialogModule } from './dialogs/download-zip/download-zip.dialog.module';
+import { DirectiveModule } from './directives/directive.module';
 import { PipeModule } from './pipes/pipe.module';
 
-import { AlfrescoApiService } from './services/alfresco-api.service';
-import { TranslationService } from './services/translation.service';
-import { SortingPickerModule } from './sorting-picker/sorting-picker.module';
-import { IconModule } from './icon/icon.module';
-import { TranslateLoaderService } from './services/translate-loader.service';
-import { ExtensionsModule } from '@alfresco/adf-extensions';
-import { directionalityConfigFactory } from './services/directionality-config-factory';
-import { DirectionalityConfigService } from './services/directionality-config.service';
-import { SearchTextModule } from './search-text/search-text-input.module';
-import { versionCompatibilityFactory } from './services/version-compatibility-factory';
-import { VersionCompatibilityService } from './services/version-compatibility.service';
 import { AlfrescoJsClientsModule } from '@alfresco/adf-core/api';
-import { LegacyApiClientModule } from './api-factories/legacy-api-client.module';
-import { RichTextEditorModule } from './rich-text-editor/rich-text-editor.module';
+import { ExtensionsModule } from '@alfresco/adf-extensions';
 import { HttpClientModule, HttpClientXsrfModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthBearerInterceptor } from './services/auth-bearer.interceptor';
+import { AlfrescoApiServiceWithAngularBasedHttpClient } from './api-factories/alfresco-api-service-with-angular-based-http-client';
+import { AlfrescoApiLoaderService, createAlfrescoApiInstance } from './api-factories/alfresco-api-v2-loader.service';
+import { LegacyApiClientModule } from './api-factories/legacy-api-client.module';
 import { loadAppConfig } from './app-config/app-config.loader';
 import { AppConfigService } from './app-config/app-config.service';
+import { IconModule } from './icon/icon.module';
+import { RichTextEditorModule } from './rich-text-editor/rich-text-editor.module';
+import { SearchTextModule } from './search-text/search-text-input.module';
+import { AlfrescoApiService } from './services/alfresco-api.service';
+import { AuthBearerInterceptor } from './services/auth-bearer.interceptor';
+import { directionalityConfigFactory } from './services/directionality-config-factory';
+import { DirectionalityConfigService } from './services/directionality-config.service';
 import { StorageService } from './services/storage.service';
-import { AlfrescoApiLoaderService, createAlfrescoApiInstance } from './api-factories/alfresco-api-v2-loader.service';
-import { AlfrescoApiServiceWithAngularBasedHttpClient } from './api-factories/alfresco-api-service-with-angular-based-http-client';
+import { TranslateLoaderService } from './services/translate-loader.service';
+import { TranslationService } from './services/translation.service';
+import { versionCompatibilityFactory } from './services/version-compatibility-factory';
+import { VersionCompatibilityService } from './services/version-compatibility.service';
+import { SortingPickerModule } from './sorting-picker/sorting-picker.module';
 
 interface Config {
     readonly useAngularBasedHttpClientInAlfrescoJs: boolean;
@@ -158,7 +158,7 @@ const defaultConfig: Config = { useAngularBasedHttpClientInAlfrescoJs: false };
     ]
 })
 export class CoreModule {
-    static forRoot(config: Config = defaultConfig): ModuleWithProviders<CoreModule> {
+    static forRoot(_config: Config = defaultConfig): ModuleWithProviders<CoreModule> {
         return {
             ngModule: CoreModule,
             providers: [
@@ -189,10 +189,7 @@ export class CoreModule {
                     multi: true
                 },
                 { provide: HTTP_INTERCEPTORS, useClass: AuthBearerInterceptor, multi: true },
-                ...(config.useAngularBasedHttpClientInAlfrescoJs
-                    ? [{ provide: AlfrescoApiService, useClass: AlfrescoApiServiceWithAngularBasedHttpClient }]
-                    : []
-                )
+                { provide: AlfrescoApiService, useClass: AlfrescoApiServiceWithAngularBasedHttpClient }
             ]
         };
     }
