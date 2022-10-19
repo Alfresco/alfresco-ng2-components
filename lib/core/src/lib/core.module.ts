e9a03be46c9b56e3798cedc1a11d6eccec1087bf
Robert Duda <Robert.Duda@Hyland.com>
2022-10-19 15:03:27 +0200

    minor

--- lib/core/src/lib/core.module.ts
+++ lib/core/src/lib/core.module.ts
@@ -60,9 +60,11 @@ import { DirectionalityConfigService } from './services/directionality-config.se
 import { SearchTextModule } from './search-text/search-text-input.module';
 import { versionCompatibilityFactory } from './services/version-compatibility-factory';
 import { VersionCompatibilityService } from './services/version-compatibility.service';
+import { AlfrescoJsClientsModule } from '../../api/clients/alfresco-js-clients.module';
 import { LegacyApiClientModule } from './api-factories/legacy-api-client.module';
 import { RichTextEditorModule } from './rich-text-editor/rich-text-editor.module';
-import { AlfrescoJsClientsModule } from '../../api/clients/alfresco-js-clients.module';
+import { HttpClientModule, HttpClientXsrfModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthBearerInterceptor } from './services/auth-bearer.interceptor';
 
 @NgModule({
     imports: [
