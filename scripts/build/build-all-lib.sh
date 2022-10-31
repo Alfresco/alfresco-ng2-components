cebe2501fa99b6981e519814b2e6110438007bc8
Maurizio Vitale <maurizio.vitale@alfresco.com>
2022-10-31 13:55:52 +0000

    [AAE-11253] adf-cli - Get rid of build cli (#7932)
    * Get rid of build cli
    
    * Remove duplication
    
    * Remove build-cli.sh script

--- scripts/build/build-all-lib.sh
+++ scripts/build/build-all-lib.sh
@@ -8,7 +8,7 @@ echo "====== Run lib ====="
 
 if [ "$CI" = "true" ]; then
     echo "Building libs for production with NX_FLAG $NX_CALCULATION_FLAGS"
-    NODE_OPTIONS="--max-old-space-size=8192" $(npm bin)/nx affected:build $NX_CALCULATION_FLAGS --prod --exclude="demoshell,cli,testing" || exit 1
+    NODE_OPTIONS="--max-old-space-size=8192" $(npm bin)/nx affected:build $NX_CALCULATION_FLAGS --prod --exclude="demoshell,testing" || exit 1
 else
     echo "Building libs for development with NX_FLAG $NX_CALCULATION_FLAGS"
     NODE_OPTIONS="--max-old-space-size=8192" $(npm bin)/nx affected:build $NX_CALCULATION_FLAGS --exclude=demoshell || exit 1
@@ -19,8 +19,3 @@ echo "====== run core ====="
 
 echo "====== Run testing ====="
 ./scripts/build/build-testing.sh || exit 1
-
-echo "====== Run Cli ====="
-./scripts/build/build-cli.sh || exit 1
-
-
