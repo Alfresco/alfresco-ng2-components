65dba2d9dc99de18176c23323a8f6ed137704a63
Maurizio Vitale <maurizio.vitale@alfresco.com>
2022-11-01 16:51:12 +0000

    Split docker tag from deploy

---
+++ scripts/travis/release/docker-tag.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/../../../
+
+if [[ $TRAVIS_BRANCH =~ ^master(-patch.*)?$ ]]; then
+    export TAGS=$(grep -m1 version package.json | awk '{ print $2 }' | sed 's/[", ]//g')
+else
+    if [[ "${TRAVIS_PULL_REQUEST_BRANCH}" != "" ]];
+    then
+        export TAGS=""$TRAVIS_PULL_REQUEST_BRANCH-$TRAVIS_BUILD_NUMBER""
+    else
+        export TAGS="$TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER,$TRAVIS_BRANCH"
+    fi;
+
+fi;
+
+if [[ $TRAVIS_BRANCH =~ AAE-11259-decouple-build-release ]]; then
+    export TAGS=test
+fi
+
+echo $TAGS
+
--- scripts/travis/release/release-docker.sh
+++ scripts/travis/release/release-docker.sh
@@ -1,58 +1,22 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/../../../
 
+echo "ℹ️ demo-shell: Running the docker with tag" $TAGS
 
-if [[ $TRAVIS_EVENT_TYPE == "pull_request" ]];
-then
-    HEAD_COMMIT_HASH=${TRAVIS_PULL_REQUEST_SHA:-${TRAVIS_COMMIT}}
-    COMMIT_MESSAGE=`git log --format=%B -n 1 $HEAD_COMMIT_HASH`
-fi;
+DOCKER_PROJECT_ARGS="PROJECT_NAME=demo-shell"
 
-echo "ℹ️ Check Docker Image release for $COMMIT_MESSAGE type $TRAVIS_EVENT_TYPE on branch $TRAVIS_PULL_REQUEST_BRANCH"
+# Publish Image to docker
+./node_modules/@alfresco/adf-cli/bin/adf-cli docker --loginCheck --loginUsername "$DOCKER_REPOSITORY_USER" --loginPassword "$DOCKER_REPOSITORY_PASSWORD" --loginRepo "$DOCKER_REPOSITORY_DOMAIN" --dockerRepo "$DOCKER_REPOSITORY" --buildArgs "$DOCKER_PROJECT_ARGS" --dockerTags "$TAGS" --pathProject "$(pwd)"
 
-if [[ $TRAVIS_EVENT_TYPE == "pull_request" && $COMMIT_MESSAGE == *"[create docker image]"* ]];
-then
+echo "ℹ️ storybook-shell: Running the docker with tag" $TAGS
 
-    if [[ $TRAVIS_BRANCH =~ ^develop(-patch.*)?$ || $TRAVIS_BRANCH =~ ^master(-patch.*)?$ ]];
-    then
+DOCKER_PROJECT_ARGS="PROJECT_NAME=storybook/stories"
 
-        cd $DIR/../../../
+echo "{}" > $DIR/../../../dist/storybook/stories/app.config.json
 
-        if [[ $TRAVIS_BRANCH =~ ^master(-patch.*)?$ ]]; then
-            TAGS=$(grep -m1 version package.json | awk '{ print $2 }' | sed 's/[", ]//g')
-        else
-            if [[ "${TRAVIS_PULL_REQUEST_BRANCH}" != "" ]];
-            then
-                TAGS=""$TRAVIS_PULL_REQUEST_BRANCH-$TRAVIS_BUILD_NUMBER""
-            else
-                TAGS="$TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER,$TRAVIS_BRANCH"
-            fi;
+# Publish Image to docker
+./node_modules/@alfresco/adf-cli/bin/adf-cli docker --loginCheck --loginUsername "$DOCKER_REPOSITORY_USER" --loginPassword "$DOCKER_REPOSITORY_PASSWORD" --loginRepo "$DOCKER_REPOSITORY_DOMAIN" --dockerRepo "$DOCKER_REPOSITORY_STORYBOOK" --buildArgs "$DOCKER_PROJECT_ARGS" --dockerTags "$TAGS" --pathProject "$(pwd)"
 
-        fi;
-
-        if [[ $TRAVIS_BRANCH =~ AAE-11259-decouple-build-release ]]; then
-            TAGS=test
-        fi
-
-        echo "ℹ️ demo-shell: Running the docker with tag" $TAGS
-
-        DOCKER_PROJECT_ARGS="PROJECT_NAME=demo-shell"
-
-        # Publish Image to docker
-        ./node_modules/@alfresco/adf-cli/bin/adf-cli docker --loginCheck --loginUsername "$DOCKER_REPOSITORY_USER" --loginPassword "$DOCKER_REPOSITORY_PASSWORD" --loginRepo "$DOCKER_REPOSITORY_DOMAIN" --dockerRepo "$DOCKER_REPOSITORY" --buildArgs "$DOCKER_PROJECT_ARGS" --dockerTags "$TAGS" --pathProject "$(pwd)"
-
-        echo "ℹ️ storybook-shell: Running the docker with tag" $TAGS
-
-        DOCKER_PROJECT_ARGS="PROJECT_NAME=storybook/stories"
-
-        echo "{}" > $DIR/../../../dist/storybook/stories/app.config.json
-
-        # Publish Image to docker
-        ./node_modules/@alfresco/adf-cli/bin/adf-cli docker --loginCheck --loginUsername "$DOCKER_REPOSITORY_USER" --loginPassword "$DOCKER_REPOSITORY_PASSWORD" --loginRepo "$DOCKER_REPOSITORY_DOMAIN" --dockerRepo "$DOCKER_REPOSITORY_STORYBOOK" --buildArgs "$DOCKER_PROJECT_ARGS" --dockerTags "$TAGS" --pathProject "$(pwd)"
-
-    fi;
-else
-    echo "✅ No need to release a docker image"
-fi;
 
