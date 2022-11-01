6ee09bae9da64ece11b996bf22d668ef969068d8
Maurizio Vitale <maurizio.vitale@alfresco.com>
2022-11-01 11:28:44 +0000

    [AAE-11255] adf-cli - Get rid of the build-testing and call copydist when needed (#7933)
    * Get rid of the build-testing and call copydist when needed
    
    * Use the relative path instead of node_modules and avoid to build cli/testing in the before e2e
    
    * Try to build cli and testing as part of install
    
    * Build cli and testing before e2e
    
    * Copy cli bundle under dist
    
    * Copy cli bundle under dist
    
    * Copy testing into node_modules to fix protractor.js
    
    * Created a bundle target to handle build and move of cli/testing
    
    * Change from build target to something custom to avoid to exclude the packages
    
    * Remove the build-testing.sh script

--- scripts/build/build-all-lib.sh
+++ scripts/build/build-all-lib.sh
@@ -8,7 +8,7 @@ echo "====== Run lib ====="
 
 if [ "$CI" = "true" ]; then
     echo "Building libs for production with NX_FLAG $NX_CALCULATION_FLAGS"
-    NODE_OPTIONS="--max-old-space-size=8192" $(npm bin)/nx affected:build $NX_CALCULATION_FLAGS --prod --exclude="demoshell,testing" || exit 1
+    NODE_OPTIONS="--max-old-space-size=8192" $(npm bin)/nx affected:build $NX_CALCULATION_FLAGS --prod --exclude="demoshell" || exit 1
 else
     echo "Building libs for development with NX_FLAG $NX_CALCULATION_FLAGS"
     NODE_OPTIONS="--max-old-space-size=8192" $(npm bin)/nx affected:build $NX_CALCULATION_FLAGS --exclude=demoshell || exit 1
@@ -17,5 +17,3 @@ fi
 echo "====== run core ====="
 ./scripts/build/build-core.sh || exit 1
 
-echo "====== Run testing ====="
-./scripts/build/build-testing.sh || exit 1
