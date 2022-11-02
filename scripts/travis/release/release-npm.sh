1c46bb83a0c3eddc28a77a5637c923486efd643d
Maurizio Vitale <maurizio.vitale@alfresco.com>
2022-11-02 11:31:54 +0000

    [AAE-11259] Pipeline - Decoupling the build from release (#7935)
    * Decoupling the build from release
    
    * test if we are still able to release on npm and docker
    
    * Split docker tag from deploy
    
    * Fix storybook
    
    * Decouple build lib/apps from releasing
    
    * Remove useless or

--- scripts/travis/release/release-npm.sh
+++ scripts/travis/release/release-npm.sh
@@ -4,26 +4,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR/../../../
 
-if [[ $TRAVIS_EVENT_TYPE == "push"  ]] || [[ $TRAVIS_EVENT_TYPE == "cron" ]] || [[ $TRAVIS_EVENT_TYPE == "api" ]]
+if [[ $TRAVIS_BRANCH =~ ^master(-patch.*)?$ ]]
 then
+    TAG_NPM=latest
+fi
 
-    if [[ $TRAVIS_BRANCH =~ ^master(-patch.*)?$ ]]
-    then
-        TAG_NPM=latest
-    fi
+if [[ $TRAVIS_BRANCH =~ ^develop(-patch.*)?$ ]]
+then
+    TAG_NPM=alpha
+fi
 
-    if [[ $TRAVIS_BRANCH =~ ^develop(-patch.*)?$ ]]
-    then
-        TAG_NPM=alpha
-    fi
-
-    if [[ $TRAVIS_BRANCH =~ angular-14-rebase ]]
-    then
-        TAG_NPM=a14
-    fi
-
-    echo "Publishing on npm with tag $TAG_NPM"
-    ./node_modules/@alfresco/adf-cli/bin/adf-cli npm-publish --npmRegistry $NPM_REGISTRY_ADDRESS --tokenRegistry $NPM_REGISTRY_TOKEN --tag $TAG_NPM --pathProject "$(pwd)"
-else
-    echo "PR No need to release in NPM"
-fi;
+echo "Publishing on npm with tag $TAG_NPM"
+./node_modules/@alfresco/adf-cli/bin/adf-cli npm-publish --npmRegistry $NPM_REGISTRY_ADDRESS --tokenRegistry $NPM_REGISTRY_TOKEN --tag $TAG_NPM --pathProject "$(pwd)"
