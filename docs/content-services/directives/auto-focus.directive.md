e45c6891e4cc44c1cc05b0bd2ca6a2fe9ff786e4
MichalKinas <113341662+MichalKinas@users.noreply.github.com>
2022-11-02 15:28:04 +0100

    [ACS-3701] Add auto focus directive (#7921)
    * [ACS-3701] Add auto focus directive
    
    * [ACS-3701] Linter fix
    
    * [ACS-3701] Another lint fixes

---
+++ docs/content-services/directives/auto-focus.directive.md
@@ -0,0 +1,20 @@
+---
+Title: Auto Focus directive
+Added: v5.1.0
+Status: Active
+Last reviewed: 2022-10-25
+---
+
+# [Auto Focus directive](../../../lib/content-services/src/lib/directives/auto-focus.directive.ts "Defined in auto-focus.directive.ts")
+
+Automatically focuses HTML element after content is initialized.
+
+## Basic Usage
+
+```html
+<button adf-auto-focus>...</button>
+```
+
+## Class members
+
+### Properties
---
+++ lib/content-services/src/lib/directives/auto-focus.directive.spec.ts
@@ -0,0 +1,52 @@
+/*!
+ * @license
+ * Copyright 2019 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component } from '@angular/core';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { AutoFocusDirective } from './auto-focus.directive';
+import { By } from '@angular/platform-browser';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+    template: `<div tabindex="0" adf-auto-focus> Test</div>`
+})
+class AutoFoucsTestComponent {}
+
+describe('AutoFocusDirective', () => {
+    let fixture: ComponentFixture<AutoFoucsTestComponent>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                TranslateModule.forRoot()
+            ],
+            declarations: [
+                AutoFocusDirective,
+                AutoFoucsTestComponent
+            ]
+        });
+        fixture = TestBed.createComponent(AutoFoucsTestComponent);
+    });
+
+    it('should focus the element after content is initialized', fakeAsync(() => {
+        const autoFocusElement = fixture.debugElement.query(By.directive(AutoFocusDirective)).nativeElement;
+        spyOn(autoFocusElement, 'focus');
+        fixture.detectChanges();
+        tick(200);
+        expect(autoFocusElement.focus).toHaveBeenCalled();
+    }));
+});
