35c4d94d99921bfea4ae3da07fe4733c46a0c35f
Denys Vuika <denys.vuika@gmail.com>
2022-11-08 14:26:59 +0000

    try fix the testing lib issues

--- scripts/ci/job_hooks/before_e2e.sh
+++ scripts/ci/job_hooks/before_e2e.sh
@@ -3,4 +3,9 @@
  # Download protractor-smartrunner artifact related to this particular job from S3, if exists
 ./scripts/ci/utils/artifact-from-s3.sh -a "$S3_SMART_RUNNER_PATH/$TRAVIS_JOB_ID.tar.bz2" -o "$SMART_RUNNER_DIRECTORY"
 
+# The adf-testing is not installed through NPM for this reason it needs to be built
+#   in addition the dist folder needs to be moved as part of the node modules
+#   in this way the protractor.config.js can use require('@alfresco/adf-testing');
+nx run testing:bundle
+
 exit 0
