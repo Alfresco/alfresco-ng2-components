1af3aabdfb7d18a8c942ed2d0bd7802a4f66034a
Aleksander Sklorz <Aleksander.Sklorz@hyland.com>
2022-11-18 11:56:00 +0100

    ACS-3640 Corrected one more selector

--- lib/testing/src/lib/protractor/core/pages/snackbar.page.ts
+++ lib/testing/src/lib/protractor/core/pages/snackbar.page.ts
@@ -22,7 +22,7 @@ import { BrowserActions } from '../utils/browser-actions';
 export class SnackbarPage {
 
     notificationSnackBar = $$(`[data-automation-id='adf-snackbar-message-content']`).first();
-    snackBarAction = $('simple-snack-bar button span');
+    snackBarAction = $(`[data-automation-id='adf-snackbar-message-content-action-button']`);
     snackBarContainerCss = $$('.mat-snack-bar-container');
 
     async waitForSnackBarToAppear(timeout = 5000) {
