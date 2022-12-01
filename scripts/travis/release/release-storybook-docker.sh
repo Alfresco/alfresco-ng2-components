6cbafed58070239a658b5c7126eeff87b70cd4fc
Marco Carrozzo <Marco.Carrozzo@hyland.com>
2022-12-01 16:04:19 +0100

    docker release dry run

--- scripts/travis/release/release-storybook-docker.sh
+++ scripts/travis/release/release-storybook-docker.sh
@@ -10,6 +10,13 @@ DOCKER_PROJECT_ARGS="PROJECT_NAME=storybook/stories"
 echo "{}" > $DIR/../../../dist/storybook/stories/app.config.json
 
 # Publish Image to docker
-./node_modules/@alfresco/adf-cli/bin/adf-cli docker --loginCheck --loginUsername "$DOCKER_REPOSITORY_USER" --loginPassword "$DOCKER_REPOSITORY_PASSWORD" --loginRepo "$DOCKER_REPOSITORY_DOMAIN" --dockerRepo "$DOCKER_REPOSITORY_STORYBOOK" --buildArgs "$DOCKER_PROJECT_ARGS" --dockerTags "$TAGS" --pathProject "$(pwd)"
-
-
+./node_modules/@alfresco/adf-cli/bin/adf-cli docker \
+--loginCheck \
+--loginUsername "$DOCKER_REPOSITORY_USER" \
+--loginPassword "$DOCKER_REPOSITORY_PASSWORD" \
+--loginRepo "$DOCKER_REPOSITORY_DOMAIN" \
+--dockerRepo "$DOCKER_REPOSITORY_STORYBOOK" \
+--buildArgs "$DOCKER_PROJECT_ARGS" \
+--dockerTags "$TAGS" \
+--pathProject "$(pwd)"
+--dryrun
