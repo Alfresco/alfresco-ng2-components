d50aa91922023d735191cae2e1304b7603dec3c4
Amedeo Lepore <amedeo.lepore@hyland.com>
2023-02-02 18:39:22 +0100

    Fix alfresco-apps upstream build 260285240: export search-text-input.model from content-services (#8227)

--- lib/content-services/src/lib/search-text/public-api.ts
+++ lib/content-services/src/lib/search-text/public-api.ts
@@ -19,3 +19,4 @@ export * from './animations';
 export * from './search-text-input.component';
 export * from './search-trigger.directive';
 export * from './search-text-input.module';
+export * from './models/search-text-input.model';
