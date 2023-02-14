b1311c6966c9acd13e33af6c90fa399f03d9abe0
MichalKinas <113341662+MichalKinas@users.noreply.github.com>
2023-02-14 11:04:07 +0100

    [ACS-4124] Display only as much tags as fits container in tag node list (#8247)
    * [ACS-4124] Display only as much tags as fits container in tag node list
    
    * [ACS-4124] CR fixes
    
    * [ACS-4124] Add hidden, lint fixes, event propagation stopped

--- lib/content-services/src/lib/tag/tag-node-list.component.ts
+++ lib/content-services/src/lib/tag/tag-node-list.component.ts
@@ -15,11 +15,12 @@
  * limitations under the License.
  */
 
-import { Component, EventEmitter, Input, OnChanges, Output, ViewEncapsulation, OnDestroy, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, ViewEncapsulation, OnDestroy, OnInit, ViewChild, ElementRef, ViewChildren, QueryList } from '@angular/core';
 import { TagService } from './services/tag.service';
 import { TagEntry } from '@alfresco/js-api';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+import { MatChip } from '@angular/material/chips';
 
 /**
  *
@@ -30,10 +31,10 @@ import { takeUntil } from 'rxjs/operators';
     selector: 'adf-tag-node-list',
     templateUrl: './tag-node-list.component.html',
     styleUrls: ['./tag-node-list.component.scss'],
-    encapsulation: ViewEncapsulation.None,
-    host: { class: 'adf-tag-node-list' }
+    encapsulation: ViewEncapsulation.None
 })
 export class TagNodeListComponent implements OnChanges, OnDestroy, OnInit {
+    /* eslint no-underscore-dangle: ["error", { "allow": ["_elementRef"] }]*/
     /** The identifier of a node. */
     @Input()
     nodeId: string;
@@ -42,7 +43,20 @@ export class TagNodeListComponent implements OnChanges, OnDestroy, OnInit {
     @Input()
     showDelete = true;
 
-    tagsEntries: TagEntry[];
+    /** Should limit number of tags displayed */
+    @Input()
+    limitTagsDisplayed = false;
+
+    @ViewChild('nodeListContainer')
+    containerView: ElementRef;
+
+    @ViewChildren(MatChip)
+    tagChips: QueryList<MatChip>;
+
+    tagsEntries: TagEntry[] = [];
+    calculationsDone = false;
+    columnFlexDirection = false;
+    undisplayedTagsCount = 0;
 
     /** Emitted when a tag is selected. */
     @Output()
@@ -66,6 +80,14 @@ export class TagNodeListComponent implements OnChanges, OnDestroy, OnInit {
         this.tagService.refresh
             .pipe(takeUntil(this.onDestroy$))
             .subscribe(() => this.refreshTag());
+
+        this.results
+            .pipe(takeUntil(this.onDestroy$))
+            .subscribe(() => {
+                if (this.limitTagsDisplayed && this.tagsEntries.length > 0) {
+                    this.calculateTagsToDisplay();
+                }
+        });
     }
 
     ngOnDestroy() {
@@ -86,5 +108,38 @@ export class TagNodeListComponent implements OnChanges, OnDestroy, OnInit {
         this.tagService.removeTag(this.nodeId, tag).subscribe(() => {
             this.refreshTag();
         });
+    }
+
+    displayAllTags(event: Event): void {
+        event.preventDefault();
+        event.stopPropagation();
+        this.limitTagsDisplayed = false;
+        this.refreshTag();
+    }
+
+    private calculateTagsToDisplay() {
+        let tagsToDisplay = 1;
+        const containerWidth: number = this.containerView.nativeElement.clientWidth;
+        const viewMoreBtnWidth: number = this.containerView.nativeElement.children[1].offsetWidth;
+        const tagChipMargin = this.getTagChipMargin(this.tagChips.get(0));
+        const tagChipsWidth: number = this.tagChips.reduce((acc, val, index) => {
+            if (containerWidth - viewMoreBtnWidth > acc + val._elementRef.nativeElement.offsetWidth) {
+                tagsToDisplay = index + 1;
+            }
+            return acc + val._elementRef.nativeElement.offsetWidth + tagChipMargin;
+        }, 0);
+        if ((containerWidth - tagChipsWidth) <= 0) {
+            this.columnFlexDirection = tagsToDisplay === 1 && (containerWidth < (this.tagChips.get(0)._elementRef.nativeElement.offsetWidth + viewMoreBtnWidth));
+            this.undisplayedTagsCount = this.tagsEntries.length - tagsToDisplay;
+            this.tagsEntries = this.tagsEntries.slice(0, tagsToDisplay);
+        } else {
+            this.limitTagsDisplayed = false;
+        }
+        this.calculationsDone = true;
+    }
+
+    private getTagChipMargin(chip: MatChip): number {
+        const tagChipStyles = window.getComputedStyle(chip._elementRef.nativeElement);
+        return parseInt(tagChipStyles.marginLeft, 10) + parseInt(tagChipStyles.marginRight, 10);
     }
 }
