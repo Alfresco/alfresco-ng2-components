9ae2f166744d0420d21fc97860350ddb423ae473
maurizio-cacace <Maurizio.Cacace@hyland.com>
2023-03-01 09:30:02 +0100

    AAE-11622 remove unused scripts

--- scripts/ci/check-env/check-ps-env.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-cd $DIR/../../../
-
-echo "====== Check PS UP ====="
-
-./node_modules/@alfresco/adf-cli/bin/adf-cli init-aps-env --host "$E2E_HOST" -u "$E2E_USERNAME" -p "$E2E_PASSWORD" --license "$AWS_S3_BUCKET_ACTIVITI_LICENSE"
