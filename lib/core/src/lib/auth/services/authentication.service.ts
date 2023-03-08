b9ceb7f7586b690081b8f9d564654be838e345d8
Amedeo Lepore <amedeo.lepore@hyland.com>
2023-03-08 17:47:10 +0100

    [AAE-12501] Replace oauth2Auth with authentication service, define get username as abstract

--- lib/core/src/lib/auth/services/authentication.service.ts
+++ lib/core/src/lib/auth/services/authentication.service.ts
@@ -161,6 +161,24 @@ export class AuthenticationService extends BaseAuthenticationService {
         return false;
     }
 
+    /**
+     * Gets the ECM username.
+     *
+     * @returns The ECM username
+     */
+    getEcmUsername(): string {
+        return this.alfrescoApi.getInstance().getEcmUsername();
+    }
+
+    /**
+     * Gets the BPM username
+     *
+     * @returns The BPM username
+     */
+    getBpmUsername(): string {
+        return this.alfrescoApi.getInstance().getBpmUsername();
+    }
+
     isImplicitFlow(): boolean {
         const oauth2: OauthConfigModel = Object.assign({}, this.appConfig.get<OauthConfigModel>(AppConfigValues.OAUTHCONFIG, null));
         return !!oauth2?.implicitFlow;
@@ -179,5 +197,11 @@ export class AuthenticationService extends BaseAuthenticationService {
         return this.storageService.getItem(JwtHelperService.USER_ACCESS_TOKEN);
     }
 
-    reset() {}
+    reset() { }
+
+    once(event: string): Observable<any> {
+        return new Observable((subscriber) => {
+            this.alfrescoApi.getInstance().once(event, () => subscriber.next());
+        });
+    }
 }
