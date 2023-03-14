a4d57e62f8285994d38eee16132c136d7b3436ee
Maurizio Cacace <Maurizio.Cacace@hyland.com>
2023-03-14 14:18:43 +0100

    AAE-11622 - remove unused scripts from pipeline (#8329)
    * AAE-11622 remove unused scripts
    
    * AAE-11622 remove unused scripts
    
    * AAE-11622 remove unused scripts
    
    * AAE-11622 remove unused scripts

--- scripts/ci/check-env/check-ps-cloud-env.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-cd $DIR/../../../
-
-echo "====== Check PS Cloud UP ====="
-
-./node_modules/@alfresco/adf-cli/bin/adf-cli init-aae-env --oauth "$E2E_HOST" --host "$E2E_HOST_APA" --modelerUsername "$E2E_MODELER_USERNAME" --modelerPassword "$E2E_MODELER_PASSWORD" --devopsUsername "$E2E_DEVOPS_USERNAME" --devopsPassword "$E2E_DEVOPS_PASSWORD" --clientId 'alfresco' || exit 1
