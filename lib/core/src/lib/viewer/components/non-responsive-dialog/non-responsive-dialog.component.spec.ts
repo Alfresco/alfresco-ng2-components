50a7caef59a067d2077f6b3b1e6832c54d11eb99
swapnil.verma <swapnil.verma@globallogic.com>
2023-03-24 12:37:51 +0530

    [ACA-4676] Added unit tests for NonResponsivePreviewDialog

--- lib/core/src/lib/viewer/components/non-responsive-dialog/non-responsive-dialog.component.spec.ts
+++ lib/core/src/lib/viewer/components/non-responsive-dialog/non-responsive-dialog.component.spec.ts
@@ -1,23 +1,58 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NonResponsiveDialogComponent } from './non-responsive-dialog.component';
+import { By } from '@angular/platform-browser';
+import { MatDialogRef } from '@angular/material/dialog';
+import { NonResponsivePreviewActionsEnum } from '../../models/non-responsive-preview-actions.enum';
+import { TranslateModule } from '@ngx-translate/core';
+import { CoreTestingModule } from '@alfresco/adf-core';
+
+const mockDialog = {
+    close: jasmine.createSpy('close')
+};
 
 describe('NonResponsiveDialogComponent', () => {
-  let component: NonResponsiveDialogComponent;
-  let fixture: ComponentFixture<NonResponsiveDialogComponent>;
+    let matDialogRef: MatDialogRef<NonResponsiveDialogComponent>;
+    let fixture: ComponentFixture<NonResponsiveDialogComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NonResponsiveDialogComponent ]
-    })
-    .compileComponents();
+    const getButton = (buttonId: string) => {
+        return fixture.debugElement.query(By.css(buttonId)).nativeElement;
+    };
 
-    fixture = TestBed.createComponent(NonResponsiveDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        TestBed.configureTestingModule({
+            declarations: [NonResponsiveDialogComponent],
+            imports: [
+                TranslateModule.forRoot(),
+                CoreTestingModule
+            ],
+            providers: [
+                { provide: MatDialogRef, useValue: mockDialog },
+            ]
+        });
+        matDialogRef = TestBed.inject(MatDialogRef);
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(NonResponsiveDialogComponent);
+        fixture.detectChanges();
+    });
+
+    it('should emit NonResponsivePreviewActions.WAIT when clicking on the wait button', async () => {
+        const waitButton = getButton('#waitButton');
+        waitButton.dispatchEvent(new Event('click'));
+
+        await fixture.detectChanges()
+        await fixture.whenStable();
+
+        expect(matDialogRef.close).toHaveBeenCalledWith(NonResponsivePreviewActionsEnum.WAIT);
+    });
+
+    it('should emit NonResponsivePreviewActions.DOWNLOAD when clicking on the wait button', async () => {
+        const waitButton = getButton('#downloadButton');
+        waitButton.dispatchEvent(new Event('click'));
+
+        await fixture.detectChanges()
+        await fixture.whenStable();
+
+        expect(matDialogRef.close).toHaveBeenCalledWith(NonResponsivePreviewActionsEnum.DOWNLOAD);
+    });
 });
