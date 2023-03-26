e157d9a880e3dcfa56d18328ea57a3aed483ca61
Aleksander Sklorz <Aleksander.Sklorz@hyland.com>
2023-03-26 22:43:28 +0200

    ACS-4126 Added documentation

--- lib/content-services/src/lib/tag/services/tag.service.ts
+++ lib/content-services/src/lib/tag/services/tag.service.ts
@@ -191,6 +191,14 @@ export class TagService {
         );
     }
 
+    /**
+     * Assign tags to node. If tag is new then tag is also created additionally, if tag already exists then it is just assigned.
+     *
+     * @param nodeId Id of node to which tags should be assigned.
+     * @param tags List of tags to create and assign or just assign if they already exist.
+     *
+     * @return Just linked tags to node.
+     */
     assignTagsToNode(nodeId: string, tags: TagBody[]): Observable<TagPaging> {
         return from(this.tagsApi.assignTagsToNode(nodeId, tags)).pipe(
             tap((data) => this.refresh.emit(data))
