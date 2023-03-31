40ac60927c8c0050cc4bfd71416946924e54cd0e
alfresco-build <alfresco-build@hyland.com>
2023-03-31 09:23:06 +0100

    [AAE-11472] - fixed typo

--- lib/cli/scripts/init-aae-env.ts
+++ lib/cli/scripts/init-aae-env.ts
@@ -379,7 +379,7 @@ async function checkIfAppIsReleased(missingApps: any [], tag?: string) {
             await sleep(TIME);
 
             const appInfrastructure = {
-                connector: currentAbsentApp.infrastructure?.connector(),
+                connector: currentAbsentApp.infrastructure?.connectors(),
                 bridges: currentAbsentApp.infrastructure?.bridges
             };
 
