047e9886eddabe5756200f6b54e9afb9b22e97d8
Amedeo Lepore <amedeo.lepore@hyland.com>
2023-04-04 10:39:18 +0200

    [AAE-12501] Enable Auth Module to use the angular oauth2 oidc library

--- demo-shell/src/app/app.module.ts
+++ demo-shell/src/app/app.module.ts
@@ -140,7 +140,7 @@ registerLocaleData(localeSv);
         environment.e2e ? NoopAnimationsModule : BrowserAnimationsModule,
         ReactiveFormsModule,
         RouterModule.forRoot(appRoutes, { useHash: true, relativeLinkResolution: 'legacy' }),
-        ...(environment.oidc ? [AuthModule.forRoot({ useHash: true })] : []),
+        AuthModule.forRoot({ useHash: true }),
         FormsModule,
         HttpClientModule,
         MaterialModule,
