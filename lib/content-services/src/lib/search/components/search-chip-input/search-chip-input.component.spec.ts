9e871074c9f91ca490992577d951a235a627dfc8
MichalKinas <michal.kinas@hyland.com>
2023-06-01 14:36:02 +0200

    [ACS-5181] Add unit tests to logical search components

--- lib/content-services/src/lib/search/components/search-chip-input/search-chip-input.component.spec.ts
+++ lib/content-services/src/lib/search/components/search-chip-input/search-chip-input.component.spec.ts
@@ -15,4 +15,132 @@
  * limitations under the License.
  */
 
-describe('SearchChipInputComponent', () => {});
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatChip } from '@angular/material/chips';
+import { By } from '@angular/platform-browser';
+import { TranslateModule } from '@ngx-translate/core';
+import { Subject } from 'rxjs';
+import { ContentTestingModule } from '../../../testing/content.testing.module';
+import { SearchChipInputComponent } from './search-chip-input.component';
+
+describe('SearchChipInputComponent', () => {
+    let component: SearchChipInputComponent;
+    let fixture: ComponentFixture<SearchChipInputComponent>;
+    const onResetSubject = new Subject<void>();
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [SearchChipInputComponent],
+            imports: [
+                TranslateModule.forRoot(),
+                ContentTestingModule
+            ]
+        });
+
+        fixture = TestBed.createComponent(SearchChipInputComponent);
+        component = fixture.componentInstance;
+        component.onReset = onResetSubject.asObservable();
+        fixture.detectChanges();
+    });
+
+    afterEach(() => removeAllChips());
+
+    function getChipInput(): HTMLInputElement {
+        return fixture.debugElement.query(By.css('input')).nativeElement;
+    }
+
+    function getChipList(): MatChip[] {
+        return fixture.debugElement.queryAll(By.css('mat-chip')).map((chip) => chip.nativeElement);
+    }
+
+    function getChipValue(index: number): string {
+        return fixture.debugElement.queryAll(By.css('mat-chip span')).map((chip) => chip.nativeElement)[index].innerText;
+    }
+
+    function enterNewChip(value: string) {
+        const input = getChipInput();
+        input.value = value;
+        fixture.detectChanges();
+        input.dispatchEvent(new KeyboardEvent('keydown', {keyCode: 13}));
+        fixture.detectChanges();
+    }
+
+    function removeAllChips() {
+        const chips = getChipList();
+        if (!!chips && chips.length > 0) {
+            chips.forEach((chip) => chip.remove());
+        }
+    }
+
+    it('should display label provided as component input', () => {
+        const label = 'Test';
+        component.label = label;
+        fixture.detectChanges();
+        const matLabel = fixture.debugElement.query(By.css('mat-label')).nativeElement.innerText;
+        expect(matLabel).toBe(label);
+    });
+
+    it('should display proper placeholder for chip input', () => {
+        const input = getChipInput();
+        expect(input.placeholder).toBe('SEARCH.LOGICAL_SEARCH.SEARCH_CHIP_INPUT.ADD_PHRASE');
+    });
+
+    it('should not display any chips initially', () => {
+        const chips = getChipList();
+        expect(chips).toEqual([]);
+    });
+
+    it('should add new chip when input has value and enter was hit', () => {
+        const phrasesChangedSpy = spyOn(component.phrasesChanged, 'emit');
+        enterNewChip('test');
+        expect(phrasesChangedSpy).toHaveBeenCalledOnceWith(['test']);
+        expect(getChipList().length).toBe(1);
+        expect(getChipValue(0)).toBe('test');
+    });
+
+    it('should add input value as whole phrase even if it contains whitespaces and special signs', () => {
+        const phrase = 'test another world &*,.;""!@#$$%^*()[]-+=';
+        enterNewChip(phrase);
+        expect(getChipList().length).toBe(1);
+        expect(getChipValue(0)).toBe(phrase);
+    });
+
+    it('should add new chip when input is blurred', () => {
+        const input = getChipInput();
+        input.value = 'test';
+        fixture.detectChanges();
+        input.dispatchEvent(new InputEvent('blur'));
+        fixture.detectChanges();
+        expect(input.value).toBe('');
+        expect(getChipList().length).toBe(1);
+        expect(getChipValue(0)).toBe('test');
+    });
+
+    it('should not add new chip when input is blurred if addOnBlur is false', () => {
+        component.addOnBlur = false;
+        const input = getChipInput();
+        input.value = 'test';
+        fixture.detectChanges();
+        input.dispatchEvent(new InputEvent('blur'));
+        fixture.detectChanges();
+        expect(input.value).toBe('test');
+        expect(getChipList().length).toBe(0);
+    });
+
+    it('should clear the input after new chip is added', () => {
+        const input = getChipInput();
+        enterNewChip('test2');
+        expect(input.value).toBe('');
+    });
+
+    it('should reset all chips when onReset event is emitted', () => {
+        enterNewChip('test1');
+        enterNewChip('test2');
+        enterNewChip('test3');
+        const phrasesChangedSpy = spyOn(component.phrasesChanged, 'emit');
+        onResetSubject.next();
+        fixture.detectChanges();
+        expect(phrasesChangedSpy).toHaveBeenCalledOnceWith([]);
+        expect(getChipList()).toEqual([]);
+    });
+});
--- lib/content-services/src/lib/search/components/search-logical-filter/search-logical-filter.component.spec.ts
+++ lib/content-services/src/lib/search/components/search-logical-filter/search-logical-filter.component.spec.ts
@@ -15,4 +15,174 @@
  * limitations under the License.
  */
 
-describe('SearchLogicalFilterComponent', () => {});
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { TranslateModule } from '@ngx-translate/core';
+import { ContentTestingModule } from '../../../testing/content.testing.module';
+import { SearchChipInputComponent } from '../search-chip-input/search-chip-input.component';
+import { LogicalSearchCondition, LogicalSearchFields, SearchLogicalFilterComponent } from './search-logical-filter.component';
+
+describe('SearchLogicalFilterComponent', () => {
+    let component: SearchLogicalFilterComponent;
+    let fixture: ComponentFixture<SearchLogicalFilterComponent>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [SearchLogicalFilterComponent, SearchChipInputComponent],
+            imports: [
+                TranslateModule.forRoot(),
+                ContentTestingModule
+            ]
+        });
+
+        fixture = TestBed.createComponent(SearchLogicalFilterComponent);
+        component = fixture.componentInstance;
+        component.id = 'logic';
+        component.context = {
+            queryFragments: {
+                logic: ''
+            },
+            update: () => {}
+        } as any;
+        component.settings = { field: 'field1,field2', allowUpdateOnChange: true, hideDefaultAction: false };
+        fixture.detectChanges();
+    });
+
+    function getChipInputs(): HTMLInputElement[] {
+        return fixture.debugElement.queryAll(By.css('adf-search-chip-input input')).map((input) => input.nativeElement);
+    }
+
+    function getChipInputsLabels(): string[] {
+        return fixture.debugElement.queryAll(By.css('adf-search-chip-input mat-label')).map((label) => label.nativeElement.innerText);
+    }
+
+    function enterNewPhrase(value: string, index: number) {
+        const inputs = getChipInputs();
+        inputs[index].value = value;
+        fixture.detectChanges();
+        inputs[index].dispatchEvent(new KeyboardEvent('keydown', {keyCode: 13}));
+        fixture.detectChanges();
+    }
+
+    function clickApplyBtn() {
+        const applyBtn: HTMLButtonElement = fixture.debugElement.query(By.css('[data-automation-id="logical-filter-btn-apply"]')).nativeElement;
+        applyBtn.click();
+        fixture.detectChanges();
+    }
+
+    it('should update display value on init', () => {
+        spyOn(component, 'updateDisplayValue').and.callThrough();
+        spyOn(component.displayValue$, 'next');
+        component.ngOnInit();
+        expect(component.updateDisplayValue).toHaveBeenCalled();
+        expect(component.displayValue$.next).toHaveBeenCalledOnceWith('');
+    });
+
+    it('should not have valid value initially', () => {
+        expect(component.hasValidValue()).toBeFalse();
+    });
+
+    it('should contain 3 chip input components with correct labels', () => {
+        const labels = getChipInputsLabels();
+        expect(labels.length).toBe(3);
+        expect(labels[0]).toBe('SEARCH.LOGICAL_SEARCH.MATCH_ALL_LABEL');
+        expect(labels[1]).toBe('SEARCH.LOGICAL_SEARCH.MATCH_ANY_LABEL');
+        expect(labels[2]).toBe('SEARCH.LOGICAL_SEARCH.EXCLUDE_LABEL');
+    });
+
+    it('should has valid value after phrase is entered', () => {
+        enterNewPhrase('test', 0);
+        expect(component.hasValidValue()).toBeTrue();
+    });
+
+    it('should update display value after phrases changes', () => {
+        spyOn(component, 'onPhraseChange').and.callThrough();
+        spyOn(component.displayValue$, 'next');
+        enterNewPhrase('test2', 0);
+        expect(component.onPhraseChange).toHaveBeenCalled();
+        expect(component.displayValue$.next).toHaveBeenCalledOnceWith(` SEARCH.LOGICAL_SEARCH.${Object.keys(LogicalSearchFields)[0]}:  test2`);
+    });
+
+    it('should display each phrase in display value', () => {
+        spyOn(component, 'onPhraseChange').and.callThrough();
+        spyOn(component.displayValue$, 'next');
+        enterNewPhrase('test2', 0);
+        expect(component.onPhraseChange).toHaveBeenCalled();
+        expect(component.displayValue$.next).toHaveBeenCalledOnceWith(` SEARCH.LOGICAL_SEARCH.${Object.keys(LogicalSearchFields)[0]}:  test2`);
+    });
+
+    it('should have correct display value after each field has at least one phrase', () => {
+        spyOn(component, 'onPhraseChange').and.callThrough();
+        spyOn(component.displayValue$, 'next');
+        enterNewPhrase('test1', 0);
+        enterNewPhrase('test2', 1);
+        enterNewPhrase('test3', 2);
+        const displayVal1 = ` SEARCH.LOGICAL_SEARCH.${Object.keys(LogicalSearchFields)[0]}:  test1`;
+        const displayVal2 = ` SEARCH.LOGICAL_SEARCH.${Object.keys(LogicalSearchFields)[1]}:  test2`;
+        const displayVal3 = ` SEARCH.LOGICAL_SEARCH.${Object.keys(LogicalSearchFields)[2]}:  test3`;
+        expect(component.onPhraseChange).toHaveBeenCalled();
+        expect(component.displayValue$.next).toHaveBeenCalledWith(displayVal1 + displayVal2 + displayVal3);
+    });
+
+    it('should set correct value and update display value', () => {
+        spyOn(component.displayValue$, 'next');
+        const searchCondition: LogicalSearchCondition = { matchAll: ['test1'], matchAny: ['test2'], exclude: ['test3'] };
+        component.setValue(searchCondition);
+        expect(component.getCurrentValue()).toEqual(searchCondition);
+        expect(component.displayValue$.next).toHaveBeenCalled();
+    });
+
+    it('should reset value and display value when reset button is clicked', () => {
+        const searchCondition: LogicalSearchCondition = { matchAll: ['test1'], matchAny: ['test2'], exclude: ['test3'] };
+        component.setValue(searchCondition);
+        fixture.detectChanges();
+        spyOn(component.context, 'update');
+        spyOn(component.displayValue$, 'next');
+        const resetBtn: HTMLButtonElement = fixture.debugElement.query(By.css('[data-automation-id="logical-filter-btn-clear"]')).nativeElement;
+        resetBtn.click();
+        expect(component.context.queryFragments[component.id]).toBe('');
+        expect(component.context.update).toHaveBeenCalled();
+        expect(component.getCurrentValue()).toEqual({ matchAll: [], matchAny: [], exclude: [] });
+        expect(component.displayValue$.next).toHaveBeenCalledWith('');
+    });
+
+    it('should form correct query from match all field', () => {
+        spyOn(component.context, 'update');
+        enterNewPhrase('test1', 0);
+        enterNewPhrase('test2', 0);
+        clickApplyBtn();
+        expect(component.context.update).toHaveBeenCalled();
+        expect(component.context.queryFragments[component.id]).toBe('((field1:"test1" AND field1:"test2") OR (field2:"test1" AND field2:"test2"))');
+    });
+
+    it('should form correct query from match any field', () => {
+        spyOn(component.context, 'update');
+        enterNewPhrase('test3', 1);
+        enterNewPhrase('test4', 1);
+        clickApplyBtn();
+        expect(component.context.update).toHaveBeenCalled();
+        expect(component.context.queryFragments[component.id]).toBe('((field1:"test3" OR field1:"test4") OR (field2:"test3" OR field2:"test4"))');
+    });
+
+    it('should form correct query from exclude field', () => {
+        spyOn(component.context, 'update');
+        enterNewPhrase('test5', 2);
+        enterNewPhrase('test6', 2);
+        clickApplyBtn();
+        expect(component.context.update).toHaveBeenCalled();
+        expect(component.context.queryFragments[component.id]).toBe('((NOT field1:"test5" AND NOT field1:"test6") AND (NOT field2:"test5" AND NOT field2:"test6"))');
+    });
+
+    it('should form correct joined query from all fields', () => {
+        spyOn(component.context, 'update');
+        enterNewPhrase('test1', 0);
+        enterNewPhrase('test2', 1);
+        enterNewPhrase('test3', 2);
+        clickApplyBtn();
+        const subQuery1 = '((field1:"test1") OR (field2:"test1"))';
+        const subQuery2 = '((field1:"test2") OR (field2:"test2"))';
+        const subQuery3 = '((NOT field1:"test3") AND (NOT field2:"test3"))';
+        expect(component.context.update).toHaveBeenCalled();
+        expect(component.context.queryFragments[component.id]).toBe(`${subQuery1} AND ${subQuery2} AND ${subQuery3}`);
+    });
+});
