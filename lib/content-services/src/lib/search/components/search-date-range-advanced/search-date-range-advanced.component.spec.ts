be81683a69869c66ce0534c2845325a7f3bcb74d
swapnil.verma <swapnil.verma@globallogic.com>
2023-06-13 17:41:10 +0530

    [ACS-4985] Added test cases for SearchFilterTabbedComponent. Added test case placeholders for SearchDateRangeAdvancedComponent

--- lib/content-services/src/lib/search/components/search-date-range-advanced/search-date-range-advanced.component.spec.ts
+++ lib/content-services/src/lib/search/components/search-date-range-advanced/search-date-range-advanced.component.spec.ts
@@ -3,21 +3,53 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SearchDateRangeAdvancedComponent } from './search-date-range-advanced.component';
 
 describe('SearchDateRangeAdvancedComponent', () => {
-  let component: SearchDateRangeAdvancedComponent;
-  let fixture: ComponentFixture<SearchDateRangeAdvancedComponent>;
+    let component: SearchDateRangeAdvancedComponent;
+    let fixture: ComponentFixture<SearchDateRangeAdvancedComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SearchDateRangeAdvancedComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [SearchDateRangeAdvancedComponent]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(SearchDateRangeAdvancedComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(SearchDateRangeAdvancedComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should not set any date filter in context when Anytime option is selected', () => {
+
+    });
+
+    it('should set proper date filter in context when In the last option is selected', () => {
+
+    });
+
+    it('should not set any date filter in context when In the last option is selected, but no value is provided', () => {
+
+    });
+
+    it('should set proper date filter in context when Between option is selected', () => {
+
+    });
+
+    it('should not set any date filter in context when Between option is selected, but no value is provided in the dates', () => {
+
+    });
+
+    it('should clear context when widget is reset', () => {
+
+    });
+
+    it('should update display label with proper text when In the last/Between option is selected and values are properly set', () => {
+
+    });
+
+    it('should set values if initial value is provided', () => {
+
+    });
+
+    it('should not allow selecting a date after the current date when Between option is selected', () => {
+
+    });
 });
