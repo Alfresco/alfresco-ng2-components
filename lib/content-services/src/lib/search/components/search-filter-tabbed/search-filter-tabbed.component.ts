bcee978e11f164b3bd633576e6bb478b3980f556
swapnil.verma <swapnil.verma@globallogic.com>
2023-06-14 17:49:35 +0530

    [ACS-4985] Added license headers and re-ordered imports

--- lib/content-services/src/lib/search/components/search-filter-tabbed/search-filter-tabbed.component.ts
+++ lib/content-services/src/lib/search/components/search-filter-tabbed/search-filter-tabbed.component.ts
@@ -1,10 +1,27 @@
+/*!
+ * @license
+ * Copyright © 2005-2023 Hyland Software, Inc. and its affiliates. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { AfterViewInit, Component, OnDestroy, OnInit, QueryList, ViewChildren, ViewEncapsulation } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { SearchWidget } from '../../models/search-widget.interface';
 import { SearchWidgetSettings } from '../../models/search-widget-settings.interface';
 import { SearchQueryBuilderService } from '../../services/search-query-builder.service';
 import { SearchWidgetContainerComponent } from '../search-widget-container/search-widget-container.component';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'adf-search-filter-tabbed',
@@ -13,11 +30,11 @@ import { takeUntil } from 'rxjs/operators';
   encapsulation: ViewEncapsulation.None
 })
 export class SearchFilterTabbedComponent implements SearchWidget, OnInit, AfterViewInit, OnDestroy {
-    context: SearchQueryBuilderService;
+    context?: SearchQueryBuilderService;
     displayValue$: Subject<string> = new Subject<string>();
     id: string;
     isActive: boolean;
-    settings: SearchWidgetSettings;
+    settings?: SearchWidgetSettings;
     startValue: any;
     displayLabelMap: Map<string, string> = new Map<string, string>();
 
