f2071caf27b0001f00d1cfafa39f52dff3f9cc60
Aleksander Sklorz <Aleksander.Sklorz@hyland.com>
2023-06-21 14:13:46 +0200

    ACS-4985 Fixed issue with nx build, some clean ups, using changes in configuration

---
+++ lib/content-services/src/lib/search/components/search-filter-tabbed/search-filter-tab.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SearchFilterTabDirective } from './search-filter-tab.directive';
+
+describe('SearchFilterTabDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SearchFilterTabDirective();
+    expect(directive).toBeTruthy();
+  });
+});
