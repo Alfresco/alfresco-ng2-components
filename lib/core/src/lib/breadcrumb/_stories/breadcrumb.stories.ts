037dce0ae7a8b508b42f515abf26b4168640361d
Popovics András <popovicsandras@users.noreply.github.com>
2023-06-27 15:09:37 +0200

    [HXCS-1479] Core breadcrumbs component (#8695)
    * Moving breadcrumb component from Hyland UI to ADF and renaming selectors and entities with proper prefix
    
    * Cleaning up a bit the breadcumbs
    
    * Stabilising breadcrumbs and adding storibook
    
    * Fix change requests

---
+++ lib/core/src/lib/breadcrumb/_stories/breadcrumb.stories.ts
@@ -0,0 +1,75 @@
+/*!
+ * @license
+ * Copyright © 2005-2023 Hyland Software, Inc. and its affiliates. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { BreadcrumbComponent } from '../components/breadcrumb/breadcrumb.component';
+import { BreadcrumbItemComponent } from '../components/breadcrumb-item/breadcrumb-item.component';
+import { DemoBreadcrumbComponent } from './demo-breadcrumb.component';
+import { CoreStoryModule } from '../../testing/core.story.module';
+
+// https://stackoverflow.com/a/58210459/8820824
+type NonFunctionPropertyNames<T> = {[K in keyof T]: T[K] extends () => any ? never : K}[keyof T];
+type NonFunctionProperties<T> = Pick<T, NonFunctionPropertyNames<T>>;
+type StoryWithoutFunction<T> = NonFunctionProperties<Story<T>>;
+
+function storybookCopyStory<T>( story: Story<T>, annotations?: StoryWithoutFunction<T> ): Story<T> {
+  const cloned = story.bind({});
+  return Object.assign(cloned, annotations);
+}
+
+const meta: Meta = {
+  title: 'Core/Breadcrumb',
+  decorators: [
+    moduleMetadata({
+      imports: [
+        CoreStoryModule,
+        BreadcrumbComponent,
+        BreadcrumbItemComponent,
+        MatButtonModule,
+        MatMenuModule,
+        MatIconModule
+      ]
+    })
+  ],
+  args: {
+    compact: false,
+    showBreadcrumbItemWithMenu: false
+  },
+  argTypes: {
+    compact: {control: 'boolean'},
+    showBreadcrumbItemWithMenu: {control: 'boolean'}
+  }
+};
+export default meta;
+
+export const breadcrumb: Story = args => ({
+  component: DemoBreadcrumbComponent,
+  props: args
+});
+
+export const compact = storybookCopyStory(breadcrumb);
+compact.args = {
+  compact: true
+};
+
+export const withMenu = storybookCopyStory(breadcrumb);
+withMenu.args = {
+  showBreadcrumbItemWithMenu: true
+};
---
+++ lib/core/src/lib/breadcrumb/components/breadcrumb/breadcrumb.component.ts
@@ -0,0 +1,98 @@
+/*!
+ * @license
+ * Copyright © 2005-2023 Hyland Software, Inc. and its affiliates. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { AfterContentInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ContentChildren, EventEmitter, Input, OnChanges, Output, QueryList, SimpleChanges, TemplateRef, ViewChildren } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { map, startWith } from 'rxjs/operators';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { BreadcrumbFocusDirective } from '../../directives/breadcrumb-focus.directive';
+import { BreadcrumbItemComponent } from '../breadcrumb-item/breadcrumb-item.component';
+
+@Component({
+    standalone: true,
+    selector: 'adf-breadcrumb',
+    templateUrl: './breadcrumb.component.html',
+    styleUrls: ['./breadcrumb.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [ CommonModule, MatIconModule, TranslateModule, MatButtonModule, MatTooltipModule ]
+})
+export class BreadcrumbComponent implements AfterContentInit, OnChanges {
+    private _breadcrumbTemplateRefs: Array<TemplateRef<unknown>> = [];
+
+    @Input()
+    compact = false;
+
+    @Output()
+    compactChange: EventEmitter<boolean> = new EventEmitter();
+
+    @ViewChildren(BreadcrumbFocusDirective)
+    breadcrumbFocusItems!: QueryList<BreadcrumbFocusDirective>;
+
+    @ContentChildren(BreadcrumbItemComponent)
+    breadcrumbItems!: QueryList<BreadcrumbItemComponent>;
+
+    selectedBreadcrumbs: Array<TemplateRef<unknown>> = [];
+
+    constructor(private cdr: ChangeDetectorRef) {}
+
+    ngAfterContentInit() {
+        this.breadcrumbItems.changes
+            .pipe(
+                startWith(this.breadcrumbItems),
+                map((breadcrumbItems: QueryList<BreadcrumbItemComponent>) =>
+                    this.mapToTemplateRefs(breadcrumbItems)
+                )
+            )
+            .subscribe((templateRefs) => {
+                this._breadcrumbTemplateRefs = templateRefs;
+                this.setBreadcrumbs(templateRefs);
+            });
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes.compact) {
+            this.setBreadcrumbs(this._breadcrumbTemplateRefs);
+        }
+    }
+
+    toggleCompact(compact = false) {
+        this.compact = compact;
+        this.setBreadcrumbs(this._breadcrumbTemplateRefs);
+        this.compactChange.emit(this.compact);
+        if (!compact) {
+            this.breadcrumbFocusItems.get(1)?.focusOnFirstFocusableElement();
+        }
+    }
+
+    private setBreadcrumbs(breadcrumbs: Array<TemplateRef<unknown>>) {
+        this.selectedBreadcrumbs =
+            this.compact && breadcrumbs.length > 2
+                ? [breadcrumbs[0], breadcrumbs[breadcrumbs.length - 1]]
+                : [...breadcrumbs];
+        this.cdr.detectChanges();
+    }
+
+    private mapToTemplateRefs( breadcrumbItems: QueryList<BreadcrumbItemComponent> ) {
+        return breadcrumbItems
+            .toArray()
+            .map((breadcrumbItem) => breadcrumbItem.templateRef);
+    }
+}
