27c82c3aea2c0b7f85cf08ec59c8a03247fff331
swapnil.verma <swapnil.verma@globallogic.com>
2023-07-10 12:55:25 +0530

    [ACS-4985] Fixed code smell in regex

--- lib/content-services/src/lib/search/components/search-date-range-advanced-tabbed/search-date-range-advanced/search-date-range-advanced.component.ts
+++ lib/content-services/src/lib/search/components/search-date-range-advanced-tabbed/search-date-range-advanced/search-date-range-advanced.component.ts
@@ -158,6 +158,6 @@ export class SearchDateRangeAdvancedComponent implements OnInit, OnDestroy {
 
     onLastDateValueChanged(event: Event) {
         let value: string = event.target['value'];
-        event.target['value'] = value.replace(/[-.]*0*([1-9]*[0-9]*)/g, '$1');
+        event.target['value'] = value.replace(/[-.]*0*([1-9]*\d*)/g, '$1');
     }
 }
