ac8a11b2ec71ff00061528224f34f9123c00202a
eromano <eugenioromano16@gmail.com>
2023-08-04 17:53:38 +0200

    fix PC

--- lib/process-services-cloud/src/lib/task/task-filters/services/task-filter-cloud.service.spec.ts
+++ lib/process-services-cloud/src/lib/task/task-filters/services/task-filter-cloud.service.spec.ts
@@ -35,7 +35,6 @@ import { NotificationCloudService } from '../../../services/notification-cloud.s
 import { ProcessServiceCloudTestingModule } from '../../../testing/process-service-cloud.testing.module';
 import { IdentityUserService } from '../../../people/services/identity-user.service';
 import { ApolloModule } from 'apollo-angular';
-import { AuthModule } from '@alfresco/adf-core';
 
 describe('TaskFilterCloudService', () => {
     let service: TaskFilterCloudService;
@@ -248,7 +247,7 @@ describe('Inject [LocalPreferenceCloudService] into the TaskFilterCloudService',
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [HttpClientTestingModule, AuthModule, ApolloModule],
+            imports: [HttpClientTestingModule, ProcessServiceCloudTestingModule, ApolloModule],
             providers: [
                 { provide: TASK_FILTERS_SERVICE_TOKEN, useClass: LocalPreferenceCloudService }
             ]
