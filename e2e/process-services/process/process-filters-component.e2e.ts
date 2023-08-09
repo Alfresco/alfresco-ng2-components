d6b3a7362f2337dadc5106b520a165d775e54fb6
Denys Vuika <denys.vuika@gmail.com>
2023-08-09 18:14:48 -0400

    [ci:force] remove duplicated tests (already present in ACA/ADW)

--- e2e/process-services/process/process-filters-component.e2e.ts
+++ e2e/process-services/process/process-filters-component.e2e.ts
@@ -15,26 +15,17 @@
  * limitations under the License.
  */
 
-import { createApiService,
-    ApplicationsUtil,
-    BrowserActions,
-    LoginPage,
-    StartProcessPage,
-    UsersActions
-} from '@alfresco/adf-testing';
+import { createApiService, ApplicationsUtil, LoginPage, StartProcessPage, UsersActions } from '@alfresco/adf-testing';
 import { NavigationBarPage } from '../../core/pages/navigation-bar.page';
 import { ProcessServicesPage } from './../pages/process-services.page';
 import { ProcessFiltersPage } from './../pages/process-filters.page';
 import { ProcessServiceTabBarPage } from './../pages/process-service-tab-bar.page';
 import { ProcessDetailsPage } from './../pages/process-details.page';
 import { ProcessListPage } from './../pages/process-list.page';
-import { RuntimeAppDefinitionsApi, UserFiltersApi, UserProcessInstanceFilterRepresentation } from '@alfresco/js-api';
 import { browser } from 'protractor';
 import { ProcessListDemoPage } from './../pages/process-list-demo.page';
-import CONSTANTS = require('../../util/constants');
 
 describe('Process Filters Test', () => {
-
     const app = browser.params.resources.Files.APP_WITH_DATE_FIELD_FORM;
 
     const loginPage = new LoginPage();
@@ -50,10 +41,6 @@ describe('Process Filters Test', () => {
     const apiService = createApiService();
     const usersActions = new UsersActions(apiService);
     const applicationsService = new ApplicationsUtil(apiService);
-    const userFiltersApi = new UserFiltersApi(apiService.getInstance());
-    const appsApi = new RuntimeAppDefinitionsApi(apiService.getInstance());
-
-    let appModel; let user;
 
     const processTitle = {
         running: 'Test_running',
@@ -69,11 +56,10 @@ describe('Process Filters Test', () => {
     };
 
     beforeAll(async () => {
-
         await apiService.loginWithProfile('admin');
-        user = await usersActions.createUser();
+        const user = await usersActions.createUser();
         await apiService.login(user.username, user.password);
-        appModel = await applicationsService.importPublishDeployApp(app.file_path);
+        await applicationsService.importPublishDeployApp(app.file_path);
         await loginPage.login(user.username, user.password);
     });
 
@@ -136,27 +122,6 @@ describe('Process Filters Test', () => {
         await processDetailsPage.propertiesList.waitVisible();
     });
 
-    it('[C280407] Should be able to access the filters with URL', async () => {
-        const defaultFiltersNumber = 3;
-        let processFilterUrl;
-
-        const deployedAppId = await applicationsService.getAppDefinitionId(appModel.id);
-
-        processFilterUrl = browser.baseUrl + '/activiti/apps/' + deployedAppId + '/processes/';
-        const taskAppFilters = await userFiltersApi.getUserProcessInstanceFilters({ appId: deployedAppId });
-
-        await processServicesPage.goToApp(app.title);
-        await processServiceTabBarPage.clickProcessButton();
-        await expect(await processListPage.isProcessListDisplayed()).toEqual(true);
-
-        await expect(taskAppFilters.size).toBe(defaultFiltersNumber);
-        for (const filter of taskAppFilters.data) {
-            await BrowserActions.getUrl(processFilterUrl + filter.id);
-            await expect(await processListPage.isProcessListDisplayed()).toEqual(true);
-            await processFiltersPage.checkFilterIsHighlighted(filter.name);
-        }
-    });
-
     it('[C260463] Should Cancel process be displayed in Completed process filters', async () => {
         await processServicesPage.goToApp(app.title);
         await processServiceTabBarPage.clickProcessButton();
@@ -177,81 +142,4 @@ describe('Process Filters Test', () => {
         await processFiltersPage.selectFromProcessList(processTitle.canceled);
         await processDetailsPage.propertiesList.waitVisible();
     });
-
-    it('[C213262] Default process filters', async () => {
-        await processServicesPage.goToApp(app.title);
-        await processServiceTabBarPage.clickProcessButton();
-        await expect(await processListPage.isProcessListDisplayed()).toEqual(true);
-
-        await processFiltersPage.clickCreateProcessButton();
-        await processFiltersPage.clickNewProcessDropdown();
-        await startProcessPage.enterProcessName(processTitle.one);
-        await startProcessPage.clickFormStartProcessButton();
-        await expect(await processListPage.isProcessListDisplayed()).toEqual(true);
-
-        await processListDemoPage.checkProcessIsDisplayed(processTitle.one);
-        await processFiltersPage.checkFilterIsHighlighted(processFilter.running);
-        await processDetailsPage.propertiesList.waitVisible();
-        await checkProcessInfoDrawer({ name: processTitle.one });
-
-        await processFiltersPage.clickCreateProcessButton();
-        await processFiltersPage.clickNewProcessDropdown();
-        await startProcessPage.enterProcessName(processTitle.two);
-        await startProcessPage.clickFormStartProcessButton();
-        await expect(await processListPage.isProcessListDisplayed()).toEqual(true);
-
-        await processListDemoPage.checkProcessIsDisplayed(processTitle.one);
-        await processListDemoPage.checkProcessIsDisplayed(processTitle.two);
-
-        await processDetailsPage.cancelProcessButton.click();
-        await processListDemoPage.checkProcessIsNotDisplayed(processTitle.canceled);
-
-        await processFiltersPage.clickCompletedFilterButton();
-        await processFiltersPage.checkFilterIsHighlighted(processFilter.completed);
-        await processListDemoPage.checkProcessIsDisplayed(processTitle.two);
-        await processFiltersPage.selectFromProcessList(processTitle.two);
-
-        await processFiltersPage.clickAllFilterButton();
-        await processFiltersPage.checkFilterIsHighlighted(processFilter.all);
-        await processListDemoPage.checkProcessIsDisplayed(processTitle.two);
-        await processFiltersPage.selectFromProcessList(processTitle.two);
-    });
-
-    it('[C260384] Edit default filter', async () => {
-        const runningFilter = (await getFilter()).find(filter => filter.name === 'Running');
-        await userFiltersApi
-            .updateUserProcessInstanceFilter(runningFilter.id, { ...runningFilter, name: 'Edited Running' });
-
-        await processServicesPage.goToApp(app.title);
-        await processServiceTabBarPage.clickProcessButton();
-        await processFiltersPage.checkFilterIsNotDisplayed('Running');
-        await processFiltersPage.checkFilterIsDisplayed('Edited Running');
-    });
-
-    it('[C260385] Delete default filter', async () => {
-        const allFilter = (await getFilter()).find(filter => filter.name === 'All');
-        await userFiltersApi.deleteUserProcessInstanceFilter(allFilter.id);
-
-        await processServicesPage.goToApp(app.title);
-        await processServiceTabBarPage.clickProcessButton();
-        await processFiltersPage.checkFilterIsNotDisplayed('All');
-    });
-
-    async function getFilter(): Promise<UserProcessInstanceFilterRepresentation[]> {
-        const apps = await appsApi.getAppDefinitions();
-        const { id: appId = 0 } = apps.data.find((application) => application.name === appModel.name);
-
-        const filters = await userFiltersApi.getUserProcessInstanceFilters({ appId });
-        return filters.data;
-    }
-
-    async function checkProcessInfoDrawer({ name }) {
-        await expect(await processDetailsPage.checkProcessTitleIsDisplayed()).toEqual(name);
-        await expect(await processDetailsPage.getProcessStatus()).toEqual(CONSTANTS.PROCESS_STATUS.RUNNING);
-        await expect(await processDetailsPage.getEndDate()).toEqual(CONSTANTS.PROCESS_END_DATE);
-        await expect(await processDetailsPage.getProcessCategory()).toEqual(CONSTANTS.PROCESS_CATEGORY);
-        await expect(await processDetailsPage.getBusinessKey()).toEqual(CONSTANTS.PROCESS_BUSINESS_KEY);
-        await expect(await processDetailsPage.getCreatedBy()).toEqual(`${user.firstName} ${user.lastName}`);
-        await expect(await processDetailsPage.getProcessDescription()).toEqual(CONSTANTS.PROCESS_DESCRIPTION);
-    }
 });
