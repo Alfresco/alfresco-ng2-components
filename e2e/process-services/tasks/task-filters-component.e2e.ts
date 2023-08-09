c9fd5be1364184bafcb53be929dfda2cbf67b186
Denys Vuika <denys.vuika@gmail.com>
2023-08-09 18:02:15 -0400

    [ci:force] remove duplicated tests (already present in ACA/ADW)

--- e2e/process-services/tasks/task-filters-component.e2e.ts
+++ e2e/process-services/tasks/task-filters-component.e2e.ts
@@ -18,7 +18,6 @@
 import { createApiService,
     ApplicationsUtil,
     LoginPage, ModelsActions,
-    UserFiltersUtil,
     UserModel,
     UsersActions
 } from '@alfresco/adf-testing';
@@ -28,203 +27,146 @@ import { TasksPage } from '../pages/tasks.page';
 import { TasksListPage } from '../pages/tasks-list.page';
 import { TaskDetailsPage } from '../pages/task-details.page';
 import { TaskFiltersDemoPage } from '../pages/task-filters-demo.page';
-import { UserProcessInstanceFilterRepresentation } from '@alfresco/js-api';
 import { browser } from 'protractor';
 
 describe('Task', () => {
+    const app = browser.params.resources.Files.APP_WITH_DATE_FIELD_FORM;
 
-    describe('Filters', () => {
+    const loginPage = new LoginPage();
+    const navigationBarPage = new NavigationBarPage();
+    const processServicesPage = new ProcessServicesPage();
+    const tasksPage = new TasksPage();
+    const tasksListPage = new TasksListPage();
+    const taskDetailsPage = new TaskDetailsPage();
+    const taskFiltersDemoPage = new TaskFiltersDemoPage();
 
-        const app = browser.params.resources.Files.APP_WITH_DATE_FIELD_FORM;
+    const apiService = createApiService();
+    const usersActions = new UsersActions(apiService);
+    const modelsActions = new ModelsActions(apiService);
 
-        const loginPage = new LoginPage();
-        const navigationBarPage = new NavigationBarPage();
-        const processServicesPage = new ProcessServicesPage();
-        const tasksPage = new TasksPage();
-        const tasksListPage = new TasksListPage();
-        const taskDetailsPage = new TaskDetailsPage();
-        const taskFiltersDemoPage = new TaskFiltersDemoPage();
+    let appId: number;
+    let user: UserModel;
 
-        const apiService = createApiService();
-        const usersActions = new UsersActions(apiService);
-        const modelsActions = new ModelsActions(apiService);
+    beforeEach(async () => {
+        await apiService.loginWithProfile('admin');
+        user = await usersActions.createUser();
 
-        let appId: number; let user: UserModel;
+        await apiService.login(user.username, user.password);
+        const applicationsService = new ApplicationsUtil(apiService);
+        const { id } = await applicationsService.importPublishDeployApp(app.file_path);
+        appId = id;
 
-        beforeEach(async () => {
-            await apiService.loginWithProfile('admin');
-            user = await usersActions.createUser();
-
-            await apiService.login(user.username, user.password);
-            const applicationsService = new ApplicationsUtil(apiService);
-            const { id } = await applicationsService.importPublishDeployApp(app.file_path);
-            appId = id;
-
-            await loginPage.login(user.username, user.password);
-            await navigationBarPage.navigateToProcessServicesPage();
-            await processServicesPage.checkApsContainer();
-            await processServicesPage.goToApp(app.title);
-        });
-
-        afterEach(async () => {
-            await modelsActions.deleteModel(appId);
-            await apiService.loginWithProfile('admin');
-            await usersActions.deleteTenant(user.tenantId);
-            await navigationBarPage.clickLogoutButton();
-        });
-
-        it('[C279967] Should display default filters when an app is deployed', async () => {
-            await taskFiltersDemoPage.involvedTasksFilter().checkTaskFilterIsDisplayed();
-            await taskFiltersDemoPage.myTasksFilter().checkTaskFilterIsDisplayed();
-            await taskFiltersDemoPage.queuedTasksFilter().checkTaskFilterIsDisplayed();
-            await taskFiltersDemoPage.completedTasksFilter().checkTaskFilterIsDisplayed();
-        });
-
-        it('[C260330] Should display Task Filter List when app is in Task Tab', async () => {
-            await tasksPage.createTask({ name: 'Test' });
-            await taskFiltersDemoPage.myTasksFilter().clickTaskFilter();
-            await tasksListPage.checkContentIsDisplayed('Test');
-            await expect(await taskFiltersDemoPage.checkActiveFilterActive()).toBe('My Tasks');
-            await expect(await taskDetailsPage.checkTaskDetailsDisplayed()).toBeDefined();
-
-            await taskFiltersDemoPage.queuedTasksFilter().clickTaskFilter();
-            await expect(await taskFiltersDemoPage.checkActiveFilterActive()).toBe('Queued Tasks');
-            await tasksListPage.checkContentIsNotDisplayed('Test');
-            await expect(await taskDetailsPage.checkTaskDetailsEmpty()).toBeDefined();
-
-            await taskFiltersDemoPage.involvedTasksFilter().clickTaskFilter();
-            await expect(await taskFiltersDemoPage.checkActiveFilterActive()).toBe('Involved Tasks');
-            await tasksListPage.checkContentIsDisplayed('Test');
-            await expect(await taskDetailsPage.checkTaskDetailsDisplayed()).toBeDefined();
-
-            await taskFiltersDemoPage.completedTasksFilter().clickTaskFilter();
-            await expect(await taskFiltersDemoPage.checkActiveFilterActive()).toBe('Completed Tasks');
-            await tasksListPage.checkContentIsNotDisplayed('Test');
-            await expect(await taskDetailsPage.checkTaskDetailsEmpty()).toBeDefined();
-        });
-
-        it('[C260348] Should display task in Complete Tasks List when task is completed', async () => {
-            await taskFiltersDemoPage.myTasksFilter().checkTaskFilterIsDisplayed();
-            await taskFiltersDemoPage.queuedTasksFilter().checkTaskFilterIsDisplayed();
-            await taskFiltersDemoPage.involvedTasksFilter().checkTaskFilterIsDisplayed();
-            await taskFiltersDemoPage.completedTasksFilter().checkTaskFilterIsDisplayed();
-
-            const task = await tasksPage.createNewTask();
-            await task.addName('Test');
-            await task.clickStartButton();
-            await taskFiltersDemoPage.myTasksFilter().clickTaskFilter();
-            await tasksListPage.checkContentIsDisplayed('Test');
-            await expect(await taskFiltersDemoPage.checkActiveFilterActive()).toBe('My Tasks');
-            await expect(await taskDetailsPage.checkTaskDetailsDisplayed()).toBeDefined();
-
-            await taskFiltersDemoPage.queuedTasksFilter().clickTaskFilter();
-            await expect(await taskFiltersDemoPage.checkActiveFilterActive()).toBe('Queued Tasks');
-            await expect(await tasksListPage.getNoTasksFoundMessage()).toBe('No Tasks Found');
-            await expect(await taskDetailsPage.getEmptyTaskDetailsMessage()).toBe('No task details found');
-
-            await taskFiltersDemoPage.involvedTasksFilter().clickTaskFilter();
-            await expect(await taskFiltersDemoPage.checkActiveFilterActive()).toBe('Involved Tasks');
-            await tasksListPage.checkContentIsDisplayed('Test');
-            await expect(await taskDetailsPage.checkTaskDetailsDisplayed()).toBeDefined();
-
-            await taskFiltersDemoPage.completedTasksFilter().clickTaskFilter();
-            await expect(await taskFiltersDemoPage.checkActiveFilterActive()).toBe('Completed Tasks');
-            await expect(await tasksListPage.getNoTasksFoundMessage()).toBe('No Tasks Found');
-            await expect(await taskDetailsPage.getEmptyTaskDetailsMessage()).toBe('No task details found');
-        });
-
-        it('[C260349] Should sort task by name when Name sorting is clicked', async () => {
-            await tasksPage.createTask({ name: 'Test1' });
-            await taskDetailsPage.clickCompleteTask();
-
-            await tasksPage.createTask({ name: 'Test2' });
-            await taskDetailsPage.clickCompleteTask();
-
-            await tasksPage.createTask({ name: 'Test3' });
-            await tasksPage.createTask({ name: 'Test4' });
-
-            await tasksListPage.checkContentIsDisplayed('Test4');
-            await tasksListPage.checkRowIsSelected('Test4');
-            await tasksListPage.checkContentIsDisplayed('Test3');
-            await taskDetailsPage.checkTaskDetailsDisplayed();
-
-            await tasksPage.clickSortByNameAsc();
-            await expect(await tasksListPage.getDataTable().contentInPosition(1)).toBe('Test3');
-            await tasksPage.clickSortByNameDesc();
-            await expect(await tasksListPage.getDataTable().contentInPosition(1)).toBe('Test4');
-
-            await taskFiltersDemoPage.completedTasksFilter().clickTaskFilter();
-            await tasksListPage.checkContentIsDisplayed('Test1');
-            await tasksListPage.checkContentIsDisplayed('Test2');
-            await expect(await tasksListPage.getDataTable().contentInPosition(1)).toBe('Test2');
-
-            await tasksPage.clickSortByNameAsc();
-            await expect(await tasksListPage.getDataTable().contentInPosition(1)).toBe('Test1');
-
-            await taskFiltersDemoPage.involvedTasksFilter().clickTaskFilter();
-            await tasksListPage.checkContentIsDisplayed('Test3');
-            await tasksListPage.checkContentIsDisplayed('Test4');
-        });
-
-        it('[C277264] Should display task filter results when task filter is selected', async () => {
-            await tasksPage.createTask({ name: 'Test' });
-
-            await taskFiltersDemoPage.myTasksFilter().clickTaskFilter();
-            await tasksListPage.checkContentIsDisplayed('Test');
-            await expect(await taskDetailsPage.getTaskDetailsTitle()).toBe('Test');
-        });
+        await loginPage.login(user.username, user.password);
+        await navigationBarPage.navigateToProcessServicesPage();
+        await processServicesPage.checkApsContainer();
+        await processServicesPage.goToApp(app.title);
     });
 
-    describe('Custom Filters', () => {
-        const loginPage = new LoginPage();
-        const navigationBarPage = new NavigationBarPage();
-        const processServicesPage = new ProcessServicesPage();
-        const taskFiltersDemoPage = new TaskFiltersDemoPage();
+    afterEach(async () => {
+        await modelsActions.deleteModel(appId);
+        await apiService.loginWithProfile('admin');
+        await usersActions.deleteTenant(user.tenantId);
+        await navigationBarPage.clickLogoutButton();
+    });
 
-        const apiService = createApiService();
-        const userFiltersApi = new UserFiltersUtil(apiService);
-        const usersActions = new UsersActions(apiService);
+    it('[C279967] Should display default filters when an app is deployed', async () => {
+        await taskFiltersDemoPage.involvedTasksFilter().checkTaskFilterIsDisplayed();
+        await taskFiltersDemoPage.myTasksFilter().checkTaskFilterIsDisplayed();
+        await taskFiltersDemoPage.queuedTasksFilter().checkTaskFilterIsDisplayed();
+        await taskFiltersDemoPage.completedTasksFilter().checkTaskFilterIsDisplayed();
+    });
 
-        let user;
-        let appId: number;
+    it('[C260330] Should display Task Filter List when app is in Task Tab', async () => {
+        await tasksPage.createTask({ name: 'Test' });
+        await taskFiltersDemoPage.myTasksFilter().clickTaskFilter();
+        await tasksListPage.checkContentIsDisplayed('Test');
+        await expect(await taskFiltersDemoPage.checkActiveFilterActive()).toBe('My Tasks');
+        await expect(await taskDetailsPage.checkTaskDetailsDisplayed()).toBeDefined();
 
-        const app = browser.params.resources.Files.APP_WITH_PROCESSES;
+        await taskFiltersDemoPage.queuedTasksFilter().clickTaskFilter();
+        await expect(await taskFiltersDemoPage.checkActiveFilterActive()).toBe('Queued Tasks');
+        await tasksListPage.checkContentIsNotDisplayed('Test');
+        await expect(await taskDetailsPage.checkTaskDetailsEmpty()).toBeDefined();
 
-        beforeAll(async () => {
-            await apiService.loginWithProfile('admin');
-            user = await usersActions.createUser();
+        await taskFiltersDemoPage.involvedTasksFilter().clickTaskFilter();
+        await expect(await taskFiltersDemoPage.checkActiveFilterActive()).toBe('Involved Tasks');
+        await tasksListPage.checkContentIsDisplayed('Test');
+        await expect(await taskDetailsPage.checkTaskDetailsDisplayed()).toBeDefined();
 
-            await apiService.login(user.username, user.password);
-            const applicationsService = new ApplicationsUtil(apiService);
-            const importedApp = await applicationsService.importPublishDeployApp(app.file_path);
-            appId = await applicationsService.getAppDefinitionId(importedApp.id);
+        await taskFiltersDemoPage.completedTasksFilter().clickTaskFilter();
+        await expect(await taskFiltersDemoPage.checkActiveFilterActive()).toBe('Completed Tasks');
+        await tasksListPage.checkContentIsNotDisplayed('Test');
+        await expect(await taskDetailsPage.checkTaskDetailsEmpty()).toBeDefined();
+    });
 
-            await loginPage.login(user.username, user.password);
-        });
+    it('[C260348] Should display task in Complete Tasks List when task is completed', async () => {
+        await taskFiltersDemoPage.myTasksFilter().checkTaskFilterIsDisplayed();
+        await taskFiltersDemoPage.queuedTasksFilter().checkTaskFilterIsDisplayed();
+        await taskFiltersDemoPage.involvedTasksFilter().checkTaskFilterIsDisplayed();
+        await taskFiltersDemoPage.completedTasksFilter().checkTaskFilterIsDisplayed();
 
-        afterAll(async () => {
-            await apiService.loginWithProfile('admin');
-            await usersActions.deleteTenant(user.tenantId);
-        });
+        const task = await tasksPage.createNewTask();
+        await task.addName('Test');
+        await task.clickStartButton();
+        await taskFiltersDemoPage.myTasksFilter().clickTaskFilter();
+        await tasksListPage.checkContentIsDisplayed('Test');
+        await expect(await taskFiltersDemoPage.checkActiveFilterActive()).toBe('My Tasks');
+        await expect(await taskDetailsPage.checkTaskDetailsDisplayed()).toBeDefined();
 
-        beforeEach(async () => {
-            await navigationBarPage.navigateToProcessServicesPage();
-            await processServicesPage.checkApsContainer();
-            await processServicesPage.goToApp(app.title);
-        });
+        await taskFiltersDemoPage.queuedTasksFilter().clickTaskFilter();
+        await expect(await taskFiltersDemoPage.checkActiveFilterActive()).toBe('Queued Tasks');
+        await expect(await tasksListPage.getNoTasksFoundMessage()).toBe('No Tasks Found');
+        await expect(await taskDetailsPage.getEmptyTaskDetailsMessage()).toBe('No task details found');
 
-        it('[C260350] Should display a new filter when a filter is added', async () => {
-            const newFilter = new UserProcessInstanceFilterRepresentation({
-                name: 'New Task Filter',
-                appId,
-                icon: 'glyphicon-filter',
-                filter: { sort: 'created-desc', state: 'completed', assignment: 'involved' }
-            });
-            const { id } = await userFiltersApi.createUserTaskFilter(newFilter);
+        await taskFiltersDemoPage.involvedTasksFilter().clickTaskFilter();
+        await expect(await taskFiltersDemoPage.checkActiveFilterActive()).toBe('Involved Tasks');
+        await tasksListPage.checkContentIsDisplayed('Test');
+        await expect(await taskDetailsPage.checkTaskDetailsDisplayed()).toBeDefined();
 
-            await browser.refresh();
-            await taskFiltersDemoPage.customTaskFilter('New Task Filter').checkTaskFilterIsDisplayed();
-            await userFiltersApi.deleteUserTaskFilter(id);
-        });
+        await taskFiltersDemoPage.completedTasksFilter().clickTaskFilter();
+        await expect(await taskFiltersDemoPage.checkActiveFilterActive()).toBe('Completed Tasks');
+        await expect(await tasksListPage.getNoTasksFoundMessage()).toBe('No Tasks Found');
+        await expect(await taskDetailsPage.getEmptyTaskDetailsMessage()).toBe('No task details found');
+    });
+
+    it('[C260349] Should sort task by name when Name sorting is clicked', async () => {
+        await tasksPage.createTask({ name: 'Test1' });
+        await taskDetailsPage.clickCompleteTask();
+
+        await tasksPage.createTask({ name: 'Test2' });
+        await taskDetailsPage.clickCompleteTask();
+
+        await tasksPage.createTask({ name: 'Test3' });
+        await tasksPage.createTask({ name: 'Test4' });
+
+        await tasksListPage.checkContentIsDisplayed('Test4');
+        await tasksListPage.checkRowIsSelected('Test4');
+        await tasksListPage.checkContentIsDisplayed('Test3');
+        await taskDetailsPage.checkTaskDetailsDisplayed();
+
+        await tasksPage.clickSortByNameAsc();
+        await expect(await tasksListPage.getDataTable().contentInPosition(1)).toBe('Test3');
+        await tasksPage.clickSortByNameDesc();
+        await expect(await tasksListPage.getDataTable().contentInPosition(1)).toBe('Test4');
+
+        await taskFiltersDemoPage.completedTasksFilter().clickTaskFilter();
+        await tasksListPage.checkContentIsDisplayed('Test1');
+        await tasksListPage.checkContentIsDisplayed('Test2');
+        await expect(await tasksListPage.getDataTable().contentInPosition(1)).toBe('Test2');
+
+        await tasksPage.clickSortByNameAsc();
+        await expect(await tasksListPage.getDataTable().contentInPosition(1)).toBe('Test1');
+
+        await taskFiltersDemoPage.involvedTasksFilter().clickTaskFilter();
+        await tasksListPage.checkContentIsDisplayed('Test3');
+        await tasksListPage.checkContentIsDisplayed('Test4');
+    });
+
+    it('[C277264] Should display task filter results when task filter is selected', async () => {
+        await tasksPage.createTask({ name: 'Test' });
+
+        await taskFiltersDemoPage.myTasksFilter().clickTaskFilter();
+        await tasksListPage.checkContentIsDisplayed('Test');
+        await expect(await taskDetailsPage.getTaskDetailsTitle()).toBe('Test');
     });
 });
