be76b0bfbcf777f430e04450d33dfbc9553854f8
Aayush Rohila <aayush.rohila@contractors.onbase.com>
2023-08-14 16:49:10 +0530

    [ACS-5654] Unit tests

--- lib/content-services/src/lib/document-list/components/document-list.component.spec.ts
+++ lib/content-services/src/lib/document-list/components/document-list.component.spec.ts
@@ -63,6 +63,7 @@ import { matIconRegistryMock } from '../../testing/mat-icon-registry-mock';
 import { domSanitizerMock } from '../../testing/dom-sanitizer-mock';
 import { MatDialog } from '@angular/material/dialog';
 import { FileAutoDownloadComponent } from './file-auto-download/file-auto-download.component';
+import { ShareDataTableAdapter } from '../public-api';
 
 const mockDialog = {
     open: jasmine.createSpy('open')
@@ -276,6 +277,15 @@ describe('DocumentList', () => {
         documentList.ngOnChanges(changes);
 
         expect(documentList.selection).toEqual([]);
+    });
+
+    it('should show the header when there are no records in the table but filter is active', () => {
+        documentList.data = new ShareDataTableAdapter(thumbnailService, contentService, []);
+        documentList.filterValue =  { $thumbnail: 'TYPE:"cm:folder"' };
+
+        fixture.detectChanges();
+
+        expect(documentList.showHeader).toEqual('always');
     });
 
     it('should reloadWithoutResettingSelection not reset the selection', () => {
