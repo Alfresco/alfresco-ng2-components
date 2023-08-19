f1523fe9d046d9e746769fb0de6957cf4b6394c8
Yasa-Nataliya <yasa.nataliya@globallogic.com>
2023-08-19 16:53:03 +0530

    minor fixes

--- e2e/core/pages/metadata-view.page.ts
+++ e2e/core/pages/metadata-view.page.ts
@@ -114,10 +114,6 @@ export class MetadataViewPage {
         await BrowserVisibility.waitUntilElementIsVisible(this.editIconGeneral);
     }
 
-    async generaleditIconNotDisplayed(): Promise<void> {
-        await BrowserVisibility.waitUntilElementIsNotVisible(this.editIconGeneral);
-    }
-
     async editIconClick(): Promise<void> {
         await BrowserActions.clickExecuteScript('button[data-automation-id="meta-data-card-toggle-edit"]');
     }
