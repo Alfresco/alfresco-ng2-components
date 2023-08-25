5c7d2e5222782ff672744284cdd079fc10eee602
Yasa-Nataliya <yasa.nataliya@globallogic.com>
2023-08-25 11:30:50 +0530

    [ACS-5645]code modification

--- lib/content-services/src/lib/tag/tags-creator/tags-creator.component.spec.ts
+++ lib/content-services/src/lib/tag/tags-creator/tags-creator.component.spec.ts
@@ -304,6 +304,13 @@ describe('TagsCreatorComponent', () => {
                 expect(error).toBe('TAG.TAGS_CREATOR.ERRORS.REQUIRED');
             }));
 
+            it('should not show error for required if tags are changed', fakeAsync(() => {
+                typeTag('');
+                component.tags = ['new tag 1', 'new tag 2'];
+                fixture.detectChanges();
+                expect(getFirstError()).toBeUndefined();
+            }));
+
             it('should show error when duplicated already added tag', fakeAsync(() => {
                 const tag = 'Some tag';
 
@@ -323,13 +330,6 @@ describe('TagsCreatorComponent', () => {
                 fixture.detectChanges();
 
                 expect(getFirstError()).toBe('TAG.TAGS_CREATOR.ERRORS.ALREADY_ADDED_TAG');
-            }));
-
-            it('should not show error for required if tags are changed', fakeAsync(() => {
-                typeTag('');
-                component.tags = ['new tag 1', 'new tag 2'];
-                fixture.detectChanges();
-                expect(getFirstError()).toBeUndefined();
             }));
 
             it('should show error when duplicated already existing tag', fakeAsync(() => {
