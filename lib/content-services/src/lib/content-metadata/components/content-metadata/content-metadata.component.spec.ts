33b32b5409f8aa055d9a1a28ccc2ab60e6801f4d
Yasa-Nataliya <yasa.nataliya@globallogic.com>
2023-09-06 01:29:48 +0530

    [ACS-5645]linting fixes

--- lib/content-services/src/lib/content-metadata/components/content-metadata/content-metadata.component.spec.ts
+++ lib/content-services/src/lib/content-metadata/components/content-metadata/content-metadata.component.spec.ts
@@ -493,7 +493,7 @@ describe('ContentMetadataComponent', () => {
             component.editable = false;
             component.toggleEditMode(ButtonType.GeneralInfo);
             expect(component.editable).toBe(true);
-          });
+        });
 
         it('should toggle tags editable', () => {
             component.editableTags = false;
