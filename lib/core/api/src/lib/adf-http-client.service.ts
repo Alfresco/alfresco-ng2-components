12fe0c4f61aec1b59cda76d56e2b547943d5b261
Amedeo Lepore <amedeo.lepore@hyland.com>
2023-09-06 18:04:34 +0200

    Update error message

--- lib/core/api/src/lib/adf-http-client.service.ts
+++ lib/core/api/src/lib/adf-http-client.service.ts
@@ -216,7 +216,7 @@ export class AdfHttpClient implements ee.Emitter,JsApiHttpClient {
                 }
 
                 eventEmitter.emit('error', err);
-                apiClientEmitter.emit('error', err);
+                apiClientEmitter.emit('error', { ...err, response: { req: err } });
 
                 if (err.status === 401) {
                     eventEmitter.emit('unauthorized');
