73bbaa26aefefef2cf084ccf097a20f5dca478ee
Yasa-Nataliya <yasa.nataliya@globallogic.com>
2023-09-08 14:47:52 +0530

    [ACS-5645]fixes for e2e

--- lib/testing/src/lib/protractor/core/pages/viewer.page.ts
+++ lib/testing/src/lib/protractor/core/pages/viewer.page.ts
@@ -377,6 +377,7 @@ export class ViewerPage {
 
     async clickCloseButton(): Promise<void> {
         await BrowserActions.click(this.closeButton);
+        await browser.sleep(browser.params.testConfig.timeouts.medium);
     }
 
     async clickPreviousPageButton(): Promise<void> {
