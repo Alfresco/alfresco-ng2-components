ca93a8cf63df9e4b3d23ca6406c6abc646d4d337
Yasa-Nataliya <yasa.nataliya@globallogic.com>
2023-09-22 20:55:27 +0530

    [ACS-5645] added group panel lock changes

--- lib/content-services/src/lib/content-metadata/components/content-metadata/content-metadata.component.ts
+++ lib/content-services/src/lib/content-metadata/components/content-metadata/content-metadata.component.ts
@@ -208,11 +208,18 @@ export class ContentMetadataComponent implements OnChanges, OnInit, OnDestroy {
     @Output()
     editableCategoriesChange = new EventEmitter<boolean>();
 
+    /** Emitted when content's group state is changed. **/
+    @Output()
+    groupChange = new EventEmitter<CardViewGroup>();
+
     @Input()
     editableCategories = false;
 
     @Input()
     editableTags = false;
+
+    @Input()
+    group: CardViewGroup;
 
     private _assignedTags: string[] = [];
     private assignedTagsEntries: TagEntry[] = [];
@@ -241,7 +248,6 @@ export class ContentMetadataComponent implements OnChanges, OnInit, OnDestroy {
     hasAllowableOperations = false;
     editableGroup: CardViewGroup;
     buttonType = ButtonType;
-    group: CardViewGroup;
 
     constructor(
         private contentMetadataService: ContentMetadataService,
@@ -462,6 +468,7 @@ export class ContentMetadataComponent implements OnChanges, OnInit, OnDestroy {
                 break;
             case ButtonType.Group:
                 group.editable = !group.editable;
+                this.groupChange.emit(group);
                 this.editableGroup = group.editable ? group : null;
                 if (group.editable) {
                     group.expanded = true;
