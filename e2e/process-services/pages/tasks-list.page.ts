9d289743da98ddd9aa2fac6dbdc631eb94e50bad
Aleksander Sklorz <Aleksander.Sklorz@hyland.com>
2023-09-28 09:44:07 +0200

    ACS-5613 Fixed e2e

--- e2e/process-services/pages/tasks-list.page.ts
+++ e2e/process-services/pages/tasks-list.page.ts
@@ -29,15 +29,15 @@ export class TasksListPage {
     }
 
     getRowsDisplayedWithSameName(taskName: string): Promise<string> {
-        return this.dataTable.getRowsWithSameColumnValues('Name', taskName);
+        return this.dataTable.getRowsWithSameColumnValues('Task Name', taskName);
     }
 
     async checkContentIsDisplayed(taskName: string): Promise<void> {
-        await this.dataTable.checkContentIsDisplayed('Name', taskName);
+        await this.dataTable.checkContentIsDisplayed('Task Name', taskName);
     }
 
     async checkContentIsNotDisplayed(taskName: string): Promise<void> {
-        await this.dataTable.checkContentIsNotDisplayed('Name', taskName);
+        await this.dataTable.checkContentIsNotDisplayed('Task Name', taskName);
     }
 
     async checkRowIsSelected(taskName: string): Promise<void> {
@@ -50,7 +50,7 @@ export class TasksListPage {
     }
 
     getAllRowsNameColumn(): Promise<string[]> {
-        return this.dataTable.getAllRowsColumnValues('Name');
+        return this.dataTable.getAllRowsColumnValues('Task Name');
     }
 
     async checkTaskListIsLoaded(): Promise<void> {
