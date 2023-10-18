3faf255f93e1db3235d56794cd44d02ddaad8bb0
Denys Vuika <denys.vuika@gmail.com>
2023-10-18 08:39:56 +0100

    migrate cardview component to date-fns

--- lib/core/src/lib/card-view/components/card-view-dateitem/card-view-dateitem.component.ts
+++ lib/core/src/lib/card-view/components/card-view-dateitem/card-view-dateitem.component.ts
@@ -17,26 +17,26 @@
 
 import { Component, Input, OnDestroy, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { DateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
-import { DatetimeAdapter, MAT_DATETIME_FORMATS, MatDatetimepickerComponent } from '@mat-datetimepicker/core';
-import { MAT_MOMENT_DATETIME_FORMATS, MomentDatetimeAdapter } from '@mat-datetimepicker/moment';
-import moment, { Moment } from 'moment';
+import { DatetimeAdapter, MAT_DATETIME_FORMATS, MatDatetimepickerComponent, MatDatetimepickerInputEvent } from '@mat-datetimepicker/core';
 import { CardViewDateItemModel } from '../../models/card-view-dateitem.model';
 import { UserPreferencesService, UserPreferenceValues } from '../../../common/services/user-preferences.service';
-import { MomentDateAdapter } from '../../../common/utils/moment-date-adapter';
-import { MOMENT_DATE_FORMATS } from '../../../common/utils/moment-date-formats.model';
 import { AppConfigService } from '../../../app-config/app-config.service';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { BaseCardView } from '../base-card-view';
 import { ClipboardService } from '../../../clipboard/clipboard.service';
 import { TranslationService } from '../../../translation/translation.service';
+import { ADF_DATE_FORMATS, AdfDateFnsAdapter } from '../../../common/utils/date-fns-adapter';
+import { ADF_DATETIME_FORMATS, AdfDateTimeFnsAdapter } from '../../../common/utils/datetime-fns-adapter';
+import { DateFnsUtils } from '../../../common';
+import { isValid } from 'date-fns';
 
 @Component({
     providers: [
-        { provide: DateAdapter, useClass: MomentDateAdapter },
-        { provide: MAT_DATE_FORMATS, useValue: MOMENT_DATE_FORMATS },
-        { provide: DatetimeAdapter, useClass: MomentDatetimeAdapter },
-        { provide: MAT_DATETIME_FORMATS, useValue: MAT_MOMENT_DATETIME_FORMATS }
+        { provide: MAT_DATE_FORMATS, useValue: ADF_DATE_FORMATS },
+        { provide: MAT_DATETIME_FORMATS, useValue: ADF_DATETIME_FORMATS },
+        { provide: DateAdapter, useClass: AdfDateFnsAdapter },
+        { provide: DatetimeAdapter, useClass: AdfDateTimeFnsAdapter }
     ],
     selector: 'adf-card-view-dateitem',
     templateUrl: './card-view-dateitem.component.html',
@@ -60,12 +60,12 @@ export class CardViewDateItemComponent extends BaseCardView<CardViewDateItemMode
     @ViewChild('datetimePicker')
     public datepicker: MatDatetimepickerComponent<any>;
 
-    valueDate: Moment;
+    valueDate: Date;
     dateFormat: string;
 
     private onDestroy$ = new Subject<boolean>();
 
-    constructor(private dateAdapter: DateAdapter<Moment>,
+    constructor(private dateAdapter: DateAdapter<Date>,
                 private userPreferencesService: UserPreferencesService,
                 private appConfig: AppConfigService,
                 private clipboardService: ClipboardService,
@@ -82,10 +82,10 @@ export class CardViewDateItemComponent extends BaseCardView<CardViewDateItemMode
                 this.property.locale = locale;
             });
 
-        (this.dateAdapter as MomentDateAdapter).overrideDisplayFormat = 'MMM DD';
+        (this.dateAdapter as AdfDateFnsAdapter).displayFormat = 'MMM DD';
 
         if (this.property.value) {
-            this.valueDate = moment(this.property.value, this.dateFormat);
+            this.valueDate = DateFnsUtils.parseDate(this.property.value, this.dateFormat);
         } else if (this.property.multivalued && !this.property.value) {
             this.property.value = [];
         }
@@ -112,12 +112,11 @@ export class CardViewDateItemComponent extends BaseCardView<CardViewDateItemMode
         this.datepicker.open();
     }
 
-    onDateChanged(newDateValue) {
-        if (newDateValue) {
-            const momentDate = moment(newDateValue.value, this.dateFormat, true);
-            if (momentDate.isValid()) {
-                this.valueDate = momentDate;
-                this.property.value = momentDate.toDate();
+    onDateChanged(event: MatDatetimepickerInputEvent<Date>) {
+        if (event.value) {
+            if (isValid(event.value)) {
+                this.valueDate = event.value;
+                this.property.value = event.value;
                 this.update();
             }
         }
@@ -135,11 +134,10 @@ export class CardViewDateItemComponent extends BaseCardView<CardViewDateItemMode
         this.clipboardService.copyContentToClipboard(valueToCopy, clipboardMessage);
     }
 
-    addDateToList(newDateValue) {
-        if (newDateValue) {
-            const momentDate = moment(newDateValue.value, this.dateFormat, true);
-            if (momentDate.isValid()) {
-                this.property.value.push(momentDate.toDate());
+    addDateToList(event: MatDatetimepickerInputEvent<Date>) {
+        if (event.value) {
+            if (isValid(event.value)) {
+                this.property.value.push(event.value);
                 this.update();
             }
         }
