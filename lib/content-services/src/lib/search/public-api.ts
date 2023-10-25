2607b76c7c85890887865273f634e089deb3ccd6
swapnil.verma <swapnil.verma@globallogic.com>
2023-10-25 12:10:31 +0530

    [ACS-6107] Fixed missing export of SearchDateRangeTabbedComponent

--- lib/content-services/src/lib/search/public-api.ts
+++ lib/content-services/src/lib/search/public-api.ts
@@ -63,6 +63,7 @@ export * from './components/search-filter-chips/search-filter-menu-card/search-f
 export * from './components/search-facet-field/search-facet-field.component';
 export * from './components/search-logical-filter/search-logical-filter.component';
 export * from './components/search-date-range-tabbed/search-date-range/search-date-range.component';
+export * from './components/search-date-range-tabbed/search-date-range-tabbed.component';
 export * from './components/search-filter-tabbed/search-filter-tabbed.component';
 export * from './components/reset-search.directive';
 export * from './components/search-chip-autocomplete-input/search-chip-autocomplete-input.component';
