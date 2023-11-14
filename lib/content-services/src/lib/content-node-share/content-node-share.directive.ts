52ac7b6f7959782f0d979a6b450f5b4bb48d1af9
Vito Albano <vito.albano@hyland.com>
2023-11-14 00:38:17 +0000

    migration for material'

--- lib/content-services/src/lib/content-node-share/content-node-share.directive.ts
+++ lib/content-services/src/lib/content-node-share/content-node-share.directive.ts
@@ -16,7 +16,7 @@
  */
 
 import { Directive, Input, HostListener, OnChanges, NgZone, OnDestroy } from '@angular/core';
-import { MatLegacyDialog as MatDialog } from '@angular/material/legacy-dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { NodeEntry, NodesApi } from '@alfresco/js-api';
 
 import { ShareDialogComponent } from './content-node-share.dialog';
