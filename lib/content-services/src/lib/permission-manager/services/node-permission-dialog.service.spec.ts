e013ee25100ea4e9f853547faf9dc79a6a824432
Vito Albano <vito.albano@hyland.com>
2023-11-14 00:25:17 +0000

    Changed ng version before material migration

--- lib/content-services/src/lib/permission-manager/services/node-permission-dialog.service.spec.ts
+++ lib/content-services/src/lib/permission-manager/services/node-permission-dialog.service.spec.ts
@@ -17,7 +17,7 @@
 
 import { TestBed } from '@angular/core/testing';
 import { NodePermissionDialogService } from './node-permission-dialog.service';
-import { MatDialog } from '@angular/material/dialog';
+import { MatLegacyDialog as MatDialog } from '@angular/material/legacy-dialog';
 import { of, Subject, throwError } from 'rxjs';
 import { ContentTestingModule } from '../../testing/content.testing.module';
 import { NodePermissionService } from './node-permission.service';
--- lib/core/src/lib/datatable/components/json-cell/json-cell.component.spec.ts
+++ lib/core/src/lib/datatable/components/json-cell/json-cell.component.spec.ts
@@ -22,7 +22,7 @@ import { CoreTestingModule } from '../../../testing/core.testing.module';
 import { JsonCellComponent } from './json-cell.component';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { HarnessLoader } from '@angular/cdk/testing';
-import { MatButtonHarness } from '@angular/material/button/testing';
+import { MatLegacyButtonHarness as MatButtonHarness } from '@angular/material/legacy-button/testing';
 
 describe('JsonCellComponent', () => {
     let loader: HarnessLoader;
