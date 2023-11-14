18a586b50bdc352decf9b47212c8e75048ab9fd0
Vito Albano <vito.albano@hyland.com>
2023-11-14 01:10:57 +0000

    Upgrading NX and start fixing styles

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,5 @@
 module.exports = {
-    stories: [],
-    addons: ['@storybook/addon-essentials'],
     framework: '@storybook/angular',
-    staticDirs: [ { from: '../../../demo-shell/src/app.config.json', to: 'app.config.json' } ],
+    staticDirs: [{ from: '../../../demo-shell/src/app.config.json', to: 'app.config.json' }],
     core: { builder: 'webpack5' }
 };
