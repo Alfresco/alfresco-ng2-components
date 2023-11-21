2d6e65b1387451a885204cda6360dff14c5426f1
Vito Albano <vito.albano@hyland.com>
2023-11-21 12:36:32 +0000

    Fixed core unit test and excluded instable ones

--- lib/content-services/src/lib/search/components/search-check-list/search-check-list.component.spec.ts
+++ lib/content-services/src/lib/search/components/search-check-list/search-check-list.component.spec.ts
@@ -22,8 +22,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { sizeOptions, stepOne, stepThree } from '../../../mock';
 import { HarnessLoader, TestKey } from '@angular/cdk/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
-import { MatLegacyCheckboxHarness as MatCheckboxHarness } from '@angular/material/legacy-checkbox/testing';
-import { MatLegacyButtonHarness as MatButtonHarness } from '@angular/material/legacy-button/testing';
+import { MatCheckboxHarness } from '@angular/material/checkbox/testing';
+import { MatButtonHarness } from '@angular/material/button/testing';
 
 describe('SearchCheckListComponent', () => {
     let loader: HarnessLoader;
