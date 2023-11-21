8b77ffbdc84910e54d0f7f69b29b6d2624036f36
Denys Vuika <denys.vuika@gmail.com>
2023-11-21 13:19:07 +0000

    rework bundle checks

--- scripts/github/build/npm-check-bundles.sh
+++ scripts/github/build/npm-check-bundles.sh
@@ -2,55 +2,24 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-eval VERSION=""
-
-eval projects=( "adf-core"
+eval projects=( "js-api"
+    "adf-core"
     "adf-insights"
     "adf-content-services"
     "adf-extensions"
     "adf-testing"
     "adf-process-services"
     "adf-process-services-cloud"
-    "eslint-plugin-eslint-angular"
-    "js-api" )
-
-show_help() {
-    echo "Usage: npm-check-bundles.sh"
-    echo "-r or -registry to check  -r 'http://npm.local.me:8080/' "
-    echo "-v or -version to check  -v 1.4.0 "
-    echo ""
-}
-
-change_registry() {
-    echo $1
-    npm set registry $1
-}
-
-set_npm_registry() {
-    npm set registry https://registry.npmjs.org/
-}
-
-version() {
-   VERSION=$1
-}
+    "eslint-plugin-eslint-angular" )
 
 error_out() {
-      printf '\033[%sm%s\033[m\n' "$@"
-      # usage color "31;5" "string"
-      # 0 default
-      # 5 blink, 1 strong, 4 underlined
-      # fg: 31 red,  32 green, 33 yellow, 34 blue, 35 purple, 36 cyan, 37 white
-      # bg: 40 black, 41 red, 44 blue, 45 purple
-      }
-
-while [[ $1 == -* ]]; do
-    case "$1" in
-      -h|--help|-\?) show_help; exit 0;;
-      -r)  change_registry $2; shift 2;;
-      -v|--version)  version $2; shift 2;;
-      -*) echo "invalid option: $1" 1>&2; show_help; exit 1;;
-    esac
-done
+    printf '\033[%sm%s\033[m\n' "$@"
+    # usage color "31;5" "string"
+    # 0 default
+    # 5 blink, 1 strong, 4 underlined
+    # fg: 31 red,  32 green, 33 yellow, 34 blue, 35 purple, 36 cyan, 37 white
+    # bg: 40 black, 41 red, 44 blue, 45 purple
+}
 
 rm -rf temp
 mkdir temp
@@ -59,34 +28,38 @@ cd temp
 for PACKAGE in ${projects[@]}
 do
     mkdir $PACKAGE
-    cd  $PACKAGE
-    npm pack '@alfresco/'$PACKAGE@$VERSION
-    tar zxf 'alfresco-'$PACKAGE-$VERSION.tgz
+    cd $PACKAGE
+
+    PKG_VERSION=$(npm view @alfresco/$PACKAGE@alpha version)
+    echo "Inspecting: $PACKAGE@$PKG_VERSION"
+
+    npm pack '@alfresco/'$PACKAGE@$PKG_VERSION
+    tar zxf 'alfresco-'$PACKAGE-$PKG_VERSION.tgz
 
     if [ $PACKAGE == 'js-api' ]; then
         if [ ! -f package/esm2020/'alfresco-js-api.mjs' ]; then
             error_out '31;1' "esm2020/alfresco-js-api.mjs not found!" >&2
             exit 1
         else
-            echo "esm2020 ok!"
+            echo "esm2020: ok"
         fi
     fi
 
     if [ $PACKAGE == 'adf-testing' ]; then
         if [ ! -f package/'main.js' ]; then
-            error_out '31;1' "$PACKAGE testing not ok!" >&2
+            error_out '31;1' "$PACKAGE testing: main.js not found!" >&2
             exit 1
         else
-            echo "testing ok!"
+            echo "testing: ok"
         fi
     fi
 
     if [ $PACKAGE == 'adf-core' ]; then
         if [ ! -f package/lib/prebuilt-themes/'adf-blue-orange.css' ]; then
-            error_out '31;1' "$PACKAGE prebuilt-theme not found!" >&2
+            error_out '31;1' "$PACKAGE prebuilt-theme: adf-blue-orange.css not found!" >&2
             exit 1
         else
-            echo "prebuilt-theme ok!"
+            echo "prebuilt-theme: ok"
         fi
     fi
 
@@ -95,28 +68,28 @@ do
             error_out '31;1' "fesm2015/$PACKAGE.mjs not found!" >&2
             exit 1
         else
-            echo "fesm2015 ok!"
+            echo "fesm2015: ok"
         fi
 
         if [ ! -f package/esm2020/$PACKAGE'.mjs' ]; then
             error_out '31;1' "esm2020/$PACKAGE.mjs not found!" >&2
             exit 1
         else
-            echo "esm2020 ok!"
+            echo "esm2020: ok"
         fi
 
         if [ ! -f package/fesm2020/$PACKAGE'.mjs' ]; then
             error_out '31;1' "fesm2020/$PACKAGE.mjs not found!" >&2
             exit 1
         else
-            echo "fesm2020 ok!"
+            echo "fesm2020: ok"
         fi
 
         if [ ! -f package/README.md ]; then
             error_out '31;1' "$PACKAGE readme not found!" >&2
             exit 1
         else
-            echo "readme ok!"
+            echo "readme: ok"
         fi
 
         if [ ! -f package/bundles/assets/$PACKAGE/i18n/en.json ]; then
@@ -129,7 +102,7 @@ do
                 exit 1
             fi
         else
-            echo "i18n ok!"
+            echo "i18n: ok"
         fi
     fi
 
@@ -138,5 +111,3 @@ done
 
 cd ..
 rm -rf temp
-
-set_npm_registry
