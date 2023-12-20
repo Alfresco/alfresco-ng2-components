3ac3d0dbef9663b53c2aee2bb979a542388f74df
Vito Albano <vito.albano@hyland.com>
2023-12-20 23:11:36 +0000

    Fixed search e2es

--- e2e/process-services-cloud/task-list/task-header-cloud.e2e.ts
+++ e2e/process-services-cloud/task-list/task-header-cloud.e2e.ts
@@ -236,7 +236,7 @@ describe('Task Header cloud component', () => {
         await taskHeaderCloudPage.statusCardTextItem.checkElementIsReadonly();
     });
 
-    it('[C291991] Should be able to assign a task only to the users that have access to the selected app', async () => {
+    fit('[C291991] Should be able to assign a task only to the users that have access to the selected app', async () => {
         await tasksCloudDemoPage.clickStartNewTaskButton();
         const currentAssignee = await peopleCloudComponentPage.getChipAssignee();
         expect(currentAssignee).toContain(testUser.firstName);
