6a80413880c634cb9b014acb2ff1a6bfff154c7c
Amedeo Lepore <amedeo.lepore@hyland.com>
2024-01-18 13:17:45 +0100

    [AAE-12521] move navigation to the guard to fix infinite loop issue with code flow auth

--- lib/core/src/lib/auth/oidc/oidc-auth.guard.spec.ts
+++ lib/core/src/lib/auth/oidc/oidc-auth.guard.spec.ts
@@ -16,57 +16,90 @@
  */
 
 import { TestBed } from '@angular/core/testing';
-import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 import { OidcAuthGuard } from './oidc-auth.guard';
-
-const state: RouterStateSnapshot = {
-  root: new ActivatedRouteSnapshot(),
-  url: 'http://example.com'
-};
-const routeSnapshot = new ActivatedRouteSnapshot();
+import { AuthService } from './auth.service';
 
 describe('OidcAuthGuard', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      providers: [OidcAuthGuard]
-    });
-  });
+    let oidcAuthGuard: OidcAuthGuard;
+    let authServiceSpy: jasmine.SpyObj<AuthService>;
+    let routerSpy: jasmine.SpyObj<Router>;
 
-  describe('#canActivate', () => {
-    it('should return false if the user is not authenticated, and call login method', () => {
-      const authService = { authenticated: false, login: jasmine.createSpy() } as unknown as AuthService;
-      const authGuard = new OidcAuthGuard(authService);
+    beforeEach(() => {
+        const routerSpyObj = jasmine.createSpyObj('Router', ['navigateByUrl']);
+        const authSpy = jasmine.createSpyObj('AuthService', ['loginCallback']);
 
-      expect(authGuard.canActivate(routeSnapshot, state)).toEqual(false);
-      expect(authService.login).toHaveBeenCalled();
-    });
+        TestBed.configureTestingModule({
+            providers: [
+                OidcAuthGuard,
+                { provide: AuthService, useValue: authSpy },
+                { provide: Router, useValue: routerSpyObj }
+            ],
+            imports: [RouterTestingModule]
+        });
 
-    it('should return true if the user is authenticated', () => {
-      const authService = { authenticated: true } as unknown as AuthService;
-      const authGuard = new OidcAuthGuard(authService);
-
-      expect(authGuard.canActivate(routeSnapshot, state)).toEqual(true);
-    });
-  });
-
-  describe('#canActivateChild', () => {
-    it('should return false if the user is not authenticated, and call login method', () => {
-      const authService = { authenticated: false, login: jasmine.createSpy() } as unknown as AuthService;
-      const authGuard = new OidcAuthGuard(authService);
-
-      expect(authGuard.canActivateChild(routeSnapshot, state)).toEqual(false);
-      expect(authService.login).toHaveBeenCalled();
+        routerSpy = TestBed.inject(Router) as jasmine.SpyObj<Router>;
+        oidcAuthGuard = TestBed.inject(OidcAuthGuard);
+        authServiceSpy = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
     });
 
-    it('should return true if the user is authenticated', () => {
-      const authService = { authenticated: true } as unknown as AuthService;
-      const authGuard = new OidcAuthGuard(authService);
+    describe('canActivate', () => {
+        it('should return true if is authenticated', () => {
+            authServiceSpy.authenticated = true;
 
-      expect(authGuard.canActivateChild(routeSnapshot, state)).toEqual(true);
+            const result = oidcAuthGuard.canActivate();
+
+            expect(result).toBe(true);
+        });
+
+        it('should call isAuthenticated and return the result', () => {
+            const isAuthenticatedSpy = spyOn<any>(oidcAuthGuard, '_isAuthenticated').and.returnValue(true);
+
+            const result = oidcAuthGuard.canActivate();
+
+            expect(isAuthenticatedSpy).toHaveBeenCalled();
+            expect(result).toBe(true);
+        });
     });
-  });
 
+    describe('canActivateChild', () => {
+        it('should call isAuthenticated and return its result', () => {
+            const isAuthenticatedSpy = spyOn<any>(oidcAuthGuard, '_isAuthenticated').and.returnValue(true);
+
+            const result = oidcAuthGuard.canActivateChild();
+
+            expect(isAuthenticatedSpy).toHaveBeenCalled();
+            expect(result).toBe(true);
+        });
+    });
+
+    describe('isAuthenticated', () => {
+        it('should return true if is authenticated', () => {
+            authServiceSpy.authenticated = true;
+
+            const result = oidcAuthGuard['_isAuthenticated']();
+
+            expect(result).toBe(true);
+        });
+
+        it('should call loginCallback and navigateByUrl if not authenticated', async () => {
+            authServiceSpy.authenticated = false;
+            authServiceSpy.loginCallback.and.returnValue(Promise.resolve('/fake-route'));
+
+            await oidcAuthGuard.canActivate();
+
+            expect(authServiceSpy.loginCallback).toHaveBeenCalled();
+            expect(routerSpy.navigateByUrl).toHaveBeenCalledWith('/fake-route', { replaceUrl: true });
+        });
+
+        it('should navigate to default route if loginCallback fails', async () => {
+            authServiceSpy.authenticated = false;
+            authServiceSpy.loginCallback.and.returnValue(Promise.reject(new Error()));
+
+            await oidcAuthGuard.canActivate();
+
+            expect(routerSpy.navigateByUrl).toHaveBeenCalledWith('/', { replaceUrl: true });
+        });
+    });
 });
