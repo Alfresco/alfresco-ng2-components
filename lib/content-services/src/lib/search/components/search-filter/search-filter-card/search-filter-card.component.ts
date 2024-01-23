fdab441d45a1661fd3d81a595e36a0881bbe4982
Diogo Bastos <50139916+DiogoABastos@users.noreply.github.com>
2024-01-23 08:35:50 +0000

    [AAE-19610] Submit query on change event for search date range tabbed widget (#9256)
    * [AAE-19610] Add default actions to search date range tabbed widget
    
    * [AAE-19610] Update filter on change
    
    * [AAE-19610] Add tests
    
    * [AAE-19610] Fix lint issue
    
    * [AAE-19610] Add debounceTime
    
    * [AAE-19610] Add wrapper to search-filter
    
    * [AAE-19610] Fix lint
    
    * [AAE-19610] Remove leftover changes

---
+++ lib/content-services/src/lib/search/components/search-filter/search-filter-card/search-filter-card.component.ts
@@ -0,0 +1,41 @@
+/*!
+ * @license
+ * Copyright © 2005-2023 Hyland Software, Inc. and its affiliates. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component, Input, ViewChild, ViewEncapsulation } from '@angular/core';
+import { SearchWidgetContainerComponent } from '../../search-widget-container/search-widget-container.component';
+import { SearchCategory } from '../../../models/search-category.interface';
+
+@Component({
+    selector: 'adf-search-filter-card',
+    templateUrl: './search-filter-card.component.html',
+    encapsulation: ViewEncapsulation.None
+})
+export class SearchFilterCardComponent {
+    @Input()
+    category: SearchCategory;
+
+    @ViewChild(SearchWidgetContainerComponent, { static: false })
+    widgetContainerComponent: SearchWidgetContainerComponent;
+
+    clear() {
+        this.widgetContainerComponent.resetInnerWidget();
+    }
+
+    apply() {
+        this.widgetContainerComponent.applyInnerWidget();
+    }
+}
