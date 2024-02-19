c7077e4f86b90813143933f8bc4560af54688b56
Vito Albano <vito.albano@hyland.com>
2024-02-19 15:53:44 +0000

    Rebased to the latest

--- lib/content-services/src/lib/content-node-selector/content-node-selector.component.spec.ts
+++ lib/content-services/src/lib/content-node-selector/content-node-selector.component.spec.ts
@@ -123,6 +123,8 @@ describe('ContentNodeSelectorComponent', () => {
         fixture.destroy();
     });
 
+    const getTabInfoButton = () => fixture.debugElement.query(By.css('[data-automation-id="adf-content-node-selector-disabled-tab-info-icon"]'));
+
     const enableLocalUpload = () => {
         component.data.showLocalUploadButton = true;
         component.hasAllowableOperations = true;
