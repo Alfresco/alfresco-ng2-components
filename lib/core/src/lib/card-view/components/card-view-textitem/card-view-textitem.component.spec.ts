fd4e3401766027ecee051e2e7d7089c1ff0db4c5
Vito Albano <vito.albano@hyland.com>
2024-03-07 21:56:10 +0000

    [affected:*][ci:force] Fixing rebase

--- lib/core/src/lib/card-view/components/card-view-textitem/card-view-textitem.component.spec.ts
+++ lib/core/src/lib/card-view/components/card-view-textitem/card-view-textitem.component.spec.ts
@@ -31,7 +31,7 @@ import { DebugElement, SimpleChange } from '@angular/core';
 import { CardViewItemValidator } from '../../interfaces/card-view-item-validator.interface';
 import { HarnessLoader } from '@angular/cdk/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
-import { MatChipHarness, MatChipListHarness } from '@angular/material/chips/testing';
+import { MatChipGridHarness, MatChipHarness } from '@angular/material/chips/testing';
 import { MatInputHarness } from '@angular/material/input/testing';
 
 describe('CardViewTextItemComponent', () => {
@@ -41,9 +41,8 @@ describe('CardViewTextItemComponent', () => {
 
     const expectedErrorMessages = [{ message: 'Something went wrong' } as CardViewItemValidator];
 
-    const getTextField = (key: string): HTMLInputElement => {
-        return fixture.debugElement.query(By.css(`[data-automation-id="card-textitem-value-${key}"]`)).nativeElement;
-    };
+    const getTextField = (key: string): HTMLInputElement =>
+        fixture.debugElement.query(By.css(`[data-automation-id="card-textitem-value-${key}"]`)).nativeElement;
 
     const updateTextField = (key: string, value) => {
         const editInput = getTextField(key);
@@ -58,9 +57,8 @@ describe('CardViewTextItemComponent', () => {
         return textItemInput.value;
     };
 
-    const getErrorElements = (key: string, includeItems = false): DebugElement[] => {
-        return fixture.debugElement.queryAll(By.css(`[data-automation-id="card-textitem-error-${key}"]${includeItems ? ' li' : ''}`));
-    };
+    const getErrorElements = (key: string, includeItems = false): DebugElement[] =>
+        fixture.debugElement.queryAll(By.css(`[data-automation-id="card-textitem-error-${key}"]${includeItems ? ' li' : ''}`));
 
     const getTextFieldError = (key: string): string => {
         const textItemInputErrors = getErrorElements(key, true);
