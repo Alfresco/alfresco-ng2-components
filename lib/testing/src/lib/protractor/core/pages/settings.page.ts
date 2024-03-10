cf85b5c18c2f1180894edce4e2e690f0ea28b374
Vito Albano <vito.albano@hyland.com>
2024-03-10 00:27:43 +0000

    [affected:*][ci:force] Fixed e2e with new notation

--- lib/testing/src/lib/protractor/core/pages/settings.page.ts
+++ lib/testing/src/lib/protractor/core/pages/settings.page.ts
@@ -102,7 +102,7 @@ export class SettingsPage {
     async setSilentLogin(enableToggle) {
         await BrowserVisibility.waitUntilElementIsVisible(this.silentLoginToggleElement);
 
-        const isChecked = (await BrowserActions.getAttribute(this.silentLoginToggleElement, 'class')).includes(materialLocators.Checked.root);
+        const isChecked = (await BrowserActions.getAttribute(this.silentLoginToggleElement, 'class')).includes(materialLocators.Slide.toggle.checked);
 
         if (isChecked && !enableToggle || !isChecked && enableToggle) {
             await BrowserActions.click(this.silentLoginToggleLabel);
@@ -112,7 +112,7 @@ export class SettingsPage {
     async setImplicitFlow(enableToggle) {
         await BrowserVisibility.waitUntilElementIsVisible(this.implicitFlowElement);
 
-        const isChecked = (await BrowserActions.getAttribute(this.implicitFlowElement, 'class')).includes(materialLocators.Checked.root);
+        const isChecked = (await BrowserActions.getAttribute(this.implicitFlowElement, 'class')).includes(materialLocators.Slide.toggle.checked);
 
         if (isChecked && !enableToggle || !isChecked && enableToggle) {
             await BrowserActions.click(this.implicitFlowLabel);
