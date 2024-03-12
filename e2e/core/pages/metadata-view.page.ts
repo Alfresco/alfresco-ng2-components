638d0a7f7a8c0451b5a298abd11d069e9188b78c
Amedeo Lepore <amedeo.lepore85@gmail.com>
2024-03-12 12:18:36 +0100

    [AAE-21083] Fix e2e Content: Metadata - C245652 - fix tab active selector

--- e2e/core/pages/metadata-view.page.ts
+++ e2e/core/pages/metadata-view.page.ts
@@ -115,7 +115,7 @@ export class MetadataViewPage {
 
     async clickOnPropertiesTab(): Promise<void> {
         const propertiesTab = element(
-            by.cssContainingText(`.adf-info-drawer-layout-content div${materialLocators.Tab.labels.class} div ${materialLocators.Tab.label.content.class}`, `Properties`)
+            by.cssContainingText(`.adf-info-drawer-layout-content div${materialLocators.Tab.labels.container.class} div ${materialLocators.Tab.label.content.class}`, `Properties`)
         );
         await BrowserActions.click(propertiesTab);
     }
