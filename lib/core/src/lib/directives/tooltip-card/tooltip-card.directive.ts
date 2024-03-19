2243b2e2bbc9eb0bb89420bafd4e8a95fc1730db
wiktord2000 <wiktor.danielewski@hyland.com>
2024-03-19 14:47:07 +0100

    AAE-21315 Update TooltipCardDirective

--- lib/core/src/lib/directives/tooltip-card/tooltip-card.directive.ts
+++ lib/core/src/lib/directives/tooltip-card/tooltip-card.directive.ts
@@ -64,7 +64,7 @@ export class TooltipCardDirective implements OnInit, OnDestroy {
     @HostListener('mouseenter')
     show() {
         const tooltipRef: ComponentRef<TooltipCardComponent>
-            = this.overlayRef.attach(new ComponentPortal(TooltipCardComponent));
+            = this.overlayRef?.attach(new ComponentPortal(TooltipCardComponent));
         tooltipRef.instance.text = this.text;
         tooltipRef.instance.image = this.image;
         tooltipRef.instance.width = this.width;
@@ -73,6 +73,6 @@ export class TooltipCardDirective implements OnInit, OnDestroy {
 
     @HostListener('mouseleave')
     hide() {
-        this.overlayRef.detach();
+        this.overlayRef?.detach();
     }
 }
