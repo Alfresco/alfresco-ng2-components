df2976705a5fe0bd91c0327e0cfbf5fa8c73dfff
wiktord2000 <wiktor.danielewski@hyland.com>
2024-03-21 10:15:59 +0100

    AAE-21315 Add spaces

--- lib/core/src/lib/directives/tooltip-card/tooltip-card.directive.ts
+++ lib/core/src/lib/directives/tooltip-card/tooltip-card.directive.ts
@@ -43,7 +43,7 @@ export class TooltipCardDirective implements OnInit, OnDestroy {
     }
 
     ngOnDestroy(): void {
-        if(this.overlayRef){
+        if (this.overlayRef) {
             this.hide();
         }
     }
