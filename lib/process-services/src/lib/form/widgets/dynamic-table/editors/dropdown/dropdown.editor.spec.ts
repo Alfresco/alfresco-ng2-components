90494031e01ea301077e746b0ff86e0ee3f3ce71
Wojciech Duda <69160975+wojd0@users.noreply.github.com>
2024-04-04 16:55:28 +0200

    AAE-20141 dropdown editor remake

--- lib/process-services/src/lib/form/widgets/dynamic-table/editors/dropdown/dropdown.editor.spec.ts
+++ lib/process-services/src/lib/form/widgets/dynamic-table/editors/dropdown/dropdown.editor.spec.ts
@@ -17,22 +17,29 @@
 
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Observable, of, throwError } from 'rxjs';
-import { CoreTestingModule, FormFieldModel, FormModel, FormService } from '@alfresco/adf-core';
+import { FormFieldModel, FormModel, FormService, TranslationMock, TranslationService } from '@alfresco/adf-core';
 import { DynamicTableColumnOption } from '../models/dynamic-table-column-option.model';
 import { DynamicTableColumn } from '../models/dynamic-table-column.model';
 import { DynamicTableRow } from '../models/dynamic-table-row.model';
 import { DynamicTableModel } from '../models/dynamic-table.widget.model';
 import { DropdownEditorComponent } from './dropdown.editor';
-import { TranslateModule } from '@ngx-translate/core';
 import { TaskFormService } from '../../../../services/task-form.service';
 import { ProcessDefinitionService } from '../../../../services/process-definition.service';
-import { HarnessLoader } from '@angular/cdk/testing';
-import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { MatSelectHarness } from '@angular/material/select/testing';
 import { FormModule } from '../../../../form.module';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatOptionModule } from '@angular/material/core';
 
 describe('DropdownEditorComponent', () => {
+    let fixture: ComponentFixture<DropdownEditorComponent>;
     let component: DropdownEditorComponent;
+    let loader: HarnessLoader;
     let formService: FormService;
     let taskFormService: TaskFormService;
     let processDefinitionService: ProcessDefinitionService;
@@ -43,12 +50,23 @@ describe('DropdownEditorComponent', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [TranslateModule.forRoot(), CoreTestingModule, FormModule]
+            imports: [
+                TranslateModule.forRoot(),
+                CommonModule,
+                NoopAnimationsModule,
+                MatFormFieldModule,
+                MatSelectModule,
+                MatOptionModule,
+                FormModule
+            ],
+            providers: [{ provide: TranslationService, useClass: TranslationMock }]
         });
-
-        formService = new FormService();
+        formService = TestBed.inject(FormService);
         taskFormService = TestBed.inject(TaskFormService);
         processDefinitionService = TestBed.inject(ProcessDefinitionService);
+
+        fixture = TestBed.createComponent(DropdownEditorComponent);
+        component = fixture.componentInstance;
 
         row = { value: { dropdown: 'one' } } as DynamicTableRow;
         column = {
@@ -56,217 +74,189 @@ describe('DropdownEditorComponent', () => {
             options: [
                 { id: '1', name: 'one' },
                 { id: '2', name: 'two' }
-            ]
+            ],
+            editable: true
         } as DynamicTableColumn;
 
         form = new FormModel({ taskId: '<task-id>' });
-        table = new DynamicTableModel(new FormFieldModel(form, { id: '<field-id>' }), formService);
+        table = new DynamicTableModel(new FormFieldModel(form, { id: '<field-id>', isVisible: true }), formService);
         table.rows.push(row);
         table.columns.push(column);
 
-        component = new DropdownEditorComponent(formService, taskFormService, processDefinitionService, null);
         component.table = table;
         component.row = row;
         component.column = column;
+        loader = TestbedHarnessEnvironment.loader(fixture);
     });
 
-    it('should require table field to setup', () => {
-        table.field = null;
-        component.ngOnInit();
-        expect(component.value).toBeNull();
-        expect(component.options).toEqual([]);
+    describe('dropdown is populated manually', () => {
+        beforeEach(() => {
+            column = {
+                id: 'dropdown',
+                options: [
+                    { id: '1', name: 'one' },
+                    { id: '2', name: 'two' }
+                ]
+            } as DynamicTableColumn;
+
+            component.column = column;
+        });
+
+        it('should require table field to setup', () => {
+            table.field = null;
+            fixture.detectChanges();
+            expect(component.value).toBeNull();
+            expect(component.options).toEqual([]);
+        });
+
+        it('should setup with manual mode', () => {
+            row.value[column.id] = 'two';
+            fixture.detectChanges();
+            expect(component.options).toEqual(column.options);
+            expect(component.value).toBe(row.value[column.id]);
+        });
+
+        it('should setup empty columns for manual mode', () => {
+            column.options = null;
+            fixture.detectChanges();
+            expect(component.options).toEqual([]);
+        });
+
+        it('should setup with REST mode', () => {
+            column.optionType = 'rest';
+            row.value[column.id] = 'twelve';
+
+            const restResults: DynamicTableColumnOption[] = [
+                { id: '11', name: 'eleven' },
+                { id: '12', name: 'twelve' }
+            ];
+
+            spyOn(taskFormService, 'getRestFieldValuesColumn').and.returnValue(
+                new Observable((observer) => {
+                    observer.next(restResults);
+                    observer.complete();
+                })
+            );
+
+            fixture.detectChanges();
+
+            expect(taskFormService.getRestFieldValuesColumn).toHaveBeenCalledWith(form.taskId, table.field.id, column.id);
+
+            expect(column.options).toEqual(restResults);
+            expect(component.options).toEqual(restResults);
+            expect(component.value).toBe(row.value[column.id]);
+        });
+
+        it('should create empty options array on REST response', () => {
+            column.optionType = 'rest';
+
+            spyOn(taskFormService, 'getRestFieldValuesColumn').and.returnValue(
+                new Observable((observer) => {
+                    observer.next(null);
+                    observer.complete();
+                })
+            );
+
+            fixture.detectChanges();
+
+            expect(taskFormService.getRestFieldValuesColumn).toHaveBeenCalledWith(form.taskId, table.field.id, column.id);
+
+            expect(column.options).toEqual([]);
+            expect(component.options).toEqual([]);
+            expect(component.value).toBe(row.value[column.id]);
+        });
+
+        it('should handle REST error getting options with task id', () => {
+            column.optionType = 'rest';
+            const error = 'error';
+
+            spyOn(taskFormService, 'getRestFieldValuesColumn').and.returnValue(throwError(error));
+            spyOn(component, 'handleError').and.stub();
+
+            component.ngOnInit();
+            expect(component.handleError).toHaveBeenCalledWith(error);
+        });
+
+        it('should handle REST error getting option with processDefinitionId', () => {
+            column.optionType = 'rest';
+            const procForm = new FormModel({ processDefinitionId: '<process-definition-id>' });
+            const procTable = new DynamicTableModel(new FormFieldModel(procForm, { id: '<field-id>' }), formService);
+            component.table = procTable;
+            const error = 'error';
+
+            spyOn(processDefinitionService, 'getRestFieldValuesColumnByProcessId').and.returnValue(throwError(error));
+            spyOn(component, 'handleError').and.stub();
+
+            fixture.detectChanges();
+            expect(component.handleError).toHaveBeenCalledWith(error);
+        });
+
+        it('should update row on value change', () => {
+            const event = { value: 'two' };
+            component.onValueChanged(row, column, event);
+            expect(row.value[column.id]).toBe(column.options[1]);
+        });
     });
 
-    it('should setup with manual mode', () => {
-        row.value[column.id] = 'two';
-        component.ngOnInit();
-        expect(component.options).toEqual(column.options);
-        expect(component.value).toBe(row.value[column.id]);
+    describe('dropdown is populated via taskId', () => {
+        let getRestFieldValuesColumnSpy: jasmine.Spy;
+
+        beforeEach(async () => {
+            form = new FormModel({ taskId: '<task-id>' });
+            table = new DynamicTableModel(new FormFieldModel(form, { id: '<field-id>' }), formService);
+            component.table = table;
+            component.table.field = new FormFieldModel(form, {
+                id: 'dropdown-id',
+                name: 'date-name',
+                type: 'dropdown',
+                readOnly: 'false',
+                restUrl: 'fake-rest-url'
+            });
+            component.column.optionType = 'rest';
+            component.table.field.isVisible = true;
+            getRestFieldValuesColumnSpy = spyOn(taskFormService, 'getRestFieldValuesColumn').and.returnValue(of(column.options));
+        });
+
+        it('should show visible dropdown widget', async () => {
+            const select = await loader.getHarness(MatSelectHarness.with({ selector: '#dropdown' }));
+            await select.open();
+            const options = await select.getOptions();
+
+            expect(getRestFieldValuesColumnSpy).toHaveBeenCalled();
+            expect(component.options.length).toBe(2);
+            expect(options.length).toBe(3);
+        });
     });
 
-    it('should setup empty columns for manual mode', () => {
-        column.options = null;
-        component.ngOnInit();
-        expect(component.options).toEqual([]);
-    });
-
-    it('should setup with REST mode', () => {
-        column.optionType = 'rest';
-        row.value[column.id] = 'twelve';
-
-        const restResults: DynamicTableColumnOption[] = [
-            { id: '11', name: 'eleven' },
-            { id: '12', name: 'twelve' }
-        ];
-
-        spyOn(taskFormService, 'getRestFieldValuesColumn').and.returnValue(
-            new Observable((observer) => {
-                observer.next(restResults);
-                observer.complete();
-            })
-        );
-
-        component.ngOnInit();
-
-        expect(taskFormService.getRestFieldValuesColumn).toHaveBeenCalledWith(form.taskId, table.field.id, column.id);
-
-        expect(column.options).toEqual(restResults);
-        expect(component.options).toEqual(restResults);
-        expect(component.value).toBe(row.value[column.id]);
-    });
-
-    it('should create empty options array on REST response', () => {
-        column.optionType = 'rest';
-
-        spyOn(taskFormService, 'getRestFieldValuesColumn').and.returnValue(
-            new Observable((observer) => {
-                observer.next(null);
-                observer.complete();
-            })
-        );
-
-        component.ngOnInit();
-
-        expect(taskFormService.getRestFieldValuesColumn).toHaveBeenCalledWith(form.taskId, table.field.id, column.id);
-
-        expect(column.options).toEqual([]);
-        expect(component.options).toEqual([]);
-        expect(component.value).toBe(row.value[column.id]);
-    });
-
-    it('should handle REST error getting options with task id', () => {
-        column.optionType = 'rest';
-        const error = 'error';
-
-        spyOn(taskFormService, 'getRestFieldValuesColumn').and.returnValue(throwError(error));
-        spyOn(component, 'handleError').and.stub();
-
-        component.ngOnInit();
-        expect(component.handleError).toHaveBeenCalledWith(error);
-    });
-
-    it('should handle REST error getting option with processDefinitionId', () => {
-        column.optionType = 'rest';
-        const procForm = new FormModel({ processDefinitionId: '<process-definition-id>' });
-        const procTable = new DynamicTableModel(new FormFieldModel(procForm, { id: '<field-id>' }), formService);
-        component.table = procTable;
-        const error = 'error';
-
-        spyOn(processDefinitionService, 'getRestFieldValuesColumnByProcessId').and.returnValue(throwError(error));
-        spyOn(component, 'handleError').and.stub();
-
-        component.ngOnInit();
-        expect(component.handleError).toHaveBeenCalledWith(error);
-    });
-
-    it('should update row on value change', () => {
-        const event = { value: 'two' };
-        component.onValueChanged(row, column, event);
-        expect(row.value[column.id]).toBe(column.options[1]);
-    });
-
-    describe('when template is ready', () => {
-        let dropDownEditorComponent: DropdownEditorComponent;
-        let fixture: ComponentFixture<DropdownEditorComponent>;
-        let dynamicTable: DynamicTableModel;
-        let loader: HarnessLoader;
+    describe('dropdown is populated via processDefinitionId', () => {
+        let getRestFieldValuesColumnByProcessId: jasmine.Spy;
 
         beforeEach(() => {
-            fixture = TestBed.createComponent(DropdownEditorComponent);
-            dropDownEditorComponent = fixture.componentInstance;
-            loader = TestbedHarnessEnvironment.loader(fixture);
+            form = new FormModel({ processDefinitionId: '<proc-id>' });
+            table = new DynamicTableModel(new FormFieldModel(form, { id: '<field-id>' }), formService);
+            component.table = table;
+            component.table.field = new FormFieldModel(form, {
+                id: 'dropdown-id',
+                name: 'date-name',
+                type: 'dropdown',
+                readOnly: 'false',
+                restUrl: 'fake-rest-url'
+            });
+            component.column.optionType = 'rest';
+            component.table.field.isVisible = true;
+            getRestFieldValuesColumnByProcessId = spyOn(processDefinitionService, 'getRestFieldValuesColumnByProcessId').and.returnValue(
+                of(column.options)
+            );
         });
 
-        afterEach(() => {
-            fixture.destroy();
-        });
+        it('should show visible dropdown widget', async () => {
+            const select = await loader.getHarness(MatSelectHarness.with({ selector: '#dropdown' }));
+            await select.open();
 
-        describe('and dropdown is populated via taskId', () => {
-            let getRestFieldValuesColumnSpy: jasmine.Spy;
-
-            beforeEach(async () => {
-                row = { value: { dropdown: 'one' } } as DynamicTableRow;
-                column = {
-                    id: 'column-id',
-                    optionType: 'rest',
-                    options: [
-                        { id: '1', name: 'one' },
-                        { id: '2', name: 'two' }
-                    ]
-                } as DynamicTableColumn;
-                form = new FormModel({ taskId: '<task-id>' });
-                dynamicTable = new DynamicTableModel(new FormFieldModel(form, { id: '<field-id>' }), formService);
-                dynamicTable.rows.push(row);
-                dynamicTable.columns.push(column);
-                dropDownEditorComponent.table = dynamicTable;
-                dropDownEditorComponent.column = column;
-                dropDownEditorComponent.row = row;
-                dropDownEditorComponent.table.field = new FormFieldModel(form, {
-                    id: 'dropdown-id',
-                    name: 'date-name',
-                    type: 'dropdown',
-                    readOnly: 'false',
-                    restUrl: 'fake-rest-url'
-                });
-                dropDownEditorComponent.table.field.isVisible = true;
-                getRestFieldValuesColumnSpy = spyOn(taskFormService, 'getRestFieldValuesColumn').and.returnValue(of(column.options));
-                fixture.detectChanges();
-            });
-
-            it('should show visible dropdown widget', async () => {
-                const select = await loader.getHarness(MatSelectHarness.with({ selector: '#column-id' }));
-                await select.open();
-                const options = await select.getOptions();
-
-                expect(getRestFieldValuesColumnSpy).toHaveBeenCalled();
-                expect(dropDownEditorComponent.options.length).toBe(2);
-                expect(options.length).toBe(3);
-            });
-        });
-
-        describe('and dropdown is populated via processDefinitionId', () => {
-            let getRestFieldValuesColumnByProcessId: jasmine.Spy;
-
-            beforeEach(() => {
-                row = { value: { dropdown: 'one' } } as DynamicTableRow;
-                column = {
-                    id: 'column-id',
-                    optionType: 'rest',
-                    options: [
-                        { id: '1', name: 'one' },
-                        { id: '2', name: 'two' }
-                    ]
-                } as DynamicTableColumn;
-                form = new FormModel({ processDefinitionId: '<proc-id>' });
-                dynamicTable = new DynamicTableModel(new FormFieldModel(form, { id: '<field-id>' }), formService);
-                dynamicTable.rows.push(row);
-                dynamicTable.columns.push(column);
-                dropDownEditorComponent.table = dynamicTable;
-                dropDownEditorComponent.column = column;
-                dropDownEditorComponent.row = row;
-                dropDownEditorComponent.table.field = new FormFieldModel(form, {
-                    id: 'dropdown-id',
-                    name: 'date-name',
-                    type: 'dropdown',
-                    readOnly: 'false',
-                    restUrl: 'fake-rest-url'
-                });
-                dropDownEditorComponent.table.field.isVisible = true;
-                getRestFieldValuesColumnByProcessId = spyOn(processDefinitionService, 'getRestFieldValuesColumnByProcessId').and.returnValue(
-                    of(column.options)
-                );
-                fixture.detectChanges();
-            });
-
-            it('should show visible dropdown widget', async () => {
-                const select = await loader.getHarness(MatSelectHarness.with({ selector: '#column-id' }));
-                await select.open();
-                const options = await select.getOptions();
-
-                expect(getRestFieldValuesColumnByProcessId).toHaveBeenCalled();
-                expect(dropDownEditorComponent.options.length).toBe(2);
-                expect(options.length).toBe(3);
-            });
+            const options = await select.getOptions();
+            expect(getRestFieldValuesColumnByProcessId).toHaveBeenCalled();
+            expect(component.options.length).toBe(2);
+            expect(options.length).toBe(3);
         });
     });
 });
