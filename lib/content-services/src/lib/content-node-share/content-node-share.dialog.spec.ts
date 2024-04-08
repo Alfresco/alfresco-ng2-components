895b2ed3a3aaadbdc1186dc39424645e735e9b75
Jacek Pluta <jacek.pluta@hyland.com>
2024-04-08 15:35:05 +0200

    [ACS-7535][ACS-7537][ACS-7536][ACA] Move/Copy dialogs, Share dialog, Aspects dialog

--- lib/content-services/src/lib/content-node-share/content-node-share.dialog.spec.ts
+++ lib/content-services/src/lib/content-node-share/content-node-share.dialog.spec.ts
@@ -300,7 +300,7 @@ describe('ShareDialogComponent', () => {
 
         fixture.detectChanges();
         expect(fixture.debugElement.query(By.css('[data-automation-id="adf-content-share-expiration-field"]')).componentInstance.floatLabel).toBe(
-            'always'
+            'auto'
         );
     });
 
