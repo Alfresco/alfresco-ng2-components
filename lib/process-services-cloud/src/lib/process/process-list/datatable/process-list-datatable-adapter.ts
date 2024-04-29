512e8dd63da4af43167eb905c11cf7e4c02effe0
Wojciech Duda <69160975+wojd0@users.noreply.github.com>
2024-04-29 13:49:44 +0200

    AAE-21967 use column id to retrieve variable column types

--- lib/process-services-cloud/src/lib/process/process-list/datatable/process-list-datatable-adapter.ts
+++ lib/process-services-cloud/src/lib/process/process-list/datatable/process-list-datatable-adapter.ts
@@ -20,17 +20,13 @@ import { ProcessListDataColumnCustomData, PROCESS_LIST_CUSTOM_VARIABLE_COLUMN } 
 import { ProcessInstanceCloudListViewModel } from '../models/perocess-instance-cloud-view.model';
 
 export class ProcessListDatatableAdapter extends ObjectDataTableAdapter {
-    constructor(
-        data: ProcessInstanceCloudListViewModel[],
-        schema: DataColumn<ProcessListDataColumnCustomData>[]
-    ) {
+    constructor(data: ProcessInstanceCloudListViewModel[], schema: DataColumn<ProcessListDataColumnCustomData>[]) {
         super(data, schema);
     }
 
     getColumnType(row: DataRow, col: DataColumn<ProcessListDataColumnCustomData>): string {
         if (col.customData?.columnType === PROCESS_LIST_CUSTOM_VARIABLE_COLUMN) {
-            const variableDisplayName = col.title;
-            const columnType = row.obj.variablesMap?.[variableDisplayName]?.type;
+            const columnType = row.obj.variablesMap?.[col.id]?.type;
             return columnType ?? 'text';
         }
 
