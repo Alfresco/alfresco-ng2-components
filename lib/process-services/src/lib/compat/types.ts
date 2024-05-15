a6b39a19fecbbcb55dacc5d83bf7cbc3f5dc8e81
Denys Vuika <denys.vuika@gmail.com>
2024-05-15 18:13:18 -0400

    update tests, backwards compatibility

--- lib/process-services/src/lib/compat/types.ts
+++ lib/process-services/src/lib/compat/types.ts
@@ -1,4 +1,10 @@
-import { ProcessInstanceRepresentation, UserTaskFilterRepresentation } from '@alfresco/js-api';
+import {
+    AppDefinitionRepresentation,
+    LightUserRepresentation,
+    ProcessInstanceRepresentation,
+    UserRepresentation,
+    UserTaskFilterRepresentation
+} from '@alfresco/js-api';
 
 /** @deprecated use js-api/ProcessInstanceRepresentation instead */
 export type ProcessInstance = ProcessInstanceRepresentation;
@@ -8,3 +14,12 @@ export type FilterRepresentationModel = UserTaskFilterRepresentation;
 
 /** @deprecated use js-api/UserTaskFilterRepresentation instead */
 export type FilterParamsModel = UserTaskFilterRepresentation;
+
+/** @deprecated use js-api/UserRepresentation instead */
+export type BpmUserModel = UserRepresentation;
+
+/** @deprecated use js-api/AppDefinitionRepresentation instead */
+export type AppDefinitionRepresentationModel = AppDefinitionRepresentation;
+
+/** @deprecated use js-api/LightUserRepresentation instead */
+export type UserProcessModel = LightUserRepresentation;
