f1f306c0322e9ae68adfb95af2e3a7688b0b35f9
Bartosz Sekula <Bartosz.Sekula@hyland.com>
2024-05-23 11:52:37 +0200

    Update packages

--- scripts/github/release/set-migrations.js
+++ scripts/github/release/set-migrations.js
@@ -1,31 +1,47 @@
 const fs = require('fs');
-// const semver = require('semver');
+
+const getFile = (path) => {
+    const rawFile = fs.readFileSync(path, 'utf8');
+    const file = JSON.parse(rawFile);
+
+    return file;
+};
 
 module.exports = async () => {
     const corePackagePath = 'lib/core/package.json';
-    const coreSchematicsPath = 'lib/core/schematics/migrations/collection.json';
+    const jaApiConfigPackagePath = 'lib/js-api/package.json';
+    const coreMigrationConfigPath = 'lib/core/schematics/migrations/collection.json';
 
-    const rawCorePackage = fs.readFileSync(corePackagePath, 'utf8');
-    const corePackage = JSON.parse(rawCorePackage);
-
-    const rawCoreSchematics = fs.readFileSync(coreSchematicsPath, 'utf8');
-    const coreSchematics = JSON.parse(rawCoreSchematics);
+    const corePackage = getFile(corePackagePath);
+    const coreMigration = getFile(coreMigrationConfigPath);
+    const jsApiPackage = getFile(jaApiConfigPackagePath);
 
     const coreVersion = corePackage.version;
 
     const coreMigrations = [
         {
             name: 'move-out-alfresco-api',
-            version: '<=7.0.0'
+            version: '7.0.0'
         }
     ];
 
     coreMigrations.forEach((migration) => {
-        coreSchematics['schematics'][migration.name]['version'] = coreVersion;
+        coreMigration['schematics'][migration.name]['version'] = coreVersion;
+        coreMigration['packageJsonUpdates'][migration.name]['version'] = coreVersion;
+        const packagesToUpdate = coreMigration['packageJsonUpdates'][migration.name]['packages'];
+
+        Object.keys(packagesToUpdate).forEach((packageName) => {
+            if (packageName === '@alfresco/js-api') {
+                packagesToUpdate[packageName]['version'] = jsApiPackage.version;
+            } else {
+                packagesToUpdate[packageName]['version'] = coreVersion;
+            }
+        });
     });
 
     console.log('Set migration for', coreVersion);
-    console.log('coreSchematics', coreSchematics.schematics);
+    console.log('coreSchematics', coreMigration.schematics);
+    console.log('packageJsonUpdates', JSON.stringify(coreMigration.packageJsonUpdates));
 
-    fs.writeFileSync(coreSchematicsPath, JSON.stringify(coreSchematics, null, 4));
+    fs.writeFileSync(coreMigrationConfigPath, JSON.stringify(coreMigration, null, 4));
 };
