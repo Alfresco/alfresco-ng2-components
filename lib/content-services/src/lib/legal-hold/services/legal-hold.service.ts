ee2445dd9a78b8d73b42156af3f8f4e860060db6
DaryaBalvanovich <darya.balvanovich1@hyland.com>
2024-06-04 18:13:23 +0200

    ACS-8055 create legal-hold service

---
+++ lib/content-services/src/lib/legal-hold/services/legal-hold.service.ts
@@ -0,0 +1,61 @@
+/*!
+ * @license
+ * Copyright © 2005-2024 Hyland Software, Inc. and its affiliates. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { AlfrescoApiService } from '@alfresco/adf-core';
+import { ContentPagingQuery, Hold, LegalHoldApi } from '@alfresco/js-api';
+import { Injectable } from '@angular/core';
+import { Observable, from, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class LegalHoldService {
+    private _legalHoldApi: LegalHoldApi;
+    get legalHoldApi(): LegalHoldApi {
+        this._legalHoldApi = this._legalHoldApi ?? new LegalHoldApi(this.apiService.getInstance());
+        return this._legalHoldApi;
+    }
+
+    constructor(private apiService: AlfrescoApiService) {}
+
+    /**
+     * Gets the list of holds assigned to the node.
+     *
+     * @param filePlanId The identifier of a file plan. You can also use the -filePlan- alias.
+     * @param options Optional parameters supported by JS-API
+     * @returns List of assigned holds
+     */
+    getHolds(filePlanId: string, options: ContentPagingQuery = {}): Observable<Hold[]> {
+        const queryOptions = Object.assign({
+            maxItems: options?.maxItems,
+            skipCount: options?.skipCount
+        });
+
+        return from(this.legalHoldApi.getHolds(filePlanId, queryOptions)).pipe(
+            map((holds) =>
+                holds.list?.entries?.map((entity) => ({
+                    id: entity.entry.id,
+                    name: entity.entry.name,
+                    reason: entity.entry.reason,
+                    description: entity.entry.description
+                }))
+            ),
+            catchError((err) => throwError(err))
+        );
+    }
+}
