5fbbba3f53255256c9d360b3abc4f030de5d310b
Darya Balvanovich <darya.balvanovich@hyland.com>
2024-06-19 23:05:19 +0200

    ACS-7690 add bulk method

--- lib/js-api/src/api/gs-core-rest-api/api/legal-hold.api.ts
+++ lib/js-api/src/api/gs-core-rest-api/api/legal-hold.api.ts
@@ -118,7 +118,7 @@ export class LegalHoldApi extends BaseApi {
      * @param holds array of holds
      * @returns Promise<NodeChildAssociationPaging>
      */
-    createHold(filePlanId: string, holds: Hold[]): Promise<CreateHoldEntry> {
+    createHold(filePlanId: string, holds: Hold[]): Promise<HoldEntry> {
         throwIfNotDefined(filePlanId, 'filePlanId');
         throwIfNotDefined(holds, 'holds');
 
@@ -130,7 +130,30 @@ export class LegalHoldApi extends BaseApi {
             path: '/file-plans/{filePlanId}/holds',
             pathParams,
             bodyParam: holds,
-            returnType: CreateHoldEntry
+            returnType: HoldEntry
+        });
+    }
+
+    /**
+     * List of legal holds
+     *
+     * @param filePlanId The identifier of a file plan. You can also use the -filePlan- alias.
+     * @param holds array of holds
+     * @returns Promise<NodeChildAssociationPaging>
+     */
+    createHolds(filePlanId: string, holds: Hold[]): Promise<HoldPaging> {
+        throwIfNotDefined(filePlanId, 'filePlanId');
+        throwIfNotDefined(holds, 'holds');
+
+        const pathParams = {
+            filePlanId
+        };
+
+        return this.post({
+            path: '/file-plans/{filePlanId}/holds',
+            pathParams,
+            bodyParam: holds,
+            returnType: HoldPaging
         });
     }
 
