bbe7698c579eb4e4f4aa789ad255b6b7744b98db
swapnil.verma <swapnil.verma@globallogic.com>
2024-06-26 11:40:00 +0530

    Updated AuthGuardSsoRoleService to be a functional guard

--- lib/core/src/lib/auth/guard/auth-guard-sso-role.service.ts
+++ lib/core/src/lib/auth/guard/auth-guard-sso-role.service.ts
@@ -15,63 +15,67 @@
  * limitations under the License.
  */
 
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { ActivatedRouteSnapshot, Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { UserAccessService } from '../services/user-access.service';
 
-@Injectable({
-    providedIn: 'root'
-})
-export class AuthGuardSsoRoleService implements CanActivate {
-    constructor(private userAccessService: UserAccessService,
-                private router: Router,
-                private dialog: MatDialog) {
+const userAccessService = inject(UserAccessService);
+const router = inject(Router);
+const dialog = inject(MatDialog);
+
+/**
+ * Function to validate if the current user has/does not have the provided set of roles
+ * @param rolesToCheck list of roles that the user needs to be checked
+ * @param excludedRoles list of roles that the user should not have
+ * @returns boolean flag corresponding to whether the user has/does not have the provided set of roles/excluded roles
+ */
+function validateRoles(rolesToCheck: string[], excludedRoles?: string[]): boolean {
+    if (excludedRoles?.length > 0) {
+        return hasRoles(rolesToCheck) && !hasRoles(excludedRoles);
     }
-
-    canActivate(route: ActivatedRouteSnapshot): boolean {
-        this.userAccessService.fetchUserAccess();
-        let hasRealmRole = false;
-        let hasClientRole = true;
-        if (route.data) {
-            if (route.data['roles']) {
-                const rolesToCheck: string[] = route.data['roles'];
-                if (rolesToCheck.length === 0) {
-                    hasRealmRole = true;
-                } else {
-                    const excludedRoles = route.data['excludedRoles'] || [];
-                    hasRealmRole = this.validateRoles(rolesToCheck, excludedRoles);
-                }
-            }
-
-            if (route.data['clientRoles']) {
-                const clientRoleName = route.params[route.data['clientRoles']];
-                const rolesToCheck = route.data['roles'];
-                hasClientRole = this.userAccessService.hasApplicationAccess(clientRoleName, rolesToCheck);
-            }
-        }
-        const hasRole = hasRealmRole && hasClientRole;
-
-        if (!hasRole && route?.data && route.data['redirectUrl']) {
-            this.router.navigate(['/' + route.data['redirectUrl']]);
-        }
-
-        if (!hasRole) {
-            this.dialog.closeAll();
-        }
-
-        return hasRole;
-    }
-
-    private validateRoles(rolesToCheck: string[], excludedRoles?: string[]): boolean {
-        if (excludedRoles?.length > 0) {
-            return this.hasRoles(rolesToCheck) && !this.hasRoles(excludedRoles);
-        }
-        return this.hasRoles(rolesToCheck);
-    }
-
-    private hasRoles(roles: string[] = []): boolean {
-        return this.userAccessService.hasGlobalAccess(roles);
-    }
-
+    return hasRoles(rolesToCheck);
 }
+
+/**
+ * Function to check if the current user has access to a provided set of roles
+ * @param roles list of roles to check
+ * @returns boolean flag corresponding to whether the user has roles or not
+ */
+function hasRoles(roles: string[] = []): boolean {
+    return userAccessService.hasGlobalAccess(roles);
+}
+
+export const AuthGuardSsoRoleService = (route: ActivatedRouteSnapshot): boolean => {
+    userAccessService.fetchUserAccess();
+    let hasRealmRole = false;
+    let hasClientRole = true;
+    if (route.data) {
+        if (route.data['roles']) {
+            const rolesToCheck: string[] = route.data['roles'];
+            if (rolesToCheck.length === 0) {
+                hasRealmRole = true;
+            } else {
+                const excludedRoles = route.data['excludedRoles'] || [];
+                hasRealmRole = validateRoles(rolesToCheck, excludedRoles);
+            }
+        }
+
+        if (route.data['clientRoles']) {
+            const clientRoleName = route.params[route.data['clientRoles']];
+            const rolesToCheck = route.data['roles'];
+            hasClientRole = userAccessService.hasApplicationAccess(clientRoleName, rolesToCheck);
+        }
+    }
+    const hasRole = hasRealmRole && hasClientRole;
+
+    if (!hasRole && route?.data && route.data['redirectUrl']) {
+        router.navigate(['/' + route.data['redirectUrl']]);
+    }
+
+    if (!hasRole) {
+        dialog.closeAll();
+    }
+
+    return hasRole;
+};
