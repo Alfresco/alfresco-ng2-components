5628fb843712aae5cbd4fab6cdbd4ba07a7152f2
VitoAlbano <vito.albano.123@gmail.com>
2024-07-09 00:02:25 +0100

    Migrating to NG16 - check base migration

--- e2e/process-services/process/start-process-component.e2e.ts
+++ e2e/process-services/process/start-process-component.e2e.ts
@@ -247,7 +247,7 @@ describe('Start Process Component', () => {
                 expect(await startProcessPage.isStartProcessButtonEnabled()).toEqual(true);
             });
 
-            it('[C286509] Should select automatically the processDefinition when the app contains only one', async () => {
+            fit('[C286509] Should select automatically the processDefinition when the app contains only one', async () => {
                 await processServicesPage.goToApp(simpleApp.title);
                 await processServiceTabBarPage.clickProcessButton();
                 await processFiltersPage.clickCreateProcessButton();
