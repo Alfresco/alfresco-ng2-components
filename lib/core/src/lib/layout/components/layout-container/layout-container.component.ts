80bcb8494c808279479af6d33f45aaa14cfe5e9f
Denys Vuika <denys.vuika@gmail.com>
2024-07-09 15:10:50 -0400

    consolidate animations

--- lib/core/src/lib/layout/components/layout-container/layout-container.component.ts
+++ lib/core/src/lib/layout/components/layout-container/layout-container.component.ts
@@ -17,9 +17,9 @@
 
 import { Component, Input, ViewChild, OnInit, OnDestroy, ViewEncapsulation, OnChanges, SimpleChanges } from '@angular/core';
 import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
-import { sidenavAnimation, contentAnimation } from '../../helpers/animations';
 import { Direction } from '@angular/cdk/bidi';
 import { CommonModule } from '@angular/common';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
     selector: 'adf-layout-container',
@@ -28,7 +28,32 @@ import { CommonModule } from '@angular/common';
     templateUrl: './layout-container.component.html',
     styleUrls: ['./layout-container.component.scss'],
     encapsulation: ViewEncapsulation.None,
-    animations: [sidenavAnimation, contentAnimation]
+    animations: [
+        trigger('sidenavAnimation', [
+            state('expanded', style({ width: '{{ width }}px' }), { params: { width: 0 } }),
+            state('compact', style({ width: '{{ width }}px' }), { params: { width: 0 } }),
+            transition('compact <=> expanded', animate('0.4s cubic-bezier(0.25, 0.8, 0.25, 1)'))
+        ]),
+        trigger('contentAnimationLeft', [
+            state(
+                'expanded',
+                style({
+                    'margin-left': '{{ margin-left }}px',
+                    'margin-right': '{{ margin-right }}px'
+                }),
+                { params: { 'margin-left': 0, 'margin-right': 0 } }
+            ),
+            state(
+                'compact',
+                style({
+                    'margin-left': '{{ margin-left }}px',
+                    'margin-right': '{{ margin-right }}px'
+                }),
+                { params: { 'margin-left': 0, 'margin-right': 0 } }
+            ),
+            transition('expanded <=> compact', animate('400ms cubic-bezier(0.25, 0.8, 0.25, 1)'))
+        ])
+    ]
 })
 export class LayoutContainerComponent implements OnInit, OnDestroy, OnChanges {
     @Input() sidenavMin: number;
