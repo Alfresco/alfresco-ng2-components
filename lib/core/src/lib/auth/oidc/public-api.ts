e56add620cfb9df0a389a2e0a955157d18341da2
mauriziovitale <maurizio.vitale@hyland.com>
2024-07-17 13:50:11 +0200

    Simulate breaking change

--- lib/core/src/lib/auth/oidc/public-api.ts
+++ lib/core/src/lib/auth/oidc/public-api.ts
@@ -17,7 +17,7 @@
 
 export * from './auth-routing.module';
 export * from './auth.module';
-export * from './auth.service';
+// export * from './auth.service';
 export * from './oidc-auth.guard';
 export * from './redirect-auth.service';
 export * from './view/authentication-confirmation/authentication-confirmation.component';
