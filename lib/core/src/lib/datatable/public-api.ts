b5960e4c3e513b82141688cc3d48288eab3bfc5c
Denys Vuika <denys.vuika@gmail.com>
2024-07-23 12:38:45 -0400

    fix missing exports

--- lib/core/src/lib/datatable/public-api.ts
+++ lib/core/src/lib/datatable/public-api.ts
@@ -15,32 +15,35 @@
  * limitations under the License.
  */
 
-export * from './data/datatable-adapter';
-
-export * from './data/data-row.model';
-export * from './data/data-row-event.model';
-export * from './data/data-column.model';
-export * from './data/data-sorting.model';
-
-export * from './data/object-datarow.model';
-export * from './data/object-datatable-adapter';
-export * from './data/object-datacolumn.model';
-
-export * from './components/data-cell.event';
-export * from './components/data-row-action.event';
-export * from './directives/drop-zone.directive';
-
+export * from './components/amount-cell/amount-cell.component';
+export * from './components/boolean-cell/boolean-cell.component';
+export * from './components/columns-selector/columns-selector.component';
 export * from './components/datatable/datatable.component';
 export * from './components/datatable-cell/datatable-cell.component';
 export * from './components/datatable-row/datatable-row.component';
 export * from './components/date-cell/date-cell.component';
 export * from './components/empty-list/empty-list.component';
 export * from './components/filesize-cell/filesize-cell.component';
+export * from './components/icon-cell/icon-cell.component';
 export * from './components/json-cell/json-cell.component';
 export * from './components/location-cell/location-cell.component';
-export * from './components/columns-selector/columns-selector.component';
+export * from './components/number-cell/number-cell.component';
+export * from './components/data-row-action.event';
+export * from './components/data-cell.event';
+
+export * from './data/datatable-adapter';
+export * from './data/data-row.model';
+export * from './data/data-row-event.model';
+export * from './data/data-column.model';
+export * from './data/data-sorting.model';
+export * from './data/object-datarow.model';
+export * from './data/object-datatable-adapter';
+export * from './data/object-datacolumn.model';
 export * from './data/data-table.schema';
 
+export * from './data-column';
+
+export * from './directives/drop-zone.directive';
 export * from './directives/loading-template.directive';
 export * from './directives/no-content-template.directive';
 export * from './directives/no-permission-template.directive';
@@ -52,6 +55,4 @@ export * from './directives/main-data-table-action-template.directive';
 
 export * from './services/datatable.service';
 
-
 export * from './datatable.module';
-export * from './data-column';
