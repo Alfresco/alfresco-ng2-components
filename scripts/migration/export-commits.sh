97106892be9a3ea4f1068256c5cb2b42049eeb2d
VitoAlbano <vito.albano.123@gmail.com>
2024-08-19 23:08:10 +0100

    [MIGRATION] - Fixed some issues for project and package json

--- scripts/migration/export-commits.sh
+++ scripts/migration/export-commits.sh
@@ -14,10 +14,10 @@ if [ $? -eq 0 ]; then
 
     git cherry-pick  --quit
     echo -e "\e[34m===> Merge changes from cherry pick branch: ng16-develop cherry pick list\e[0m"
-    git cherry-pick `git log --pretty=format:"%H" origin/develop..origin/ng16-develop --reverse` -x
+    git cherry-pick `git log --pretty=format:"%H" origin/develop..origin/ng16-develop --grep="[MIGRATION]" --reverse` -x
     if [ $? -eq 0 ]; then
         echo -e "\e[34m===> Cherry Picking complete! Pushing... \e[0m"
-        # git push origin ng16-migration --force --no-verify
+        git push origin ng16-migration --force --no-verify
         echo -e "\e[34m===> Pushed \e[0m"
     else
         echo -e "\e[31mThere was some error during cherry-picking of ng16-develop\e[0m"
