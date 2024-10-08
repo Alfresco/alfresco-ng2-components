b98afa704e95c956c8b0b15d73ba2920589604e2
Wojciech Duda <69160975+wojd0@users.noreply.github.com>
2024-10-08 16:53:18 +0200

    AAE-26321 Add missing providers to depending tests

--- lib/core/src/lib/auth/guard/auth-guard-sso-role.service.spec.ts
+++ lib/core/src/lib/auth/guard/auth-guard-sso-role.service.spec.ts
@@ -18,9 +18,10 @@
 import { TestBed } from '@angular/core/testing';
 import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthGuardSsoRoleService } from './auth-guard-sso-role.service';
-import { JwtHelperService } from '../services/jwt-helper.service';
+import { JWT_STORAGE_SERVICE, JwtHelperService } from '../services/jwt-helper.service';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { NoopTranslateModule } from '../../testing/noop-translate.module';
+import { StorageService } from '../../common';
 
 describe('Auth Guard SSO role service', () => {
     let jwtHelperService: JwtHelperService;
@@ -29,7 +30,8 @@ describe('Auth Guard SSO role service', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [NoopTranslateModule, MatDialogModule]
+            imports: [NoopTranslateModule, MatDialogModule],
+            providers: [{ provide: JWT_STORAGE_SERVICE, useClass: StorageService }]
         });
         localStorage.clear();
         jwtHelperService = TestBed.inject(JwtHelperService);
