821fb6cf1107b1fcce9535bfe9ad7c6319930b43
Ehsan Rezaei <ehsan.rezaei@hyland.com>
2024-11-26 17:36:19 +0100

    AAE-20808 Removed comments

--- lib/process-services-cloud/src/lib/services/web-socket.service.ts
+++ lib/process-services-cloud/src/lib/services/web-socket.service.ts
@@ -17,7 +17,7 @@
 
 import { createClient } from 'graphql-ws';
 import { Inject, Injectable } from '@angular/core';
-import { AuthenticationService } from '@alfresco/adf-core';
+import { AppConfigService, AuthenticationService } from '@alfresco/adf-core';
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import { WebSocketLink } from '@apollo/client/link/ws';
 import {
@@ -59,12 +59,11 @@ export class WebSocketService {
 
     constructor(
         private readonly apollo: Apollo,
-        // private readonly appConfigService: AppConfigService,
+        private readonly appConfigService: AppConfigService,
         private readonly authService: AuthenticationService,
         @Inject(FeaturesServiceToken) private featuresService: IFeaturesService
     ) {
-        this.host = 'https://aae-rc-apa.envalfresco.com';
-        // this.host = this.appConfigService.get('bpmHost', '');
+        this.host = this.appConfigService.get('bpmHost', '');
     }
 
     public getSubscription<T>(options: serviceOptions): Observable<FetchResult<T>> {
