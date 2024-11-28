51ca965a249cc697607ec2c90447a54946914942
Ehsan Rezaei <ehsan.rezaei@hyland.com>
2024-11-28 11:01:56 +0100

    Trying to fix js-api unit tests

--- lib/js-api/jest.config.ts
+++ lib/js-api/jest.config.ts
@@ -2,21 +2,22 @@
 export default {
     displayName: 'js-api',
     preset: '../../jest.preset.js',
+    testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-    globals: {
-        'ts-jest': {
-            tsconfig: '<rootDir>/tsconfig.spec.json',
-            stringifyContentPathRegex: '\\.(html|svg)$',
-        },
-    },
     coverageDirectory: '../../../coverage/libs/js-api',
     transform: {
-        '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+        '^.+\\.(ts|mjs|js|html)$': [
+            'jest-preset-angular',
+            {
+                tsconfig: '<rootDir>/tsconfig.spec.json',
+                stringifyContentPathRegex: '\\.(html|svg)$'
+            }
+        ]
     },
     transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
     snapshotSerializers: [
         'jest-preset-angular/build/serializers/no-ng-attributes',
         'jest-preset-angular/build/serializers/ng-snapshot',
-        'jest-preset-angular/build/serializers/html-comment',
-    ],
+        'jest-preset-angular/build/serializers/html-comment'
+    ]
 };
