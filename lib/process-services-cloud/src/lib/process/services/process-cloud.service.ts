2b39ee9392976d7f3479a8ce02224106afff0988
Denys Vuika <denys.vuika@gmail.com>
2025-01-08 14:54:36 -0500

    cleanup [ci:force]

--- lib/process-services-cloud/src/lib/process/services/process-cloud.service.ts
+++ lib/process-services-cloud/src/lib/process/services/process-cloud.service.ts
@@ -22,12 +22,11 @@ import { ProcessInstanceCloud } from '../start-process/models/process-instance-c
 import { BaseCloudService } from '../../services/base-cloud.service';
 import { ProcessDefinitionCloud } from '../../models/process-definition-cloud.model';
 import { ApplicationVersionModel, ApplicationVersionResponseModel } from '../../models/application-version.model';
-import { ProcessCloudInterface } from './process-cloud.interface';
 
 @Injectable({
     providedIn: 'root'
 })
-export class ProcessCloudService extends BaseCloudService implements ProcessCloudInterface {
+export class ProcessCloudService extends BaseCloudService {
     dataChangesDetected = new Subject<ProcessInstanceCloud>();
 
     /**
