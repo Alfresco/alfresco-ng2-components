d85ad3a397526b4d3753b07044a7c3a1cebaa03f
tomek.hanaj <tomek.hanaj@hyland.com>
2025-01-09 14:17:46 +0100

    [AAE-29973] updated dynamic component inputs and outputs

--- lib/process-services-cloud/src/lib/screen/components/screen-cloud/screen-cloud.component.ts
+++ lib/process-services-cloud/src/lib/screen/components/screen-cloud/screen-cloud.component.ts
@@ -16,9 +16,11 @@
  */
 
 import { CommonModule } from '@angular/common';
-import { Component, ComponentRef, inject, Input, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { Component, ComponentRef, EventEmitter, inject, Input, OnInit, Output, ViewChild, ViewContainerRef } from '@angular/core';
 import { ScreenRenderingService } from '../../../services/public-api';
 import { MatCardModule } from '@angular/material/card';
+import { UserTaskCustomUi } from './screen-cloud.interface';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
     selector: 'adf-cloud-task-screen',
@@ -35,29 +37,72 @@ export class TaskScreenCloudComponent implements OnInit {
     /** Screen id to fetch corresponding screen widget. */
     @Input()
     screenId: string = '';
+    @Input()
+    processInstanceID: string = '';
+    @Input()
+    taskName: string = '';
     /** Toggle readonly state of the task. */
     @Input()
     readOnly = false;
 
+    /** Emitted when the task is saved. */
+    @Output()
+    taskSaved = new EventEmitter();
+
+    /** Emitted when the task is completed. */
+    @Output()
+    taskCompleted = new EventEmitter();
+
+    /** Emitted when there is an error. */
+    @Output()
+    error = new EventEmitter<any>();
+
     @ViewChild('container', { read: ViewContainerRef, static: true })
     container: ViewContainerRef;
-    componentRef: ComponentRef<any>;
+    componentRef: ComponentRef<UserTaskCustomUi>;
 
     private readonly screenRenderingService = inject(ScreenRenderingService);
 
     ngOnInit() {
+        this.createDynamicComponent();
+    }
+
+    createDynamicComponent() {
         if (this.screenId) {
             const componentType = this.screenRenderingService.resolveComponentType({ type: this.screenId });
             this.componentRef = this.container.createComponent(componentType);
-            if (this.taskId) {
-                this.componentRef.setInput('taskId', this.taskId);
-            }
-            if (this.appName) {
-                this.componentRef.setInput('appName', this.appName);
-            }
-            if (this.screenId) {
-                this.componentRef.setInput('screenId', this.screenId);
-            }
+            this.setInputsForDynamicComponent();
+            this.subscribeToOutputs();
+        }
+    }
+
+    setInputsForDynamicComponent(): void {
+        if (this.taskId) {
+            this.componentRef.setInput('taskId', this.taskId);
+        }
+        if (this.appName) {
+            this.componentRef.setInput('appName', this.appName);
+        }
+        if (this.screenId) {
+            this.componentRef.setInput('screenId', this.screenId);
+        }
+        if (this.processInstanceID) {
+            this.componentRef.setInput('processInstanceID', this.processInstanceID);
+        }
+        if (this.taskName) {
+            this.componentRef.setInput('taskName', this.taskName);
+        }
+    }
+
+    subscribeToOutputs() {
+        if (this.componentRef.instance?.taskSaved) {
+            this.componentRef.instance.taskSaved.pipe(takeUntilDestroyed()).subscribe(() => this.taskSaved.emit());
+        }
+        if (this.componentRef.instance?.taskCompleted) {
+            this.componentRef.instance.taskCompleted.pipe(takeUntilDestroyed()).subscribe(() => this.taskCompleted.emit());
+        }
+        if (this.componentRef.instance?.error) {
+            this.componentRef.instance.error.pipe(takeUntilDestroyed()).subscribe((data) => this.error.emit(data));
         }
     }
 }
