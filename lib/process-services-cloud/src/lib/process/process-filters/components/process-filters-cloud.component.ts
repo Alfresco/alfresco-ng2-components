f41510c55acdaa735803b9f23acde3261e73564f
Soumyajit Chakraborty <soumyajit.chakraborty@hyland.com>
2025-01-23 22:04:02 +0530

    testing AAE-28121

---
+++ lib/process-services-cloud/src/lib/process/process-filters/components/process-filters-cloud.component.ts
@@ -0,0 +1,327 @@
+/*!
+ * @license
+ * Copyright © 2005-2024 Hyland Software, Inc. and its affiliates. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges, OnDestroy, OnInit, ViewEncapsulation, inject } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { ProcessFilterCloudService } from '../services/process-filter-cloud.service';
+import { ProcessFilterCloudModel } from '../models/process-filter-cloud.model';
+import { AppConfigService, TranslationService } from '@alfresco/adf-core';
+import { FilterParamsModel } from '../../../task/task-filters/models/filter-cloud.model';
+import { debounceTime, takeUntil, tap } from 'rxjs/operators';
+import { ProcessListCloudService } from '../../../process/process-list/services/process-list-cloud.service';
+import { PROCESS_SEARCH_API_METHOD_TOKEN } from '../../../services/cloud-token.service';
+import { ProcessFilterCloudAdapter } from '../../process-list/models/process-cloud-query-request.model';
+
+@Component({
+    selector: 'adf-cloud-process-filters',
+    templateUrl: './process-filters-cloud.component.html',
+    styleUrls: ['./process-filters-cloud.component.scss'],
+    encapsulation: ViewEncapsulation.None
+})
+export class ProcessFiltersCloudComponent implements OnInit, OnChanges, OnDestroy {
+    /** (required) The application name */
+    @Input()
+    appName: string = '';
+
+    /** (optional) The filter to be selected by default */
+    @Input()
+    filterParam: FilterParamsModel;
+
+    /** (optional) Toggles showing an icon by the side of each filter */
+    @Input()
+    showIcons: boolean = false;
+
+    /** Emitted when a filter is being selected based on the filterParam input. */
+    @Output()
+    filterSelected = new EventEmitter<ProcessFilterCloudModel>();
+
+    /** Emitted when a filter is being clicked from the UI. */
+    @Output()
+    filterClicked = new EventEmitter<ProcessFilterCloudModel>();
+
+    /** Emitted when filters are loaded successfully */
+    @Output()
+    success = new EventEmitter<any>();
+
+    /** Emitted when any error occurs while loading the filters */
+    @Output()
+    error = new EventEmitter<any>();
+
+    /** Emitted when filter is updated. */
+    @Output()
+    updatedFilter: EventEmitter<{ filterKey: string; filterValue: number }> = new EventEmitter<{ filterKey: string; filterValue: number }>();
+
+    filters$: Observable<ProcessFilterCloudModel[]>;
+    currentFilter?: ProcessFilterCloudModel;
+    filters: ProcessFilterCloudModel[] = [];
+    counters: { [key: string]: number } = {};
+    enableNotifications: boolean;
+    currentFiltersValues: { [key: string]: number } = {};
+    updatedFiltersSet = new Set<string>();
+
+    private onDestroy$ = new Subject<boolean>();
+
+    private readonly processFilterCloudService = inject(ProcessFilterCloudService);
+    private readonly translationService = inject(TranslationService);
+    private readonly appConfigService = inject(AppConfigService);
+    private readonly processListCloudService = inject(ProcessListCloudService);
+    private readonly searchMethod = inject<'GET' | 'POST'>(PROCESS_SEARCH_API_METHOD_TOKEN, { optional: true });
+
+    ngOnInit() {
+        this.enableNotifications = this.appConfigService.get('notifications', true);
+        if (this.appName === '') {
+            this.getFilters(this.appName);
+        }
+        this.initProcessNotification();
+        this.getFilterKeysAfterExternalRefreshing();
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+        const appName = changes['appName'];
+        const filter = changes['filterParam'];
+        if (appName?.currentValue) {
+            this.getFilters(appName.currentValue);
+        } else if (filter && filter.currentValue !== filter.previousValue) {
+            this.selectFilterAndEmit(filter.currentValue);
+        }
+    }
+
+    /**
+     * Fetch the filter list based on appName
+     *
+     * @param appName application name
+     */
+    getFilters(appName: string): void {
+        this.filters$ = this.processFilterCloudService.getProcessFilters(appName);
+
+        this.filters$.pipe(takeUntil(this.onDestroy$)).subscribe({
+            next: (res) => {
+                this.resetFilter();
+                this.filters = res || [];
+                this.initFilterCounters();
+                this.selectFilterAndEmit(this.filterParam);
+                this.success.emit(res);
+                this.updateFilterCounters();
+            },
+            error: (err: any) => {
+                this.error.emit(err);
+            }
+        });
+    }
+
+    /**
+     * Initialize counter collection for filters
+     */
+    initFilterCounters() {
+        this.filters.forEach((filter) => (this.counters[filter.key] = 0));
+    }
+
+    /**
+     * Pass the selected filter as next
+     *
+     * @param paramFilter filter model
+     */
+    selectFilter(paramFilter: FilterParamsModel) {
+        if (!paramFilter) {
+            return;
+        }
+
+        const preferredFilter = this.filters.find((filter) => paramFilter.id === filter.id);
+        this.currentFilter =
+            preferredFilter ??
+            this.filters.find(
+                (filter, index) =>
+                    paramFilter.id === filter.id ||
+                    (paramFilter.name && this.checkFilterNamesEquality(paramFilter.name, filter.name)) ||
+                    (paramFilter.key && paramFilter.key === filter.key) ||
+                    paramFilter.index === index
+            ); // fallback to preserve the previous behavior
+    }
+
+    /**
+     * Check equality of the filter names by translating the given name strings
+     *
+     * @param name1 source name
+     * @param name2 target name
+     * @returns `true` if filter names are equal, otherwise `false`
+     */
+    private checkFilterNamesEquality(name1: string, name2: string): boolean {
+        const translatedName1 = this.translationService.instant(name1);
+        const translatedName2 = this.translationService.instant(name2);
+
+        return translatedName1.toLocaleLowerCase() === translatedName2.toLocaleLowerCase();
+    }
+
+    /**
+     * Selects and emits the given filter
+     *
+     * @param newParamFilter new parameter filter
+     */
+    selectFilterAndEmit(newParamFilter: FilterParamsModel) {
+        if (newParamFilter) {
+            this.selectFilter(newParamFilter);
+            this.filterSelected.emit(this.currentFilter);
+        } else {
+            this.currentFilter = undefined;
+        }
+    }
+
+    /**
+     * Select filter with the id
+     *
+     * @param id filter id
+     */
+    selectFilterById(id: string) {
+        this.selectFilterAndEmit({ id });
+    }
+
+    /**
+     * Selects and emits the clicked filter
+     *
+     * @param filter filter model
+     */
+    onFilterClick(filter: ProcessFilterCloudModel) {
+        if (filter) {
+            this.selectFilter(filter);
+            this.filterClicked.emit(this.currentFilter);
+            this.updateFilterCounter(this.currentFilter);
+            this.updatedFiltersSet.delete(filter.key);
+        } else {
+            this.currentFilter = undefined;
+        }
+    }
+
+    /**
+     * Select as default process filter the first in the list
+     */
+    selectDefaultProcessFilter() {
+        if (!this.isFilterListEmpty()) {
+            this.currentFilter = this.filters[0];
+        }
+    }
+
+    /**
+     * Get current filter
+     *
+     * @returns filter model
+     */
+    getCurrentFilter(): ProcessFilterCloudModel {
+        return this.currentFilter;
+    }
+
+    /**
+     * Check if the filter list is empty
+     *
+     * @returns `true` if filter list is empty, otherwise `false`
+     */
+    isFilterListEmpty(): boolean {
+        return this.filters === undefined || (this.filters && this.filters.length === 0);
+    }
+
+    /**
+     * Reset the filters
+     */
+    private resetFilter() {
+        this.filters = [];
+        this.currentFilter = undefined;
+    }
+
+    ngOnDestroy() {
+        this.onDestroy$.next(true);
+        this.onDestroy$.complete();
+    }
+
+    isActiveFilter(filter: ProcessFilterCloudModel): boolean {
+        return this.currentFilter.name === filter.name;
+    }
+
+    initProcessNotification(): void {
+        if (this.appName && this.enableNotifications) {
+            this.processFilterCloudService
+                .getProcessNotificationSubscription(this.appName)
+                .pipe(debounceTime(1000), takeUntil(this.onDestroy$))
+                .subscribe(() => {
+                    this.updateFilterCounters();
+                });
+        }
+    }
+
+    /**
+     * Iterate over filters and update counters
+     */
+    updateFilterCounters(): void {
+        this.filters.forEach((filter: ProcessFilterCloudModel) => {
+            this.updateFilterCounter(filter);
+        });
+    }
+
+    /**
+     *  Get current value for filter and check if value has changed
+     *
+     * @param filter filter
+     */
+    updateFilterCounter(filter: ProcessFilterCloudModel): void {
+        this.fetchProcessFilterCounter(filter)
+            .pipe(
+                tap((filterCounter) => {
+                    this.checkIfFilterValuesHasBeenUpdated(filter.key, filterCounter);
+                })
+            )
+            .subscribe((data) => {
+                this.counters = {
+                    ...this.counters,
+                    [filter.key]: data
+                };
+            });
+    }
+
+    getFilterCounter(filter: ProcessFilterCloudModel): Observable<number> {
+        return this.fetchProcessFilterCounter(filter);
+    }
+
+    checkIfFilterValuesHasBeenUpdated(filterKey: string, filterValue: number): void {
+        if (!this.currentFiltersValues[filterKey]) {
+            this.currentFiltersValues[filterKey] = filterValue;
+            return;
+        }
+        if (this.currentFiltersValues[filterKey] !== filterValue) {
+            this.currentFiltersValues[filterKey] = filterValue;
+            this.updatedFilter.emit({ filterKey, filterValue });
+            this.updatedFiltersSet.add(filterKey);
+        }
+    }
+
+    isFilterUpdated(filterName: string): boolean {
+        return this.updatedFiltersSet.has(filterName);
+    }
+
+    /**
+     * Get filer key when filter was refreshed by external action
+     *
+     */
+    getFilterKeysAfterExternalRefreshing(): void {
+        this.processFilterCloudService.filterKeyToBeRefreshed$.pipe(takeUntil(this.onDestroy$)).subscribe((filterKey: string) => {
+            this.updatedFiltersSet.delete(filterKey);
+        });
+    }
+
+    private fetchProcessFilterCounter(filter: ProcessFilterCloudModel): Observable<number> {
+        return this.searchMethod === 'POST'
+            ? this.processListCloudService.getProcessListCounter(new ProcessFilterCloudAdapter(filter))
+            : this.processListCloudService.getProcessCounter(filter.appName, filter.status);
+    }
+}
