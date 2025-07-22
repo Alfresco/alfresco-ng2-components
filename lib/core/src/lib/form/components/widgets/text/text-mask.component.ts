46f817ba1d42fe556949df6144297b03f14a39e4
Isoumyajit <soumyajit.chakraborty@hyland.com>
2025-07-22 18:08:44 +0530

    AAE-36716 fixing text-form-field blur mechanism in control Value Accessor

--- lib/core/src/lib/form/components/widgets/text/text-mask.component.ts
+++ lib/core/src/lib/form/components/widgets/text/text-mask.component.ts
@@ -75,6 +75,11 @@ export class InputMaskDirective implements OnChanges, ControlValueAccessor {
         }
     }
 
+    @HostListener('blur')
+    onBlur() {
+        this._onTouched();
+    }
+
     ngOnChanges(changes: SimpleChanges) {
         if (changes['inputMask']?.currentValue['mask']) {
             this.inputMask = changes['inputMask'].currentValue;
