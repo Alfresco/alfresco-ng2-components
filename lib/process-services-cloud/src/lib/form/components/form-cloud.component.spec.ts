35f86facb95c8ad2dafcb32f58baf469a3e3617f
Enrico Hilgendorf <enrico.hilgendorf@hyland.com>
2025-09-04 13:47:46 +0200

    [ci:force] Fixed a tests

--- lib/process-services-cloud/src/lib/form/components/form-cloud.component.spec.ts
+++ lib/process-services-cloud/src/lib/form/components/form-cloud.component.spec.ts
@@ -1773,6 +1773,7 @@ describe('retrieve metadata on submit', () => {
     });
 
     it('should handle outcomeId correctly when completing form with confirmation dialog', () => {
+        let matDialog = TestBed.inject(MatDialog);
         spyOn(matDialog, 'open').and.returnValue({ afterClosed: () => of(true) } as any);
         spyOn(formComponent['formCloudService'], 'completeTaskForm').and.returnValue(of({} as any));
 
