c5c558d04da243be85e87f157141d3a2a7f46822
VitoAlbano <vito.albano@hyland.com>
2025-09-05 13:44:31 +0100

    AAE-30882 - Checking why is ok locally but fails on CI

--- lib/js-api/src/axiosHttpClient.ts
+++ lib/js-api/src/axiosHttpClient.ts
@@ -258,7 +258,7 @@ export class AxiosHttpClient implements HttpClient {
 
         try {
             document.cookie = `CSRF-TOKEN=${token};path=/`;
-        } catch (err) {
+        } catch {
             /* continue regardless of error */
         }
     }
