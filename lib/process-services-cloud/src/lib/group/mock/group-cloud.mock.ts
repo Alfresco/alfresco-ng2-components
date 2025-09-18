f8ae68c92007cb6c58457f4dd2337f019aeb6cb7
DominikIwanek <dominik.iwanek@hyland.com>
2025-09-18 14:19:21 +0200

    [ACS-10117] Deprecate ADF Storybook and custom Docker builds

---
+++ lib/process-services-cloud/src/lib/group/mock/group-cloud.mock.ts
@@ -0,0 +1,23 @@
+/*!
+ * @license
+ * Copyright © 2005-2025 Hyland Software, Inc. and its affiliates. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { IdentityGroupModel } from '../models/identity-group.model';
+
+export const mockVegetableAubergine: IdentityGroupModel = { id: 'aubergine', name: 'Vegetable Aubergine' };
+export const mockMeatChicken: IdentityGroupModel = { id: 'chicken', name: 'Meat Chicken' };
+
+export const mockFoodGroups = [mockVegetableAubergine, mockMeatChicken];
