5b32a491f6a6c831253d227818a109d4265bdacc
DominikIwanek <dominik.iwanek@hyland.com>
2025-09-18 08:50:12 +0200

    [ACS-10117] Deprecate ADF Storybook and custom Docker builds

---
+++ lib/core/src/lib/login/components/login-dialog/login-dialog.component.ts
@@ -0,0 +1,59 @@
+/*!
+ * @license
+ * Copyright © 2005-2025 Hyland Software, Inc. and its affiliates. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component, Inject, ViewChild, ViewEncapsulation } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { TranslatePipe } from '@ngx-translate/core';
+import { LoginDialogPanelComponent } from '../login-dialog-panel/login-dialog-panel.component';
+import { LoginDialogComponentData } from './login-dialog-component-data.interface';
+
+/** @deprecated this component will be removed because it's unused */
+@Component({
+    selector: 'adf-login-dialog',
+    templateUrl: './login-dialog.component.html',
+    styleUrls: ['./login-dialog.component.scss'],
+    imports: [MatDialogModule, LoginDialogPanelComponent, TranslatePipe, MatButtonModule],
+    encapsulation: ViewEncapsulation.None
+})
+export class LoginDialogComponent {
+    @ViewChild('adfLoginPanel', { static: true })
+    loginPanel: LoginDialogPanelComponent;
+
+    buttonActionName = '';
+
+    constructor(@Inject(MAT_DIALOG_DATA) public data: LoginDialogComponentData) {
+        this.buttonActionName = data.actionName ? `LOGIN.DIALOG.${data.actionName.toUpperCase()}` : 'LOGIN.DIALOG.CHOOSE';
+    }
+
+    close() {
+        this.data.logged.complete();
+    }
+
+    submitForm(): void {
+        this.loginPanel.submitForm();
+    }
+
+    onLoginSuccess(event: any) {
+        this.data.logged.next(event);
+        this.close();
+    }
+
+    isFormValid() {
+        return this.loginPanel ? this.loginPanel.isValid() : false;
+    }
+}
---
+++ lib/process-services-cloud/src/lib/app/components/app-list-cloud/app-list-cloud.component.ts
@@ -0,0 +1,133 @@
+/*!
+ * @license
+ * Copyright © 2005-2025 Hyland Software, Inc. and its affiliates. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { CustomEmptyContentTemplateDirective, EmptyContentComponent } from '@alfresco/adf-core';
+import { AfterContentInit, Component, ContentChild, EventEmitter, Input, OnInit, Output, ViewEncapsulation } from '@angular/core';
+import { Observable, of, Subject } from 'rxjs';
+import { AppsProcessCloudService } from '../../services/apps-process-cloud.service';
+import { ApplicationInstanceModel } from '../../models/application-instance.model';
+import { catchError } from 'rxjs/operators';
+import { CommonModule } from '@angular/common';
+import { TranslatePipe } from '@ngx-translate/core';
+import { AppDetailsCloudComponent } from '../app-details-cloud/app-details-cloud.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatLineModule } from '@angular/material/core';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+export const LAYOUT_LIST: string = 'LIST';
+export const LAYOUT_GRID: string = 'GRID';
+export const DEPLOYED_STATUS: string = 'DEPLOYED';
+
+/** @deprecated this component will be removed because it's unused */
+@Component({
+    selector: 'adf-cloud-app-list',
+    imports: [
+        CommonModule,
+        TranslatePipe,
+        AppDetailsCloudComponent,
+        MatIconModule,
+        MatLineModule,
+        MatListModule,
+        EmptyContentComponent,
+        MatProgressSpinnerModule
+    ],
+    templateUrl: './app-list-cloud.component.html',
+    styleUrls: ['./app-list-cloud.component.scss'],
+    encapsulation: ViewEncapsulation.None
+})
+export class AppListCloudComponent implements OnInit, AfterContentInit {
+    @ContentChild(CustomEmptyContentTemplateDirective)
+    emptyCustomContent: CustomEmptyContentTemplateDirective;
+
+    /**
+     * Defines the layout of the apps. There are two possible
+     * values, "GRID" and "LIST".
+     */
+    @Input()
+    layoutType: string = LAYOUT_GRID;
+
+    /** Emitted when an app entry is clicked. */
+    @Output()
+    appClick = new EventEmitter<ApplicationInstanceModel>();
+
+    apps$: Observable<any>;
+    loadingError$ = new Subject<boolean>();
+    hasEmptyCustomContentTemplate: boolean = false;
+
+    constructor(private appsProcessCloudService: AppsProcessCloudService) {}
+
+    ngOnInit() {
+        if (!this.isValidType()) {
+            this.setDefaultLayoutType();
+        }
+
+        this.apps$ = this.appsProcessCloudService.getDeployedApplicationsByStatus(DEPLOYED_STATUS).pipe(
+            catchError(() => {
+                this.loadingError$.next(true);
+                return of();
+            })
+        );
+    }
+
+    ngAfterContentInit() {
+        if (this.emptyCustomContent) {
+            this.hasEmptyCustomContentTemplate = true;
+        }
+    }
+
+    onSelectApp(app: ApplicationInstanceModel): void {
+        this.appClick.emit(app);
+    }
+
+    /**
+     * Check if the value of the layoutType property is an allowed value
+     *
+     * @returns `true` if layout type is valid, otherwise `false`
+     */
+    isValidType(): boolean {
+        if (this.layoutType && (this.layoutType === LAYOUT_LIST || this.layoutType === LAYOUT_GRID)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Assign the default value to LayoutType
+     */
+    setDefaultLayoutType(): void {
+        this.layoutType = LAYOUT_GRID;
+    }
+
+    /**
+     * Check if the layout type is LIST
+     *
+     * @returns `true` if the layout is list, otherwise `false`
+     */
+    isList(): boolean {
+        return this.layoutType === LAYOUT_LIST;
+    }
+
+    /**
+     * Check if the layout type is GRID
+     *
+     * @returns `true` if layout is grid, otherwise `false`
+     */
+    isGrid(): boolean {
+        return this.layoutType === LAYOUT_GRID;
+    }
+}
