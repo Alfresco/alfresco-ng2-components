59b148ea034a06acfe7e8011c2222e868e7d7d93
tomasz hanaj <12088991+tomaszhanaj@users.noreply.github.com>
2025-10-28 14:41:25 +0100

    AAE-37713 Amount locale display (#11285)
    * [AAE-37713] amount widget formats value based on locale
    
    * [AAE-37713] added unit test with currency icon
    
    * [AAE-37713] provided amount widget config as observable
    
    * [AAE-37713] applied pr comments

--- lib/core/src/lib/form/components/widgets/amount/amount.widget.ts
+++ lib/core/src/lib/form/components/widgets/amount/amount.widget.ts
@@ -17,7 +17,7 @@
 
 /* eslint-disable @angular-eslint/component-selector */
 
-import { NgIf } from '@angular/common';
+import { CurrencyPipe, NgIf } from '@angular/common';
 import { Component, OnInit, ViewEncapsulation, InjectionToken, Inject, Optional } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -26,12 +26,15 @@ import { TranslatePipe } from '@ngx-translate/core';
 import { FormService } from '../../../services/form.service';
 import { ErrorWidgetComponent } from '../error/error.component';
 import { WidgetComponent } from '../widget.component';
+import { isObservable, Observable } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 export interface AmountWidgetSettings {
     showReadonlyPlaceholder: boolean;
+    enableDisplayBasedOnLocale: boolean;
 }
 
-export const ADF_AMOUNT_SETTINGS = new InjectionToken<AmountWidgetSettings>('adf-amount-settings');
+export const ADF_AMOUNT_SETTINGS = new InjectionToken<Observable<AmountWidgetSettings> | AmountWidgetSettings>('adf-amount-settings');
 
 @Component({
     selector: 'amount-widget',
@@ -49,13 +52,23 @@ export const ADF_AMOUNT_SETTINGS = new InjectionToken<AmountWidgetSettings>('adf
         '(select)': 'event($event)'
     },
     imports: [MatFormFieldModule, MatInputModule, FormsModule, ErrorWidgetComponent, TranslatePipe, NgIf],
+    providers: [CurrencyPipe],
     encapsulation: ViewEncapsulation.None
 })
 export class AmountWidgetComponent extends WidgetComponent implements OnInit {
     static DEFAULT_CURRENCY: string = '$';
     private showPlaceholder = true;
 
+    amountWidgetValue: string;
     currency: string = AmountWidgetComponent.DEFAULT_CURRENCY;
+    currencyDisplay: string | boolean = 'symbol';
+    decimalProperty: string;
+    enableDisplayBasedOnLocale: boolean;
+    locale: string;
+    notShowDecimalDigits = '1.0-0';
+    showDecimalDigits = '1.2-2';
+    showReadonlyPlaceholder: boolean;
+    valueAsNumber: number;
 
     get placeholder(): string {
         return this.showPlaceholder ? this.field.placeholder : '';
@@ -63,22 +76,97 @@ export class AmountWidgetComponent extends WidgetComponent implements OnInit {
 
     constructor(
         public formService: FormService,
-        @Inject(ADF_AMOUNT_SETTINGS)
-        @Optional()
-        private settings: AmountWidgetSettings
+        @Optional() @Inject(ADF_AMOUNT_SETTINGS) settings: Observable<AmountWidgetSettings> | AmountWidgetSettings,
+        private currencyPipe: CurrencyPipe
     ) {
         super(formService);
+        if (isObservable(settings)) {
+            settings.pipe(takeUntilDestroyed()).subscribe((data: AmountWidgetSettings) => {
+                this.updateSettingsBasedProperties(data);
+            });
+        } else {
+            this.updateSettingsBasedProperties(settings);
+        }
     }
 
     ngOnInit() {
         if (this.field) {
             if (this.field.currency) {
                 this.currency = this.field.currency;
+            } else {
+                if (this.enableDisplayBasedOnLocale) {
+                    this.currency = '';
+                    this.currencyDisplay = '';
+                }
             }
 
             if (this.field.readOnly) {
-                this.showPlaceholder = this.settings?.showReadonlyPlaceholder;
+                this.showPlaceholder = this.showReadonlyPlaceholder;
+            }
+            this.setInitialValues();
+        }
+    }
+
+    amountWidgetOnBlur(): void {
+        if (this.enableDisplayBasedOnLocale) {
+            if (this.amountWidgetValue) {
+                this.valueAsNumber = parseFloat(this.amountWidgetValue);
+                this.amountWidgetValue = this.currencyPipe.transform(
+                    this.amountWidgetValue,
+                    this.currency,
+                    this.currencyDisplay,
+                    this.decimalProperty
+                );
+            } else {
+                this.valueAsNumber = null;
+                this.amountWidgetValue = null;
             }
         }
+        this.markAsTouched();
+    }
+
+    amountWidgetOnFocus(): void {
+        if (this.enableDisplayBasedOnLocale) {
+            const hasValue = this.valueAsNumber === 0 || this.valueAsNumber;
+            this.amountWidgetValue = hasValue ? this.valueAsNumber.toString() : null;
+        }
+    }
+
+    getLocale(): string {
+        const defaultLocale = 'en-US';
+        if (typeof window?.navigator === 'undefined') {
+            return defaultLocale;
+        }
+        const wn = window.navigator as Navigator;
+        let lang = wn.languages ? wn.languages[0] : defaultLocale;
+        lang = lang || wn.language;
+        return lang;
+    }
+
+    onFieldChangedAmountWidget(): void {
+        this.field.value = this.amountWidgetValue;
+        super.onFieldChanged(this.field);
+    }
+
+    setInitialValues(): void {
+        if (this.enableDisplayBasedOnLocale) {
+            this.decimalProperty = this.field.enableFractions ? this.showDecimalDigits : this.notShowDecimalDigits;
+            this.locale = this.getLocale();
+            this.valueAsNumber = this.field.value;
+            this.amountWidgetValue = this.currencyPipe.transform(
+                this.field.value,
+                this.currency,
+                this.currencyDisplay,
+                this.decimalProperty,
+                this.locale
+            );
+        } else {
+            this.amountWidgetValue = this.field.value;
+        }
+    }
+
+    updateSettingsBasedProperties(data: AmountWidgetSettings): void {
+        this.enableDisplayBasedOnLocale = data?.enableDisplayBasedOnLocale ?? false;
+        this.showReadonlyPlaceholder = data?.showReadonlyPlaceholder;
     }
 }
