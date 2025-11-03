6513f03d2744acf311ba11f2e34e1c82c994f724
wiktord2000 <wiktor.danielewski@hyland.com>
2025-11-03 22:41:54 +0100

    AAE-39612 Add unit tests for the Data Table Widget

--- lib/process-services-cloud/src/lib/form/components/widgets/data-table/data-table.widget.spec.ts
+++ lib/process-services-cloud/src/lib/form/components/widgets/data-table/data-table.widget.spec.ts
@@ -96,6 +96,24 @@ describe('DataTableWidgetComponent', () => {
         });
     });
 
+    describe('event tracking', () => {
+        let eventSpy: jasmine.Spy;
+
+        beforeEach(() => {
+            eventSpy = spyOn(widget, 'event').and.callThrough();
+            widget.field = new FormFieldModel(new FormModel(), {});
+            fixture.detectChanges();
+        });
+
+        it('should call event method only once when widget is clicked', () => {
+            const clickEvent = new MouseEvent('click', { bubbles: true });
+            fixture.debugElement.nativeElement.dispatchEvent(clickEvent);
+
+            expect(eventSpy).toHaveBeenCalledTimes(1);
+            expect(eventSpy).toHaveBeenCalledWith(clickEvent);
+        });
+    });
+
     it('should display label', () => {
         fixture.detectChanges();
 
