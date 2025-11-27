3fad23899b03de511f8c51b9ad08efd622f59b84
Wojciech Duda <wojciechduda03@gmail.com>
2025-11-27 17:58:08 +0100

    AAE-40269 Update to storybook v10.1.0

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,8 +1,10 @@
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
 import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
     framework: {
-        name: '@storybook/angular',
+        name: getAbsolutePath('@storybook/angular'),
         options: {}
     },
     staticDirs: [],
@@ -10,3 +12,7 @@ const config: StorybookConfig = {
 };
 
 export default config;
+
+function getAbsolutePath(value: string): any {
+    return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
+}
--- lib/content-services/.storybook/main.ts
+++ lib/content-services/.storybook/main.ts
@@ -1,3 +1,5 @@
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
 import type { StorybookConfig } from '@storybook/angular';
 import rootMain from '../../../.storybook/main';
 
@@ -9,9 +11,13 @@ const config: StorybookConfig = {
         { from: '../src/lib/assets/images', to: 'assets/images' }
     ],
     framework: {
-        name: '@storybook/angular',
+        name: getAbsolutePath('@storybook/angular'),
         options: {}
     }
 };
 
 export default config;
+
+function getAbsolutePath(value: string): any {
+    return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
+}
