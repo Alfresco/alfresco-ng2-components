127075989375f5b4dbbdfae38dbd93fe3d0f8c2e
Diogo Bastos <diogo.bastos@hyland.com>
2025-12-17 11:55:19 +0000

    AAE-40896 Create adf-icon directive

---
+++ lib/core/src/lib/icon/icon.directive.ts
@@ -0,0 +1,45 @@
+/*!
+ * @license
+ * Copyright © 2005-2025 Hyland Software, Inc. and its affiliates. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Directive, ElementRef, inject, Input, OnChanges } from '@angular/core';
+import { ICON_ALIAS_MAP_TOKEN } from './icon-alias-map.token';
+import { MatIcon } from '@angular/material/icon';
+
+@Directive({
+    selector: 'mat-icon[adf-icon]'
+})
+export class IconDirective implements OnChanges {
+    private readonly matIcon = inject(MatIcon);
+    private readonly elementRef = inject(ElementRef);
+    private readonly aliasMap = inject(ICON_ALIAS_MAP_TOKEN, { optional: true });
+
+    @Input() name: string;
+
+    ngOnChanges() {
+        const iconAlias = this.aliasMap?.[this.name];
+
+        iconAlias ? this.setSvgIcon(iconAlias) : this.appendLigatureText();
+    }
+
+    private setSvgIcon(icon: string) {
+        this.matIcon.svgIcon = icon;
+    }
+
+    private appendLigatureText() {
+        this.elementRef.nativeElement.appendChild(document.createTextNode(this.name));
+    }
+}
