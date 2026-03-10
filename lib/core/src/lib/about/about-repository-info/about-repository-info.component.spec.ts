09ab31189f1613a7bd805d6ec162ba0782917db2
Aleksander Sklorz <Aleksander.Sklorz@hyland.com>
2026-03-10 15:39:55 +0100

    [ACS-10267] Addressed copilot comments

---
+++ lib/core/src/lib/about/about-repository-info/about-repository-info.component.spec.ts
@@ -0,0 +1,77 @@
+/*!
+ * @license
+ * Copyright © 2005-2025 Hyland Software, Inc. and its affiliates. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AboutRepositoryInfoComponent, RepositoryInfo, UnitTestingUtils } from '@alfresco/adf-core';
+
+describe('AboutRepositoryInfoComponent', () => {
+    let component: AboutRepositoryInfoComponent;
+    let fixture: ComponentFixture<AboutRepositoryInfoComponent>;
+    let unitTestingUtils: UnitTestingUtils;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [AboutRepositoryInfoComponent]
+        });
+        fixture = TestBed.createComponent(AboutRepositoryInfoComponent);
+        component = fixture.componentInstance;
+        unitTestingUtils = new UnitTestingUtils(fixture.debugElement);
+    });
+
+    describe('License', () => {
+        it('should render license header', () => {
+            component.data = {
+                status: {},
+                license: {
+                    holder: 'Some holder'
+                }
+            } as RepositoryInfo;
+
+            fixture.detectChanges();
+            expect(unitTestingUtils.getInnerTextByDataAutomationId('adf-about-repository-info-header-license')).toBe('ABOUT.LICENSE.TITLE');
+        });
+    });
+
+    describe('Status', () => {
+        it('should render status header', () => {
+            component.data = {
+                status: {
+                    isReadOnly: true
+                }
+            } as RepositoryInfo;
+
+            fixture.detectChanges();
+            expect(unitTestingUtils.getInnerTextByDataAutomationId('adf-about-repository-info-header-status')).toBe('ABOUT.STATUS.TITLE');
+        });
+    });
+
+    describe('Modules', () => {
+        it('should render modules header', () => {
+            component.data = {
+                status: {},
+                modules: [
+                    {
+                        title: 'Module 1'
+                    }
+                ]
+            } as RepositoryInfo;
+
+            fixture.detectChanges();
+            expect(unitTestingUtils.getInnerTextByDataAutomationId('adf-about-repository-info-header-modules')).toBe('ABOUT.MODULES.TITLE');
+        });
+    });
+});
