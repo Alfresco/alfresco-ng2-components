c74d80620067d33c1824b845883c672522394ba5
VitoAlbano <vito.albano@hyland.com>
2026-05-06 22:15:52 +0100

    [AAE-30881] - Fixed sonarcloud issue

--- lib/core/src/lib/auth/oidc/web-crypto-jwks-validation-handler.spec.ts
+++ lib/core/src/lib/auth/oidc/web-crypto-jwks-validation-handler.spec.ts
@@ -30,7 +30,7 @@ function base64UrlEncode(data: ArrayBuffer | string): string {
     for (const byte of bytes) {
         binary += String.fromCharCode(byte);
     }
-    return btoa(binary).replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+    return btoa(binary).replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
 }
 
 interface JwksKey extends JsonWebKey {
