c7bd56f24457427e5eb3ef59f8f3b17935d37435
Anamika Dey <anamika.dey@hyland.com>
2026-07-11 00:23:53 +0530

    AAE-43974 Fix for alfresco-content-app e2e failure (#12055)
    * AAE-43974 Fix for alfresco-content-app e2e failure
    
    * AAE-43974 Refactored based on review comments
    
    * AAE-43974 Removing unit test cases written for isError as it is being tested by passwordErrorStateMatcher
    
    * [ci:force] re-trigger CI

--- lib/core/viewer/pdf/src/lib/components/pdf-viewer-password-dialog/pdf-viewer-password-dialog.ts
+++ lib/core/viewer/pdf/src/lib/components/pdf-viewer-password-dialog/pdf-viewer-password-dialog.ts
@@ -19,6 +19,7 @@ import { Component, OnInit, ViewEncapsulation, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { ReactiveFormsModule, UntypedFormControl, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { TranslatePipe } from '@ngx-translate/core';
@@ -38,12 +39,15 @@ export class PdfPasswordDialogComponent implements OnInit {
     data = inject(MAT_DIALOG_DATA);
 
     passwordFormControl: UntypedFormControl;
+    passwordErrorStateMatcher: ErrorStateMatcher = {
+        isErrorState: () => this.isError()
+    };
 
     ngOnInit() {
         this.passwordFormControl = new UntypedFormControl('', [Validators.required]);
     }
 
-    isError(): boolean {
+    private isError(): boolean {
         return this.data.reason === pdfjsLib.PasswordResponses.INCORRECT_PASSWORD;
     }
 
