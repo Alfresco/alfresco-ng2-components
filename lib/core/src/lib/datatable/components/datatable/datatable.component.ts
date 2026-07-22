378863c7f2b1e88682d7df07e034bb6efeca9efa
Denys Vuika <denys.vuika@gmail.com>
2026-07-22 17:32:56 +0100

    add missing import

--- lib/core/src/lib/datatable/components/datatable/datatable.component.ts
+++ lib/core/src/lib/datatable/components/datatable/datatable.component.ts
@@ -59,7 +59,7 @@ import { ObjectDataTableAdapter } from '../../data/object-datatable-adapter';
 import { DataCellEvent } from '../data-cell.event';
 import { DataRowActionEvent } from '../data-row-action.event';
 import { buffer, debounceTime, filter, map, share } from 'rxjs/operators';
-import { CdkDrag, CdkDragDrop, CdkDragHandle, CdkDropList, moveItemInArray } from '@angular/cdk/drag-drop';
+import { CdkDrag, CdkDragDrop, CdkDragHandle, CdkDragPlaceholder, CdkDropList, moveItemInArray } from '@angular/cdk/drag-drop';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ResizeEvent } from '../../directives/resizable/types';
@@ -122,7 +122,8 @@ export type ShowHeaderMode = (typeof ShowHeaderMode)[keyof typeof ShowHeaderMode
         JsonCellComponent,
         AmountCellComponent,
         NumberCellComponent,
-        MatTooltipModule
+        MatTooltipModule,
+        CdkDragPlaceholder
     ],
     templateUrl: './datatable.component.html',
     styleUrls: ['./datatable.component.scss'],
