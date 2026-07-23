2c461eb3b84c3f9addd6b9836de3efa3b4cb2323
Denys Vuika <denys.vuika@gmail.com>
2026-07-23 11:51:35 +0100

    fix: correct import path for rootMain in Storybook configuration

--- lib/stories/.storybook/main.ts
+++ lib/stories/.storybook/main.ts
@@ -1,5 +1,5 @@
 import type { StorybookConfig } from '@storybook/angular';
-import rootMain from '../../../.storybook/main';
+import rootMain from '../../../.storybook/main.ts';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
