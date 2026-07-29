711c1f46fd0322e86eca2cba42608fd0d37a4b3c
Bartosz Sekula <Bartosz.Sekula@hyland.com>
2026-07-29 12:08:41 +0200

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,58 @@
-import path from "path";
-import { fileURLToPath } from "url";
-import js from "@eslint/js";
-import { defineConfig, globalIgnores } from "@eslint/config-helpers";
-import { FlatCompat } from "@eslint/eslintrc";
-import { fixupConfigRules } from "@eslint/compat";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import js from '@eslint/js';
+import { defineConfig, globalIgnores } from '@eslint/config-helpers';
+import { FlatCompat } from '@eslint/eslintrc';
+import { fixupConfigRules } from '@eslint/compat';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-  const compat = new FlatCompat({
+const compat = new FlatCompat({
     baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all,
-  });
+    allConfig: js.configs.all
+});
+
 export default defineConfig([
-  globalIgnores([".angular", "nxcache", "node_modules", "dist", "coverage", ".github", ".vscode", "scripts", "/angular.json", "docs/**/*.md", "lib/js-api/docs/**/*.md", ".storybook", "webpack.config.js", "lib/core/src/lib/icon", "*.log", "node_modules", "bundles", ".idea/", "*.iml", ".env.*", ".env", "dist", "tmp", "temp", ".history", ".ng_pkg_build/", "coverage/", "out-tsc", "/reports/", "e2e-result-*", "licenses.txt", ".DS_Store", "desktop.ini", ".angular", ".nx", "nxcache", ".husky", ".cursor/rules/nx-rules.mdc", ".github/instructions/nx.instructions.md", "lib/eslint-angular/dist/"]),
-  {
-    extends: fixupConfigRules(compat.extends(
-      "./.eslintrc.js"
-    )),
-  }
+    globalIgnores([
+        '.angular',
+        '.DS_Store',
+        '.env',
+        '.env.*',
+        '.github',
+        '.history',
+        '.husky',
+        '.idea/',
+        '.ng_pkg_build/',
+        '.nx',
+        '.storybook',
+        '.vscode',
+        '*.iml',
+        '*.log',
+        '/angular.json',
+        '/reports/',
+        'bundles',
+        'coverage',
+        'desktop.ini',
+        'dist',
+        'docs/**/*.md',
+        'e2e-result-*',
+        'lib/core/src/lib/icon',
+        'lib/eslint-angular/dist/',
+        'lib/js-api/docs/**/*.md',
+        'licenses.txt',
+        'node_modules',
+        'nxcache',
+        'out-tsc',
+        'scripts',
+        'temp',
+        'tmp',
+        'webpack.config.js',
+        '.cursor/rules/nx-rules.mdc',
+        '.github/instructions/nx.instructions.md'
+    ]),
+    {
+        extends: fixupConfigRules(compat.extends('./.eslintrc.js'))
+    }
 ]);
