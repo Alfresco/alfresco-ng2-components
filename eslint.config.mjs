cb8eeb33414285123a6a620fbed20c84d887dd91
Bartosz Sekula <Bartosz.Sekula@hyland.com>
2026-07-30 21:38:28 +0200

    cr

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -197,7 +197,7 @@ export default [
                         'accessor',
                         'enumMember'
                     ],
-                    format: null,
+                    format: undefined,
                     modifiers: ['requiresQuotes']
                 }
             ],
